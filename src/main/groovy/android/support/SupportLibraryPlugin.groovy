3978f4a4ca09c7a7e24f5fc5657ab1849b1a0590
Aurimas Liutikas <aurimas@google.com>
2017-07-14 09:45:59 -0700

    Move to using AGP 3.0.0 Alpha 7.
    Test: ./gradlew buildOnServer
    Change-Id: I12dc59f9b5fde9bfc7167250e16fb19b5c87389c

--- src/main/groovy/android/support/SupportLibraryPlugin.groovy
+++ src/main/groovy/android/support/SupportLibraryPlugin.groovy
@@ -101,7 +101,7 @@ class SupportLibraryPlugin implements Plugin<Project> {
             error 'NewApi'
 
             // TODO(aurimas): figure out the issue with missing translation check
-            disable 'MissingTranslation'
+            disable 'MissingTranslation', 'ExtraTranslation'
         }
 
         // Set baseline file for all legacy lint warnings.
