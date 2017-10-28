e9a16e75e2c82b04927adfb0dd57039c9d35ad0f
Aurimas Liutikas <aurimas@google.com>
2017-10-27 17:55:06 -0700

    Move setting maven group directly to each build.gradle.
    Test: ./gradlew buildOnServer
    Change-Id: If83e8d3d4bb8c9dd260f65c3898b6f0fa8ffc524

---
+++ src/main/java/android/support/LibraryGroups.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.support;
+
+/**
+ * The list of maven group names of all the libraries in this project.
+ */
+public class LibraryGroups {
+    public static final String SUPPORT = "com.android.support";
+    public static final String ROOM = "android.arch.persistence.room";
+    public static final String PERSISTENCE = "android.arch.persistence";
+    public static final String LIFECYCLE = "android.arch.lifecycle";
+    public static final String ARCH_CORE = "android.arch.core";
+    public static final String PAGING = "android.arch.paging";
+    public static final String NAVIGATION = "android.arch.navigation";
+}
