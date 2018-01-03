8b37b73db4398d5a608f65f0594ab6014c7a972a
Aurimas Liutikas <aurimas@google.com>
2018-01-03 13:20:03 -0800

    Move to using SDK 27 for jetpack branch.
    Test: ./gradlew assembleDebug
    Change-Id: I20220a2ce043a4a94527a41ffe2c0b11e9c1ed85

--- src/main/kotlin/android/support/SupportConfig.kt
+++ src/main/kotlin/android/support/SupportConfig.kt
@@ -24,7 +24,7 @@ object SupportConfig {
     const val DEFAULT_MIN_SDK_VERSION = 14
     const val INSTRUMENTATION_RUNNER = "android.support.test.runner.AndroidJUnitRunner"
     const val BUILD_TOOLS_VERSION = "27.0.1"
-    const val CURRENT_SDK_VERSION = 26
+    const val CURRENT_SDK_VERSION = 27
 
     fun getKeystore(project: Project): File {
         val supportRoot = (project.rootProject.property("ext") as ExtraPropertiesExtension)
