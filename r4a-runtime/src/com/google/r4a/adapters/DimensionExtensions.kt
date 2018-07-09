921014e085ae716b8a7279eee6024dc4a6f14f7e
Leland Richardson <lelandr@google.com>
2018-07-09 12:43:57 -0700

    Add supported operators to Dimension
    Change-Id: I8774e50c96b36b8948370bae6026ed78dc5866ab

--- r4a-runtime/src/com/google/r4a/adapters/DimensionExtensions.kt
+++ r4a-runtime/src/com/google/r4a/adapters/DimensionExtensions.kt
@@ -14,9 +14,48 @@ const val UNIT_TYPE_IN = 5
 const val UNIT_TYPE_MM = 6
 // TODO(lmr): degrees/radians ?
 
+
+private inline val Int.scalar: Dimension get() = IntDimension(this, UNIT_TYPE_PX)
+private inline val Float.scalar: Dimension get() = FloatDimension(this, UNIT_TYPE_PX)
+private inline val Double.scalar: Dimension get() = FloatDimension(this.toFloat(), UNIT_TYPE_PX)
+
 interface Dimension {
     fun toIntPixels(metrics: DisplayMetrics): Int
     fun toFloatPixels(metrics: DisplayMetrics): Float
+
+    operator fun times(rhs: Int): Dimension = CombinedDimension(this, rhs.scalar, INT_MULT, FLOAT_MULT)
+    operator fun div(rhs: Int): Dimension = CombinedDimension(this, rhs.scalar, INT_DIV, FLOAT_DIV)
+    operator fun times(rhs: Float): Dimension = CombinedDimension(this, rhs.scalar, INT_MULT, FLOAT_MULT)
+    operator fun div(rhs: Float): Dimension = CombinedDimension(this, rhs.scalar, INT_DIV, FLOAT_DIV)
+    operator fun times(rhs: Double): Dimension = CombinedDimension(this, rhs.scalar, INT_MULT, FLOAT_MULT)
+    operator fun div(rhs: Double): Dimension = CombinedDimension(this, rhs.scalar, INT_DIV, FLOAT_DIV)
+    operator fun plus(dim: Dimension): Dimension = CombinedDimension(this, dim, INT_PLUS, FLOAT_PLUS)
+}
+
+operator fun Int.times(dim: Dimension): Dimension = CombinedDimension(this.scalar, dim, INT_MULT, FLOAT_MULT)
+operator fun Int.div(dim: Dimension): Dimension = CombinedDimension(this.scalar, dim, INT_DIV, FLOAT_DIV)
+operator fun Float.times(dim: Dimension): Dimension = CombinedDimension(this.scalar, dim, INT_MULT, FLOAT_MULT)
+operator fun Float.div(dim: Dimension): Dimension = CombinedDimension(this.scalar, dim, INT_DIV, FLOAT_DIV)
+operator fun Double.times(dim: Dimension): Dimension = CombinedDimension(this.scalar, dim, INT_MULT, FLOAT_MULT)
+operator fun Double.div(dim: Dimension): Dimension = CombinedDimension(this.scalar, dim, INT_DIV, FLOAT_DIV)
+
+private val INT_PLUS = { a: Int, b: Int -> a + b }
+private val FLOAT_PLUS = { a: Float, b: Float -> a + b }
+private val INT_MULT = { a: Int, b: Int -> a * b }
+private val FLOAT_MULT = { a: Float, b: Float -> a * b }
+private val INT_DIV = { a: Int, b: Int -> a / b }
+private val FLOAT_DIV = { a: Float, b: Float -> a / b }
+private val INT_MINUS = { a: Int, b: Int -> a - b }
+private val FLOAT_MINUS = { a: Float, b: Float -> a - b }
+
+private data class CombinedDimension(
+    val left: Dimension,
+    val right: Dimension,
+    val iop: (Int, Int) -> Int,
+    val fop: (Float, Float) -> Float
+) : Dimension {
+    override fun toIntPixels(metrics: DisplayMetrics) = iop(left.toIntPixels(metrics), right.toIntPixels(metrics))
+    override fun toFloatPixels(metrics: DisplayMetrics) = fop(left.toFloatPixels(metrics), right.toFloatPixels(metrics))
 }
 
 data class IntDimension(val value: Int, val unit: Int) : Dimension {
@@ -45,26 +84,26 @@ data class FloatDimension(val value: Float, val unit: Int) : Dimension {
     }
 }
 
-inline val Int.px get() = IntDimension(this, UNIT_TYPE_PX)
-inline val Int.dp get() = IntDimension(this, UNIT_TYPE_DIP)
-inline val Int.dip get() = IntDimension(this, UNIT_TYPE_DIP)
-inline val Int.sp get() = IntDimension(this, UNIT_TYPE_SP)
-inline val Int.pt get() = IntDimension(this, UNIT_TYPE_PT)
-inline val Int.inches get() = IntDimension(this, UNIT_TYPE_IN)
-inline val Int.mm get() = IntDimension(this, UNIT_TYPE_MM)
+inline val Int.px: Dimension get() = IntDimension(this, UNIT_TYPE_PX)
+inline val Int.dp: Dimension get() = IntDimension(this, UNIT_TYPE_DIP)
+inline val Int.dip: Dimension get() = IntDimension(this, UNIT_TYPE_DIP)
+inline val Int.sp: Dimension get() = IntDimension(this, UNIT_TYPE_SP)
+inline val Int.pt: Dimension get() = IntDimension(this, UNIT_TYPE_PT)
+inline val Int.inches: Dimension get() = IntDimension(this, UNIT_TYPE_IN)
+inline val Int.mm: Dimension get() = IntDimension(this, UNIT_TYPE_MM)
 
-inline val Float.px get() = FloatDimension(this, UNIT_TYPE_PX)
-inline val Float.dp get() = FloatDimension(this, UNIT_TYPE_DIP)
-inline val Float.dip get() = FloatDimension(this, UNIT_TYPE_DIP)
-inline val Float.sp get() = FloatDimension(this, UNIT_TYPE_SP)
-inline val Float.pt get() = FloatDimension(this, UNIT_TYPE_PT)
-inline val Float.inches get() = FloatDimension(this, UNIT_TYPE_IN)
-inline val Float.mm get() = FloatDimension(this, UNIT_TYPE_MM)
+inline val Float.px: Dimension get() = FloatDimension(this, UNIT_TYPE_PX)
+inline val Float.dp: Dimension get() = FloatDimension(this, UNIT_TYPE_DIP)
+inline val Float.dip: Dimension get() = FloatDimension(this, UNIT_TYPE_DIP)
+inline val Float.sp: Dimension get() = FloatDimension(this, UNIT_TYPE_SP)
+inline val Float.pt: Dimension get() = FloatDimension(this, UNIT_TYPE_PT)
+inline val Float.inches: Dimension get() = FloatDimension(this, UNIT_TYPE_IN)
+inline val Float.mm: Dimension get() = FloatDimension(this, UNIT_TYPE_MM)
 
-inline val Double.px get() = FloatDimension(this.toFloat(), UNIT_TYPE_PX)
-inline val Double.dp get() = FloatDimension(this.toFloat(), UNIT_TYPE_DIP)
-inline val Double.dip get() = FloatDimension(this.toFloat(), UNIT_TYPE_DIP)
-inline val Double.sp get() = FloatDimension(this.toFloat(), UNIT_TYPE_SP)
-inline val Double.pt get() = FloatDimension(this.toFloat(), UNIT_TYPE_PT)
-inline val Double.inches get() = FloatDimension(this.toFloat(), UNIT_TYPE_IN)
-inline val Double.mm get() = FloatDimension(this.toFloat(), UNIT_TYPE_MM)
+inline val Double.px: Dimension get() = FloatDimension(this.toFloat(), UNIT_TYPE_PX)
+inline val Double.dp: Dimension get() = FloatDimension(this.toFloat(), UNIT_TYPE_DIP)
+inline val Double.dip: Dimension get() = FloatDimension(this.toFloat(), UNIT_TYPE_DIP)
+inline val Double.sp: Dimension get() = FloatDimension(this.toFloat(), UNIT_TYPE_SP)
+inline val Double.pt: Dimension get() = FloatDimension(this.toFloat(), UNIT_TYPE_PT)
+inline val Double.inches: Dimension get() = FloatDimension(this.toFloat(), UNIT_TYPE_IN)
+inline val Double.mm: Dimension get() = FloatDimension(this.toFloat(), UNIT_TYPE_MM)
