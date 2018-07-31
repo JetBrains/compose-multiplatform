0c90bc15c1bf417e7cff3e87efc448da51fec46a
Sumir Kataria <sumir@google.com>
2018-07-31 15:28:10 -0700

    Update WorkManager to 1.0.0-alpha06.
    Test: N/A
    Change-Id: I2815b0680c8274f6d7baf8f0eda8b2bda39a4ef6

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -87,7 +87,7 @@ object LibraryVersions {
     /**
      * Version code for WorkManager
      */
-    val WORKMANAGER = Version("1.0.0-alpha05")
+    val WORKMANAGER = Version("1.0.0-alpha06")
 
     /**
      * Version code for Jetifier
