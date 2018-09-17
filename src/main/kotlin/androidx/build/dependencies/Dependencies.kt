2ad8c3ce86efa8fd9f2371a3f9654771f1242dab
Andrew Lewis <andrewlewis@google.com>
2018-09-17 14:18:34 +0100

    Add back checkerframework
    This is required to avoid errors in presubmit like in
    https://android-build.googleplex.com/builds/pending/P6628810/androidx_host_tests/latest/view/logs/build_error.log
    
    Test: n/a
    Bug: 80232248
    Change-Id: I8e562169f6e6495bbb474e47a64a5a7914b95431

--- src/main/kotlin/androidx/build/dependencies/Dependencies.kt
+++ src/main/kotlin/androidx/build/dependencies/Dependencies.kt
@@ -22,6 +22,7 @@ const val AUTO_VALUE_ANNOTATIONS = "com.google.auto.value:auto-value-annotations
 const val AUTO_VALUE_PARCEL = "com.ryanharter.auto.value:auto-value-parcel:0.2.6"
 const val ANTLR = "org.antlr:antlr4:4.5.3"
 const val APACHE_COMMONS_CODEC = "commons-codec:commons-codec:1.10"
+const val CHECKER_FRAMEWORK = "org.checkerframework:checker-qual:2.5.0"
 const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:1.1.0@aar"
 const val DEXMAKER_MOCKITO = "com.linkedin.dexmaker:dexmaker-mockito:2.19.0"
 const val ESPRESSO_CONTRIB = "androidx.test.espresso:espresso-contrib:3.1.0-alpha3"
