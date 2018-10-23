2910799eefa5cc0d2d736818905def313977c9cb
Ian Lake <ilake@google.com>
2018-10-23 13:38:31 -0700

    Update docs to use Navigation 1.0.0-alpha07
    Test: ./gradlew bOS
    Change-Id: I3b5c725b1bc8a3895296bf4eda067a9183269f83

--- src/main/kotlin/androidx/build/PublishDocsRules.kt
+++ src/main/kotlin/androidx/build/PublishDocsRules.kt
@@ -91,7 +91,7 @@ val RELEASE_RULE = docsRules("public", false) {
     prebuilts(LibraryGroups.LIFECYCLE, "2.0.0")
     prebuilts(LibraryGroups.ARCH_CORE, "2.0.0")
     prebuilts(LibraryGroups.PAGING, "2.1.0-alpha01")
-    prebuilts(LibraryGroups.NAVIGATION, "1.0.0-alpha06")
+    prebuilts(LibraryGroups.NAVIGATION, "1.0.0-alpha07")
     prebuilts(LibraryGroups.WORKMANAGER, "1.0.0-alpha10")
     default(Ignore)
 }
