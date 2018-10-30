d08c35d31689f907645c397c65e39a49bc01d6dc
Ian Lake <ilake@google.com>
2018-10-30 13:50:05 -0700

    Update androidx.activity to 1.0.0-alpha02
    Test: ./gradlew checkApi
    Change-Id: Iefcf31d49fe311aaffb7b305ea5781d12930ca7e

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -20,7 +20,7 @@ package androidx.build
  * The list of versions codes of all the libraries in this project.
  */
 object LibraryVersions {
-    val ACTIVITY = Version("1.0.0-alpha01")
+    val ACTIVITY = Version("1.0.0-alpha02")
     val ANIMATION = Version("1.0.0-alpha01")
     val APPCOMPAT = Version("1.1.0-alpha01")
     val ARCH_CORE = Version("2.0.0")
