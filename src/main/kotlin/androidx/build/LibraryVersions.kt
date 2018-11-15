203260b8429c522f68dd870712ba08bce01b7109
Daniel Santiago Rivera <danysantiago@google.com>
2018-11-15 11:32:45 -0800

    Support custom FTS tokenizers.
    Refactored tokenizer API to accept a String instead of a Enum so that
    users can provide their custom tokenizer name when they ship their
    compiled SQLite with such tokenizer. Note that if a custom tokenizer
    is defined room will not verify the tokenizer nor its arguments during
    compile time since the DB for verification will probably not have
    the tokenizer.
    
    Also bumped Room version to 2.1.0-alpha03
    
    Bug: 119234881
    Test: ./gradlew room:room-compiler:test \
                    room:room-migration:test \
                    room:integration-tests:testapp:cC
    
    Change-Id: I56f17077bd8f9be056f1ae5a9d051d8dbc864ddc

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -79,7 +79,7 @@ object LibraryVersions {
     val RECOMMENDATION = Version("1.0.0")
     val RECYCLERVIEW = Version("1.1.0-alpha01")
     val REMOTECALLBACK = Version("1.0.0-alpha01")
-    val ROOM = Version("2.1.0-alpha02")
+    val ROOM = Version("2.1.0-alpha03")
     val SLICE = Version("1.1.0-alpha01")
     val SLICE_BENCHMARK = Version("1.0.0")
     val SLICE_BUILDERS_KTX = Version("1.0.0-alpha6")
