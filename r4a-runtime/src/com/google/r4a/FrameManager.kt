8540a7bae84839a78126efa334f9043768089122
Chuck Jazdzewski <chuckj@google.com>
2018-12-11 10:50:56 -0800

    [R4A] Fix model data classes to no longer stack fault
    Any framed object that implemented `hashCode` would potentially
    exhibit a simular crash.
    
    With this change the frame manager no longer calls the object's
    implementation of `hashCode` as it needs to track object identity
    for the invalidation scopes.
    
    Fixes: b/120836313
    Change-Id: I2ce079ade2d1f09d89648cfd05fcb1bc98648183

--- r4a-runtime/src/com/google/r4a/FrameManager.kt
+++ r4a-runtime/src/com/google/r4a/FrameManager.kt
@@ -5,17 +5,44 @@ import com.google.r4a.frames.open
 import com.google.r4a.frames.commit
 import com.google.r4a.frames.registerCommitObserver
 import com.google.r4a.frames.inFrame
+import java.lang.ref.WeakReference
 
 import java.util.*
 import kotlin.collections.ArrayList
 
 fun <T> isolated(block: () -> T) = FrameManager.isolated(block)
 
+/**
+ * Ignore the object's implementation of hashCode and equals as they will change for data classes
+ * that are mutated. The read observer needs to track the object identity, not the object value.
+ */
+private class WeakIdentity<T>(value: T) {
+    // Save the hash code of value as it might be reclaimed making value.hashCode inaccessable
+    private val myHc = System.identityHashCode(value)
+
+    // Preserve a weak reference to the value to prevent read observers from leaking observed values
+    private val weakValue = WeakReference(value)
+
+    // Ignore the equality of value and use object identity instead
+    override fun equals(other: Any?): Boolean = this === other || (other is WeakIdentity<*>) && other.value === value && value !== null
+    override fun hashCode(): Int = myHc
+
+    val value: T? get() = weakValue.get()
+}
+
+/**
+ * The frame manager manages the priority frame in the main thread.
+ *
+ * Once the FrameManager has started there is always an open frame in the main thread. If a model object is committed in any
+ * frame then the frame manager schedules the current frame to commit with the Choreographer and a new frame is open. Any
+ * model objects read during composition are recorded in an invalidations map. If they are mutated during a frame the recompose
+ * scope that was active during the read is invalidated.
+ */
 internal object FrameManager {
     private var started = false
     private var commitPending = false
     private var reclaimPending = false
-    private var invalidations = WeakHashMap<Any, MutableSet<RecomposeScope>>()
+    private var invalidations = HashMap<WeakIdentity<Any>, MutableSet<RecomposeScope>>()
     private var removeCommitObserver: (() -> Unit)? = null
     private var compositions = WeakHashMap<CompositionContext, Boolean>()
 
@@ -34,7 +61,7 @@ internal object FrameManager {
         if (inFrame) commit()
         removeCommitObserver?.let { it() }
         started = false
-        invalidations = WeakHashMap()
+        invalidations = HashMap()
         compositions = WeakHashMap()
     }
 
@@ -74,7 +101,7 @@ internal object FrameManager {
     private val readObserver: (read: Any) -> Unit = { read ->
         (CompositionContext.current as? ComposerCompositionContext)?.composer?.currentInvalidate?.let {
             synchronized(this) {
-                invalidations.getOrPut(read) { mutableSetOf() }.add(it)
+                invalidations.getOrPut(WeakIdentity(read)) { mutableSetOf() }.add(it)
             }
         }
     }
@@ -92,7 +119,7 @@ internal object FrameManager {
     private val commitObserver: (committed: Set<Any>) -> Unit = { committed ->
         val currentInvalidations = synchronized(this) {
             committed.mapNotNull {
-                invalidations[it] as Set<RecomposeScope>?
+                invalidations[WeakIdentity(it)] as Set<RecomposeScope>?
             }.reduceSet()
         }
         currentInvalidations.forEach { scope -> scope.invalidate?.let { it() } }
@@ -107,13 +134,14 @@ internal object FrameManager {
         synchronized(this) {
             if (reclaimPending) {
                 reclaimPending = false
-                val removes = invalidations.mapNotNull { entry ->
-                    val framed = entry.key
+                val removes = invalidations.mapNotNull loop@{ entry ->
+                    val identity = entry.key
+                    if (identity.value == null) return@loop identity
                     val invalidations = entry.value
                     invalidations.removeIf { !it.valid }
-                    (if (invalidations.isEmpty()) framed else null)
+                    (if (invalidations.isEmpty()) identity else null)
                 }
-                removes.forEach { framed -> invalidations.remove(framed) }
+                removes.forEach { identity -> invalidations.remove(identity) }
             }
         }
     }
