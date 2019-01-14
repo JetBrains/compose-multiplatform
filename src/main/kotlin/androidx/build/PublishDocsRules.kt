85338ca08555defdf489ea8cbe50575308e9bb4f
Sumir Kataria <sumir@google.com>
2019-01-14 11:44:22 -0800

    Update docs rules to publish WorkManager 1.0.0-beta02.
    Test: genDocs.
    Change-Id: I7e2848a51b1dc044b9f5524a7a082590cb50fc6c

--- src/main/kotlin/androidx/build/PublishDocsRules.kt
+++ src/main/kotlin/androidx/build/PublishDocsRules.kt
@@ -105,7 +105,7 @@ val RELEASE_RULE = docsRules("public", false) {
     prebuilts(LibraryGroups.PAGING, "2.1.0-beta01")
     ignore(LibraryGroups.NAVIGATION, "navigation-testing")
     prebuilts(LibraryGroups.NAVIGATION, "1.0.0-alpha09")
-    prebuilts(LibraryGroups.WORKMANAGER, "1.0.0-beta01")
+    prebuilts(LibraryGroups.WORKMANAGER, "1.0.0-beta02")
     default(Ignore)
 }
 
