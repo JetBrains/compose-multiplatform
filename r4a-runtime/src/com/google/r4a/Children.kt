a9bd30a225f4b8396ca637e2187b51c04b19a67f
Jim S <jsproch@google.com>
2019-01-24 01:52:51 -0800

    Add kdocs for our annotations
    This was one of the things that came up in the early user studies,
    people expected to learn about the annotation by <CTRL-CLICK>ing
    into the annotation.
    
    Change-Id: Iafcbf81d8a100f866651317269b82a339a5bdbbc

--- r4a-runtime/src/com/google/r4a/Children.kt
+++ r4a-runtime/src/com/google/r4a/Children.kt
@@ -1,5 +1,13 @@
 package com.google.r4a
 
+/**
+ * @Children can be applied to one parameter of a composable function, to indicate that this
+ * parameter represents the composable's children.  The @Children parameter is generally used
+ * to describe the composable's sub-nodes within the composable's view hierarchy.  The type
+ * of the @Children parameter is assumed to be @Composable unless otherwise specified.
+ * To learn more, see the "children" section of the r4a-composable documentation available
+ * here: https://goto.google.com/r4a-composable
+ */
 @Target(
     AnnotationTarget.VALUE_PARAMETER,
     AnnotationTarget.PROPERTY,
--- r4a-runtime/src/com/google/r4a/UnionType.kt
+++ r4a-runtime/src/com/google/r4a/UnionType.kt
@@ -1,12 +1,17 @@
-/*
- * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
- * that can be found in the license/LICENSE.txt file.
- */
-
 package com.google.r4a
 
 import kotlin.reflect.KClass
 
+/**
+ * @UnionType can be used when an attribute can be one of several different types.
+ * The annotation indicates that the Kotlin type has been expanded to a common
+ * subtype (often Any) for the purpose of accepting one of a multiplicity of
+ * subtypes.  The @UnionType must specify the intended subtypes, which can then be enforced
+ * by the compiler.  @UnionType is particularly useful when a composable function
+ * is wrapping a native View because it allows a composable to accept the multiplicity
+ * of types corresponding to the overloaded setters for that attribute.
+ * Learn more at: https://goto.google.com/r4a-uniontype
+ */
 @MustBeDocumented
 @Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.TYPE, AnnotationTarget.TYPE_PARAMETER)
