4328ae85844f770d82f9a0b5ee6ed4da4d20d834
Jelle Fresen <jellefresen@google.com>
2019-04-02 12:25:27 +0100

    Bumping recyclerview to 1.1.0-alpha05
    Test: ./gradlew checkApi
    Change-Id: I74eba342c400713a1a69888d1557a816794e7178

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -77,7 +77,7 @@ object LibraryVersions {
     val PERSISTENCE = Version("2.0.1")
     val PREFERENCE = Version("1.1.0-alpha05")
     val RECOMMENDATION = Version("1.1.0-alpha01")
-    val RECYCLERVIEW = Version("1.1.0-alpha04")
+    val RECYCLERVIEW = Version("1.1.0-alpha05")
     val REMOTECALLBACK = Version("1.0.0-alpha02")
     val ROOM = Version("2.1.0-alpha06")
     val SAVEDSTATE = Version("1.0.0-alpha03")
