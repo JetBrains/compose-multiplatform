5c898ef0291bbcb239994258f936a40a26454115
Leland Richardson <lelandr@google.com>
2019-05-03 14:32:16 -0700

    Remove unused ExplorerApp
    Bug: 131924230
    
    Change-Id: Ie0946d3254769f860e71a89e101754bbabd20a8f

--- r4a-runtime/testData/projects/ExplorerApp/common/src/main/java/com/google/r4a/examples/explorerapp/common/adapters/RoundedImgeViewAttributeAdapter.kt
+++
@@ -1,7 +0,0 @@
-package com.google.r4a.examples.explorerapp.common.adapters
-
-import androidx.ui.androidview.adapters.*
-import com.makeramen.roundedimageview.RoundedImageView
-
-fun RoundedImageView.setCornerRadius(cornerRadius: Dimension) = setCornerRadius(cornerRadius.toFloatPixels(metrics))
-fun RoundedImageView.setBorderWidth(borderWidth: Dimension) = setBorderWidth(borderWidth.toFloatPixels(metrics))
