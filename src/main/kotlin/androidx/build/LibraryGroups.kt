aosp-mirror/platform_frameworks_support

Add role AndroidX library.
Bug: 128721328
Bug: 131809779
Test: presubmit
Change-Id: Ife35d265dceb5374f24254ca403b221926291f40

buildSrc/src/main/kotlin/androidx/build/LibraryGroups.kt
@@ -69,6 +69,7 @@ object LibraryGroups {
     val PRINT = LibraryGroup("androidx.print", false)
     val RECOMMENDATION = LibraryGroup("androidx.recommendation", false)
     val RECYCLERVIEW = LibraryGroup("androidx.recyclerview", false)
+    val ROLE = LibraryGroup("androidx.role", false)
     val SAVEDSTATE = LibraryGroup("androidx.savedstate", false)
     val SECURITY = LibraryGroup("androidx.security", false)
     val SHARETARGET = LibraryGroup("androidx.sharetarget", false)

buildSrc/src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -83,6 +83,7 @@ object LibraryVersions {
     val RECOMMENDATION = Version("1.1.0-alpha01")
     val RECYCLERVIEW = Version("1.1.0-alpha06")
     val REMOTECALLBACK = Version("1.0.0-alpha02")
+    val ROLE = Version("1.0.0-alpha01")
     val ROOM = Version("2.1.0-beta02")
     val SAVEDSTATE = Version("1.0.0-beta02")
     val SECURITY = Version("1.0.0-alpha02")

JetBrains/compose-multiplatform

src/main/kotlin/androidx/build/LibraryGroups.kt
@@ -39,7 +39,7 @@ object LibraryGroups {
     val CONTENTPAGER = LibraryGroup("androidx.contentpager")
     val COORDINATORLAYOUT = LibraryGroup("androidx.coordinatorlayout")
     val COMPOSE = LibraryGroup("androidx.compose", false)
-    val CORE = LibraryGroup("androidx.core")
+    val CORE = LibraryGroup("androidx.core", false)
     val CURSORADAPTER = LibraryGroup("androidx.cursoradapter")
     val CUSTOMVIEW = LibraryGroup("androidx.customview")
     val DOCUMENTFILE = LibraryGroup("androidx.documentfile")

src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -44,6 +44,7 @@ object LibraryVersions {
     val COMPOSE = Version("1.0.0-alpha01")
     val COORDINATORLAYOUT = Version("1.1.0-alpha02")
     val CORE = Version("1.1.0-rc01")
+    val CORE_ROLE = Version("1.0.0-alpha01")
     val CURSORADAPTER = Version("1.1.0-alpha01")
     val CUSTOMVIEW = Version("1.1.0-alpha01")
     val DOCUMENTFILE = Version("1.1.0-alpha01")