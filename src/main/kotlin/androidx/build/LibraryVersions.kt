1c4a4c4cf3869a6ea74a7b595d57d863507d4a8d
Ian Lake <ilake@google.com>
2019-06-10 13:48:44 -0700

    Move Fragment to 1.2.0-alpha01
    Test: ./gradlew bOS
    Change-Id: Ied3dbf286a8ba10f7b216e69172f54b2df256c5c

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -54,7 +54,7 @@ object LibraryVersions {
     val EMOJI = Version("1.1.0-alpha01")
     val ENTERPRISE = Version("1.0.0-alpha03")
     val EXIFINTERFACE = Version("1.1.0-alpha02")
-    val FRAGMENT = Version("1.1.0-rc01")
+    val FRAGMENT = Version("1.2.0-alpha01")
     val FUTURES = Version("1.0.0-beta02")
     val GRIDLAYOUT = Version("1.1.0-alpha01")
     val HEIFWRITER = Version("1.1.0-alpha01")
