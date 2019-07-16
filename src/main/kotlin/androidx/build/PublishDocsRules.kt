675c3a72d3f01b17472ac420516fe8c2c305f006
Nick Anthony <nickanthony@google.com>
2019-07-16 06:55:16 -0400

    Updated PublishDocsRules.kt for work-2.2.0-beta01
    This commit was generated from the command:
    ./import_release_prebuilts.py 5725177 --groups work
    
    Test: ./gradlew buildOnServer
    Change-Id: I69ac006125d37d38aba6d23764525f0f00ec97cd

--- src/main/kotlin/androidx/build/PublishDocsRules.kt
+++ src/main/kotlin/androidx/build/PublishDocsRules.kt
@@ -126,7 +126,7 @@ val RELEASE_RULE = docsRules("public", false) {
     prebuilts(LibraryGroups.WEBKIT, "1.1.0-alpha01")
     ignore(LibraryGroups.WORK.group, "work-gcm")
     ignore(LibraryGroups.WORK.group, "work-foreground")
-    prebuilts(LibraryGroups.WORK, "2.1.0")
+    prebuilts(LibraryGroups.WORK, "2.2.0-beta01")
     default(Ignore)
 }
 
