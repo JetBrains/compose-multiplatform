4035b38fa340f62b5eee696ddcad1dbadc950cd2
Kevin Chyn <kchyn@google.com>
2019-07-18 18:15:20 -0700

    Update androidx.biometric to beta01
    Fixes: 137887749
    
    Test: ./gradlew updateApi
    Test: ./gradlew buildOnServer
    Change-Id: I996e6cd5efd9afb66c4cd3a460dacc8e71c26cb5

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -32,7 +32,7 @@ object LibraryVersions {
     val ASYNCLAYOUTINFLATER = Version("1.1.0-alpha01")
     val AUTOFILL = Version("1.0.0-alpha02")
     val BENCHMARK = Version("1.0.0-alpha04")
-    val BIOMETRIC = Version("1.0.0-alpha05")
+    val BIOMETRIC = Version("1.0.0-beta01")
     val BROWSER = Version("1.2.0-alpha05")
     val CAMERA = Version("1.0.0-alpha04")
     val CAMERA_EXTENSIONS = Version("1.0.0-alpha01")
