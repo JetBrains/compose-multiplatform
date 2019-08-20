2503391d8c25c007dd4b2f86a7541c1462c08916
Nick Anthony <nickanthony@google.com>
2019-08-20 14:45:20 -0400

    Updated PublishDocsRules.kt for appcompat-1.1.0
    This commit was generated from the command:
    ./import_release_prebuilts.py 5815950 --groups appcompat
    
    Test: ./gradlew buildOnServer
    Change-Id: I111f15047ea34b7118728c080180c6ff3fd79656

--- src/main/kotlin/androidx/build/PublishDocsRules.kt
+++ src/main/kotlin/androidx/build/PublishDocsRules.kt
@@ -31,7 +31,7 @@ val RELEASE_RULE = docsRules("public", false) {
     prebuilts(LibraryGroups.ACTIVITY, "1.1.0-alpha02")
     prebuilts(LibraryGroups.ADS, "1.0.0-alpha01")
     prebuilts(LibraryGroups.ANNOTATION, "annotation", "1.1.0")
-    prebuilts(LibraryGroups.APPCOMPAT, "1.1.0-rc01")
+    prebuilts(LibraryGroups.APPCOMPAT, "1.1.0")
     prebuilts(LibraryGroups.ARCH_CORE, "2.1.0")
     prebuilts(LibraryGroups.ASYNCLAYOUTINFLATER, "1.0.0")
     prebuilts(LibraryGroups.AUTOFILL, "1.0.0-alpha02")
