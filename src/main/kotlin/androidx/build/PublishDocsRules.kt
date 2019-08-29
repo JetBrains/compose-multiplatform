70f7f6f4e8b2273e6e1f5f6222512fe0c1c1f6c7
Nick Anthony <nickanthony@google.com>
2019-08-29 17:39:56 -0400

    Updated PublishDocsRules.kt for fragment-1.2.0-alpha03
    This commit was generated from the command:
    ./import_release_prebuilts.py 5837550 --groups fragment
    
    Test: ./gradlew buildOnServer
    Change-Id: Iac76e4185d4bcd1a33c8e3e2d143d55a66c3101c

--- src/main/kotlin/androidx/build/PublishDocsRules.kt
+++ src/main/kotlin/androidx/build/PublishDocsRules.kt
@@ -66,7 +66,7 @@ val RELEASE_RULE = docsRules("public", false) {
     prebuilts(LibraryGroups.EMOJI, "1.0.0")
     prebuilts(LibraryGroups.ENTERPRISE, "1.0.0-alpha03")
     prebuilts(LibraryGroups.EXIFINTERFACE, "1.1.0-beta01")
-    prebuilts(LibraryGroups.FRAGMENT, "1.2.0-alpha02")
+    prebuilts(LibraryGroups.FRAGMENT, "1.2.0-alpha03")
     prebuilts(LibraryGroups.GRIDLAYOUT, "1.0.0")
     prebuilts(LibraryGroups.HEIFWRITER, "1.0.0")
     prebuilts(LibraryGroups.INTERPOLATOR, "1.0.0")
