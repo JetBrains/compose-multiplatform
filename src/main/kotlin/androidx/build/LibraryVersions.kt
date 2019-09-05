1e73c11330dafe18c93879e6cb9517e9173cdf7a
Filip Pavlis <pavlis@google.com>
2019-09-05 13:11:43 +0100

    Bump up version of jetifier.
    New version to release as it will contain navigation mapping fixes.
    
    Bug: N/A
    Test: N/A
    Change-Id: I83c4c0cc06f40eac2e10e4413a734a47aeeca1e6

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -63,7 +63,7 @@ object LibraryVersions {
     val HEIFWRITER = Version("1.1.0-alpha01")
     val INSPECTION = Version("1.0.0-alpha01")
     val INTERPOLATOR = Version("1.1.0-alpha01")
-    val JETIFIER = Version("1.0.0-beta06")
+    val JETIFIER = Version("1.0.0-beta07")
     val LEANBACK = Version("1.1.0-alpha03")
     val LEANBACK_PREFERENCE = Version("1.1.0-alpha03")
     val LEGACY = Version("1.1.0-alpha01")
