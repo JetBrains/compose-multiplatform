4ef52428e6c60ba0905db91712cdbe7c96bb5750
Jin Seok Park <jinpark@google.com>
2019-09-09 20:12:05 +0900

    Add API to check if specified mime type is supported
    Bug: 138845596
    Test: N/A
    Change-Id: I79566bc78e5b5a67d4bef936cc751ed3a74fec10

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -57,7 +57,7 @@ object LibraryVersions {
     val DYNAMICANIMATION_KTX = Version("1.0.0-alpha03")
     val EMOJI = Version("1.1.0-alpha01")
     val ENTERPRISE = Version("1.0.0-rc01")
-    val EXIFINTERFACE = Version("1.1.0-beta02")
+    val EXIFINTERFACE = Version("1.2.0-alpha01")
     val FRAGMENT = Version("1.2.0-beta01")
     val FUTURES = Version("1.0.0-rc01")
     val GRIDLAYOUT = Version("1.1.0-alpha01")
