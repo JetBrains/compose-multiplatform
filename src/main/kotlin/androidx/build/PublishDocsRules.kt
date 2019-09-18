032393f5ee995cd20db1e506fa3e101b9e52aef3
Nick Anthony <nickanthony@google.com>
2019-09-18 17:08:00 -0400

    Updated PublishDocsRules.kt for concurrent-1.0.0
    This commit was generated from the command:
    ./import_release_prebuilts.py 5884696 --group concurrent
    
    Test: ./gradlew buildOnServer
    Change-Id: I729dd7bef07a4d589c64d56e5018cd621714b324

--- src/main/kotlin/androidx/build/PublishDocsRules.kt
+++ src/main/kotlin/androidx/build/PublishDocsRules.kt
@@ -53,7 +53,7 @@ val RELEASE_RULE = docsRules("public", false) {
             .addStubs("car/stubs/android.car.jar")
     prebuilts(LibraryGroups.CARDVIEW, "1.0.0")
     prebuilts(LibraryGroups.COLLECTION, "1.1.0")
-    prebuilts(LibraryGroups.CONCURRENT, "1.0.0-rc01")
+    prebuilts(LibraryGroups.CONCURRENT, "1.0.0")
     prebuilts(LibraryGroups.CONTENTPAGER, "1.0.0")
     prebuilts(LibraryGroups.COORDINATORLAYOUT, "1.1.0-beta01")
     prebuilts(LibraryGroups.CORE, "core", "1.2.0-alpha04")
