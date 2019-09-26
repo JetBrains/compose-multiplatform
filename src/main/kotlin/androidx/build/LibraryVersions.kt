1cd6862818e61135c716bc1cc284ebcb83a62ac5
Chris Craik <ccraik@google.com>
2019-09-26 12:44:13 -0700

    Bump RV version to rc02
    Test: none
    
    Change-Id: I683d2cf67279bc6360dce454985456d6e79eb312

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -88,7 +88,7 @@ object LibraryVersions {
     val PERCENTLAYOUT = Version("1.1.0-alpha01")
     val PREFERENCE = Version("1.2.0-alpha01")
     val RECOMMENDATION = Version("1.1.0-alpha01")
-    val RECYCLERVIEW = Version("1.1.0-rc01")
+    val RECYCLERVIEW = Version("1.1.0-rc02")
     val RECYCLERVIEW_SELECTION = Version("1.1.0-alpha07")
     val REMOTECALLBACK = Version("1.0.0-alpha02")
     val ROOM = Version("2.2.0-beta01")
