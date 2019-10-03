3c10602f6cce617d57f738382f41c6585345ed50
Arvind Kumar Sugumar <asugumar@google.com>
2019-10-03 14:27:44 -0700

    Bumping androidx.autofill to rc01.
    Test: version bump.
    Change-Id: If620d1c357091ee00b9cddc2d2873da8c4336a49

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -32,7 +32,7 @@ object LibraryVersions {
     val ARCH_CORE_TESTING = ARCH_CORE
     val ARCH_RUNTIME = Version("2.2.0-alpha01")
     val ASYNCLAYOUTINFLATER = Version("1.1.0-alpha01")
-    val AUTOFILL = Version("1.0.0-beta02")
+    val AUTOFILL = Version("1.0.0-rc01")
     val BENCHMARK = Version("1.0.0-beta01")
     val BIOMETRIC = Version("1.0.0-rc01")
     val BROWSER = Version("1.2.0-alpha11")
