34bdb8875a1816a8f2913368f3f5bbf7cd91f6d6
Aurimas Liutikas <aurimas@google.com>
2019-11-26 16:02:02 -0800

    Make root project task dependency by buildOnServer lazy
    - Remove bOS dependency on distDocs. This task does not exist on root project.
    Our docs are built via docs-runner project.
    - Move adding dependency by buildOnServer to a lazy way (away from
    tasks.all)
    
    Test: ./gradlew bOS --dry-run --console=plain -> compare before and
    after this patch.
    Change-Id: I24edbfc14489d486d1925b0ab36e6284c6b2648a

--- src/main/kotlin/androidx/build/AndroidXPlugin.kt
+++ src/main/kotlin/androidx/build/AndroidXPlugin.kt
@@ -281,13 +281,7 @@ class AndroidXPlugin : Plugin<Project> {
 
         val projectModules = ConcurrentHashMap<String, String>()
         extra.set("projects", projectModules)
-        tasks.all { task ->
-            if (task.name.startsWith(Release.DIFF_TASK_PREFIX) ||
-                    "distDocs" == task.name ||
-                    CheckExternalDependencyLicensesTask.TASK_NAME == task.name) {
-                buildOnServerTask.dependsOn(task)
-            }
-        }
+        buildOnServerTask.dependsOn(tasks.named(CheckExternalDependencyLicensesTask.TASK_NAME))
         subprojects { project ->
             if (project.path == ":docs-runner") {
                 project.tasks.all { task ->
