12df7cc7b9b9c5b5d8489c499bc3171ea54d5daa
Dustin Lam <dustinlam@google.com>
2019-12-05 16:08:22 +0000

    Revert "Use a separate version tag for concurrent-futures-ktx"
    This reverts commit 5ec6aa992d46cd16960f466b9367bef7944a64a2.
    
    Reason for revert: Libraries developed together
    
    Change-Id: I2e314ea72af8352f5466767a5e5da05473909619

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -62,7 +62,6 @@ object LibraryVersions {
     val EXIFINTERFACE = Version("1.2.0-beta01")
     val FRAGMENT = Version("1.3.0-alpha01")
     val FUTURES = Version("1.1.0-alpha01")
-    val FUTURES_KTX = Version("1.0.0-alpha01")
     val GRIDLAYOUT = Version("1.1.0-alpha01")
     val HEIFWRITER = Version("1.1.0-alpha01")
     val INSPECTION = Version("1.0.0-alpha01")
