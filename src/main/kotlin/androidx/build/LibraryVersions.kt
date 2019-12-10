3e133a55ffd7957e00e722a1f55d283412790ea6
Yuichi Araki <yaraki@google.com>
2019-12-10 10:30:25 +0900

    SeekableAnimatedVectorDrawable
    Add a new library 'vectordrawable-seekable'.
    
    This is similar to 'vectordrawable-animated', but different in some important points.
    
    - It does not delegate to the platform version of AnimatedVectorDrawable.
    - It uses the new 'androidx.animation' instead of the platform versions.
    
    Bug: 145715553
    Test: ./gradlew :vectordrawable:vectordrawable-seekable:connectedCheck
    Change-Id: Ifa97c5557adcd83b89bfe630d420f0b3c2291470

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -108,6 +108,7 @@ object LibraryVersions {
     val UI = Version("0.1.0-dev05")
     val VECTORDRAWABLE = Version("1.2.0-alpha01")
     val VECTORDRAWABLE_ANIMATED = Version("1.2.0-alpha01")
+    val VECTORDRAWABLE_SEEKABLE = Version("1.0.0-alpha01")
     val VERSIONED_PARCELABLE = Version("1.2.0-alpha01")
     val VIEWPAGER = Version("1.1.0-alpha01")
     val VIEWPAGER2 = Version("1.1.0-alpha01")
