9c527445a90961a9bbe1723ff4f9a1d18b6d2b02
Matthew Fraschilla <fraschilla@google.com>
2019-12-11 15:35:16 -0800

    Start api tracking for nav-testing
    Test: N/A
    Change-Id: I040ba8674b1d678db0f709bc9e235af1a5ee8d76

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -83,7 +83,6 @@ object LibraryVersions {
     val MEDIAROUTER = Version("1.2.0-alpha01")
     val MESSAGEBROWSER_BROWSER = Version("1.0.0-alpha01")
     val NAVIGATION = Version("2.3.0-alpha01")
-    val NAVIGATION_TESTING = Version("1.0.0-alpha08") // Unpublished
     val PAGING = Version("3.0.0-alpha01")
     val PALETTE = Version("1.1.0-alpha01")
     val PRINT = Version("1.1.0-alpha01")
