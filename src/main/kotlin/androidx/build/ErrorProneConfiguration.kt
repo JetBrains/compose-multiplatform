7ffb51a88d724e0f128b7efcf59c4ba04b4a188b
Sergey <zakharovsergey1000@gmail.com>
2019-12-31 16:50:28 +0400

    Fix and enforce HidingField errorprone warnings
    Test: ./gradlew runErrorProne
    Bug: 145764247
    Change-Id: Ibb973f951fe3e1f6441ec921f12badfab2b018ca

--- src/main/kotlin/androidx/build/ErrorProneConfiguration.kt
+++ src/main/kotlin/androidx/build/ErrorProneConfiguration.kt
@@ -102,6 +102,7 @@ private fun JavaCompile.configureWithErrorProne(toolChain: ErrorProneToolChain) 
             "-Xep:UnusedVariable:ERROR",
             "-Xep:UnusedMethod:ERROR",
             "-Xep:FutureReturnValueIgnored:ERROR",
+            "-Xep:HidingField:ERROR",
             "-Xep:AutoValueFinalMethods:ERROR",
 
             // Nullaway
