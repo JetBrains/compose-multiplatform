3825bd54f19e3fc5e1cd27deba3d7b4277c88e86
Louis Pullen-Freilich <lpf@google.com>
2020-01-30 14:54:37 +0000

    Update buildtools to 29.0.3
    Test: ./gradlew bOS in main and ui/ subdir
    Change-Id: Ie5b3a94a94c50b2a4dd22dd4f284b38740fcc2e4

--- src/main/kotlin/androidx/build/SupportConfig.kt
+++ src/main/kotlin/androidx/build/SupportConfig.kt
@@ -23,7 +23,7 @@ import java.io.File
 object SupportConfig {
     const val DEFAULT_MIN_SDK_VERSION = 14
     const val INSTRUMENTATION_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
-    const val BUILD_TOOLS_VERSION = "28.0.3"
+    const val BUILD_TOOLS_VERSION = "29.0.3"
 
     /**
      * The Android SDK version to use for compilation.
