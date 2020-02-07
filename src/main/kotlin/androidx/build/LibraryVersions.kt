6068c12a061cfbcb7569c87f1023e96162c39093
Ram Parameswaran <rampara@google.com>
2020-02-06 21:19:52 -0800

    Deprecate car lib
    Bug: 141561591
    Test: Manual
    
    Change-Id: Ia056aa3d357b2a963465b6095c5eb43d47ac926c

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -38,8 +38,6 @@ object LibraryVersions {
     val CAMERA = Version("1.0.0-beta04")
     val CAMERA_EXTENSIONS = Version("1.0.0-alpha11")
     val CAMERA_VIEW = Version("1.0.0-alpha011")
-    val CAR = Version("1.0.0-alpha8")
-    val CAR_MODERATOR = Version("1.0.0-alpha1")
     val CARDVIEW = Version("1.1.0-alpha01")
     val COLLECTION = Version("1.2.0-alpha01")
     val CONTENTPAGER = Version("1.1.0-alpha01")
