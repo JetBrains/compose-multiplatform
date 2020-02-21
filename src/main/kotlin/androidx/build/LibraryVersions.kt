d2f0c50063b69f7f94d321841001262d70c41313
Nick Anthony <nickanthony@google.com>
2020-02-21 16:30:46 -0500

    Update Navigation to 2.3.0-alpha03
    Test: gw checkApi
    Change-Id: Ie8650d1bd0c9d82b2ac6dde4265a17b0e1d5bcb7

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -76,7 +76,7 @@ object LibraryVersions {
     val MEDIA2 = Version("1.1.0-alpha01")
     val MEDIAROUTER = Version("1.2.0-alpha01")
     val MESSAGEBROWSER_BROWSER = Version("1.0.0-alpha01")
-    val NAVIGATION = Version("2.3.0-alpha02")
+    val NAVIGATION = Version("2.3.0-alpha03")
     val PAGING = Version("3.0.0-alpha01")
     val PALETTE = Version("1.1.0-alpha01")
     val PRINT = Version("1.1.0-alpha01")
