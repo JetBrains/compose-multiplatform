da19e736268229b6e5929ac3c61cb8763d757675
Chuck Jazdzewski <chuckj@google.com>
2020-04-30 16:44:14 -0700

    Add documentation for the Immutable annotation
    Fixes: b/155156155
    Test: none, documentation
    Change-Id: Ifcfc3df8c7c85be754343534811388fda66ed7d7

--- compose-runtime/src/commonMain/kotlin/androidx/compose/Immutable.kt
+++ compose-runtime/src/commonMain/kotlin/androidx/compose/Immutable.kt
@@ -16,6 +16,28 @@
 
 package androidx.compose
 
+/**
+ * [Immutable] can be used to mark class as producing immutable instances. The immutability of the
+ * class is not validated and is a promise by the type that all publicly accessible properties
+ * and fields will not change after the instance is constructed. This is a stronger promise than
+ * `val` as it promises that the value will never change not only that values cannot be changed
+ * through a setter.
+ *
+ * [Immutable] is used by composition which enables composition optimizations that can be
+ * performed based on the assumption that values read from the type will not change.  See
+ * [StableMarker] for additional details.
+ *
+ * `data` classes that only contain `val` properties that do not have custom getters can safely
+ * be marked as [Immutable] if the types of properties are either primitive types or also
+ * [Immutable]:
+ *
+ * @sample androidx.compose.samples.simpleImmutableClass
+ *
+ * Marking `Person` immutable allows calls the `PersonView` [Composable] function to be skipped if
+ * it is the same `person` as it was during the last composition.
+ *
+ * @see StableMarker
+ */
 @MustBeDocumented
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.BINARY)
