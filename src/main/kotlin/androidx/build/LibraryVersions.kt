eb39499993fa7b4bd87060c771703fb3f6d8ac97
John Nichol <jnichol@google.com>
2020-05-01 11:14:57 +0100

    The beta release has now been cut against build 6420846. Bump to rc01 as that is the next version we intend to ship.
    Bug: 155284266
    Test: N/A
    Change-Id: I280b8062484c14123407f5bc0756d02dcebef4e3

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -115,7 +115,7 @@ object LibraryVersions {
     val VERSIONED_PARCELABLE = Version("1.2.0-alpha01")
     val VIEWPAGER = Version("1.1.0-alpha01")
     val VIEWPAGER2 = Version("1.1.0-alpha01")
-    val WEAR = Version("1.1.0-beta01")
+    val WEAR = Version("1.1.0-rc01")
     val WEBKIT = Version("1.3.0-alpha02")
     val WINDOW = Version("1.0.0-alpha02")
     val WINDOW_SIDECAR = Version("0.1.0-alpha01")
