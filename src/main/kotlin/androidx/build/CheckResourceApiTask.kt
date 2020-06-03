5d14dd857ef5246743ea4101c5e62e6368ca0298
Alan Viverette <alanv@google.com>
2020-06-03 23:58:33 +0000

    Revert "Align resource API tasks with Metalava API tasks"
    This reverts commit 08a06281e20233f9c4cb777aa158ddcd5be59414.
    
    Reason for revert: Broke ./gradlew :drawerlayout:drawerlayout:checkResourceApiRelease
    
    Bug: 129362030
    Change-Id: I55da693fa4098c2a533808feac56db6d76e5aad3

---
+++ src/main/kotlin/androidx/build/CheckResourceApiTask.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.build
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.TaskAction
+import java.io.File
+
+/**
+ * Task for detecting changes in the public resource surface
+ */
+open class CheckResourceApiTask : DefaultTask() {
+
+    @InputFiles
+    @Optional
+    var oldApiFile: File? = null
+
+    @InputFiles
+    @Optional
+    var newApiFile: File? = null
+
+    @TaskAction
+    fun checkResourceApi() {
+        if (oldApiFile == null || !oldApiFile!!.exists()) {
+            throw GradleException("No resource api file for the current version exists, please" +
+                    " run updateApi to create one.")
+        }
+        var oldResourceApi: HashSet<String> = HashSet(oldApiFile?.readLines())
+        var newResourceApi: HashSet<String> = HashSet()
+        if (newApiFile != null && newApiFile!!.exists()) {
+            newResourceApi = HashSet(newApiFile?.readLines())
+        }
+        if (oldResourceApi != newResourceApi) {
+            throw GradleException("Public resource surface changes detected, please run" +
+                    " updateApi to confirm this change is intentional.")
+        }
+    }
+}
