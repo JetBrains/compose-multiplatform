fc1659cce9a6f9248ecf2710a066088e618bd457
Alan Viverette <alanv@google.com>
2020-06-10 17:36:22 -0400

    Clean up after upstreaming platform branch
    Sets targetSdkVersion to 29 because there is additional work required to
    set it to 30. We'll deal with that later...
    
    Bug: 157027847
    Test: ./gradlew bOS
    Change-Id: Ib5acccb48c7d9c782f3ce2ada0cd818a42093b57

--- src/main/kotlin/androidx/build/SupportConfig.kt
+++ src/main/kotlin/androidx/build/SupportConfig.kt
@@ -31,7 +31,7 @@ object SupportConfig {
      * Either an integer value or a pre-release platform code, prefixed with "android-" (ex.
      * "android-28" or "android-Q") as you would see within the SDK's platforms directory.
      */
-    const val COMPILE_SDK_VERSION = "android-R"
+    const val COMPILE_SDK_VERSION = "android-30"
 
     /**
      * The Android SDK version to use for targetSdkVersion meta-data.
