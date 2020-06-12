49905119b8bdb92c458b0b18658d6299c20a57e7
Hanzhen Liang <hanzhen.liang@booking.com>
2020-06-13 00:47:02 +0200

    Fix Android Studio can not be updated issue.
    Updating Android Studio require several long-running steps,
    if users exist in any step, Android Studio can not be updated
    any more.
    
    Reason: We only check if the Android Studio install path exists or not,
    but creating install path is the first step of updating, which means
    it can not guarantee other steps is successful.
    
    Solution: We create `STUDIO_UPDATED` file in install dir after all steps
    are successful, and check by this file to see if Android Studio needs to be updated.
    Every time we update Android Studio, we need to clean up all installations
    to avoid `file already exists` issue.
    
    Since we add `INSTALL_SUCCESSFUL` to indicate AS is installed
    successfully, to avoid current users downloading the same AS
    because lack of the file, we need to check if license accepted
    file exists or not.
    
    Bug: 158862268
    Test: ./studiow
    Change-Id: I8f0a959093b9f6e794346b0d5e0588ebcb8d8df1

--- src/main/kotlin/androidx/build/studio/StudioTask.kt
+++ src/main/kotlin/androidx/build/studio/StudioTask.kt
@@ -37,7 +37,7 @@ abstract class StudioTask : DefaultTask() {
     // TODO: support -y and --update-only options? Can use @Option for this
     @TaskAction
     fun studiow() {
-        update()
+        install()
         launch()
     }
 
@@ -99,19 +99,26 @@ abstract class StudioTask : DefaultTask() {
     @get:Internal
     protected abstract val studioArchiveCreator: StudioArchiveCreator
 
+    private val licenseAcceptedFile: File by lazy {
+        File("$studioInstallationDir/STUDIOW_LICENSE_ACCEPTED")
+    }
+
     /**
-     * Updates the Studio installation and removes any old installation files if they exist.
+     * Install Studio and removes any old installation files if they exist.
      */
-    private fun update() {
-        if (!studioInstallationDir.exists()) {
+    private fun install() {
+        val successfulInstallFile = File("$studioInstallationDir/INSTALL_SUCCESSFUL")
+        if (!licenseAcceptedFile.exists() && !successfulInstallFile.exists()) {
+            // Attempt to remove any old installations in the parent studio/ folder
+            studioInstallationDir.parentFile.deleteRecursively()
             // Create installation directory and any needed parent directories
             studioInstallationDir.mkdirs()
-            // Attempt to remove any old installations in the parent studio/ folder
-            removeOldInstallations()
             studioArchiveCreator(project, studioVersions, studioArchiveName, studioArchivePath)
             println("Extracting archive...")
             extractStudioArchive()
             with(platformUtilities) { updateJvmHeapSize() }
+            // Finish install process
+            successfulInstallFile.createNewFile()
         }
     }
 
@@ -152,7 +159,6 @@ abstract class StudioTask : DefaultTask() {
     }
 
     private fun checkLicenseAgreement(services: ServiceRegistry): Boolean {
-        val licenseAcceptedFile = File("$studioInstallationDir/STUDIOW_LICENSE_ACCEPTED")
         if (!licenseAcceptedFile.exists()) {
             val licensePath = with(platformUtilities) { licensePath }
 
@@ -176,20 +182,6 @@ abstract class StudioTask : DefaultTask() {
         project.exec { execSpec -> platformUtilities.extractArchive(fromPath, toPath, execSpec) }
         // Remove studio archive once done
         File(studioArchivePath).delete()
-    }
-
-    private fun removeOldInstallations() {
-        val parentFile = studioInstallationDir.parentFile
-        parentFile.walk().maxDepth(1)
-            .filter { file ->
-                // Remove any files that aren't either the directory / archive matching the
-                // current version, and also ignore the parent `studio/` directory
-                !file.name.contains(studioInstallationDir.name) && file != parentFile
-            }
-            .forEach { file ->
-                println("Removing old installation file ${file.absolutePath}")
-                file.deleteRecursively()
-            }
     }
 
     companion object {
