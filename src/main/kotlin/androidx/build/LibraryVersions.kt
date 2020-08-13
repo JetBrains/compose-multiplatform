9697bbb8c1de11bb609501de4c1719c7405a7d07
Dustin Lam <dustinlam@google.com>
2020-08-13 11:26:37 -0700

    Bump room to 2.3.0-alpha03
    Test: ./gradlew bOS
    Change-Id: I86b8ac9f9ee5bbf96309c1fea6d0d41748a8b7da

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -89,7 +89,7 @@ object LibraryVersions {
     val RECYCLERVIEW = Version("1.2.0-alpha05")
     val RECYCLERVIEW_SELECTION = Version("2.0.0-alpha01")
     val REMOTECALLBACK = Version("1.0.0-alpha02")
-    val ROOM = Version("2.3.0-alpha02")
+    val ROOM = Version("2.3.0-alpha03")
     val SAVEDSTATE = Version("1.1.0-alpha01")
     val SECURITY = Version("1.1.0-alpha02")
     val SECURITY_BIOMETRIC = Version("1.0.0-alpha01")
