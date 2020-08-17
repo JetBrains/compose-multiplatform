c7d0fc3c050f2427fd7de7ae997865e4a675b96e
Jonathan Scott <scottjonathan@google.com>
2020-08-17 12:23:35 +0100

    Bump version of androidx.enterprise.
    Test: N/A
    Change-Id: I36e2a11b518148f13705ae4743db4c5a7b75713c

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -58,7 +58,7 @@ object LibraryVersions {
     val DYNAMICANIMATION = Version("1.1.0-alpha04")
     val DYNAMICANIMATION_KTX = Version("1.0.0-alpha04")
     val EMOJI = Version("1.2.0-alpha01")
-    val ENTERPRISE = Version("1.1.0-alpha01")
+    val ENTERPRISE = Version("1.1.0-alpha02")
     val EXIFINTERFACE = Version("1.3.0-beta01")
     val FRAGMENT = Version("1.3.0-alpha08")
     val FUTURES = Version("1.2.0-alpha01")
