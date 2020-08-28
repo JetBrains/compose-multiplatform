4a642698c74eb17ec94e64010b57721c8eaf62c4
George Mount <mount@google.com>
2020-08-28 08:57:00 -0700

    Support RenderNode on M-O devices.
    Bug: 162061312
    
    To avoid reflection, a new stub library was created to
    support RenderNode on older devices. This should also improve
    allocation performance as RenderNodes should be much smaller
    and easier to allocate than Views.
    
    This does not currently work on P devices because ART
    does not allow access to APIs in the unsupported list on P.
    
    TextInColumnBenchmark#first_draw is around 30% faster in
    my local tests.
    
    This CL will make benchmark performance slower on M-O devices,
    but it is more representative of actual performance. It turns
    out that PictureRecordingCanvas has faster access than
    DisplayListCanvas and was giving artificially fast results
    on devices that don't support hardware accelerated canvas.
    
    Test: demo on API 23, 26, 29
    Test: TextInColumnBenchmark#first_draw
    Test: New test for fallback for M RenderNode to View
    
    Change-Id: Ia10caea550f328368c4f0f4384267058d1dbc541

---
+++ ui/ui/src/androidMain/kotlin/androidx/compose/ui/platform/DeviceRenderNode.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.ui.platform
+
+import android.graphics.Outline
+import androidx.compose.ui.graphics.Canvas
+import androidx.compose.ui.graphics.CanvasHolder
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.node.OwnedLayer
+
+/**
+ * RenderNode on Q+ and RenderNode on M-P devices have different APIs. This interface
+ * unifies the access so that [RenderNodeLayer] can be used for both.
+ */
+internal interface DeviceRenderNode {
+    val uniqueId: Long
+    val left: Int
+    val top: Int
+    val right: Int
+    val bottom: Int
+    val width: Int
+    val height: Int
+    var scaleX: Float
+    var scaleY: Float
+    var translationX: Float
+    var translationY: Float
+    var elevation: Float
+    var rotationZ: Float
+    var rotationX: Float
+    var rotationY: Float
+    var pivotX: Float
+    var pivotY: Float
+    var clipToOutline: Boolean
+    var clipToBounds: Boolean
+    var alpha: Float
+    val hasDisplayList: Boolean
+
+    fun setOutline(outline: Outline?)
+    fun setPosition(left: Int, top: Int, right: Int, bottom: Int): Boolean
+    fun offsetLeftAndRight(offset: Int)
+    fun offsetTopAndBottom(offset: Int)
+    fun record(
+        canvasHolder: CanvasHolder,
+        clipPath: Path?,
+        observerScope: OwnedLayer,
+        drawBlock: (Canvas) -> Unit
+    )
+    fun getMatrix(matrix: android.graphics.Matrix)
+    fun drawInto(canvas: android.graphics.Canvas)
+}
