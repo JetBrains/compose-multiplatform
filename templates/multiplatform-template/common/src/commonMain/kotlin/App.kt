9ae0b367508eefea35147adef4d0862b2f1a46ae
Igor Demin <igor.demin@jetbrains.com>
2020-09-21 15:08:28 +0300

    App templates: change code

--- templates/multiplatform-template/common/src/commonMain/kotlin/App.kt
+++ templates/multiplatform-template/common/src/commonMain/kotlin/App.kt
@@ -1,7 +1,19 @@
 import androidx.compose.foundation.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.*
 
 @Composable
 fun App() {
-    Text("Hello, World!")
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    MaterialTheme {
+        Button(onClick = {
+            text = "Hello, ${getPlatformName()}"
+        }) {
+            Text(text)
+        }
+    }
 }
+
+expect fun getPlatformName(): String
