5e91685faa7b20ef03905de2905a90b0c457e131
Qingqing Deng <qqd@google.com>
2020-09-22 18:41:59 -0700

    [Text/TextField Selection] Rtl Handle Position.
    -- Fix the Selection Handle Position in Rtl Layout.
    -- Refactor the SelectionHandle related code.
    
    Bug: 161809552
    Bug: 161652927
    Test: Manually
    
    RelNote: Fix Rtl Handle Position.
    Change-Id: I6e1e07b76476d8e2f0be50ff022257c2379edcf7

--- foundation/foundation-text/src/commonMain/kotlin/androidx/compose/foundation/text/CoreTextField.kt
+++ foundation/foundation-text/src/commonMain/kotlin/androidx/compose/foundation/text/CoreTextField.kt
@@ -25,7 +25,7 @@ import androidx.compose.animation.core.AnimationSpec
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.keyframes
 import androidx.compose.animation.core.repeatable
-import androidx.compose.foundation.text.selection.SelectionHandle
+import androidx.compose.foundation.text.selection.TextFieldSelectionHandle
 import androidx.compose.foundation.text.selection.TextFieldSelectionManager
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
@@ -67,7 +67,7 @@ import androidx.compose.ui.platform.FontLoaderAmbient
 import androidx.compose.ui.platform.HapticFeedBackAmbient
 import androidx.compose.ui.platform.TextInputServiceAmbient
 import androidx.compose.ui.platform.TextToolbarAmbient
-import androidx.compose.ui.selection.SelectionLayout
+import androidx.compose.ui.selection.SimpleLayout
 import androidx.compose.ui.semantics.copyText
 import androidx.compose.ui.semantics.cutText
 import androidx.compose.ui.semantics.focused
@@ -431,7 +431,7 @@ fun CoreTextField(
         .then(semanticsModifier)
         .focus()
 
-    SelectionLayout(modifiers) {
+    SimpleLayout(modifiers) {
         Layout(emptyContent()) { _, constraints ->
             TextFieldDelegate.layout(
                 state.textDelegate,
@@ -461,12 +461,12 @@ fun CoreTextField(
                     val endDirection =
                         it.getBidiRunDirection(max(value.selection.end - 1, 0))
                     val directions = Pair(startDirection, endDirection)
-                    SelectionHandle(
+                    TextFieldSelectionHandle(
                         isStartHandle = true,
                         directions = directions,
                         manager = manager
                     )
-                    SelectionHandle(
+                    TextFieldSelectionHandle(
                         isStartHandle = false,
                         directions = directions,
                         manager = manager
