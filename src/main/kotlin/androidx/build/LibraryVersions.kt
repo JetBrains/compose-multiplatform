18235d1dbf7d9da7412f8a655c3a1908c98a6040
Ella Ge <eirage@chromium.org>
2020-10-01 11:10:47 -0400

    Move browser to 1.3.0-beta01
    Test: ./gradlew checkApi
    Change-Id: I58869b8a5b5a4133a96c144d2f0313513df333fa

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -33,7 +33,7 @@ object LibraryVersions {
     val AUTOFILL = Version("1.1.0-beta01")
     val BENCHMARK = Version("1.1.0-alpha02")
     val BIOMETRIC = Version("1.1.0-beta02")
-    val BROWSER = Version("1.3.0-alpha07")
+    val BROWSER = Version("1.3.0-beta01")
     val BUILDSRC_TESTS = Version("1.0.0-alpha01")
     val CAMERA = Version("1.0.0-beta11")
     val CAMERA_EXTENSIONS = Version("1.0.0-alpha18")
