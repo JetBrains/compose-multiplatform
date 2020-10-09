e2afc407c8f748d2d0100e4a38355d8a292e05c7
Nikolay Igotti <igotti@gmail.com>
2020-10-09 10:08:17 +0300

    Move examples to piublic repo deps.

--- examples/issues/settings.gradle.kts
+++ examples/issues/settings.gradle.kts
@@ -2,11 +2,11 @@ buildscript {
     repositories {
         google()
         jcenter()
-        maven("https://packages.jetbrains.team/maven/p/ui/dev")
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 
     dependencies {
-        classpath("org.jetbrains.compose:compose-gradle-plugin:0.1.0-build58")
+        classpath("org.jetbrains.compose:compose-gradle-plugin:0.1.0-build63")
         classpath("com.android.tools.build:gradle:4.0.1")
         classpath(kotlin("gradle-plugin", version = "1.4.0"))
     }
