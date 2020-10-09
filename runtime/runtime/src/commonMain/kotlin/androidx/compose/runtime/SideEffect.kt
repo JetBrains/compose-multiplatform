756c7e2a3af7ea2321015ff50c148f71189c8298
Adam Powell <adamp@google.com>
2020-10-09 14:40:05 -0700

    Add DisposableEffect and rememberUpdatedState APIs
    DisposableEffect is the counterpart to SideEffect and fills the role
    that onCommit-with-params plays. In contrast to onCommit,
    DisposableEffect's onDispose clause is required as the final statement
    in the provided block by way of returning an inlined interface
    implementation for a disposer. This is a soft requirement; no
    implementation restrictions are placed on constructing objects
    implementing the required DisposableEffectDisposable interface returned
    by onDispose to allow library implementations to plumb implementations
    through as needed.
    
    DisposableEffect does not permit usage with no subject parameters,
    directing callers to provide one by way of a deprecated overload
    shadowing the varargs overload that would otherwise permit this usage.
    Callers are instead directed to SideEffect for these use cases,
    discouraging onActive-like uses.
    
    rememberUpdatedState is useful for working with DisposableEffects and
    LaunchedTasks that configure ongoing processes or coroutines that are
    prohibitive to tear down and restart if input data changes.
    rememberUpdatedState offers a concise way to update data in
    recomposition and have those changes visible to long-lived objects that
    closed over the resulting state holder.
    
    Test: SideEffectTests.kt
    Relnote: """Added DisposableEffect counterpart API to SideEffect, filling
    the role of onCommit-with-params but with a required onDispose.
    
    Added rememberUpdatedState API to publish data from recomposition to
    ongoing or long-lived processes such as DisposableEffects or
    LaunchedTasks."""
    
    Change-Id: Id50b9b85d2303166e5abe10aea47d6934d2c8597

--- runtime/runtime/src/commonMain/kotlin/androidx/compose/runtime/SideEffect.kt
+++ runtime/runtime/src/commonMain/kotlin/androidx/compose/runtime/SideEffect.kt
@@ -27,6 +27,10 @@ package androidx.compose.runtime
  * concurrent with themselves, one another, applying changes to the composition tree, or running
  * [CompositionLifecycleObserver] event callbacks. [SideEffect]s are always run after
  * [CompositionLifecycleObserver] event callbacks.
+ *
+ * A [SideEffect] runs after **every** recomposition. To launch an ongoing task spanning
+ * potentially many recompositions, see [LaunchedTask]. To manage an event subscription or other
+ * object lifecycle, see [DisposableEffect].
  */
 @Composable
 @ComposableContract(restartable = false)
@@ -34,4 +38,215 @@ fun SideEffect(
     effect: () -> Unit
 ) {
     currentComposer.recordSideEffect(effect)
+}
+
+/**
+ * Receiver scope for [DisposableEffect] that offers the [onDispose] clause that should be
+ * the last statement in any call to [DisposableEffect].
+ */
+class DisposableEffectScope {
+    /**
+     * Provide [onDisposeEffect] to the [DisposableEffect] to run when it leaves the composition
+     * or its subject changes.
+     */
+    inline fun onDispose(
+        crossinline onDisposeEffect: () -> Unit
+    ): DisposableEffectDisposable = object : DisposableEffectDisposable {
+        override fun dispose() {
+            onDisposeEffect()
+        }
+    }
+}
+
+interface DisposableEffectDisposable {
+    fun dispose()
+}
+
+private val InternalDisposableEffectScope = DisposableEffectScope()
+
+private class DisposableEffectImpl(
+    private val effect: DisposableEffectScope.() -> DisposableEffectDisposable
+) : CompositionLifecycleObserver {
+    private var onDispose: DisposableEffectDisposable? = null
+
+    override fun onEnter() {
+        onDispose = InternalDisposableEffectScope.effect()
+    }
+
+    override fun onLeave() {
+        onDispose?.dispose()
+        onDispose = null
+    }
+}
+
+private const val DisposableEffectNoParamError =
+    "DisposableEffect must provide one or more 'subject' parameters that define the identity of " +
+        "the DisposableEffect and determine when its previous effect should be disposed and " +
+        "restart."
+
+@Composable
+@ComposableContract(restartable = false)
+@Suppress("DeprecatedCallableAddReplaceWith", "UNUSED_PARAMETER")
+@Deprecated(DisposableEffectNoParamError, level = DeprecationLevel.ERROR)
+fun DisposableEffect(
+    effect: DisposableEffectScope.() -> DisposableEffectDisposable
+): Unit = error(DisposableEffectNoParamError)
+
+/**
+ * A side effect of composition that must run for any new unique value of [subject] and must be
+ * reversed or cleaned up if [subject] changes or if the [DisposableEffect] leaves the composition.
+ *
+ * A [DisposableEffect]'s _subject_ is a value that defines the identity of the
+ * [DisposableEffect]. If a subject changes, the [DisposableEffect] must
+ * [dispose][DisposableEffectScope.onDispose] its current [effect] and reset by calling [effect]
+ * again. Examples of subjects include:
+ *
+ * * Observable objects that the effect subscribes to
+ * * Unique request parameters to an operation that must cancel and retry if those parameters change
+ *
+ * [DisposableEffect] may be used to initialize or subscribe to a subject and reinitialize
+ * when a different subject is provided, performing cleanup for the old operation before
+ * initializing the new. For example:
+ *
+ * @sample androidx.compose.runtime.samples.disposableEffectSample
+ *
+ * A [DisposableEffect] **must** include an [onDispose][DisposableEffectScope.onDispose] clause
+ * as the final statement in its [effect] block. If your operation does not require disposal
+ * it might be a [SideEffect] instead, or a [LaunchedTask] if it launches a coroutine that should
+ * be managed by the composition.
+ *
+ * There is guaranteed to be one call to [dispose][DisposableEffectScope.onDispose] for every call
+ * to [effect]. Both [effect] and [dispose][DisposableEffectScope.onDispose] will always be run
+ * on the composition's apply dispatcher and appliers are never run concurrent with themselves,
+ * one another, applying changes to the composition tree, or running [CompositionLifecycleObserver]
+ * event callbacks.
+ */
+@Composable
+@ComposableContract(restartable = false)
+fun DisposableEffect(
+    subject: Any?,
+    effect: DisposableEffectScope.() -> DisposableEffectDisposable
+) {
+    remember(subject) { DisposableEffectImpl(effect) }
+}
+
+/**
+ * A side effect of composition that must run for any new unique value of [subject1] or [subject2]
+ * and must be reversed or cleaned up if [subject1] or [subject2] changes, or if the
+ * [DisposableEffect] leaves the composition.
+ *
+ * A [DisposableEffect]'s _subject_ is a value that defines the identity of the
+ * [DisposableEffect]. If a subject changes, the [DisposableEffect] must
+ * [dispose][DisposableEffectScope.onDispose] its current [effect] and reset by calling [effect]
+ * again. Examples of subjects include:
+ *
+ * * Observable objects that the effect subscribes to
+ * * Unique request parameters to an operation that must cancel and retry if those parameters change
+ *
+ * [DisposableEffect] may be used to initialize or subscribe to a subject and reinitialize
+ * when a different subject is provided, performing cleanup for the old operation before
+ * initializing the new. For example:
+ *
+ * @sample androidx.compose.runtime.samples.disposableEffectSample
+ *
+ * A [DisposableEffect] **must** include an [onDispose][DisposableEffectScope.onDispose] clause
+ * as the final statement in its [effect] block. If your operation does not require disposal
+ * it might be a [SideEffect] instead, or a [LaunchedTask] if it launches a coroutine that should
+ * be managed by the composition.
+ *
+ * There is guaranteed to be one call to [dispose][DisposableEffectScope.onDispose] for every call
+ * to [effect]. Both [effect] and [dispose][DisposableEffectScope.onDispose] will always be run
+ * on the composition's apply dispatcher and appliers are never run concurrent with themselves,
+ * one another, applying changes to the composition tree, or running [CompositionLifecycleObserver]
+ * event callbacks.
+ */
+@Composable
+@ComposableContract(restartable = false)
+fun DisposableEffect(
+    subject1: Any?,
+    subject2: Any?,
+    effect: DisposableEffectScope.() -> DisposableEffectDisposable
+) {
+    remember(subject1, subject2) { DisposableEffectImpl(effect) }
+}
+
+/**
+ * A side effect of composition that must run for any new unique value of [subject1], [subject2]
+ * or [subject3] and must be reversed or cleaned up if [subject1], [subject2] or [subject3]
+ * changes, or if the [DisposableEffect] leaves the composition.
+ *
+ * A [DisposableEffect]'s _subject_ is a value that defines the identity of the
+ * [DisposableEffect]. If a subject changes, the [DisposableEffect] must
+ * [dispose][DisposableEffectScope.onDispose] its current [effect] and reset by calling [effect]
+ * again. Examples of subjects include:
+ *
+ * * Observable objects that the effect subscribes to
+ * * Unique request parameters to an operation that must cancel and retry if those parameters change
+ *
+ * [DisposableEffect] may be used to initialize or subscribe to a subject and reinitialize
+ * when a different subject is provided, performing cleanup for the old operation before
+ * initializing the new. For example:
+ *
+ * @sample androidx.compose.runtime.samples.disposableEffectSample
+ *
+ * A [DisposableEffect] **must** include an [onDispose][DisposableEffectScope.onDispose] clause
+ * as the final statement in its [effect] block. If your operation does not require disposal
+ * it might be a [SideEffect] instead, or a [LaunchedTask] if it launches a coroutine that should
+ * be managed by the composition.
+ *
+ * There is guaranteed to be one call to [dispose][DisposableEffectScope.onDispose] for every call
+ * to [effect]. Both [effect] and [dispose][DisposableEffectScope.onDispose] will always be run
+ * on the composition's apply dispatcher and appliers are never run concurrent with themselves,
+ * one another, applying changes to the composition tree, or running [CompositionLifecycleObserver]
+ * event callbacks.
+ */
+@Composable
+@ComposableContract(restartable = false)
+fun DisposableEffect(
+    subject1: Any?,
+    subject2: Any?,
+    subject3: Any?,
+    effect: DisposableEffectScope.() -> DisposableEffectDisposable
+) {
+    remember(subject1, subject2, subject3) { DisposableEffectImpl(effect) }
+}
+
+/**
+ * A side effect of composition that must run for any new unique value of [subjects] and must
+ * be reversed or cleaned up if any [subjects] change or if the [DisposableEffect] leaves the
+ * composition.
+ *
+ * A [DisposableEffect]'s _subject_ is a value that defines the identity of the
+ * [DisposableEffect]. If a subject changes, the [DisposableEffect] must
+ * [dispose][DisposableEffectScope.onDispose] its current [effect] and reset by calling [effect]
+ * again. Examples of subjects include:
+ *
+ * * Observable objects that the effect subscribes to
+ * * Unique request parameters to an operation that must cancel and retry if those parameters change
+ *
+ * [DisposableEffect] may be used to initialize or subscribe to a subject and reinitialize
+ * when a different subject is provided, performing cleanup for the old operation before
+ * initializing the new. For example:
+ *
+ * @sample androidx.compose.runtime.samples.disposableEffectSample
+ *
+ * A [DisposableEffect] **must** include an [onDispose][DisposableEffectScope.onDispose] clause
+ * as the final statement in its [effect] block. If your operation does not require disposal
+ * it might be a [SideEffect] instead, or a [LaunchedTask] if it launches a coroutine that should
+ * be managed by the composition.
+ *
+ * There is guaranteed to be one call to [dispose][DisposableEffectScope.onDispose] for every call
+ * to [effect]. Both [effect] and [dispose][DisposableEffectScope.onDispose] will always be run
+ * on the composition's apply dispatcher and appliers are never run concurrent with themselves,
+ * one another, applying changes to the composition tree, or running [CompositionLifecycleObserver]
+ * event callbacks.
+ */
+@Composable
+@ComposableContract(restartable = false)
+@Suppress("ArrayReturn")
+fun DisposableEffect(
+    vararg subjects: Any?,
+    effect: DisposableEffectScope.() -> DisposableEffectDisposable
+) {
+    remember(*subjects) { DisposableEffectImpl(effect) }
 }
