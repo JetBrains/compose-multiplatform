c33c06b0464c85b4f221af8e06c293ca3d80d557
Jonathan Scott <scottjonathan@google.com>
2020-10-20 12:50:46 +0100

    Bump androidx.enterprise version to 1.1.0-rc01.
    Test: ./gradlew build
    Change-Id: I49b0d27f68e08675cfdf56352a9b3ec57bb35b7c

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -59,7 +59,7 @@ object LibraryVersions {
     val DYNAMICANIMATION = Version("1.1.0-alpha04")
     val DYNAMICANIMATION_KTX = Version("1.0.0-alpha04")
     val EMOJI = Version("1.2.0-alpha03")
-    val ENTERPRISE = Version("1.1.0-beta01")
+    val ENTERPRISE = Version("1.1.0-rc01")
     val EXIFINTERFACE = Version("1.4.0-alpha01")
     val FRAGMENT = Version("1.3.0-beta02")
     val FUTURES = Version("1.2.0-alpha01")
