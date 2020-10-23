5212f30564ee96a625098d53bd52153e06fa11a9
Igor Demin <igor.demin@jetbrains.com>
2020-10-23 21:45:51 +0300

    Desktop: implement Scrollbars
    - visual style was copied from Google Docs, except there is an animation on mouse hover
    - scrollbars can be attached to ScrollableColumn/ScrollableRow and to lazy components with fixed item height
    - scrollbars can placed anywhere (even in the middle of the content), because there are individual components
    - they share state with scrollable component
    - visual style of scrollbars is configured in DesktopTheme
    - implicit attaching scrollbars to ScrollableColumn/ScrollableRow is in the future plans. Perhaps we can write something like this in the common code:
    ```
    fun ScrollableColumn(
        ...
        scrollbar: @Composable () -> Unit = DefaultVerticalScrollbar()
        children: @Composable ColumnScope.() -> Unit
    )
    
    expect fun DefaultVerticalScrollbar()
    ```
    
    Test: ./gradlew jvmTest desktopTest -Pandroidx.compose.multiplatformEnabled=true
    Change-Id: Iaed88003bc5e6491cf1de9901493ce385db62224

---
+++ foundation/foundation/src/desktopTest/kotlin/androidx/compose/foundation/ScrollbarTest.kt
@@ -0,0 +1,373 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.foundation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Providers
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.gesture.scrollorientationlocking.Orientation
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.input.mouse.MouseScrollEvent
+import androidx.compose.ui.input.mouse.MouseScrollUnit
+import androidx.compose.ui.platform.DesktopPlatform
+import androidx.compose.ui.platform.DesktopPlatformAmbient
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.ExperimentalTesting
+import androidx.compose.ui.test.assertTopPositionInRootIsEqualTo
+import androidx.compose.ui.test.down
+import androidx.compose.ui.test.junit4.ComposeTestRule
+import androidx.compose.ui.test.junit4.DesktopComposeTestRule
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performGesture
+import androidx.compose.ui.test.swipe
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.milliseconds
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.skija.Surface
+import org.junit.Assert.assertEquals
+import org.junit.Ignore
+import org.junit.Rule
+import org.junit.Test
+
+@Suppress("WrapUnaryOperator")
+@OptIn(ExperimentalTesting::class)
+class ScrollbarTest {
+    @get:Rule
+    val rule = createComposeRule()
+    private val canvas = Surface.makeRasterN32Premul(100, 100).canvas
+
+    @Test
+    fun `drag slider to the middle`() {
+        runBlocking(Dispatchers.Main) {
+            rule.setContent {
+                TestBox(size = 100.dp, childSize = 20.dp, childCount = 10, scrollbarWidth = 10.dp)
+            }
+            rule.awaitIdle()
+
+            rule.onNodeWithTag("scrollbar").performGesture {
+                swipe(start = Offset(0f, 25f), end = Offset(0f, 50f))
+            }
+            onFrame()
+            rule.onNodeWithTag("box0").assertTopPositionInRootIsEqualTo(-50.dp)
+        }
+    }
+
+    @Test
+    fun `drag slider to the edges`() {
+        runBlocking(Dispatchers.Main) {
+            rule.setContent {
+                TestBox(size = 100.dp, childSize = 20.dp, childCount = 10, scrollbarWidth = 10.dp)
+            }
+            rule.awaitIdle()
+
+            rule.onNodeWithTag("scrollbar").performGesture {
+                swipe(start = Offset(0f, 25f), end = Offset(0f, 500f))
+            }
+            onFrame()
+            rule.onNodeWithTag("box0").assertTopPositionInRootIsEqualTo(-100.dp)
+
+            rule.onNodeWithTag("scrollbar").performGesture {
+                swipe(start = Offset(0f, 99f), end = Offset(0f, -500f))
+            }
+            onFrame()
+            rule.onNodeWithTag("box0").assertTopPositionInRootIsEqualTo(0.dp)
+        }
+    }
+
+    @Test
+    fun `drag outside slider`() {
+        runBlocking(Dispatchers.Main) {
+            rule.setContent {
+                TestBox(size = 100.dp, childSize = 20.dp, childCount = 10, scrollbarWidth = 10.dp)
+            }
+            rule.awaitIdle()
+
+            rule.onNodeWithTag("scrollbar").performGesture {
+                swipe(start = Offset(10f, 25f), end = Offset(0f, 50f))
+            }
+            onFrame()
+            rule.onNodeWithTag("box0").assertTopPositionInRootIsEqualTo(0.dp)
+        }
+    }
+
+    // TODO(demin): enable after we resolve b/171889442
+    @Ignore("Enable after we resolve b/171889442")
+    @Test
+    fun `mouseScroll over slider`() {
+        runBlocking(Dispatchers.Main) {
+            rule.setContent {
+                TestBox(size = 100.dp, childSize = 20.dp, childCount = 10, scrollbarWidth = 10.dp)
+            }
+            rule.awaitIdle()
+
+            rule.performMouseScroll(0, 25, 1f)
+            onFrame()
+            rule.onNodeWithTag("box0").assertTopPositionInRootIsEqualTo(-10.dp)
+        }
+    }
+
+    // TODO(demin): enable after we resolve b/171889442
+    @Ignore("Enable after we resolve b/171889442")
+    @Test
+    fun `mouseScroll over scrollbar outside slider`() {
+        runBlocking(Dispatchers.Main) {
+            rule.setContent {
+                TestBox(size = 100.dp, childSize = 20.dp, childCount = 10, scrollbarWidth = 10.dp)
+            }
+            rule.awaitIdle()
+
+            rule.performMouseScroll(0, 99, 1f)
+            onFrame()
+            rule.onNodeWithTag("box0").assertTopPositionInRootIsEqualTo(-10.dp)
+        }
+    }
+
+    // TODO(demin): enable after we resolve b/171889442
+    @Ignore("Enable after we resolve b/171889442")
+    @Test
+    fun `vertical mouseScroll over horizontal scrollbar `() {
+        runBlocking(Dispatchers.Main) {
+            // TODO(demin): write tests for vertical mouse scrolling over
+            //  horizontalScrollbar for the case when we have two-way scrollable content:
+            //  Modifier.verticalScrollbar(...).horizontalScrollbar(...)
+            //  Content should scroll vertically.
+        }
+    }
+
+    @Test
+    fun `mouseScroll over column then drag to the beginning`() {
+        runBlocking(Dispatchers.Main) {
+            rule.setContent {
+                TestBox(size = 100.dp, childSize = 20.dp, childCount = 10, scrollbarWidth = 10.dp)
+            }
+            rule.awaitIdle()
+
+            rule.performMouseScroll(20, 25, 10f)
+            onFrame()
+            rule.onNodeWithTag("box0").assertTopPositionInRootIsEqualTo(-100.dp)
+
+            rule.onNodeWithTag("scrollbar").performGesture {
+                swipe(start = Offset(0f, 99f), end = Offset(0f, -500f))
+            }
+            onFrame()
+            rule.onNodeWithTag("box0").assertTopPositionInRootIsEqualTo(0.dp)
+        }
+    }
+
+    @Test(timeout = 3000)
+    fun `press on scrollbar outside slider`() {
+        runBlocking(Dispatchers.Main) {
+            rule.setContent {
+                TestBox(size = 100.dp, childSize = 20.dp, childCount = 20, scrollbarWidth = 10.dp)
+            }
+            rule.awaitIdle()
+
+            rule.onNodeWithTag("scrollbar").performGesture {
+                down(Offset(0f, 26f))
+            }
+
+            tryUntilSucceeded {
+                onFrame()
+                rule.onNodeWithTag("box0").assertTopPositionInRootIsEqualTo(-100.dp)
+            }
+        }
+    }
+
+    @Test(timeout = 3000)
+    fun `press on the end of scrollbar outside slider`() {
+        runBlocking(Dispatchers.Main) {
+            rule.setContent {
+                TestBox(size = 100.dp, childSize = 20.dp, childCount = 20, scrollbarWidth = 10.dp)
+            }
+            rule.awaitIdle()
+
+            rule.onNodeWithTag("scrollbar").performGesture {
+                down(Offset(0f, 99f))
+            }
+
+            tryUntilSucceeded {
+                onFrame()
+                rule.onNodeWithTag("box0").assertTopPositionInRootIsEqualTo(-300.dp)
+            }
+        }
+    }
+
+    @Suppress("SameParameterValue")
+    @OptIn(ExperimentalFoundationApi::class)
+    @Test(timeout = 3000)
+    fun `scroll by less than one page in lazy list`() {
+        runBlocking(Dispatchers.Main) {
+            lateinit var state: LazyListState
+
+            rule.setContent {
+                state = rememberLazyListState()
+                LazyTestBox(
+                    state,
+                    size = 100.dp,
+                    childSize = 20.dp,
+                    childCount = 20,
+                    scrollbarWidth = 10.dp
+                )
+            }
+            rule.awaitIdle()
+
+            rule.onNodeWithTag("scrollbar").performGesture {
+                swipe(start = Offset(0f, 0f), end = Offset(0f, 11f), duration = 1.milliseconds)
+            }
+            onFrame()
+            assertEquals(2, state.firstVisibleItemIndex)
+            assertEquals(4, state.firstVisibleItemScrollOffset)
+        }
+    }
+
+    @Suppress("SameParameterValue")
+    @OptIn(ExperimentalFoundationApi::class)
+    @Test(timeout = 3000)
+    fun `scroll by more than one page in lazy list`() {
+        runBlocking(Dispatchers.Main) {
+            lateinit var state: LazyListState
+
+            rule.setContent {
+                state = rememberLazyListState()
+                LazyTestBox(
+                    state,
+                    size = 100.dp,
+                    childSize = 20.dp,
+                    childCount = 20,
+                    scrollbarWidth = 10.dp
+                )
+            }
+            rule.awaitIdle()
+
+            rule.onNodeWithTag("scrollbar").performGesture {
+                swipe(start = Offset(0f, 0f), end = Offset(0f, 26f), duration = 1.milliseconds)
+            }
+            onFrame()
+            assertEquals(5, state.firstVisibleItemIndex)
+            assertEquals(4, state.firstVisibleItemScrollOffset)
+        }
+    }
+
+    private suspend fun tryUntilSucceeded(block: suspend () -> Unit) {
+        while (true) {
+            try {
+                block()
+                break
+            } catch (e: Throwable) {
+                delay(10)
+            }
+        }
+    }
+
+    // TODO(demin): move to DesktopComposeTestRule?
+    private suspend fun onFrame() {
+        (rule as DesktopComposeTestRule).owners?.onFrame(canvas, 100, 100, 0)
+        rule.awaitIdle()
+    }
+
+    private fun ComposeTestRule.performMouseScroll(x: Int, y: Int, delta: Float) {
+        (this as DesktopComposeTestRule).owners!!.onMouseScroll(
+            x, y, MouseScrollEvent(MouseScrollUnit.Line(delta), Orientation.Vertical)
+        )
+    }
+
+    @Composable
+    private fun TestBox(
+        size: Dp,
+        childSize: Dp,
+        childCount: Int,
+        scrollbarWidth: Dp,
+    ) = withTestEnvironment {
+        Box(Modifier.size(size)) {
+            val state = rememberScrollState()
+
+            ScrollableColumn(
+                Modifier.fillMaxSize().testTag("column"),
+                state
+            ) {
+                repeat(childCount) {
+                    Box(Modifier.size(childSize).testTag("box$it"))
+                }
+            }
+
+            VerticalScrollbar(
+                adapter = rememberScrollbarAdapter(state),
+                modifier = Modifier
+                    .width(scrollbarWidth)
+                    .fillMaxHeight()
+                    .testTag("scrollbar")
+            )
+        }
+    }
+
+    @Suppress("SameParameterValue")
+    @OptIn(ExperimentalFoundationApi::class)
+    @Composable
+    private fun LazyTestBox(
+        state: LazyListState,
+        size: Dp,
+        childSize: Dp,
+        childCount: Int,
+        scrollbarWidth: Dp,
+    ) = withTestEnvironment {
+        Box(Modifier.size(size)) {
+            LazyColumnFor(
+                (0 until childCount).toList(),
+                Modifier.fillMaxSize().testTag("column"),
+                state
+            ) {
+                Box(Modifier.size(childSize).testTag("box$it"))
+            }
+
+            VerticalScrollbar(
+                adapter = rememberScrollbarAdapter(state, childCount, childSize),
+                modifier = Modifier
+                    .width(scrollbarWidth)
+                    .fillMaxHeight()
+                    .testTag("scrollbar")
+            )
+        }
+    }
+
+    @Composable
+    private fun withTestEnvironment(content: @Composable () -> Unit) = Providers(
+        ScrollbarStyleAmbient provides ScrollbarStyle(
+            minimalHeight = 16.dp,
+            thickness = 8.dp,
+            shape = RectangleShape,
+            hoverDurationMillis = 300,
+            unhoverColor = Color.Black,
+            hoverColor = Color.Red
+        ),
+        DesktopPlatformAmbient provides DesktopPlatform.MacOS,
+        children = content
+    )
+}
