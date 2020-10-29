d0859c665c2795739933ca3ac407df0caabadebc
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2020-10-29 16:14:53 +0300

    Adjust settings for Windows
    Enable dir chooser by default.
    Also, treat menu as true if menuGroup is not null.

--- examples/imageviewer/desktop/build.gradle.kts
+++ examples/imageviewer/desktop/build.gradle.kts
@@ -36,6 +36,9 @@ compose.desktop {
             }
             windows {
                 iconFile.set(iconsRoot.resolve("icon-windows.ico"))
+                menuGroup = "Compose Examples"
+                // see https://wixtoolset.org/documentation/manual/v3/howtos/general/generate_guids.html
+                upgradeUuid = "18159995-d967-4CD2-8885-77BFA97CFA9F"
             }
             linux {
                 iconFile.set(iconsRoot.resolve("icon-linux.png"))
