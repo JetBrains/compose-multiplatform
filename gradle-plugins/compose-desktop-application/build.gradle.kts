d1492df1167a67d0d465c18116fb7725fb9b4e37
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2020-10-29 14:19:55 +0300

    Download WiX toolset on Windows
    It is a non-optional dependency for jpackage
    (see "Dependencies" section of https://openjdk.java.net/jeps/343).

--- gradle-plugins/compose-desktop-application/build.gradle.kts
+++ gradle-plugins/compose-desktop-application/build.gradle.kts
@@ -1,8 +1,11 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm")
     id("com.gradle.plugin-publish")
     id("java-gradle-plugin")
     id("maven-publish")
+    id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 gradlePluginConfig {
@@ -13,8 +16,32 @@ gradlePluginConfig {
     implementationClass = "org.jetbrains.compose.desktop.application.ApplicationPlugin"
 }
 
+val embedded by configurations.creating
+
 dependencies {
+    fun embeddedCompileOnly(dep: String) {
+        compileOnly(dep)
+        embedded(dep)
+    }
+
     compileOnly(gradleApi())
     compileOnly(kotlin("gradle-plugin-api"))
     compileOnly(kotlin("gradle-plugin"))
+    // include relocated download task to avoid potential runtime conflicts
+    embeddedCompileOnly("de.undercouch:gradle-download-task:4.1.1")
+}
+
+val shadow = tasks.named<ShadowJar>("shadowJar") {
+    val fromPackage = "de.undercouch"
+    val toPackage = "org.jetbrains.compose.$fromPackage"
+    relocate(fromPackage, toPackage)
+    archiveClassifier.set("shadow")
+    configurations = listOf(embedded)
+    exclude("META-INF/gradle-plugins/de.undercouch.download.properties")
+}
+
+val jar = tasks.named<Jar>("jar") {
+    dependsOn(shadow)
+    from(zipTree(shadow.get().archiveFile))
+    this.duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
