07e2e6c79a7ec27889e713dfed8c4c855aec15b5
Aleksandr Litovchenko <litun@google.com>
2020-10-29 21:37:38 +0000

    Support adaptive number of columns in LazyVerticalGrid
    Fork LazyGridScope. It will allow us to modify LazyGridScope regardless of the LazyListScope.
    
    Bug: 162213211
    Test: LazyGridTest and IntervalListTest
    Relnote: LazyVerticalGrid is added.
    Change-Id: I172670e5a6648278604e9ad024d3db17c9c6974b

---
+++ foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/lazy/IntervalList.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.foundation.lazy
+
+internal class IntervalHolder<T>(
+    val startIndex: Int,
+    val size: Int,
+    val content: T
+)
+
+internal class IntervalList<T> {
+    private val intervals = mutableListOf<IntervalHolder<T>>()
+    internal var totalSize = 0
+        private set
+
+    fun add(size: Int, content: T) {
+        if (size == 0) {
+            return
+        }
+
+        val interval = IntervalHolder(
+            startIndex = totalSize,
+            size = size,
+            content = content
+        )
+        totalSize += size
+        intervals.add(interval)
+    }
+
+    fun intervalForIndex(index: Int) =
+        if (index < 0 || index >= totalSize) {
+            throw IndexOutOfBoundsException("Index $index, size $totalSize")
+        } else {
+            intervals[findIndexOfHighestValueLesserThan(intervals, index)]
+        }
+
+    /**
+     * Finds the index of the [list] which contains the highest value of [IntervalHolder.startIndex]
+     * that is less than or equal to the given [value].
+     */
+    private fun findIndexOfHighestValueLesserThan(list: List<IntervalHolder<T>>, value: Int): Int {
+        var left = 0
+        var right = list.lastIndex
+
+        while (left < right) {
+            val middle = (left + right) / 2
+
+            val middleValue = list[middle].startIndex
+            if (middleValue == value) {
+                return middle
+            }
+
+            if (middleValue < value) {
+                left = middle + 1
+
+                // Verify that the left will not be bigger than our value
+                if (value < list[left].startIndex) {
+                    return middle
+                }
+            } else {
+                right = middle - 1
+            }
+        }
+
+        return left
+    }
+}
---
+++ foundation/foundation/src/test/kotlin/androidx/compose/foundation/lazy/IntervalListTest.kt
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.foundation.lazy
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class IntervalListTest {
+
+    private val intervalList = IntervalList<Int>()
+
+    @Test
+    fun addOneItem_searchInterval() {
+        intervalList.add(1, 10)
+
+        val foundInterval = intervalList.intervalForIndex(0)
+
+        assertThat(foundInterval.content).isEqualTo(10)
+        assertThat(foundInterval.size).isEqualTo(1)
+        assertThat(foundInterval.startIndex).isEqualTo(0)
+    }
+
+    @Test
+    fun addOneItem_searchIndexOutOfBounds() {
+        intervalList.add(1, 10)
+
+        val wasException: Boolean = try {
+            intervalList.intervalForIndex(2)
+            false
+        } catch (e: IndexOutOfBoundsException) {
+            true
+        }
+
+        assertThat(wasException).isTrue()
+    }
+
+    @Test
+    fun addSingleItems_searchFirstInterval() {
+        addFiveSingleIntervals()
+
+        val foundInterval = intervalList.intervalForIndex(0)
+
+        assertThat(foundInterval.content).isEqualTo(10)
+        assertThat(foundInterval.size).isEqualTo(1)
+        assertThat(foundInterval.startIndex).isEqualTo(0)
+    }
+
+    @Test
+    fun addSingleItems_searchLastInterval() {
+        addFiveSingleIntervals()
+
+        val foundInterval = intervalList.intervalForIndex(4)
+
+        assertThat(foundInterval.content).isEqualTo(50)
+        assertThat(foundInterval.size).isEqualTo(1)
+        assertThat(foundInterval.startIndex).isEqualTo(4)
+    }
+
+    @Test
+    fun addSingleItems_searchMidInterval() {
+        addFiveSingleIntervals()
+
+        val foundInterval = intervalList.intervalForIndex(2)
+
+        assertThat(foundInterval.content).isEqualTo(30)
+        assertThat(foundInterval.size).isEqualTo(1)
+        assertThat(foundInterval.startIndex).isEqualTo(2)
+    }
+
+    @Test
+    fun addVariableItems_searchFirstInterval() {
+        addFiveVariableIntervals()
+
+        val foundInterval = intervalList.intervalForIndex(0)
+
+        assertThat(foundInterval.content).isEqualTo(10)
+        assertThat(foundInterval.size).isEqualTo(3)
+        assertThat(foundInterval.startIndex).isEqualTo(0)
+    }
+
+    @Test
+    fun addVariableItems_searchLastInterval() {
+        addFiveVariableIntervals()
+
+        val foundInterval = intervalList.intervalForIndex(22)
+
+        assertThat(foundInterval.content).isEqualTo(50)
+        assertThat(foundInterval.size).isEqualTo(11)
+        assertThat(foundInterval.startIndex).isEqualTo(12)
+    }
+
+    @Test
+    fun addVariableItems_searchMidInterval() {
+        addFiveVariableIntervals()
+
+        val foundInterval = intervalList.intervalForIndex(6)
+
+        assertThat(foundInterval.content).isEqualTo(30)
+        assertThat(foundInterval.size).isEqualTo(7)
+        assertThat(foundInterval.startIndex).isEqualTo(4)
+    }
+
+    @Test
+    fun addVariableItems_searchSecondInterval() {
+        addFiveVariableIntervals()
+
+        val foundInterval = intervalList.intervalForIndex(3)
+
+        assertThat(foundInterval.content).isEqualTo(20)
+        assertThat(foundInterval.size).isEqualTo(1)
+        assertThat(foundInterval.startIndex).isEqualTo(3)
+    }
+
+    @Test
+    fun addVariableItems_searchIndexOutOfBounds() {
+        addFiveVariableIntervals()
+
+        val wasException: Boolean = try {
+            intervalList.intervalForIndex(23)
+            false
+        } catch (e: IndexOutOfBoundsException) {
+            true
+        }
+
+        assertThat(wasException).isTrue()
+    }
+
+    private fun addFiveSingleIntervals() {
+        intervalList.add(1, 10)
+        intervalList.add(1, 20)
+        intervalList.add(1, 30)
+        intervalList.add(1, 40)
+        intervalList.add(1, 50)
+    }
+
+    private fun addFiveVariableIntervals() {
+        intervalList.add(3, 10)
+        intervalList.add(1, 20)
+        intervalList.add(7, 30)
+        intervalList.add(1, 40)
+        intervalList.add(11, 50)
+    }
+}
