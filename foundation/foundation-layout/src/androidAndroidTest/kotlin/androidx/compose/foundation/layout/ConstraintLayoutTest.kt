118ceef163cc183f8fa061ca384a9848da4b2182
Jeff Gaston <jeffrygaston@google.com>
2020-10-30 17:18:23 -0400

    ConstraintLayoutTest construct anchor list one item at a time
    a workaround for bug 172055763
    
    Bug: 172055763
    Test: Update to AGP 4.2.0-alpha15/lint 27.2.0-alpha15 and run `./gradlew :compose:foundation:foundation-layout:lintDebug`
    Change-Id: I15cdd59f87c7255d8ceb48d8c7f2f74ae10ef534

--- foundation/foundation-layout/src/androidAndroidTest/kotlin/androidx/compose/foundation/layout/ConstraintLayoutTest.kt
+++ foundation/foundation-layout/src/androidAndroidTest/kotlin/androidx/compose/foundation/layout/ConstraintLayoutTest.kt
@@ -781,6 +781,27 @@ class ConstraintLayoutTest : LayoutTest() {
         }
     }
 
+    fun listAnchors(box: ConstrainedLayoutReference): List<ConstrainScope.() -> Unit> {
+        // TODO(172055763) directly construct an immutable list when Lint supports it
+        val anchors = mutableListOf<ConstrainScope.() -> Unit>()
+        anchors.add({ start.linkTo(box.start) })
+        anchors.add({ absoluteLeft.linkTo(box.start) })
+        anchors.add({ start.linkTo(box.absoluteLeft) })
+        anchors.add({ absoluteLeft.linkTo(box.absoluteLeft) })
+        anchors.add({ end.linkTo(box.start) })
+        anchors.add({ absoluteRight.linkTo(box.start) })
+        anchors.add({ end.linkTo(box.absoluteLeft) })
+        anchors.add({ absoluteRight.linkTo(box.absoluteLeft) })
+        anchors.add({ start.linkTo(box.end) })
+        anchors.add({ absoluteLeft.linkTo(box.end) })
+        anchors.add({ start.linkTo(box.absoluteRight) })
+        anchors.add({ absoluteLeft.linkTo(box.absoluteRight) })
+        anchors.add({ end.linkTo(box.end) })
+        anchors.add({ absoluteRight.linkTo(box.end) })
+        anchors.add({ end.linkTo(box.absoluteRight) })
+        anchors.add({ absoluteRight.linkTo(box.absoluteRight) })
+        return anchors
+    }
     @Test
     fun testConstraintLayout_anchors_ltr() = with(density) {
         val size = 200.toDp()
@@ -798,24 +819,7 @@ class ConstraintLayoutTest : LayoutTest() {
                         }
                 )
 
-                val anchors = listOf<ConstrainScope.() -> Unit>(
-                    { start.linkTo(box.start) },
-                    { absoluteLeft.linkTo(box.start) },
-                    { start.linkTo(box.absoluteLeft) },
-                    { absoluteLeft.linkTo(box.absoluteLeft) },
-                    { end.linkTo(box.start) },
-                    { absoluteRight.linkTo(box.start) },
-                    { end.linkTo(box.absoluteLeft) },
-                    { absoluteRight.linkTo(box.absoluteLeft) },
-                    { start.linkTo(box.end) },
-                    { absoluteLeft.linkTo(box.end) },
-                    { start.linkTo(box.absoluteRight) },
-                    { absoluteLeft.linkTo(box.absoluteRight) },
-                    { end.linkTo(box.end) },
-                    { absoluteRight.linkTo(box.end) },
-                    { end.linkTo(box.absoluteRight) },
-                    { absoluteRight.linkTo(box.absoluteRight) }
-                )
+                val anchors = listAnchors(box)
 
                 anchors.forEachIndexed { index, anchor ->
                     val ref = createRef()
@@ -869,24 +873,7 @@ class ConstraintLayoutTest : LayoutTest() {
                             }
                     )
 
-                    val anchors = listOf<ConstrainScope.() -> Unit>(
-                        { start.linkTo(box.start) },
-                        { absoluteLeft.linkTo(box.start) },
-                        { start.linkTo(box.absoluteLeft) },
-                        { absoluteLeft.linkTo(box.absoluteLeft) },
-                        { end.linkTo(box.start) },
-                        { absoluteRight.linkTo(box.start) },
-                        { end.linkTo(box.absoluteLeft) },
-                        { absoluteRight.linkTo(box.absoluteLeft) },
-                        { start.linkTo(box.end) },
-                        { absoluteLeft.linkTo(box.end) },
-                        { start.linkTo(box.absoluteRight) },
-                        { absoluteLeft.linkTo(box.absoluteRight) },
-                        { end.linkTo(box.end) },
-                        { absoluteRight.linkTo(box.end) },
-                        { end.linkTo(box.absoluteRight) },
-                        { absoluteRight.linkTo(box.absoluteRight) }
-                    )
+                    val anchors = listAnchors(box)
 
                     anchors.forEachIndexed { index, anchor ->
                         val ref = createRef()
