fc07866143241f6be4fbb6117dfc72e7f19a37fc
Aurimas Liutikas <aurimas@google.com>
2020-11-04 03:41:05 +0000

    Revert "Ability to limit the published artifacts only to the affected ones"
    This reverts commit aab8cffaa2d7fd9def5936a46a0d199bf25d6adf.
    
    Reason for revert: this broke top-of-tree*.zip generation in postsubmit
    
    Change-Id: Ic110d2c7eb452ca4dd387ca0e2da7d6222a82acb

--- src/main/kotlin/androidx/build/BuildOnServer.kt
+++ src/main/kotlin/androidx/build/BuildOnServer.kt
@@ -48,7 +48,8 @@ open class BuildOnServer : DefaultTask() {
         val buildId = getBuildId()
 
         val filesNames = mutableListOf(
-            "androidx_aggregate_build_info.txt"
+            "androidx_aggregate_build_info.txt",
+            "top-of-tree-m2repository-all-$buildId.zip"
         )
 
         if (project.isDocumentationEnabled()) {
@@ -60,6 +61,7 @@ open class BuildOnServer : DefaultTask() {
 
         if (project.findProject(":jetifier-standalone") != null) {
             filesNames.add("jetifier-standalone.zip")
+            filesNames.add("top-of-tree-m2repository-partially-dejetified-$buildId.zip")
         }
 
         return filesNames.map { fileName -> File(distributionDirectory, fileName) }
