8144d009b765329deb9d39c8c8d7696ad612fe18
Louis Pullen-Freilich <lpf@google.com>
2020-11-05 17:40:34 +0000

    Initial ui lint checks
    Adds a lint check to ensure that functions returning a Modifier do not return a subclass / implementation of Modifier.
    
    Bug: b/172469237
    Test: lintDebug
    Change-Id: I644015997fa8aab8add59a6f3e9484254d3a54e7

---
+++ ui/ui-lint/src/main/java/androidx/compose/ui/lint/ModifierDeclarationDetector.kt
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("UnstableApiUsage")
+
+package androidx.compose.ui.lint
+
+import com.android.tools.lint.client.api.UElementHandler
+import com.android.tools.lint.detector.api.Category
+import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.Implementation
+import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.JavaContext
+import com.android.tools.lint.detector.api.LintFix
+import com.android.tools.lint.detector.api.Scope
+import com.android.tools.lint.detector.api.Severity
+import com.android.tools.lint.detector.api.SourceCodeScanner
+import com.intellij.psi.PsiType
+import org.jetbrains.kotlin.psi.KtCallableDeclaration
+import org.jetbrains.kotlin.psi.KtDeclarationWithBody
+import org.jetbrains.kotlin.psi.KtPropertyAccessor
+import org.jetbrains.uast.UMethod
+
+/**
+ * [Detector] that checks functions returning Modifiers for consistency with guidelines.
+ *
+ * - Modifier factory functions must return Modifier as their type, and not a subclass of Modifier
+ */
+class ModifierDeclarationDetector : Detector(), SourceCodeScanner {
+    override fun getApplicableUastTypes() = listOf(UMethod::class.java)
+
+    override fun createUastHandler(context: JavaContext) = object : UElementHandler() {
+        override fun visitMethod(node: UMethod) {
+            // Ignore functions that do not return
+            val returnType = node.returnType ?: return
+
+            val allReturnSuperTypes = returnType.allSuperTypes()
+            // Ignore functions that do not return Modifier or something implementing Modifier
+            if (allReturnSuperTypes.none { it.canonicalText == Modifier }) return
+
+            fun report(lintFix: LintFix? = null) {
+                context.report(
+                    ModifierFactoryReturnType,
+                    node,
+                    context.getNameLocation(node),
+                    "Modifier factory functions must have a return type of Modifier",
+                    lintFix
+                )
+            }
+
+            if (returnType.canonicalText != Modifier) {
+                val source = node.sourcePsi
+                if (source is KtCallableDeclaration && source.returnTypeString != null) {
+                    // Function declaration with an explicit return type, such as
+                    // `fun foo(): Modifier.element = Bar`. Replace the type with `Modifier`.
+                    report(
+                        LintFix.create()
+                            .replace()
+                            .name("Change return type to Modifier")
+                            .range(context.getLocation(node))
+                            .text(source.returnTypeString)
+                            .with(Modifier.split(".").last())
+                            .autoFix()
+                            .build()
+                    )
+                    return
+                }
+                if (source is KtPropertyAccessor) {
+                    // Getter declaration with an explicit return type on the getter, such as
+                    // `val foo get(): Modifier.Element = Bar`. Replace the type with `Modifier`.
+                    val getterReturnType = source.returnTypeReference?.text
+
+                    if (getterReturnType != null) {
+                        report(
+                            LintFix.create()
+                                .replace()
+                                .name("Change return type to Modifier")
+                                .range(context.getLocation(node))
+                                .text(getterReturnType)
+                                .with(Modifier.split(".").last())
+                                .autoFix()
+                                .build()
+                        )
+                        return
+                    }
+                    // Getter declaration with an impicit return type from the property, such as
+                    // `val foo: Modifier.Element get() = Bar`. Replace the type with `Modifier`.
+                    val propertyType = source.property.returnTypeString
+
+                    if (propertyType != null) {
+                        report(
+                            LintFix.create()
+                                .replace()
+                                .name("Change return type to Modifier")
+                                .range(context.getLocation(source.property))
+                                .text(propertyType)
+                                .with(Modifier.split(".").last())
+                                .autoFix()
+                                .build()
+                        )
+                        return
+                    }
+                }
+                if (source is KtDeclarationWithBody) {
+                    // Declaration without an explicit return type, such as `fun foo() = Bar`
+                    // or val foo get() = Bar
+                    // Replace the `=` with `: Modifier =`
+                    report(
+                        LintFix.create()
+                            .replace()
+                            .name("Add explicit Modifier return type")
+                            .range(context.getLocation(node))
+                            .pattern("[ \\t\\n]+=")
+                            .with(": " + Modifier.split(".").last() + " =")
+                            .autoFix()
+                            .build()
+                    )
+                    return
+                }
+            }
+        }
+    }
+
+    companion object {
+        val ModifierFactoryReturnType = Issue.create(
+            "ModifierFactoryReturnType",
+            "Modifier factory functions must return Modifier",
+            "Modifier factory functions must return Modifier as their type, and not a " +
+                "subtype of Modifier (such as Modifier.Element).",
+            Category.CORRECTNESS, 3, Severity.ERROR,
+            Implementation(
+                ModifierDeclarationDetector::class.java,
+                Scope.JAVA_FILE_SCOPE
+            )
+        )
+    }
+}
+
+/**
+ * Recursive list of the super types of this [PsiType] and all the super types of its super types.
+ */
+private fun PsiType.allSuperTypes(): List<PsiType> =
+    listOf(*superTypes + superTypes.flatMap { it.allSuperTypes() })
+
+private const val Modifier = "androidx.compose.ui.Modifier"
+
+/**
+ * TODO: UMethod.returnTypeReference is not available in LINT_API_MIN, so instead use this with a
+ * [KtCallableDeclaration].
+ * See [org.jetbrains.uast.kotlin.declarations.KotlinUMethod.returnTypeReference] on newer UAST
+ * versions.
+ */
+private val KtCallableDeclaration.returnTypeString: String?
+    get() {
+        return typeReference?.text
+    }
---
+++ ui/ui-lint/src/test/java/androidx/compose/ui/lint/ModifierDeclarationDetectorTest.kt
@@ -0,0 +1,273 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@file:Suppress("UnstableApiUsage")
+
+package androidx.compose.ui.lint
+
+import com.android.tools.lint.checks.infrastructure.LintDetectorTest
+import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.Issue
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+/* ktlint-disable max-line-length */
+@RunWith(JUnit4::class)
+
+/**
+ * Test for [ModifierDeclarationDetector].
+ */
+class ModifierDeclarationDetectorTest : LintDetectorTest() {
+    override fun getDetector(): Detector = ModifierDeclarationDetector()
+
+    override fun getIssues(): MutableList<Issue> =
+        mutableListOf(ModifierDeclarationDetector.ModifierFactoryReturnType)
+
+    private val modifierStub = kotlin(
+        """
+            package androidx.compose.ui
+
+            interface Modifier {
+                interface Element : Modifier
+            }
+
+            object TestModifier : Modifier
+        """
+    )
+
+    @Test
+    fun functionReturnsModifierElement() {
+        lint().files(
+            kotlin(
+                """
+                package androidx.compose.ui.foo
+
+                import androidx.compose.ui.Modifier
+
+                object TestModifier : Modifier.Element
+
+                fun Modifier.fooModifier(): Modifier.Element {
+                    return TestModifier
+                }
+            """
+            ),
+            modifierStub
+        )
+            .run()
+            .expect(
+                """
+src/androidx/compose/ui/foo/TestModifier.kt:8: Error: Modifier factory functions must have a return type of Modifier [ModifierFactoryReturnType]
+                fun Modifier.fooModifier(): Modifier.Element {
+                             ~~~~~~~~~~~
+1 errors, 0 warnings
+            """
+            )
+            .expectFixDiffs(
+                """
+Fix for src/androidx/compose/ui/foo/TestModifier.kt line 8: Change return type to Modifier:
+@@ -8 +8
+-                 fun Modifier.fooModifier(): Modifier.Element {
++                 fun Modifier.fooModifier(): Modifier {
+            """
+            )
+    }
+
+    @Test
+    fun getterReturnsModifierElement() {
+        lint().files(
+            kotlin(
+                """
+                package androidx.compose.ui.foo
+
+                import androidx.compose.ui.Modifier
+
+                object TestModifier : Modifier.Element
+
+                val Modifier.fooModifier get(): Modifier.Element {
+                    return TestModifier
+                }
+
+                val Modifier.fooModifier2: Modifier.Element get() {
+                    return TestModifier
+                }
+
+                val Modifier.fooModifier3: Modifier.Element get() = TestModifier
+            """
+            ),
+            modifierStub
+        )
+            .run()
+            .expect(
+                """
+src/androidx/compose/ui/foo/TestModifier.kt:8: Error: Modifier factory functions must have a return type of Modifier [ModifierFactoryReturnType]
+                val Modifier.fooModifier get(): Modifier.Element {
+                                         ~~~
+src/androidx/compose/ui/foo/TestModifier.kt:12: Error: Modifier factory functions must have a return type of Modifier [ModifierFactoryReturnType]
+                val Modifier.fooModifier2: Modifier.Element get() {
+                                                            ~~~
+src/androidx/compose/ui/foo/TestModifier.kt:16: Error: Modifier factory functions must have a return type of Modifier [ModifierFactoryReturnType]
+                val Modifier.fooModifier3: Modifier.Element get() = TestModifier
+                                                            ~~~
+3 errors, 0 warnings
+            """
+            )
+            .expectFixDiffs(
+                """
+Fix for src/androidx/compose/ui/foo/TestModifier.kt line 8: Change return type to Modifier:
+@@ -8 +8
+-                 val Modifier.fooModifier get(): Modifier.Element {
++                 val Modifier.fooModifier get(): Modifier {
+Fix for src/androidx/compose/ui/foo/TestModifier.kt line 12: Change return type to Modifier:
+@@ -12 +12
+-                 val Modifier.fooModifier2: Modifier.Element get() {
++                 val Modifier.fooModifier2: Modifier get() {
+Fix for src/androidx/compose/ui/foo/TestModifier.kt line 16: Change return type to Modifier:
+@@ -16 +16
+-                 val Modifier.fooModifier3: Modifier.Element get() = TestModifier
++                 val Modifier.fooModifier3: Modifier get() = TestModifier
+            """
+            )
+    }
+
+    @Test
+    fun functionImplicitlyReturnsModifierElement() {
+        lint().files(
+            kotlin(
+                """
+                package androidx.compose.ui.foo
+
+                import androidx.compose.ui.Modifier
+
+                object TestModifier : Modifier.Element
+
+                fun Modifier.fooModifier() = TestModifier
+            """
+            ),
+            modifierStub
+        )
+            .run()
+            .expect(
+                """
+src/androidx/compose/ui/foo/TestModifier.kt:8: Error: Modifier factory functions must have a return type of Modifier [ModifierFactoryReturnType]
+                fun Modifier.fooModifier() = TestModifier
+                             ~~~~~~~~~~~
+1 errors, 0 warnings
+            """
+            )
+            .expectFixDiffs(
+                """
+Fix for src/androidx/compose/ui/foo/TestModifier.kt line 8: Add explicit Modifier return type:
+@@ -8 +8
+-                 fun Modifier.fooModifier() = TestModifier
++                 fun Modifier.fooModifier(): Modifier = TestModifier
+            """
+            )
+    }
+
+    @Test
+    fun getterImplicitlyReturnsModifierElement() {
+        lint().files(
+            kotlin(
+                """
+                package androidx.compose.ui.foo
+
+                import androidx.compose.ui.Modifier
+
+                object TestModifier : Modifier.Element
+
+                val Modifier.fooModifier get() = TestModifier
+            """
+            ),
+            modifierStub
+        )
+            .run()
+            .expect(
+                """
+src/androidx/compose/ui/foo/TestModifier.kt:8: Error: Modifier factory functions must have a return type of Modifier [ModifierFactoryReturnType]
+                val Modifier.fooModifier get() = TestModifier
+                                         ~~~
+1 errors, 0 warnings
+            """
+            )
+            .expectFixDiffs(
+                """
+Fix for src/androidx/compose/ui/foo/TestModifier.kt line 8: Add explicit Modifier return type:
+@@ -8 +8
+-                 val Modifier.fooModifier get() = TestModifier
++                 val Modifier.fooModifier get(): Modifier = TestModifier
+            """
+            )
+    }
+
+    @Test
+    fun returnsCustomModifierImplementation() {
+        lint().files(
+            kotlin(
+                """
+                package androidx.compose.ui.foo
+
+                import androidx.compose.ui.Modifier
+
+                object TestModifier : Modifier.Element
+
+                fun Modifier.fooModifier(): TestModifier {
+                    return TestModifier
+                }
+            """
+            ),
+            modifierStub
+        )
+            .run()
+            .expect(
+                """
+src/androidx/compose/ui/foo/TestModifier.kt:8: Error: Modifier factory functions must have a return type of Modifier [ModifierFactoryReturnType]
+                fun Modifier.fooModifier(): TestModifier {
+                             ~~~~~~~~~~~
+1 errors, 0 warnings
+            """
+            )
+            .expectFixDiffs(
+                """
+Fix for src/androidx/compose/ui/foo/TestModifier.kt line 8: Change return type to Modifier:
+@@ -8 +8
+-                 fun Modifier.fooModifier(): TestModifier {
++                 fun Modifier.fooModifier(): Modifier {
+            """
+            )
+    }
+
+    @Test
+    fun noErrors() {
+        lint().files(
+            kotlin(
+                """
+                package androidx.compose.ui.foo
+
+                import androidx.compose.ui.Modifier
+
+                fun Modifier.fooModifier(): Modifier {
+                    return TestModifier
+                }
+            """
+            ),
+            modifierStub
+        )
+            .run()
+            .expectClean()
+    }
+}
+/* ktlint-enable max-line-length */
