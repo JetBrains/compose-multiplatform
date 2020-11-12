ad167aea3e1a66463aa226aa469c9308af3a731d
Daniel Santiago Rivera <danysantiago@google.com>
2020-11-12 08:23:42 -0800

    Bump androidx.room to 2.3.0-alpha04.
    Prepare next alpha release of Room.
    
    Test: ./gradlew bOS
    Relnote: n/a
    Change-Id: I10d9bed148b82e5546c6d2830e404de1f6c53a0c

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -93,7 +93,7 @@ object LibraryVersions {
     val RECYCLERVIEW = Version("1.2.0-beta01")
     val RECYCLERVIEW_SELECTION = Version("2.0.0-alpha01")
     val REMOTECALLBACK = Version("1.0.0-alpha02")
-    val ROOM = Version("2.3.0-alpha03")
+    val ROOM = Version("2.3.0-alpha04")
     val SAVEDSTATE = Version("1.1.0-beta01")
     val SECURITY = Version("1.1.0-alpha02")
     val SECURITY_BIOMETRIC = Version("1.0.0-alpha01")
