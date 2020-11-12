7ee9e15d6898124c6260c7a66eed736126a20255
Jose Alcerreca <jalc@google.com>
2020-11-12 18:15:57 +0100

    Add a new docs-snippets project to track changes in DAC docs
    The official documentation (like
    https://developer.android.com/jetpack/compose/themes) has a large number
    of code snippets that are hard-coded. Maintenance of these articles is
    proving very hard given that there are breaking changes every 2 weeks
    and that they require subject matter experts.
    
    With the snippets in AOSP, we can track changes to these files and
    easily detect the pieces of code that need updating.
    
    If we put this on a separate project, depending on artifacts, we
    wouldn't benefit from automatic refactorings, which is what most of the
    breaking changes are at this point.
    
    Bug: 173574611
    Test: N/A (build = ship)
    Change-Id: Id4a0ba4a3bfcf183aeb8a6bf0d9ca035b48fdb3a

---
+++ integration-tests/docs-snippets/src/main/java/androidx/compose/integration/docs/kotlin/Kotlin.kt
@@ -0,0 +1,155 @@
+// ktlint-disable indent https://github.com/pinterest/ktlint/issues/967
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Ignore lint warnings in documentation snippets
+@file:Suppress("unused", "UNUSED_PARAMETER", "UNUSED_VARIABLE")
+
+package androidx.compose.integration.docs.kotlin
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ConstraintLayout
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.drawBehind
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+
+/**
+ * This file lets DevRel track changes to snippets present in
+ * https://developer.android.com/jetpack/compose/kotlin
+ *
+ * No action required if it's modified.
+ */
+
+@Composable private fun KotlinSnippet1() {
+    Text(text = "Hello, Android!")
+}
+
+@Composable private fun KotlinSnippet2() {
+    Text(text = "Hello, Android!",
+        color = Color.Unspecified,
+        fontSize = TextUnit.Unspecified,
+        letterSpacing = TextUnit.Unspecified,
+        overflow = TextOverflow.Clip
+    )
+}
+
+@Composable private fun KotlinSnippet3() {
+    Button( // ...
+        onClick = {
+            // do something
+            // do something else
+        }
+    ) { /*...*/ }
+}
+
+@Composable private fun KotlinSnippet4() {
+    Column(
+        modifier = Modifier.padding(16.dp), content = {
+            Text("Some text")
+            Text("Some more text")
+            Text("Last text")
+        }
+    )
+}
+
+@Composable private fun KotlinSnippet5() {
+    Column(modifier = Modifier.padding(16.dp)) {
+        Text("Some text")
+        Text("Some more text")
+        Text("Last text")
+    }
+}
+
+@Composable private fun KotlinSnippet6() {
+    Column {
+        Text("Some text")
+        Text("Some more text")
+        Text("Last text")
+    }
+}
+
+@Composable private fun KotlinSnippet7() {
+    Row {
+        Text(
+            text = "Hello world",
+            // This Text is inside a RowScope so it has access to
+            // Alignment.CenterVertically but not to
+            // Alignment.CenterHorizontally, which would be available
+            // in a ColumnScope.
+            modifier = Modifier.align(Alignment.CenterVertically)
+        )
+    }
+}
+
+@Composable private fun KotlinSnippet8() {
+    Box(
+        modifier = Modifier.drawBehind {
+            // This method accepts a lambda of type DrawScope.() -> Unit
+            // therefore in this lambda we can access properties and functions
+            // available from DrawScope, such as the `drawRectangle` function.
+            drawRect( /*...*/ )
+        }
+    )
+}
+
+@Suppress("ASSIGNED_BUT_NEVER_ACCESSED_VARIABLE", "UNUSED_VALUE")
+@Composable private fun KotlinSnippet9() {
+    var showDialog by remember { mutableStateOf(false) }
+
+    // Updating the var automatically triggers a state change
+    showDialog = true
+}
+
+@Composable private fun KotlinSnippet10() {
+    ConstraintLayout {
+
+        val (image, title, subtitle) = createRefs()
+
+        // The `createRefs` function returns a data object;
+        // the first three components are extracted into the
+        // image, title, and subtitle variables.
+
+        // ...
+    }
+}
+
+/*
+Fakes needed for snippets to build:
+ */
+
+@Suppress("ClassName")
+private object R {
+    object string {
+        const val profile = 0
+        const val friends_list = 1
+    }
+}
+
+private fun drawRect() {}
---
+++ integration-tests/docs-snippets/src/main/java/androidx/compose/integration/docs/testing/Testing.kt
@@ -0,0 +1,232 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Ignore lint warnings in documentation snippets
+@file:Suppress("unused", "UNUSED_PARAMETER", "UNUSED_VARIABLE")
+
+package androidx.compose.integration.docs.testing
+
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.ExperimentalKeyInput
+import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.semantics.AccessibilityAction
+import androidx.compose.ui.semantics.SemanticsPropertyKey
+import androidx.compose.ui.semantics.accessibilityLabel
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.test.SemanticsMatcher
+import androidx.compose.ui.test.SemanticsNodeInteraction
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.assertAll
+import androidx.compose.ui.test.assertAny
+import androidx.compose.ui.test.assertCountEquals
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.filter
+import androidx.compose.ui.test.hasAnyAncestor
+import androidx.compose.ui.test.hasAnyDescendant
+import androidx.compose.ui.test.hasAnySibling
+import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasTestTag
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onAllNodesWithLabel
+import androidx.compose.ui.test.onChildren
+import androidx.compose.ui.test.onFirst
+import androidx.compose.ui.test.onNodeWithLabel
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performGesture
+import androidx.compose.ui.test.performKeyPress
+import androidx.compose.ui.test.performSemanticsAction
+import androidx.compose.ui.test.printToLog
+import androidx.compose.ui.test.swipeLeft
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * This file lets DevRel track changes to snippets present in
+ * https://developer.android.com/jetpack/compose/testing
+ *
+ * No action required if it's modified.
+ */
+
+@Composable private fun TestingSnippet1() {
+    MyButton(modifier = Modifier.semantics { accessibilityLabel = "Like button" })
+}
+
+private object TestingSnippet3 {
+    // file: app/src/androidTest/java/com/package/MyComposeTest.kt
+
+    class MyComposeTest {
+
+        @get:Rule
+        val composeTestRule = createAndroidComposeRule<MyActivity>()
+        // createComposeRule() if you don't need access to the activityTestRule
+
+        @Test
+        fun MyTest() {
+            // Start the app
+            composeTestRule.setContent {
+                MyAppTheme {
+                    MainScreen(uiState = exampleUiState, /*...*/)
+                }
+            }
+
+            composeTestRule.onNodeWithText("Continue").performClick()
+
+            composeTestRule.onNodeWithText("Welcome").assertIsDisplayed()
+        }
+    }
+}
+
+@Composable private fun TestingSnippets4() {
+    // single node
+    // It's API, see line below.
+    // onNode(<<SemanticsMatcher>>, useUnmergedTree = false): SemanticsNodeInteraction
+    composeTestRule.onNode(hasText("Button")) // Equivalent to onNodeWithText("Button")
+
+    // multiple nodes
+    // It's API, see line below.
+    // onAllNodes(<<SemanticsMatcher>>): SemanticsNodeInteractionCollection
+
+    // Example
+    composeTestRule.onAllNodes(hasText("Button")) // Equivalent to onAllNodesWithText("Button")
+}
+
+@Composable private fun TestingSnippets5() {
+    MyButton {
+        Text("Hello")
+        Text("World")
+    }
+
+    composeTestRule.onRoot().printToLog("TAG")
+
+    composeTestRule.onRoot(useUnmergedTree = true).printToLog("TAG")
+}
+
+@Composable private fun TestingSnippets6() {
+    composeTestRule.onNodeWithText("World", useUnmergedTree = true).assertIsDisplayed()
+}
+
+// assertions
+
+@Composable private fun TestingSnippets7() {
+    // Single matcher:
+    composeTestRule.onNode(matcher).assert(hasText("Button")) // hasText is a
+    // SemanticsMatcher
+    // Multiple matchers can use and / or
+    composeTestRule.onNode(matcher).assert(hasText("Button") or hasText("Button2"))
+}
+
+@Composable private fun TestingSnippets8() {
+    // Check number of matched nodes
+    composeTestRule.onAllNodesWithLabel("Beatle").assertCountEquals(4)
+    // At least one matches
+    composeTestRule.onAllNodesWithLabel("Beatle").assertAny(hasTestTag("Drummer"))
+    // All of them match
+    composeTestRule.onAllNodesWithLabel("Beatle").assertAll(hasClickAction())
+}
+
+@Composable private fun SemanticsNodeInteraction.TestingSnippets9() {
+    val listOfActions = listOf(
+        // start snippet
+        performClick(),
+        performSemanticsAction(key),
+        performKeyPress(keyEvent),
+        performGesture { swipeLeft() }
+        // end snippet
+    )
+}
+
+@Composable private fun TestingSnippets10() {
+    // It's API, look for changes below.
+    val matcher = SemanticsMatcher("test", { true })
+    hasParent(matcher)
+    hasAnySibling(matcher)
+    hasAnyAncestor(matcher)
+    hasAnyDescendant(matcher)
+}
+
+@Composable private fun TestingSnippets11() {
+    composeTestRule.onNode(hasParent(hasText("Button")))
+        .assertIsDisplayed()
+}
+
+@Composable private fun TestingSnippets12() {
+    composeTestRule.onNode(hasTestTag("Players"))
+        .onChildren()
+        .filter(hasClickAction())
+        .assertCountEquals(4)
+        .onFirst()
+        .assert(hasText("John"))
+}
+
+@OptIn(ExperimentalCoroutinesApi::class)
+private object TestingSnippets13 {
+    class MyTest() {
+
+        private val themeIsDark = MutableStateFlow(false)
+
+        @Before
+        fun setUp() {
+            composeTestRule.setContent {
+                JetchatTheme(
+                    isDarkTheme = themeIsDark.collectAsState(false).value
+                ) {
+                    MainScreen()
+                }
+            }
+        }
+
+        @Test
+        fun changeTheme_scrollIsPersisted() {
+            composeTestRule.onNodeWithLabel("Continue").performClick()
+
+            // Set theme to dark
+            themeIsDark.value = true
+
+            // Check that we're still on the same page
+            composeTestRule.onNodeWithLabel("Welcome").assertIsDisplayed()
+        }
+    }
+}
+
+/*
+Fakes needed for snippets to build:
+ */
+
+private val matcher = hasText("Button")
+private val text = ""
+private val composeTestRule = createAndroidComposeRule<MyActivity>()
+@Composable private fun MyButton(modifier: Modifier) {}
+@Composable private fun MyAppTheme(content: @Composable () -> Unit) {}
+@Composable private fun JetchatTheme(isDarkTheme: Boolean, content: @Composable () -> Unit) {}
+private val exampleUiState = Unit
+@Composable private fun MainScreen(uiState: Any = Unit) {}
+private class MyActivity : ComponentActivity()
+@Composable private fun MyButton(content: @Composable RowScope.() -> Unit) { }
+private lateinit var key: SemanticsPropertyKey<AccessibilityAction<() -> Boolean>>
+@OptIn(ExperimentalKeyInput::class)
+private lateinit var keyEvent: KeyEvent
