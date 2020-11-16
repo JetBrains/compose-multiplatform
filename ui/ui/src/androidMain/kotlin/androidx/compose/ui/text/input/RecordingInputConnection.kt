9898cc7e39f8c93ac630cb1f017993aac31aa79e
Siyamed Sinir <siyamed@google.com>
2020-11-15 21:48:58 -0800

    Reset input when composition or text changes
    When the text or composition changes by the application
    IME needs to be informed about this fact. Otherwise
    IME thinks that their data is correct and in synch. An easy
    culprit for this issue is: if onValueChange always rejects the
    given input, keyboard still shows/updates suggestions thinking
    that the data that was previously sent is visible to user.
    
    When the IME is not informed what is happening, also the duplicate
    text issues occurs since IME still thinks that the composition
    range is correct, and that is what the user sees.
    
    This CL changes the EditProcessor behavior to compare the previously
    suggested state to the new state it gets. If
    - the text is different
    - the composition is different
    
    It resets the input.
    
    Test: Added new tests
    Test: ./gradlew compose:ui:ui-text:test
    Test: ./gradlew compose:ui:ui-text:connectedAndroidTest
    Test: ./gradlew compose:ui:ui:test
    Test: ./gradlew compose:ui:ui:connectedAndroidTest
    Test: ./gradlew compose:foundation:foundation:test
    Test: ./gradlew compose:foundation:foundation:connectedAndroidTest
    
    RelNote: Added resetInput parameter to TextInputService#onStateUpdated
    
    Bug: 172239032
    Bug: 171860947
    Change-Id: I3e8f5404553921bd94ae424d2840ca5595b6f90b

--- ui/ui/src/androidMain/kotlin/androidx/compose/ui/text/input/RecordingInputConnection.kt
+++ ui/ui/src/androidMain/kotlin/androidx/compose/ui/text/input/RecordingInputConnection.kt
@@ -32,8 +32,8 @@ import android.view.inputmethod.InputContentInfo
 import android.view.inputmethod.InputMethodManager
 import androidx.annotation.VisibleForTesting
 
-private val DEBUG = false
-private val TAG = "RecordingIC"
+internal val DEBUG = false
+internal val TAG = "RecordingIC"
 
 /**
  * [InputConnection] implementation that binds Android IME to Compose.
@@ -82,32 +82,29 @@ internal class RecordingInputConnection(
      */
     fun updateInputState(state: TextFieldValue, imm: InputMethodManager, view: View) {
         val prev = mTextFieldValue
-        val next = state
-        mTextFieldValue = next
+        mTextFieldValue = state
 
-        if (prev == next) {
+        if (prev == state) {
             return
         }
 
         if (extractedTextMonitorMode) {
-            imm.updateExtractedText(view, currentExtractedTextRequestToken, next.toExtractedText())
+            imm.updateExtractedText(view, currentExtractedTextRequestToken, state.toExtractedText())
         }
 
-        // The candidateStart and candidateEnd is composition start and composition end in
-        // updateSelection API. Need to pass -1 if there is no composition.
-        val candidateStart = next.composition?.min ?: -1
-        val candidateEnd = next.composition?.max ?: -1
+        // updateSelection API requires -1 if there is no composition
+        val compositionStart = state.composition?.min ?: -1
+        val compositionEnd = state.composition?.max ?: -1
         if (DEBUG) {
             Log.d(
                 TAG,
                 "updateSelection(" +
-                    "selection = (${next.selection.min},${next.selection.max}), " +
-                    "compoairion = ($candidateStart, $candidateEnd)"
+                    "selection = (${state.selection.min},${state.selection.max}), " +
+                    "composition = ($compositionStart, $compositionEnd)"
             )
         }
         imm.updateSelection(
-            view, next.selection.min, next.selection.max,
-            candidateStart, candidateEnd
+            view, state.selection.min, state.selection.max, compositionStart, compositionEnd
         )
     }
 
