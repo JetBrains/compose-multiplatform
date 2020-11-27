8e2536a2fa6db786aded12e0e33d0529b69092d9
Igor Demin <igor.demin@jetbrains.com>
2020-11-27 11:03:43 +0300

    Examples. Issues. Migrate to 0.2.0-build132

--- examples/issues/build.gradle.kts
+++ examples/issues/build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
 
     dependencies {
         // __LATEST_COMPOSE_RELEASE_VERSION__
-        classpath("org.jetbrains.compose:compose-gradle-plugin:0.2.0-build128")
+        classpath("org.jetbrains.compose:compose-gradle-plugin:0.2.0-build132")
         classpath("com.android.tools.build:gradle:4.0.1")
         // __KOTLIN_COMPOSE_VERSION__
         classpath(kotlin("gradle-plugin", version = "1.4.20"))
