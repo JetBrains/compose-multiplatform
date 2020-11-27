7879b8d5701b7702c260e8c9bade7314077048f8
Igor Demin <igor.demin@jetbrains.com>
2020-11-27 10:58:06 +0300

    Multiplatform template. androidx.compose.foundation.Text is deprecated, replace by androidx.compose.material.Text

--- templates/multiplatform-template/common/src/commonMain/kotlin/App.kt
+++ templates/multiplatform-template/common/src/commonMain/kotlin/App.kt
@@ -1,6 +1,6 @@
-import androidx.compose.foundation.Text
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 
 @Composable
