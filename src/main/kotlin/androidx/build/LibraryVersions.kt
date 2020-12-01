884164be5e43df4b15dcbd01db3f5b6123982b0a
Ian Lake <ilake@google.com>
2020-11-30 16:07:18 -0800

    Update Fragment to 1.3.0-rc01
    Test: ./gradlew -p fragment checkApi
    Change-Id: Ieb147cb2ce56eae6fd31a801d5b5d80860843fa5

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -61,7 +61,7 @@ object LibraryVersions {
     val EMOJI = Version("1.2.0-alpha03")
     val ENTERPRISE = Version("1.1.0-rc01")
     val EXIFINTERFACE = Version("1.4.0-alpha01")
-    val FRAGMENT = Version("1.3.0-beta02")
+    val FRAGMENT = Version("1.3.0-rc01")
     val FUTURES = Version("1.2.0-alpha01")
     val GRIDLAYOUT = Version("1.1.0-alpha01")
     val HEIFWRITER = Version("1.1.0-alpha02")
