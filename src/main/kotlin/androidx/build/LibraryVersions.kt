b89ff6d9ab017b04a1881703242b77e8b1a12150
Chris Craik <ccraik@google.com>
2020-12-08 16:08:35 -0800

    Bump tracing to 1.1.0-alpha01
    Test: ./gradlew bOS
    
    Change-Id: Ieb6fb3c80399ab1c6bc1ab64fb7e088445a50243

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -114,7 +114,7 @@ object LibraryVersions {
     val TESTSCREENSHOT = Version("1.0.0-alpha01")
     val TEXT = Version("1.0.0-alpha01")
     val TEXTCLASSIFIER = Version("1.0.0-alpha03")
-    val TRACING = Version("1.0.0")
+    val TRACING = Version("1.1.0-alpha01")
     val TRANSITION = Version("1.4.0-rc01")
     val TVPROVIDER = Version("1.1.0-alpha02")
     val VECTORDRAWABLE = Version("1.2.0-alpha03")
