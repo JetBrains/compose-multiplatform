b27860718204d7e2607493091e88e284975a06af
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2020-12-30 08:44:39 +0300

    Fix resource loading on Android

--- examples/widgetsgallery/common/src/androidMain/kotlin/org/jetbrains/compose/demo/widgets/platform/Resources.kt
+++ examples/widgetsgallery/common/src/androidMain/kotlin/org/jetbrains/compose/demo/widgets/platform/Resources.kt
@@ -3,18 +3,25 @@ package org.jetbrains.compose.demo.widgets.platform
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.AmbientContext
+import org.jetbrains.compose.demo.widgets.R
 
 @Composable
 actual fun imageResource(res: String): ImageBitmap {
-    val context = AmbientContext.current
-    val id = context.resources.getIdentifier(res, "drawable", context.packageName)
+    val id = drawableId(res)
     return androidx.compose.ui.res.imageResource(id)
 }
 
 @Composable
 actual fun vectorResource(res: String): ImageVector {
-    val context = AmbientContext.current
-    val id = context.resources.getIdentifier(res, "drawable", context.packageName)
+    val id = drawableId(res)
     return androidx.compose.ui.res.vectorResource(id)
+}
+
+// TODO: improve resource loading
+private fun drawableId(res: String): Int {
+    val imageName = res.substringAfterLast("/").substringBeforeLast(".")
+    val drawableClass = R.drawable::class.java
+    val field = drawableClass.getDeclaredField(imageName)
+    val idValue = field.get(drawableClass) as Integer
+    return idValue.toInt()
 }
