220a7f0f380ff9bb1c0b7da49346214273d64de1
Konstantin Bulenkov <kb@jetbrains.com>
2021-01-18 11:56:38 +0100

    Jetpack Compose for IntelliJ Platform initial

---
+++ examples/intelliJPlugin/src/main/kotlin/com/jetbrains/compose/ComposeDemoAction.kt
@@ -0,0 +1,56 @@
+package com.jetbrains.compose
+
+import androidx.compose.desktop.ComposePanel
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.ui.layout.panel
+import java.awt.Dimension
+import javax.swing.JComponent
+
+
+/**
+ * @author Konstantin Bulenkov
+ */
+class ComposeDemoAction : DumbAwareAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        DemoDialog(e.project).show()
+    }
+
+    class DemoDialog(project: Project?) : DialogWrapper(project) {
+        init {
+            title = "Demo"
+            init()
+        }
+
+        override fun createCenterPanel(): JComponent {
+//            //Kotlin UI DSL example
+//            return panel {
+//                row("Text:") {textField({"Type something"}, {}, 30)}
+//                row("Text:") {textField({"Type something"}, {}, 30)}
+//                row {checkBox("Fooo")}
+//            }
+            return ComposePanel().apply {
+                setContent {
+                    Button(onClick = { println("Hi") }) {
+                        //Text("Hello") //todo: crashes IntelliJ
+                        Box(
+                            modifier = Modifier
+                                .background(color = Color.Red)
+                                .size(20.dp, 20.dp)
+                        )
+                    }
+                }
+                size = Dimension(50, 50)
+            }
+        }
+    }
+}
