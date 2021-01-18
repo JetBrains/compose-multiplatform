3125cc926e06d857c6afda3a45c08bf50e0d5792
Konstantin Bulenkov <bulenkov@users.noreply.github.com>
2021-01-18 13:24:53 +0100

    Jetpack Compose for IntelliJ Platform initial (#287)

---
+++ examples/intelliJPlugin/settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "ComposeDemoPlugin"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+    }
+}
