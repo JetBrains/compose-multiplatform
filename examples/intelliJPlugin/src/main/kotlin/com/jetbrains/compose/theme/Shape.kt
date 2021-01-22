cc30d963d86551570fc24b17f52e41f273db87ba
Roman Sedaikin <roman.sedaikin@jetbrains.com>
2021-01-22 17:55:31 +0300

    Applied theme to widgets.

---
+++ examples/intelliJPlugin/src/main/kotlin/com/jetbrains/compose/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.jetbrains.compose.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
