aef31361384bf18bf7a07ba3f9e5de63362e361e
Danila Aliev <HrAJyW4@gmail.com>
2021-02-09 02:11:14 +0400

    update compose version to 0.3.0-build150

--- components/build.gradle.kts
+++ components/build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     // __LATEST_COMPOSE_RELEASE_VERSION__
-    val composeVersion = System.getenv("COMPOSE_RELEASE_VERSION") ?: "0.3.0-build141"
+    val composeVersion = System.getenv("COMPOSE_RELEASE_VERSION") ?: "0.3.0-build150"
 
     repositories {
         google()
