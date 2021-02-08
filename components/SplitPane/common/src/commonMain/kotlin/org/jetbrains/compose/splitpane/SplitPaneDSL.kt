e72f6fb130945a98d954ef8019a2582063821bb3
Danila Aliev <HrAJyW4@gmail.com>
2021-02-09 02:23:50 +0400

    change split pane DSL
    change betrayer class from singleton interface to internal class
    now betrayer will provide MinimalSizes data class by itself
    
    remove animation init from rememberSplitState function

--- components/SplitPane/common/src/commonMain/kotlin/org/jetbrains/compose/splitpane/SplitPaneDSL.kt
+++ components/SplitPane/common/src/commonMain/kotlin/org/jetbrains/compose/splitpane/SplitPaneDSL.kt
@@ -1,22 +1,19 @@
 package org.jetbrains.compose.splitpane
 
-import androidx.compose.animation.asDisposableClock
 import androidx.compose.foundation.InteractionState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.AmbientAnimationClock
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-interface SplitPaneContext {
+interface SplitPaneScope {
 
     fun first(
         minSize: Dp = 0.dp,
-        content: @Composable () -> Unit = { Box(Modifier.size(50.dp)) }
+        content: @Composable () -> Unit
     )
 
     fun second(
@@ -29,13 +26,17 @@ interface SplitPaneContext {
     )
 }
 
-internal interface Betrayer : SplitPaneContext {
-    var firstPlaceableMinimalSize: Dp
-    var secondPlaceableMinimalSize: Dp
-    var firstPlaceableContent: @Composable () -> Unit
-    var secondPlaceableContent: @Composable () -> Unit
+internal class Betrayer : SplitPaneScope {
 
-    companion object : Betrayer {
+    private var firstPlaceableMinimalSize: Dp = 0.dp
+    private var secondPlaceableMinimalSize: Dp = 0.dp
+
+    internal val minimalSizes: MinimalSizes
+        get() = MinimalSizes(firstPlaceableMinimalSize, secondPlaceableMinimalSize)
+
+    internal lateinit var firstPlaceableContent: @Composable () -> Unit
+    internal lateinit var secondPlaceableContent: @Composable () -> Unit
+
         override fun first(
             minSize: Dp,
             content: @Composable () -> Unit
@@ -52,12 +53,6 @@ internal interface Betrayer : SplitPaneContext {
             secondPlaceableContent = content
         }
 
-        override var firstPlaceableMinimalSize: Dp = 0.dp
-        override var secondPlaceableMinimalSize: Dp = 0.dp
-        override lateinit var firstPlaceableContent: @Composable () -> Unit
-        override lateinit var secondPlaceableContent: @Composable () -> Unit
-
-    }
 }
 
 @Composable
@@ -65,12 +60,10 @@ fun rememberSplitterState(
     initial: Dp = 0.dp,
     interactionState: InteractionState? = null
 ): SplitterState {
-    val clock = AmbientAnimationClock.current.asDisposableClock()
     return remember {
         SplitterState(
             initial = initial.value,
-            interactionState = interactionState,
-            animationClock = clock
+            interactionState = interactionState
         )
     }
 }
