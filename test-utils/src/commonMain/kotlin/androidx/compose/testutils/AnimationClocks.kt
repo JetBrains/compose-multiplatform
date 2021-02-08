e22f230ba18bde53c23971aced17a71b2736270b
Jelle Fresen <jellefresen@google.com>
2021-02-08 14:10:38 +0000

    Move runBlockingWithManualClock to test-utils
    Intention is to remove it completely.
    
    Bug: 179664814
    Test: ./gradlew compose:foundation:foundation:cC
    Relnote: Removed `runBlockingWithManualClock`
    Change-Id: I15cdc97a742c764cae3b332ad52143c07f32b9bd

--- test-utils/src/commonMain/kotlin/androidx/compose/testutils/AnimationClocks.kt
+++ test-utils/src/commonMain/kotlin/androidx/compose/testutils/AnimationClocks.kt
@@ -18,7 +18,10 @@ package androidx.compose.testutils
 
 import androidx.compose.animation.core.ManualFrameClock
 import androidx.compose.animation.core.advanceClockMillis
+import androidx.compose.ui.test.ExperimentalTestApi
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.yield
 
@@ -45,5 +48,32 @@ suspend fun ManualFrameClock.advanceClockOnMainThreadMillis(millis: Long) {
         advanceClockMillis(millis)
         // Give awaiters the chance to await again
         yield()
+    }
+}
+
+/**
+ * Runs a new coroutine and blocks the current thread interruptibly until it completes, passing a
+ * new [ManualFrameClock] to the code [block]. This is intended to be used by tests instead of
+ * [runBlocking] if they want to use a [ManualFrameClock].
+ *
+ * The clock will start at time 0L and should be driven manually from your test, from the
+ * [main dispatcher][Dispatchers.Main]. Pass the clock to the animation that you want to
+ * control in your test, and then [advance][advanceClockMillis] it as necessary. After the block
+ * has completed, the clock will be forwarded with 10 second increments until it has drained all
+ * work that took frames from that clock. If the work never ends, this function never ends, so
+ * make sure that all animations driven by this clock are finite.
+ */
+@ExperimentalTestApi
+fun <R> runBlockingWithManualClock(
+    block: suspend CoroutineScope.(clock: ManualFrameClock) -> R
+) {
+    val clock = ManualFrameClock(0L)
+    return runBlocking(clock) {
+        block(clock)
+        while (clock.hasAwaiters) {
+            clock.advanceClockMillis(10_000L)
+            // Give awaiters the chance to await again
+            yield()
+        }
     }
 }
