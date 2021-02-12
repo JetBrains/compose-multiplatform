c9dce08237ed01780e4424f7d5d7d41e3c541905
Louis Pullen-Freilich <lpf@google.com>
2021-02-12 15:21:39 +0000

    Sets disabled ContentAlpha in IconButton and IconToggleButton when not enabled
    Fixes: b/179463133
    Test: IconButtonTest
    Change-Id: Icb2b44034c4e341bb46475a77e95f4b5eea6f15c

--- material/material/src/androidAndroidTest/kotlin/androidx/compose/material/IconButtonTest.kt
+++ material/material/src/androidAndroidTest/kotlin/androidx/compose/material/IconButtonTest.kt
@@ -19,14 +19,20 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.samples.IconButtonSample
 import androidx.compose.material.samples.IconToggleButtonSample
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.assertHeightIsEqualTo
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.assertIsOff
 import androidx.compose.ui.test.assertIsOn
 import androidx.compose.ui.test.assertLeftPositionInRootIsEqualTo
 import androidx.compose.ui.test.assertTopPositionInRootIsEqualTo
 import androidx.compose.ui.test.assertWidthIsEqualTo
+import androidx.compose.ui.test.hasClickAction
 import androidx.compose.ui.test.isToggleable
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
@@ -34,6 +40,7 @@ import androidx.compose.ui.test.performClick
 import androidx.compose.ui.unit.dp
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.google.common.truth.Truth
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -57,6 +64,26 @@ class IconButtonTest {
             }
             .assertWidthIsEqualTo(width)
             .assertHeightIsEqualTo(height)
+    }
+
+    @Test
+    fun iconButton_defaultSemantics() {
+        rule.setMaterialContent {
+            IconButtonSample()
+        }
+        rule.onNode(hasClickAction()).apply {
+            assertIsEnabled()
+        }
+    }
+
+    @Test
+    fun iconButton_disabledSemantics() {
+        rule.setMaterialContent {
+            IconButton(onClick = {}, enabled = false) {}
+        }
+        rule.onNode(hasClickAction()).apply {
+            assertIsNotEnabled()
+        }
     }
 
     @Test
@@ -95,6 +122,33 @@ class IconButtonTest {
     }
 
     @Test
+    fun iconButton_contentAlpha() {
+        var enabled by mutableStateOf(true)
+        var contentAlpha: Float? = null
+        var highContentAlpha: Float? = null
+        var disabledContentAlpha: Float? = null
+        rule.setMaterialContent {
+            IconButton(onClick = {}, enabled = enabled) {
+                contentAlpha = LocalContentAlpha.current
+                highContentAlpha = ContentAlpha.high
+                disabledContentAlpha = ContentAlpha.disabled
+            }
+        }
+
+        rule.runOnIdle {
+            Truth.assertThat(contentAlpha!!).isEqualTo(highContentAlpha!!)
+        }
+
+        rule.runOnIdle {
+            enabled = false
+        }
+
+        rule.runOnIdle {
+            Truth.assertThat(contentAlpha!!).isEqualTo(disabledContentAlpha!!)
+        }
+    }
+
+    @Test
     fun iconToggleButton_size() {
         val width = 48.dp
         val height = 48.dp
@@ -104,6 +158,30 @@ class IconButtonTest {
             }
             .assertWidthIsEqualTo(width)
             .assertHeightIsEqualTo(height)
+    }
+
+    @Test
+    fun iconToggleButton_defaultSemantics() {
+        rule.setMaterialContent {
+            IconToggleButtonSample()
+        }
+        rule.onNode(isToggleable()).apply {
+            assertIsEnabled()
+            assertIsOff()
+            performClick()
+            assertIsOn()
+        }
+    }
+
+    @Test
+    fun iconToggleButton_disabledSemantics() {
+        rule.setMaterialContent {
+            IconToggleButton(checked = false, onCheckedChange = {}, enabled = false) {}
+        }
+        rule.onNode(isToggleable()).apply {
+            assertIsNotEnabled()
+            assertIsOff()
+        }
     }
 
     @Test
@@ -142,14 +220,29 @@ class IconButtonTest {
     }
 
     @Test
-    fun iconToggleButton_semantics() {
+    fun iconToggleButton_contentAlpha() {
+        var enabled by mutableStateOf(true)
+        var contentAlpha: Float? = null
+        var highContentAlpha: Float? = null
+        var disabledContentAlpha: Float? = null
         rule.setMaterialContent {
-            IconToggleButtonSample()
+            IconToggleButton(checked = false, onCheckedChange = {}, enabled = enabled) {
+                contentAlpha = LocalContentAlpha.current
+                highContentAlpha = ContentAlpha.high
+                disabledContentAlpha = ContentAlpha.disabled
+            }
         }
-        rule.onNode(isToggleable()).apply {
-            assertIsOff()
-            performClick()
-            assertIsOn()
+
+        rule.runOnIdle {
+            Truth.assertThat(contentAlpha!!).isEqualTo(highContentAlpha!!)
+        }
+
+        rule.runOnIdle {
+            enabled = false
+        }
+
+        rule.runOnIdle {
+            Truth.assertThat(contentAlpha!!).isEqualTo(disabledContentAlpha!!)
         }
     }
 }
