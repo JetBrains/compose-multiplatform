9fcf0735eb196465a413a0a0f10f4b884f943b25
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2021-02-15 10:33:48 +0300

    Improve diagnosing problems with external tools
    * Fix passing 'compose.desktop.verbose' to jlink/jpackage
    * Save external tools logs to files in non-verbose mode

--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/desktop/application/tasks/AbstractJvmToolOperationTask.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/desktop/application/tasks/AbstractJvmToolOperationTask.kt
@@ -1,35 +1,21 @@
 package org.jetbrains.compose.desktop.application.tasks
 
-import org.gradle.api.DefaultTask
 import org.gradle.api.file.Directory
 import org.gradle.api.file.DirectoryProperty
-import org.gradle.api.internal.file.FileOperations
-import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.provider.Provider
-import org.gradle.api.provider.ProviderFactory
 import org.gradle.api.tasks.*
-import org.gradle.process.ExecOperations
 import org.gradle.process.ExecResult
 import org.gradle.process.ExecSpec
 import org.gradle.work.InputChanges
-import org.jetbrains.compose.desktop.application.internal.*
 import org.jetbrains.compose.desktop.application.internal.ComposeProperties
+import org.jetbrains.compose.desktop.application.internal.executableName
+import org.jetbrains.compose.desktop.application.internal.ioFile
 import org.jetbrains.compose.desktop.application.internal.notNullProperty
 import java.io.File
-import javax.inject.Inject
 
-abstract class AbstractJvmToolOperationTask(private val toolName: String) : DefaultTask() {
-    @get:Inject
-    protected abstract val objects: ObjectFactory
-    @get:Inject
-    protected abstract val providers: ProviderFactory
-    @get:Inject
-    protected abstract val execOperations: ExecOperations
-    @get:Inject
-    protected abstract val fileOperations: FileOperations
-
+abstract class AbstractJvmToolOperationTask(private val toolName: String) : AbstractComposeDesktopTask() {
     @get:LocalState
     protected val workingDir: Provider<Directory> = project.layout.buildDirectory.dir("compose/tmp/$name")
 
@@ -45,11 +31,6 @@ abstract class AbstractJvmToolOperationTask(private val toolName: String) : Defa
         set(providers.systemProperty("java.home"))
     }
 
-    @get:Internal
-    val verbose: Property<Boolean> = objects.notNullProperty<Boolean>().apply {
-        set(providers.provider { logger.isDebugEnabled }.orElse(ComposeProperties.isVerbose(providers)))
-    }
-
     protected open fun prepareWorkingDir(inputChanges: InputChanges) {
         fileOperations.delete(workingDir)
         fileOperations.mkdir(workingDir)
@@ -59,7 +40,8 @@ abstract class AbstractJvmToolOperationTask(private val toolName: String) : Defa
         freeArgs.orNull?.forEach { add(it) }
     }
 
-    protected open fun configureExec(exec: ExecSpec) {}
+    protected open fun jvmToolEnvironment():  MutableMap<String, String> =
+        HashMap()
     protected open fun checkResult(result: ExecResult) {
         result.assertNormalExitValue()
     }
@@ -85,11 +67,11 @@ abstract class AbstractJvmToolOperationTask(private val toolName: String) : Defa
         }
 
         try {
-            execOperations.exec { exec ->
-                configureExec(exec)
-                exec.executable = jtool.absolutePath
-                exec.setArgs(listOf("@${argsFile.absolutePath}"))
-            }.also { checkResult(it) }
+            runExternalTool(
+                tool = jtool,
+                args = listOf("@${argsFile.absolutePath}"),
+                environment = jvmToolEnvironment()
+            ).also { checkResult(it) }
         } finally {
             if (!ComposeProperties.preserveWorkingDir(providers).get()) {
                 fileOperations.delete(workingDir)
--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/desktop/application/tasks/AbstractNotarizationTask.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/desktop/application/tasks/AbstractNotarizationTask.kt
@@ -1,24 +1,14 @@
 package org.jetbrains.compose.desktop.application.tasks
 
-import org.gradle.api.DefaultTask
-import org.gradle.api.model.ObjectFactory
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Nested
 import org.gradle.api.tasks.Optional
-import org.gradle.process.ExecOperations
 import org.jetbrains.compose.desktop.application.dsl.MacOSNotarizationSettings
 import org.jetbrains.compose.desktop.application.internal.nullableProperty
 import org.jetbrains.compose.desktop.application.internal.validation.validate
-import javax.inject.Inject
 
-abstract class AbstractNotarizationTask(
-) : DefaultTask() {
-    @get:Inject
-    protected abstract val objects: ObjectFactory
-    @get:Inject
-    protected abstract val execOperations: ExecOperations
-
+abstract class AbstractNotarizationTask : AbstractComposeDesktopTask() {
     @get:Input
     @get:Optional
     internal val nonValidatedBundleID: Property<String?> = objects.nullableProperty()
