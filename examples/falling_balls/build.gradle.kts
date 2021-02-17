8ffe3a9aaad0bd6feb84fd96ef596864a4d6f51d
Nikolay Igotti <igotti@gmail.com>
2021-02-17 11:21:42 +0300

    Move sample to build154.

--- examples/falling_balls/build.gradle.kts
+++ examples/falling_balls/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     // __KOTLIN_COMPOSE_VERSION__
     kotlin("jvm") version "1.4.30"
     // __LATEST_COMPOSE_RELEASE_VERSION__
-    id("org.jetbrains.compose") version "0.3.0-build149"
+    id("org.jetbrains.compose") version "0.3.0-build154"
 }
 
 group = "me.user"
