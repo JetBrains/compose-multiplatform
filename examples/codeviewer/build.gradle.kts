4851d0154a0e9c76392d3be3528809ebe225f7c1
Igor Demin <igor.demin@jetbrains.com>
2021-02-20 13:56:26 +0300

    Migrate to 0.0.0-unmerged-build38

--- examples/codeviewer/build.gradle.kts
+++ examples/codeviewer/build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
 
     dependencies {
         // __LATEST_COMPOSE_RELEASE_VERSION__
-        classpath("org.jetbrains.compose:compose-gradle-plugin:0.0.0-unmerged-build37")
+        classpath("org.jetbrains.compose:compose-gradle-plugin:0.0.0-unmerged-build38")
         classpath("com.android.tools.build:gradle:4.0.1")
          // __KOTLIN_COMPOSE_VERSION__
         classpath(kotlin("gradle-plugin", version = "1.4.30"))
--- templates/multiplatform-template/build.gradle.kts
+++ templates/multiplatform-template/build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     // __LATEST_COMPOSE_RELEASE_VERSION__
-    val composeVersion = System.getenv("COMPOSE_TEMPLATE_COMPOSE_VERSION") ?: "0.0.0-unmerged-build37"
+    val composeVersion = System.getenv("COMPOSE_TEMPLATE_COMPOSE_VERSION") ?: "0.0.0-unmerged-build38"
 
     repositories {
         // TODO: remove after new build is published
