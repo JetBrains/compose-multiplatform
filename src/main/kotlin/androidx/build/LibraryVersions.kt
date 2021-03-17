5faa39a6e1c36b4bd28a02bf7a3127bbca561f83
Jeremy Woods <jbwoods@google.com>
2021-03-16 17:04:33 -0700

    Bumping Activity to version 1.3.0-alpha05
    1.3.0-alpha04 has already been released.
    
    Test: ./gradlew bOS
    Change-Id: I3232f08ee1d6fd4326d7887e48499fcd9bfd5fb3

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -20,7 +20,7 @@ package androidx.build
  * The list of versions codes of all the libraries in this project.
  */
 object LibraryVersions {
-    val ACTIVITY = Version("1.3.0-alpha04")
+    val ACTIVITY = Version("1.3.0-alpha05")
     val ADS_IDENTIFIER = Version("1.0.0-alpha04")
     val ANNOTATION = Version("1.3.0-alpha01")
     val ANNOTATION_EXPERIMENTAL = Version("1.2.0-alpha01")
