6390fc1908169998e5cdf0bd2634ec23234aadbd
Louis Pullen-Freilich <lpf@google.com>
2021-03-23 21:46:27 +0000

    Adds lint check to ensure that hex values passed to a Color are 32 bit
    The expected format for a Color's hex value is:
    
      A R G B
    0xFF000000
    
    Anything else will result in missing / unexpected values for these channels.
    
    Test: ColorDetectorTest
    Fixes: b/160157770
    Change-Id: I54492b24eb59417cd5fa90390cdaf3427fd33de8

--- src/main/kotlin/androidx/build/AndroidXUiPlugin.kt
+++ src/main/kotlin/androidx/build/AndroidXUiPlugin.kt
@@ -155,6 +155,8 @@ class AndroidXUiPlugin : Plugin<Project> {
                     error("ComposableLambdaParameterPosition")
                     error("CompositionLocalNaming")
                     error("ComposableModifierFactory")
+                    error("InvalidColorHexValue")
+                    error("MissingColorAlphaChannel")
                     error("ModifierFactoryReturnType")
                     error("ModifierFactoryExtensionFunction")
                     error("ModifierParameter")
