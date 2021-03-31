b1920cc2200cb105da0388fc43a43ec6b91b14c7
Rahul Ravikumar <rahulrav@google.com>
2021-03-30 17:30:33 -0700

    WorkManager 2.6.0-alpha02.
    Test: N/A
    Merged-In: I67a9f2da83910b5df1589f1bdbf7fbd52f35acf3
    
    Change-Id: I21864f349319f70de6c3a00ce24a9dce7f1657a4

--- src/main/kotlin/androidx/build/LibraryVersions.kt
+++ src/main/kotlin/androidx/build/LibraryVersions.kt
@@ -149,5 +149,5 @@ object LibraryVersions {
     val WINDOW = Version("1.0.0-alpha06")
     val WINDOW_EXTENSIONS = Version("1.0.0-alpha01")
     val WINDOW_SIDECAR = Version("0.1.0-alpha01")
-    val WORK = Version("2.6.0-alpha01")
+    val WORK = Version("2.6.0-alpha02")
 }
