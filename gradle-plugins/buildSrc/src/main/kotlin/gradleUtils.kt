a5edea7f950484f487c2c07ba51d815266dc8561
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2021-04-01 10:43:37 +0300

    Update copyright notices

--- gradle-plugins/buildSrc/src/main/kotlin/gradleUtils.kt
+++ gradle-plugins/buildSrc/src/main/kotlin/gradleUtils.kt
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2020-2021 JetBrains s.r.o. and respective authors and developers.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
+ */
+
 import org.gradle.api.Project
 
 inline fun <reified T> Project.configureIfExists(fn: T.() -> Unit) {
