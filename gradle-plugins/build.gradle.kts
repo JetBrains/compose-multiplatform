DetachHead/compose-jb

Split Maven artifact configuration and Gradle plugin configuration
This is needed to introduce non-Gradle plugin modules under gradle-plugins build for
Compose preview runtime.

gradle-plugins/build.gradle.kts
@@ -48,20 +48,28 @@ subprojects {
     }
 
     afterEvaluate {
-        gradlePluginConfig?.let { configureGradlePlugin(it) }
+        publicationConfig?.let { mavenPublicationConfig ->
+            configurePublication(mavenPublicationConfig)
+
+            gradlePluginConfig?.let { gradlePluginConfig ->
+                configureGradlePlugin(mavenPublicationConfig, gradlePluginConfig)
+            }
+        }
     }
 }
 
-fun Project.configureGradlePlugin(config: GradlePluginConfigExtension) {
+fun Project.configurePublication(
+    publicationConfig: PublicationConfigExtension
+) {
     // maven publication for plugin
     configureIfExists<PublishingExtension> {
         // pluginMaven is a default publication created by java-gradle-plugin
         // https://github.com/gradle/gradle/issues/10384
         publications.create<MavenPublication>("pluginMaven") {
-            artifactId = config.artifactId
+            artifactId = publicationConfig.artifactId
             pom {
-                name.set(config.displayName)
-                description.set(config.description)
+                name.set(publicationConfig.displayName)
+                description.set(publicationConfig.description)
                 url.set(BuildProperties.website)
                 licenses {
                     license {
@@ -72,22 +80,27 @@ fun Project.configureGradlePlugin(config: GradlePluginConfigExtension) {
             }
         }
     }
+}
 
+fun Project.configureGradlePlugin(
+    publicationConfig: PublicationConfigExtension,
+    gradlePluginConfig: GradlePluginConfigExtension
+) {
     // metadata for gradle plugin portal (relates to pluginBundle extension block from com.gradle.plugin-publish)
     configureIfExists<PluginBundleExtension> {
         vcsUrl = BuildProperties.vcs
         website = BuildProperties.website
-        description = config.description
+        description = publicationConfig.description
     }
 
     // gradle plugin definition (relates to gradlePlugin extension block from java-gradle-plugin)
     configureIfExists<GradlePluginDevelopmentExtension> {
         plugins {
             create("gradlePlugin") {
-                id = config.pluginId
-                displayName = config.displayName
-                description = config.description
-                implementationClass = config.implementationClass
+                id = gradlePluginConfig.pluginId
+                displayName = publicationConfig.displayName
+                description = publicationConfig.description
+                implementationClass = gradlePluginConfig.implementationClass
                 version = project.version
             }
         }

JetBrains/compose-multiplatform

gradle-plugins/build.gradle.kts
@@ -48,20 +48,28 @@ subprojects {
     }
 
     afterEvaluate {
-        gradlePluginConfig?.let { configureGradlePlugin(it) }
+        mavenPublicationConfig?.let { mavenPublicationConfig ->
+            configurePublication(mavenPublicationConfig)
+
+            gradlePluginConfig?.let { gradlePluginConfig ->
+                configureGradlePlugin(mavenPublicationConfig, gradlePluginConfig)
+            }
+        }
     }
 }
 
-fun Project.configureGradlePlugin(config: GradlePluginConfigExtension) {
+fun Project.configurePublication(
+    publicationConfig: MavenPublicationConfigExtension
+) {
     // maven publication for plugin
     configureIfExists<PublishingExtension> {
         // pluginMaven is a default publication created by java-gradle-plugin
         // https://github.com/gradle/gradle/issues/10384
         publications.create<MavenPublication>("pluginMaven") {
-            artifactId = config.artifactId
+            artifactId = publicationConfig.artifactId
             pom {
-                name.set(config.displayName)
-                description.set(config.description)
+                name.set(publicationConfig.displayName)
+                description.set(publicationConfig.description)
                 url.set(BuildProperties.website)
                 licenses {
                     license {
@@ -72,22 +80,27 @@ fun Project.configureGradlePlugin(config: GradlePluginConfigExtension) {
             }
         }
     }
+}
 
+fun Project.configureGradlePlugin(
+    publicationConfig: MavenPublicationConfigExtension,
+    gradlePluginConfig: GradlePluginConfigExtension
+) {
     // metadata for gradle plugin portal (relates to pluginBundle extension block from com.gradle.plugin-publish)
     configureIfExists<PluginBundleExtension> {
         vcsUrl = BuildProperties.vcs
         website = BuildProperties.website
-        description = config.description
+        description = publicationConfig.description
     }
 
     // gradle plugin definition (relates to gradlePlugin extension block from java-gradle-plugin)
     configureIfExists<GradlePluginDevelopmentExtension> {
         plugins {
             create("gradlePlugin") {
-                id = config.pluginId
-                displayName = config.displayName
-                description = config.description
-                implementationClass = config.implementationClass
+                id = gradlePluginConfig.pluginId
+                displayName = publicationConfig.displayName
+                description = publicationConfig.description
+                implementationClass = gradlePluginConfig.implementationClass
                 version = project.version
             }
         }