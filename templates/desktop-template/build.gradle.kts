cf9ccdb6179c32eb7a98b661efec12cebe2b21e7
Nikolay Igotti <igotti@gmail.com>
2021-04-06 09:32:56 +0300

    Switch templates to 0.4.0-build179

--- templates/desktop-template/build.gradle.kts
+++ templates/desktop-template/build.gradle.kts
@@ -3,9 +3,9 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     // __KOTLIN_COMPOSE_VERSION__
-    kotlin("jvm") version "1.4.31"
+    kotlin("jvm") version "1.4.32"
     // __LATEST_COMPOSE_RELEASE_VERSION__
-    id("org.jetbrains.compose") version (System.getenv("COMPOSE_TEMPLATE_COMPOSE_VERSION") ?: "0.3.2")
+    id("org.jetbrains.compose") version (System.getenv("COMPOSE_TEMPLATE_COMPOSE_VERSION") ?: "0.4.0-build179")
 }
 
 repositories {
--- templates/multiplatform-template/build.gradle.kts
+++ templates/multiplatform-template/build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     // __LATEST_COMPOSE_RELEASE_VERSION__
-    val composeVersion = System.getenv("COMPOSE_TEMPLATE_COMPOSE_VERSION") ?: "0.3.2"
+    val composeVersion = System.getenv("COMPOSE_TEMPLATE_COMPOSE_VERSION") ?: "0.4.0-build179"
 
     repositories {
         // TODO: remove after new build is published
@@ -14,7 +14,7 @@ buildscript {
         classpath("org.jetbrains.compose:compose-gradle-plugin:$composeVersion")
         classpath("com.android.tools.build:gradle:4.0.1")
         // __KOTLIN_COMPOSE_VERSION__
-        classpath(kotlin("gradle-plugin", version = "1.4.31"))
+        classpath(kotlin("gradle-plugin", version = "1.4.32"))
     }
 }
 
