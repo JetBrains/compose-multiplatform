2f0cd93e7170c3533617b6eb198bf7d8497760d9
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2021-04-06 11:13:31 +0300

    Specify Preview as entry point
    So it's not marked as dead code by Intellij

--- idea-plugin/src/main/kotlin/org/jetbrains/compose/desktop/idea/preview/PreviewRunLineMarkerContributor.kt
+++ idea-plugin/src/main/kotlin/org/jetbrains/compose/desktop/idea/preview/PreviewRunLineMarkerContributor.kt
@@ -1,6 +1,17 @@
 /*
- * Copyright 2020-2021 JetBrains s.r.o. and respective authors and developers.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.jetbrains.compose.desktop.idea.preview
@@ -12,6 +23,11 @@ import com.intellij.psi.impl.source.tree.LeafPsiElement
 import org.jetbrains.kotlin.lexer.KtTokens
 import org.jetbrains.kotlin.psi.KtNamedFunction
 
+/**
+ * Based on
+ * com.android.tools.idea.compose.preview.runconfiguration.ComposePreviewRunLineMarkerContributor from AOSP
+ * with modifications
+ */
 class PreviewRunLineMarkerContributor : RunLineMarkerContributor() {
     override fun getInfo(element: PsiElement): Info? {
         // Marker should be in a single LeafPsiElement. We choose the identifier and return null for other elements within the function.
