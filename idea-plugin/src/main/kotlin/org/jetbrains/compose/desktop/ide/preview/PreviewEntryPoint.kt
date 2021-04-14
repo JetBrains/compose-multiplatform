f331cdd55d156d45e176bfa90fec8dc13a09e907
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2021-04-14 10:29:30 +0300

    Prepare Intellij plugin for publishing

--- idea-plugin/src/main/kotlin/org/jetbrains/compose/desktop/ide/preview/PreviewEntryPoint.kt
+++ idea-plugin/src/main/kotlin/org/jetbrains/compose/desktop/ide/preview/PreviewEntryPoint.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.jetbrains.compose.desktop.idea.preview
+package org.jetbrains.compose.desktop.ide.preview
 
 import com.intellij.codeInspection.reference.EntryPoint
 import com.intellij.codeInspection.reference.RefElement
