5c15d0fb9bc121944254ac0bd8e0e9ac89c144c5
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2021-04-27 09:35:58 +0300

    ~ update example

--- idea-plugin/examples/desktop-project/build.gradle.kts
+++ idea-plugin/examples/desktop-project/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.compose.compose
 plugins {
     // __KOTLIN_COMPOSE_VERSION__
     kotlin("jvm") version "1.4.32"
-    id("org.jetbrains.compose") version "0.4.0-idea-preview-build57"
+    id("org.jetbrains.compose") version "0.4.0-new-preview"
 }
 
 repositories {
@@ -14,6 +14,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.4")
     implementation(compose.desktop.currentOs)
+    implementation("org.jetbrains.compose.ui:ui-tooling:0.4.0-new-preview")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 }
 
