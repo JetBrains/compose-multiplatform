ff07099ef0f576cb6b638d2526a851a3987a7c00
Jeff Gaston <jeffrygaston@google.com>
2021-04-29 12:18:49 -0400

    Switching androidx.sh from running lintDebug to lintRelease
    Progress towards no longer running any debug variants in androidx.sh
    
    Bug: 186569865
    Test: ./gradlew buildOnServer -m | grep lintDebug # and see there are no matches
    Test: Treehugger runs busytown/androidx.sh
    
    Change-Id: Ifdc87e2b3a17706136744fee839819d90eddbefe

--- private/src/main/kotlin/androidx/build/uptodatedness/TaskUpToDateValidator.kt
+++ private/src/main/kotlin/androidx/build/uptodatedness/TaskUpToDateValidator.kt
@@ -141,7 +141,7 @@ val ALLOW_RERUNNING_TASKS = setOf(
     ":external:libyuv:buildCMakeRelWithDebInfo[yuv]",
     ":hilt:hilt-navigation-compose:kaptGenerateStubsDebugKotlin",
     ":hilt:hilt-navigation-compose:kaptGenerateStubsReleaseKotlin",
-    ":lint-checks:integration-tests:copyDebugAndroidLintReports",
+    ":lint-checks:integration-tests:copyReleaseAndroidLintReports",
 
     // https://github.com/gradle/gradle/issues/17262
     ":doclava:compileJava",
@@ -180,13 +180,13 @@ val DONT_TRY_RERUNNING_TASKS = setOf(
 
     // We should be able to remove these entries when b/160392650 is fixed
     "lint",
-    "lintAnalyzeDebug",
-    "lintDebug",
+    "lintAnalyzeRelease",
+    "lintRelease",
     "lintVitalRelease",
-    "lintWithExpandProjectionDebug",
-    "lintWithoutExpandProjectionDebug",
-    "lintWithKaptDebug",
-    "lintWithKspDebug",
+    "lintWithExpandProjectionRelease",
+    "lintWithoutExpandProjectionRelease",
+    "lintWithKaptRelease",
+    "lintWithKspRelease",
 )
 
 abstract class TaskUpToDateValidator :
