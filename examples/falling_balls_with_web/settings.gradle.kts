6cdb5e1178dbf6e6317922b3746285be62428f3c
Nikolay Igotti <igotti@gmail.com>
2021-05-03 21:37:41 +0300

    Tweaks.

---
+++ examples/falling_balls_with_web/settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        mavenLocal()
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+    }
+
+}
+rootProject.name = "falling_balls_with_web"
+
