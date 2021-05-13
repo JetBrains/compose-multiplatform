ed495ea09e7cbe95dd5baea2ddf1b373ed1dfa48
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-05-13 11:34:32 +0200

    Bootrstrapping androix repositoy in lite Dockerfile done right

--- ci/docker/compose-web/bootstrap.sh
+++ ci/docker/compose-web/bootstrap.sh
@@ -1,9 +1,23 @@
-cd /androidx-main
-~/bin/repo init -u https://android.googlesource.com/platform/manifest \
-    -b androidx-main --depth=1 --partial-clone --clone-filter=blob:limit=10M
-~/bin/repo sync -c --no-tags --no-clone-bundle -j4
+#!/bin/bash
 
-mkdir -p /mnt/agent
-ln -sf /androidx-main/tools/ /mnt/agent/tools
-ln -sf /androidx-main/prebuilts/ /mnt/agent/prebuilts
-ln -sf /androidx-main/external /mnt/agent/external
+function bootstrap() {
+    cd /androidx-main
+    ~/bin/repo init -u https://android.googlesource.com/platform/manifest \
+        -b androidx-main --depth=1 --partial-clone --clone-filter=blob:limit=10M
+    ~/bin/repo sync -c --no-tags --no-clone-bundle -j4
+
+    mkdir -p /mnt/agent
+    ln -sf /androidx-main/tools/ /mnt/agent/tools
+    ln -sf /androidx-main/prebuilts/ /mnt/agent/prebuilts
+    ln -sf /androidx-main/external /mnt/agent/external
+}
+
+if [ ! -e /tmp/init ]; then
+    touch /tmp/init
+    echo "bootstrapping"
+    sleep 10
+else
+    echo "not bootstrapping, all done"
+fi
+
+/bin/bash
