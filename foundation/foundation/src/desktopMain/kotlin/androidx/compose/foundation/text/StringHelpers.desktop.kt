b3364d269fda67db078333869602ebe7395e45a0
Andrey Rudenko <andrei.rudenko@jetbrains.com>
2021-05-18 17:39:07 +0200

    Desktop: use skija's BreakIterator for characters
    Android implementation of BreakIterator for characters differs from OpenJDK implementation and is more correct. skija has an alternative implementation of BreakIterator. This CL switches desktop actual implementation of related functions to this implementation
    
    Change-Id: Iedec397ead4c023ad060ee29f04e918d938681d4
    Test: ./gradlew :compose:foundation:foundation:desktopTest
    Relnote: N/A

--- foundation/foundation/src/desktopMain/kotlin/androidx/compose/foundation/text/StringHelpers.desktop.kt
+++ foundation/foundation/src/desktopMain/kotlin/androidx/compose/foundation/text/StringHelpers.desktop.kt
@@ -16,5 +16,16 @@
 
 package androidx.compose.foundation.text
 
-internal actual fun StringBuilder.appendCodePointX(codePoint: Int): StringBuilder =
-    this.appendCodePoint(codePoint)
+import org.jetbrains.skija.BreakIterator
+
+internal actual fun String.findPrecedingBreak(index: Int): Int {
+    val it = BreakIterator.makeCharacterInstance()
+    it.setText(this)
+    return it.preceding(index)
+}
+
+internal actual fun String.findFollowingBreak(index: Int): Int {
+    val it = BreakIterator.makeCharacterInstance()
+    it.setText(this)
+    return it.following(index)
+}
