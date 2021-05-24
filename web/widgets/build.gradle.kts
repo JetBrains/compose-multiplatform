52034b64a990db85c9d4e21dbb2b188d4fe31855
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-05-24 23:30:40 +0200

    Add missing web-widgets build file

---
+++ web/widgets/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("kotlin-multiplatform")
+    id("org.jetbrains.compose")
+}
+
+
+kotlin {
+    jvm()
+    js(IR) {
+        browser() {
+            testTask {
+                testLogging.showStandardStreams = true
+                useKarma {
+                    useChromeHeadless()
+                    useFirefox()
+                }
+            }
+        }
+        binaries.executable()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-js"))
+                implementation(npm("css-typed-om", "0.4.0"))
+
+                implementation(project(":web-core"))
+            }
+        }
+
+        val jsTest by getting {
+            dependencies {
+                implementation(kotlin("test-js"))
+            }
+        }
+
+        val jvmMain by getting {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+            }
+        }
+    }
+}
