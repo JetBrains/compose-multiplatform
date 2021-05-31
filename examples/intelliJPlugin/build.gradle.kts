7b1068c1bc6594181bb73607ca79632cf1e9c080
Nikolay Igotti <igotti@gmail.com>
2021-05-31 14:03:24 +0300

    Move IDE demo to 2021.1

--- examples/intelliJPlugin/build.gradle.kts
+++ examples/intelliJPlugin/build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version = "2020.3.2"
+    version = "2021.1"
 }
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
     changeNotes("""
