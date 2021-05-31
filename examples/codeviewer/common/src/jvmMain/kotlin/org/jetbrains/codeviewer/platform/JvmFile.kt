3daa8c0b0d9c2e62d66c1776884db1db4ffdbc4d
Nikolay Igotti <igotti@gmail.com>
2021-05-31 13:47:59 +0300

    Fix deprecations.

--- examples/codeviewer/common/src/jvmMain/kotlin/org/jetbrains/codeviewer/platform/JvmFile.kt
+++ examples/codeviewer/common/src/jvmMain/kotlin/org/jetbrains/codeviewer/platform/JvmFile.kt
@@ -94,7 +94,7 @@ private fun java.io.File.readLinePositions(
                 if (isBeginOfLine) {
                     starts.add(position.toInt())
                 }
-                isBeginOfLine = byte.toChar() == '\n'
+                isBeginOfLine = Char(byte.toInt()) == '\n'
                 position++
             }
             channel.close()
