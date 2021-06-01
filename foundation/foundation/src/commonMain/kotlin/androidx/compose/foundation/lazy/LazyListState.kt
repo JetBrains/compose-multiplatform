ffd2783ca08ffc7bcfca5217b33db9d56c817fce
Andrey Kulikov <andreykulikov@google.com>
2021-06-01 15:51:19 +0100

    Move LazyListScrollPosition into a separate file
    No changes to the class content in this CL.
    
    Test: run tests in lazy package
    Change-Id: Ibded195249586ef0f1b2a2d60d7714f6dc6d6f30

--- foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/lazy/LazyListState.kt
+++ foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/lazy/LazyListState.kt
@@ -301,58 +301,6 @@ class LazyListState constructor(
     }
 }
 
-/**
- * Contains the current scroll position represented by the first visible item index and the first
- * visible item scroll offset.
- *
- * Allows reading the values without recording the state read: [index] and [scrollOffset].
- * And with recording the state read which makes such reads observable: [observableIndex] and
- * [observableScrollOffset]. It is important to not record the state read inside the measure
- * block as otherwise the extra remeasurement will be scheduled once we update the values in the
- * end of the measure block.
- */
-internal class LazyListScrollPosition(
-    initialIndex: Int = 0,
-    initialScrollOffset: Int = 0
-) {
-    var index = DataIndex(initialIndex)
-        private set
-
-    var scrollOffset = initialScrollOffset
-        private set
-
-    private val indexState = mutableStateOf(index.value)
-    val observableIndex get() = indexState.value
-
-    private val scrollOffsetState = mutableStateOf(scrollOffset)
-    val observableScrollOffset get() = scrollOffsetState.value
-
-    private var hadFirstNotEmptyLayout = false
-
-    fun update(measureResult: LazyListMeasureResult) {
-        // we ignore the index and offset from measureResult until we get at least one
-        // measurement with real items. otherwise the initial index and scroll passed to the
-        // state would be lost and overridden with zeros.
-        if (hadFirstNotEmptyLayout || measureResult.totalItemsCount > 0) {
-            hadFirstNotEmptyLayout = true
-            update(measureResult.firstVisibleItemIndex, measureResult.firstVisibleItemScrollOffset)
-        }
-    }
-
-    fun update(index: DataIndex, scrollOffset: Int) {
-        require(index.value >= 0f) { "Index should be non-negative (${index.value})" }
-        require(scrollOffset >= 0f) { "scrollOffset should be non-negative ($scrollOffset)" }
-        if (index != this.index) {
-            this.index = index
-            indexState.value = index.value
-        }
-        if (scrollOffset != this.scrollOffset) {
-            this.scrollOffset = scrollOffset
-            scrollOffsetState.value = scrollOffset
-        }
-    }
-}
-
 private object EmptyLazyListLayoutInfo : LazyListLayoutInfo {
     override val visibleItemsInfo = emptyList<LazyListItemInfo>()
     override val viewportStartOffset = 0
