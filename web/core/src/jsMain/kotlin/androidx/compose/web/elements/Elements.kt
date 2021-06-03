89247bdf746d419f1f40a7667b525c58cdff10d9
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2021-06-03 12:29:37 +0200

    Remove the scope limitation
    option could also be placed inside opt group, datalist and select

--- web/core/src/jsMain/kotlin/androidx/compose/web/elements/Elements.kt
+++ web/core/src/jsMain/kotlin/androidx/compose/web/elements/Elements.kt
@@ -249,7 +249,7 @@ fun Select(
 )
 
 @Composable
-fun DOMScope<HTMLSelectElement>.Option(
+fun Option(
     value: String,
     attrs: AttrBuilderContext<Tag.Option> = {},
     content: ContentBuilder<HTMLOptionElement>? = null
