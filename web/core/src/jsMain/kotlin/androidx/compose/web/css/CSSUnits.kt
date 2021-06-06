251359b08780dedbd5b7a2b396ca8ea332f3c058
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-06-06 23:25:44 +0200

    CSSUnit is a sealed interface

--- web/core/src/jsMain/kotlin/androidx/compose/web/css/CSSUnits.kt
+++ web/core/src/jsMain/kotlin/androidx/compose/web/css/CSSUnits.kt
@@ -6,40 +6,173 @@ interface CSSUnitValue : CSSNumericValue {
     fun asString(): String = "${value}${unit.value}"
 }
 
-sealed class CSSUnit(val value: String) {
-    object percent: CSSUnit("percent")
-    object em: CSSUnit("em")
-    object ex: CSSUnit("ex")
-    object ch: CSSUnit("ch")
-    object ic: CSSUnit("ic")
-    object rem: CSSUnit("rem")
-    object lh: CSSUnit("lh")
-    object rlh: CSSUnit("rlh")
-    object vw: CSSUnit("vw")
-    object vh: CSSUnit("vh")
-    object vi: CSSUnit("vi")
-    object vb: CSSUnit("vb")
-    object vmin: CSSUnit("vmin")
-    object vmax: CSSUnit("vmax")
-    object cm: CSSUnit("cm")
-    object mm: CSSUnit("mm")
-    object q: CSSUnit("q")
-    object pt: CSSUnit("pt")
-    object pc: CSSUnit("pc")
-    object px: CSSUnit("px")
-    object deg: CSSUnit("deg")
-    object grad: CSSUnit("grad")
-    object rad: CSSUnit("rad")
-    object turn: CSSUnit("turn")
-    object s: CSSUnit("s")
-    object ms: CSSUnit("ms")
-    object hz: CSSUnit("hz")
-    object khz: CSSUnit("khz")
-    object dpi: CSSUnit("dpi")
-    object dpcm: CSSUnit("dpcm")
-    object dppx: CSSUnit("dppx")
-    object fr: CSSUnit("fr")
-    object number: CSSUnit("number")
+sealed interface CSSUnit {
+    val value: String
+
+    object percent: CSSUnit {
+        override val value: String
+            get() = "percent"
+    }
+
+    object em: CSSUnit {
+        override val value: String
+            get() = "em"
+    }
+
+    object ex: CSSUnit {
+        override val value: String
+            get() = "ex"
+    }
+
+    object ch: CSSUnit {
+        override val value: String
+            get() = "ch"
+    }
+
+    object ic: CSSUnit {
+        override val value: String
+            get() = "ic"
+    }
+
+    object rem: CSSUnit {
+        override val value: String
+            get() = "rem"
+    }
+
+    object lh: CSSUnit {
+        override val value: String
+            get() = "lh"
+    }
+
+    object rlh: CSSUnit {
+        override val value: String
+            get() = "rlh"
+    }
+
+    object vw: CSSUnit {
+        override val value: String
+            get() = "vw"
+    }
+
+    object vh: CSSUnit {
+        override val value: String
+            get() = "vh"
+    }
+
+    object vi: CSSUnit {
+        override val value: String
+            get() = "vi"
+    }
+
+    object vb: CSSUnit {
+        override val value: String
+            get() = "vb"
+    }
+
+    object vmin: CSSUnit {
+        override val value: String
+            get() = "vmin"
+    }
+
+    object vmax: CSSUnit {
+        override val value: String
+            get() = "vmax"
+    }
+
+    object cm: CSSUnit {
+        override val value: String
+            get() = "cm"
+    }
+
+    object mm: CSSUnit {
+        override val value: String
+            get() = "mm"
+    }
+
+    object q: CSSUnit {
+        override val value: String
+            get() = "q"
+    }
+
+    object pt: CSSUnit {
+        override val value: String
+            get() = "pt"
+    }
+
+    object pc: CSSUnit {
+        override val value: String
+            get() = "pc"
+    }
+
+    object px: CSSUnit {
+        override val value: String
+            get() = "px"
+    }
+
+    object deg: CSSUnit {
+        override val value: String
+            get() = "deg"
+    }
+
+    object grad: CSSUnit {
+        override val value: String
+            get() = "grad"
+    }
+
+    object rad: CSSUnit {
+        override val value: String
+            get() = "rad"
+    }
+
+    object turn: CSSUnit {
+        override val value: String
+            get() = "turn"
+    }
+
+    object s: CSSUnit {
+        override val value: String
+            get() = "s"
+    }
+
+    object ms: CSSUnit {
+        override val value: String
+            get() = "ms"
+    }
+
+    object hz: CSSUnit {
+        override val value: String
+            get() = "hz"
+    }
+
+    object khz: CSSUnit {
+        override val value: String
+            get() = "khz"
+    }
+
+    object dpi: CSSUnit {
+        override val value: String
+            get() = "dpi"
+    }
+
+    object dpcm: CSSUnit {
+        override val value: String
+            get() = "dpcm"
+    }
+
+    object dppx: CSSUnit {
+        override val value: String
+            get() = "dppx"
+    }
+
+    object fr: CSSUnit {
+        override val value: String
+            get() = "fr"
+    }
+
+    object number: CSSUnit {
+        override val value: String
+            get() = "number"
+    }
 }
 
 interface CSSRelValue
