a1cd5baa3effc005a69ac6e1ebf208dbe7b74c24
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-06-08 21:03:24 +0200

    Update examples to 0.5.0-build221

--- examples/web-getting-started/build.gradle.kts
+++ examples/web-getting-started/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("multiplatform") version "1.5.10"
-    id("org.jetbrains.compose") version "0.0.0-web-dev-14"
+    id("org.jetbrains.compose") version "0.5.0-build221"
 }
 
 repositories {
