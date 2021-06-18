faa61bf9b99e98b8c5d5f5f32868c5bd9d948913
Nader Jawad <njawad@google.com>
2021-06-17 23:41:58 -0700

    Improve consistency of gradients with transparent colors
    On O and later, transparent colors are interpolated
    in an elegant manner, ignoring the color channel for
    the transparent part. It makes for a smooth transition
    to and from transparent values.
    
    On N and earlier, the color channel of transparent
    values are also accounted for in the gradient. This
    means that Color.Transparent will "blacken" the gradient.
    
    This CL detects transparent colors in a gradient. When
    On the ends of the gradient, the proximate color is
    used instead of the transparent color, but with an
    alpha of 0. When in the middle of a gradient, the
    stop is duplicated, with the lower side matching
    the previous color, but the alpha 0, and the higher
    side matching the next color, but alpha 0. The stop
    for both transparent colors are identical.
    
    This works only for transparent colors and not translucent
    (0 < alpha < 1). A more complex solution must be used
    to approximate gradients with translucent values.
    
    Fixes: 190783390
    Test: Added GradientTest to verify color stop logic
    Change-Id: I64525313681350e4ed456a5bdb451ffcdde11d52

--- ui/ui-graphics/src/androidMain/kotlin/androidx/compose/ui/graphics/AndroidShader.android.kt
+++ ui/ui-graphics/src/androidMain/kotlin/androidx/compose/ui/graphics/AndroidShader.android.kt
@@ -20,7 +20,10 @@ import android.graphics.BitmapShader
 import android.graphics.LinearGradient
 import android.graphics.RadialGradient
 import android.graphics.SweepGradient
+import android.os.Build
+import androidx.annotation.VisibleForTesting
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.util.fastForEachIndexed
 
 actual typealias Shader = android.graphics.Shader
 
@@ -32,13 +35,14 @@ internal actual fun ActualLinearGradientShader(
     tileMode: TileMode
 ): Shader {
     validateColorStops(colors, colorStops)
+    val numTransparentColors = countTransparentColors(colors)
     return LinearGradient(
         from.x,
         from.y,
         to.x,
         to.y,
-        colors.toIntArray(),
-        colorStops?.toFloatArray(),
+        makeTransparentColors(colors, numTransparentColors),
+        makeTransparentStops(colorStops, colors, numTransparentColors),
         tileMode.toAndroidTileMode()
     )
 }
@@ -51,12 +55,13 @@ internal actual fun ActualRadialGradientShader(
     tileMode: TileMode
 ): Shader {
     validateColorStops(colors, colorStops)
+    val numTransparentColors = countTransparentColors(colors)
     return RadialGradient(
         center.x,
         center.y,
         radius,
-        colors.toIntArray(),
-        colorStops?.toFloatArray(),
+        makeTransparentColors(colors, numTransparentColors),
+        makeTransparentStops(colorStops, colors, numTransparentColors),
         tileMode.toAndroidTileMode()
     )
 }
@@ -67,11 +72,12 @@ internal actual fun ActualSweepGradientShader(
     colorStops: List<Float>?
 ): Shader {
     validateColorStops(colors, colorStops)
+    val numTransparentColors = countTransparentColors(colors)
     return SweepGradient(
         center.x,
         center.y,
-        colors.toIntArray(),
-        colorStops?.toFloatArray()
+        makeTransparentColors(colors, numTransparentColors),
+        makeTransparentStops(colorStops, colors, numTransparentColors),
     )
 }
 
@@ -87,8 +93,100 @@ internal actual fun ActualImageShader(
     )
 }
 
-private fun List<Color>.toIntArray(): IntArray =
-    IntArray(size) { i -> this[i].toArgb() }
+/**
+ * Returns the number of transparent (alpha = 0) values that aren't at the beginning or
+ * end of the gradient so that the color stops can be added. On O and newer devices,
+ * this always returns 0 because no stops need to be added.
+ */
+@VisibleForTesting
+internal fun countTransparentColors(colors: List<Color>): Int {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        return 0
+    }
+    var numTransparentColors = 0
+    // Don't count the first and last value because we don't add stops for those
+    for (i in 1 until colors.lastIndex) {
+        if (colors[i].alpha == 0f) {
+            numTransparentColors++
+        }
+    }
+    return numTransparentColors
+}
+
+/**
+ * There was a change in behavior between Android N and O with how
+ * transparent colors are interpolated with skia gradients. More specifically
+ * Android O treats all fully transparent colors the same regardless of the
+ * rgb channels, however, Android N and older releases interpolated between
+ * the color channels as well. Because Color.Transparent is transparent black,
+ * this would introduce some muddy colors as part of gradients with transparency
+ * for Android N and below.
+ * In order to make gradient rendering consistent and match the behavior of Android O+,
+ * detect whenever Color.Transparent is used and a stop matching the color of the previous
+ * value, but alpha = 0 is added and another stop at the same point with the same color
+ * as the following value, but with alpha = 0 is used.
+ */
+@VisibleForTesting
+internal fun makeTransparentColors(
+    colors: List<Color>,
+    numTransparentColors: Int
+): IntArray {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        // No change for Android O+, map the colors directly to their argb equivalent
+        return IntArray(colors.size) { i -> colors[i].toArgb() }
+    }
+    val values = IntArray(colors.size + numTransparentColors)
+    var valuesIndex = 0
+    val lastIndex = colors.lastIndex
+    colors.fastForEachIndexed { index, color ->
+        if (color.alpha == 0f) {
+            if (index == 0) {
+                values[valuesIndex++] = colors[1].copy(alpha = 0f).toArgb()
+            } else if (index == lastIndex) {
+                values[valuesIndex++] = colors[index - 1].copy(alpha = 0f).toArgb()
+            } else {
+                val previousColor = colors[index - 1]
+                values[valuesIndex++] = previousColor.copy(alpha = 0f).toArgb()
+
+                val nextColor = colors[index + 1]
+                values[valuesIndex++] = nextColor.copy(alpha = 0f).toArgb()
+            }
+        } else {
+            values[valuesIndex++] = color.toArgb()
+        }
+    }
+    return values
+}
+
+/**
+ * See [makeTransparentColors].
+ *
+ * On N and earlier devices that have transparent values, we must duplicate the color stops for
+ * fully transparent values so that the color value before and after can be interpolated.
+ */
+@VisibleForTesting
+internal fun makeTransparentStops(
+    stops: List<Float>?,
+    colors: List<Color>,
+    numTransparentColors: Int
+): FloatArray? {
+    if (numTransparentColors == 0) {
+        return stops?.toFloatArray()
+    }
+    val newStops = FloatArray(colors.size + numTransparentColors)
+    newStops[0] = stops?.get(0) ?: 0f
+    var newStopsIndex = 1
+    for (i in 1 until colors.lastIndex) {
+        val color = colors[i]
+        val stop = stops?.get(i) ?: i.toFloat() / colors.lastIndex
+        newStops[newStopsIndex++] = stop
+        if (color.alpha == 0f) {
+            newStops[newStopsIndex++] = stop
+        }
+    }
+    newStops[newStopsIndex] = stops?.get(colors.lastIndex) ?: 1f
+    return newStops
+}
 
 private fun validateColorStops(colors: List<Color>, colorStops: List<Float>?) {
     if (colorStops == null) {
