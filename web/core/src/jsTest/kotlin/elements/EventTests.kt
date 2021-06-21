f445a929d46ec12361209e66b5b0781928f9bfc5
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2021-06-21 13:30:10 +0200

    web: Add `Options` for `addEventListener`

--- web/core/src/jsTest/kotlin/elements/EventTests.kt
+++ web/core/src/jsTest/kotlin/elements/EventTests.kt
@@ -1,6 +1,9 @@
 package org.jetbrains.compose.web.core.tests
 
+import androidx.compose.runtime.RecomposeScope
+import androidx.compose.runtime.currentRecomposeScope
 import org.jetbrains.compose.web.attributes.InputType
+import org.jetbrains.compose.web.attributes.Options
 import org.jetbrains.compose.web.dom.Button
 import org.jetbrains.compose.web.dom.Input
 import org.jetbrains.compose.web.dom.TextArea
@@ -34,6 +37,165 @@ class EventTests {
         btn.dispatchEvent(MouseEvent("click"))
 
         assertTrue(handeled)
+    }
+
+    @Test
+    fun multipleEventListenersCanBeAddedAndUsed() = runTest {
+        var handled1 = false
+        var handled2 = false
+
+        composition {
+            Button(
+                {
+                    onClick { handled1 = true }
+                    onClick { handled2 = true }
+                }
+            ) {}
+        }
+
+        assertEquals(1, root.childElementCount)
+
+        val btn = root.firstChild as HTMLElement
+        btn.dispatchEvent(MouseEvent("click"))
+
+        assertTrue(handled1)
+        assertTrue(handled2)
+    }
+
+    @Test
+    fun multipleButtonClickHandled() = runTest {
+        var count = 0
+
+        composition {
+            Button(attrs = { onClick { count++ } })
+        }
+
+        assertEquals(1, root.childElementCount)
+
+        val btn = root.firstChild as HTMLElement
+        btn.dispatchEvent(MouseEvent("click"))
+        btn.dispatchEvent(MouseEvent("click"))
+        btn.dispatchEvent(MouseEvent("click"))
+
+        assertEquals(3, count)
+    }
+
+    @Test
+    fun buttonOnClickHandlerWithOnceOptionCalledOnlyOnce() = runTest {
+        var count = 0
+
+        composition {
+            Button(attrs = { onClick(options = Options(once = Options.BooleanValue.True)) { count++ } })
+        }
+
+        assertEquals(1, root.childElementCount)
+
+        val btn = root.firstChild as HTMLElement
+        btn.dispatchEvent(MouseEvent("click"))
+        btn.dispatchEvent(MouseEvent("click"))
+        btn.dispatchEvent(MouseEvent("click"))
+
+        assertEquals(1, count)
+    }
+
+    @Test
+    fun buttonOnClickHandlerWithOnceOptionCalledAgainAfterRecomposition() = runTest {
+        var count = 0
+        var composedTimes = 0
+
+        var recomposeScope: RecomposeScope? = null
+
+        composition {
+            recomposeScope = currentRecomposeScope
+            composedTimes++
+
+            Button(attrs = { onClick(options = Options(once = Options.BooleanValue.True)) { count++ } })
+        }
+
+        assertEquals(1, composedTimes)
+
+        assertEquals(1, root.childElementCount)
+
+        val btn = root.firstChild as HTMLElement
+        btn.dispatchEvent(MouseEvent("click"))
+        btn.dispatchEvent(MouseEvent("click"))
+        btn.dispatchEvent(MouseEvent("click"))
+
+        assertEquals(1, count)
+
+        recomposeScope!!.invalidate()
+        waitForAnimationFrame()
+
+        assertEquals(2, composedTimes)
+
+        btn.dispatchEvent(MouseEvent("click"))
+        btn.dispatchEvent(MouseEvent("click"))
+        btn.dispatchEvent(MouseEvent("click"))
+
+        assertEquals(2, count)
+
+    }
+
+    @Test
+    fun removedEventListenersGetNeverCalled() = runTest {
+        var count = 0
+        var composedTimes = 0
+
+        var recomposeScope: RecomposeScope? = null
+
+        composition {
+            recomposeScope = currentRecomposeScope
+            composedTimes++
+            Button(attrs = { onClick { count++ } })
+        }
+
+        assertEquals(1, composedTimes)
+
+        val btn = root.firstChild as HTMLElement
+
+        btn.dispatchEvent(MouseEvent("click"))
+        assertEquals(1, count)
+
+        // This will force the update, thus it will cause the listeners being removed/added:
+        recomposeScope!!.invalidate()
+        waitForAnimationFrame()
+        assertEquals(2, composedTimes)
+
+        assertEquals(1, count)
+
+        btn.dispatchEvent(MouseEvent("click"))
+        assertEquals(2, count, "Removed listener should never be called")
+    }
+
+    @Test
+    fun removedEventListenersWithNotDefaultOptionsGetNeverCalled() = runTest {
+        var count = 0
+        var composedTimes = 0
+
+        var recomposeScope: RecomposeScope? = null
+
+        composition {
+            recomposeScope = currentRecomposeScope
+            composedTimes++
+            Button(attrs = { onClick(Options(passive = Options.BooleanValue.True)) { count++ } })
+        }
+
+        assertEquals(1, composedTimes)
+
+        val btn = root.firstChild as HTMLElement
+
+        btn.dispatchEvent(MouseEvent("click"))
+        assertEquals(1, count)
+
+        // This will force the update, thus it will cause the listeners being removed/added:
+        recomposeScope!!.invalidate()
+        waitForAnimationFrame()
+        assertEquals(2, composedTimes)
+
+        assertEquals(1, count)
+
+        btn.dispatchEvent(MouseEvent("click"))
+        assertEquals(2, count, "Removed listener should never be called")
     }
 
     @Test
