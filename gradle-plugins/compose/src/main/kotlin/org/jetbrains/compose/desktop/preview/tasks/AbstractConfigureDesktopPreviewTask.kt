7d2d68679592a694ef253374c29c884c3461b8c1
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2021-06-22 14:08:10 +0300

    Try configuration cache
    Spoiler: does not work with preview task yet

--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/desktop/preview/tasks/AbstractConfigureDesktopPreviewTask.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/desktop/preview/tasks/AbstractConfigureDesktopPreviewTask.kt
@@ -4,14 +4,13 @@ import org.gradle.api.file.FileCollection
 import org.gradle.api.logging.Logger as GradleLogger
 import org.gradle.api.provider.ListProperty
 import org.gradle.api.provider.Property
+import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.*
 import org.jetbrains.compose.ComposeBuildConfig
 import org.jetbrains.compose.desktop.application.internal.javaExecutable
 import org.jetbrains.compose.desktop.application.internal.notNullProperty
 import org.jetbrains.compose.desktop.tasks.AbstractComposeDesktopTask
 import org.jetbrains.compose.desktop.ui.tooling.preview.rpc.*
-import org.jetbrains.compose.internal.intProperty
-import org.jetbrains.compose.internal.stringProperty
 import java.io.File
 
 abstract class AbstractConfigureDesktopPreviewTask : AbstractComposeDesktopTask() {
@@ -28,29 +27,37 @@ abstract class AbstractConfigureDesktopPreviewTask : AbstractComposeDesktopTask(
     @get:Optional
     internal val jvmArgs: ListProperty<String> = objects.listProperty(String::class.java)
 
+    @get:Input
+    internal val previewTarget: Provider<String> =
+        project.providers.gradleProperty("compose.desktop.preview.target")
+
+    @get:Input
+    internal val idePort: Provider<String>  =
+        project.providers.gradleProperty("compose.desktop.preview.ide.port")
+
+    @get:InputFiles
+    internal val hostClasspath = project.configurations.detachedConfiguration(
+        project.dependencies.create("org.jetbrains.compose:preview-rpc:${ComposeBuildConfig.VERSION}")
+    )
+
     @TaskAction
     fun run() {
-        val previewTarget = project.stringProperty("compose.desktop.preview.target")
-        val port = project.intProperty("compose.desktop.preview.ide.port")
-        val hostClasspath = project.configurations.detachedConfiguration(
-            project.dependencies.create("org.jetbrains.compose:preview-rpc:${ComposeBuildConfig.VERSION}")
-        ).files
         val hostConfig = PreviewHostConfig(
                 javaExecutable = javaExecutable(javaHome.get()),
-                hostClasspath = hostClasspath.pathString()
+                hostClasspath = hostClasspath.files.pathString()
             )
         val previewClasspathString = previewClasspath.files.pathString()
 
         val gradleLogger = logger
         val previewLogger = GradlePreviewLoggerAdapter(gradleLogger)
 
-        val connection = getLocalConnectionOrNull(port, previewLogger, onClose = {})
+        val connection = getLocalConnectionOrNull(idePort.get().toInt(), previewLogger, onClose = {})
         if (connection != null) {
             connection.use {
                 connection.sendConfigFromGradle(
                     hostConfig,
                     previewClasspath = previewClasspathString,
-                    previewFqName = previewTarget
+                    previewFqName = previewTarget.get()
                 )
             }
         } else {
