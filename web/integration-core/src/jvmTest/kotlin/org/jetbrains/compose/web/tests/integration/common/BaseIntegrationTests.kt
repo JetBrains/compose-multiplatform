1cbfd497f685d8c495cfc63adebbce17f1e0dcd5
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-06-23 00:06:44 +0200

    WithChromDriver is no more

--- web/integration-core/src/jvmTest/kotlin/org/jetbrains/compose/web/tests/integration/common/BaseIntegrationTests.kt
+++ web/integration-core/src/jvmTest/kotlin/org/jetbrains/compose/web/tests/integration/common/BaseIntegrationTests.kt
@@ -1,15 +1,33 @@
 package org.jetbrains.compose.web.tests.integration.common
 
 import org.junit.jupiter.api.extension.ExtendWith
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.remote.RemoteWebDriver
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
+
+
+private val PATH = "http://localhost:${ServerLauncher.port}"
+
+fun WebDriver.openTestPage(test: String) {
+    get("$PATH?test=$test")
+}
+
+fun WebDriver.waitTextToBe(textId: String = "txt", value: String) {
+    WebDriverWait(this, 1).until(ExpectedConditions.textToBe(By.id(textId), value))
+}
 
 @ExtendWith(value = [StaticServerSetupExtension::class])
 abstract class BaseIntegrationTests {
 
-    companion object : WithChromeDriver {
-        override val driver: RemoteWebDriver = ChromeDriver(
+    fun openTestPage(test: String) = driver.openTestPage(test)
+    fun waitTextToBe(textId: String = "txt", value: String)  = driver.waitTextToBe(textId, value)
+
+    companion object  {
+        val driver: RemoteWebDriver = ChromeDriver(
             ChromeOptions().apply {
                 setHeadless(true)
                 addArguments("--no-sandbox")
