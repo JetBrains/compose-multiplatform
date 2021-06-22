ab0350ff43bf4ff622c2c1a7161369826e8fc932
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-06-23 00:57:26 +0200

    Introduce lazy driver configurations for Safari and Firefox (but not use them anywhere so far)

--- web/integration-core/src/jvmTest/kotlin/org/jetbrains/compose/web/tests/integration/common/BaseIntegrationTests.kt
+++ web/integration-core/src/jvmTest/kotlin/org/jetbrains/compose/web/tests/integration/common/BaseIntegrationTests.kt
@@ -5,10 +5,12 @@ import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxOptions
 import org.openqa.selenium.remote.RemoteWebDriver
+import org.openqa.selenium.safari.SafariDriver
 import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
-
 
 private val PATH = "http://localhost:${ServerLauncher.port}"
 
@@ -21,16 +23,30 @@ fun WebDriver.waitTextToBe(textId: String = "txt", value: String) {
 }
 
 object Drivers {
-    val Chrome = ChromeDriver(
-        ChromeOptions().apply {
-            setHeadless(true)
-            addArguments("--no-sandbox")
-        }
-    )
+    val Chrome by lazy {
+        ChromeDriver(
+            ChromeOptions().apply {
+                setHeadless(true)
+                addArguments("--no-sandbox")
+            }
+        )
+    }
+
+    val Firefox by lazy {
+        FirefoxDriver(
+            FirefoxOptions().apply {
+                setHeadless(true)
+            }
+        )
+    }
+
+    val Safari by lazy {
+        SafariDriver()
+    }
 }
 
 @ExtendWith(value = [StaticServerSetupExtension::class])
 abstract class BaseIntegrationTests(val driver: RemoteWebDriver) {
     fun openTestPage(test: String) = driver.openTestPage(test)
-    fun waitTextToBe(textId: String = "txt", value: String)  = driver.waitTextToBe(textId, value)
+    fun waitTextToBe(textId: String = "txt", value: String) = driver.waitTextToBe(textId, value)
 }
