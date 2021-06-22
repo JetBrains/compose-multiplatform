cd668af0ca5afdd55d478ee77f33210812793679
stevebower <stevebower@google.com>
2021-06-22 15:13:28 +0100

    Initial modules for macrobenchmark of Wear Compose components. Test: androidx.wear.compose.integration.macrobenchmark.test Bug: 187407581
    Change-Id: Iebe052e1f13d271ae7b41b98a8bc6239cd0025c3

--- src/main/kotlin/androidx/build/dependencyTracker/AffectedModuleDetector.kt
+++ src/main/kotlin/androidx/build/dependencyTracker/AffectedModuleDetector.kt
@@ -507,6 +507,10 @@ class AffectedModuleDetectorImpl constructor(
                 ":emoji2:integration-tests:init-enabled-macrobenchmark",
                 ":emoji2:integration-tests:init-enabled-macrobenchmark-target",
             ),
+            setOf(
+                ":wear:compose:integration-tests:macrobenchmark",
+                ":wear:compose:integration-tests:macrobenchmark-target"
+            ),
         )
 
         private val IGNORED_PATHS = setOf(
