EyuCoder/compose-jb

web: Inputs refactoring (wip)

web/core/src/jsMain/kotlin/androidx/compose/web/attributes/Attrs.kt
@@ -97,6 +97,9 @@ fun AttrsBuilder<HTMLFormElement>.target(value: FormTarget) =
 fun AttrsBuilder<HTMLInputElement>.type(value: InputType) =
     attr("type", value.typeStr)
 
+fun AttrsBuilder<HTMLInputElement>.type(value: InputType2<*>) =
+    attr("type", value.typeStr)
+
 fun AttrsBuilder<HTMLInputElement>.accept(value: String) =
     attr("accept", value) // type: file only
 

web/core/src/jsMain/kotlin/androidx/compose/web/elements/Elements.kt
@@ -2,19 +2,11 @@ package org.jetbrains.compose.web.dom
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ComposeNode
+import androidx.compose.web.attributes.InputAttrsBuilder
 import org.jetbrains.compose.web.DomApplier
 import org.jetbrains.compose.web.DomNodeWrapper
-import org.jetbrains.compose.web.attributes.AttrsBuilder
-import org.jetbrains.compose.web.attributes.InputType
-import org.jetbrains.compose.web.attributes.action
-import org.jetbrains.compose.web.attributes.alt
-import org.jetbrains.compose.web.attributes.forId
-import org.jetbrains.compose.web.attributes.href
-import org.jetbrains.compose.web.attributes.label
-import org.jetbrains.compose.web.attributes.src
-import org.jetbrains.compose.web.attributes.type
-import org.jetbrains.compose.web.attributes.value
 import kotlinx.browser.document
+import org.jetbrains.compose.web.attributes.*
 import org.w3c.dom.HTMLAnchorElement
 import org.w3c.dom.HTMLAreaElement
 import org.w3c.dom.HTMLAudioElement
@@ -353,6 +345,23 @@ fun A(
             attrs()
         },
         content = content
+    )
+}
+
+@Composable
+fun <K> Input(
+    type: InputType2<K>,
+    attrs: InputAttrsBuilder<K>.() -> Unit = {}
+) {
+    TagElement(
+        elementBuilder = ElementBuilder.Input,
+        applyAttrs = {
+            val iab = InputAttrsBuilder(type)
+            iab.type(type)
+            iab.attrs()
+            this.copyFrom(iab)
+        },
+        content = null
     )
 }
 

JetBrains/compose-multiplatform

web/core/src/jsMain/kotlin/androidx/compose/web/attributes/Attrs.kt
@@ -94,7 +94,7 @@ fun AttrsBuilder<HTMLFormElement>.target(value: FormTarget) =
 
 /* Input attributes */
 
-fun AttrsBuilder<HTMLInputElement>.type(value: InputType) =
+fun AttrsBuilder<HTMLInputElement>.type(value: InputType<*>) =
     attr("type", value.typeStr)
 
 fun AttrsBuilder<HTMLInputElement>.accept(value: String) =

web/core/src/jsMain/kotlin/androidx/compose/web/elements/Elements.kt
@@ -2,19 +2,12 @@ package org.jetbrains.compose.web.dom
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ComposeNode
+import androidx.compose.web.attributes.InputAttrsBuilder
+import androidx.compose.web.attributes.TextAreaAttrsBuilder
 import org.jetbrains.compose.web.DomApplier
 import org.jetbrains.compose.web.DomNodeWrapper
-import org.jetbrains.compose.web.attributes.AttrsBuilder
-import org.jetbrains.compose.web.attributes.InputType
-import org.jetbrains.compose.web.attributes.action
-import org.jetbrains.compose.web.attributes.alt
-import org.jetbrains.compose.web.attributes.forId
-import org.jetbrains.compose.web.attributes.href
-import org.jetbrains.compose.web.attributes.label
-import org.jetbrains.compose.web.attributes.src
-import org.jetbrains.compose.web.attributes.type
-import org.jetbrains.compose.web.attributes.value
 import kotlinx.browser.document
+import org.jetbrains.compose.web.attributes.*
 import org.w3c.dom.HTMLAnchorElement
 import org.w3c.dom.HTMLAreaElement
 import org.w3c.dom.HTMLAudioElement
@@ -359,19 +352,32 @@ fun A(
 }
 
 @Composable
-fun Input(
-    type: InputType = InputType.Text,
-    value: String = "",
-    attrs: AttrBuilderContext<HTMLInputElement>? = null
+fun <K> Input(
+    type: InputType<K>,
+    attrs: (InputAttrsBuilder<K>.() -> Unit)? = null
 ) {
     TagElement(
         elementBuilder = ElementBuilder.Input,
         applyAttrs = {
-            type(type)
-            value(value)
+            val iab = InputAttrsBuilder(type)
+            iab.type(type)
             if (attrs != null) {
-                attrs()
+                iab.attrs()
             }
+            this.copyFrom(iab)
+        },
+        content = null
+    )
+}
+
+@Composable
+fun <K> Input(type: InputType<K>) {
+    TagElement(
+        elementBuilder = ElementBuilder.Input,
+        applyAttrs = {
+            val iab = InputAttrsBuilder(type)
+            iab.type(type)
+            this.copyFrom(iab)
         },
         content = null
     )
@@ -569,15 +575,17 @@ fun Section(
 
 @Composable
 fun TextArea(
-    attrs: AttrBuilderContext<HTMLTextAreaElement>? = null,
+    attrs: (TextAreaAttrsBuilder.() -> Unit)? = null,
     value: String
 ) = TagElement(
     elementBuilder = ElementBuilder.TextArea,
     applyAttrs = {
-        value(value)
+        val  taab = TextAreaAttrsBuilder()
         if (attrs != null) {
-            attrs()
+            taab.attrs()
         }
+        taab.value(value)
+        this.copyFrom(taab)
     }
 ) {
     Text(value)