44a490beba7b58fc9ed7e1b675599946b2a2ee6a
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2021-07-07 11:51:17 +0200

    web: Remove deprecated input event listeners

--- web/core/src/jsMain/kotlin/androidx/compose/web/attributes/TextAreaAttrsBuilder.kt
+++ web/core/src/jsMain/kotlin/androidx/compose/web/attributes/TextAreaAttrsBuilder.kt
@@ -5,8 +5,8 @@
 
 package androidx.compose.web.attributes
 
-import org.jetbrains.compose.web.attributes.*
-import org.jetbrains.compose.web.events.WrappedTextInputEvent
+import org.jetbrains.compose.web.attributes.AttrsBuilder
+import org.jetbrains.compose.web.attributes.Options
 import org.w3c.dom.HTMLTextAreaElement
 
 class TextAreaAttrsBuilder : AttrsBuilder<HTMLTextAreaElement>() {
@@ -19,14 +19,5 @@ class TextAreaAttrsBuilder : AttrsBuilder<HTMLTextAreaElement>() {
             val text = it.nativeEvent.target.asDynamic().value.unsafeCast<String>()
             listener(SyntheticInputEvent(text, it.nativeEvent.target as HTMLTextAreaElement, it.nativeEvent))
         }
-    }
-
-    @Deprecated(
-        message = "It's not reliable as it can be applied to any input type.",
-        replaceWith = ReplaceWith("onInput(options, listener)"),
-        level = DeprecationLevel.WARNING
-    )
-    fun onTextInput(options: Options = Options.DEFAULT, listener: (WrappedTextInputEvent) -> Unit) {
-        listeners.add(TextInputEventListener(options, listener))
     }
 }
