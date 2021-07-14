f587ad57fe46532aefd4613911259615c979129e
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-07-14 20:35:50 +0200

    Remove unused casts from CSS API

--- web/core/src/jsMain/kotlin/org/jetbrains/compose/web/css/StylePropertyValue.kt
+++ web/core/src/jsMain/kotlin/org/jetbrains/compose/web/css/StylePropertyValue.kt
@@ -14,12 +14,6 @@ external interface StylePropertyString: StylePropertyValue
 inline fun StylePropertyValue(value: String): StylePropertyString = value.unsafeCast<StylePropertyString>()
 inline fun StylePropertyValue(value: Number): StylePropertyNumber = value.unsafeCast<StylePropertyNumber>()
 
-fun StylePropertyValue.asString(): String? = if (jsTypeOf(this) == "string") this.unsafeCast<String>() else null
-
-fun StylePropertyValue.asNumber(): Number? = if (jsTypeOf(this) == "number") this.unsafeCast<Number>() else null
-
-fun StylePropertyValue.asCSSStyleValue(): CSSStyleValue? = if (jsTypeOf(this) == "object") this.unsafeCast<CSSStyleValue>() else null
-
 external interface CSSStyleValue: StylePropertyValue {
     override fun toString(): String
 }
