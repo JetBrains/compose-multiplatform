54ffa395c8e8e8f604170accd5872540d7950d49
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-07-21 14:11:21 +0200

    Preparations for porting styles

--- examples/web_grid_animation/src/jsMain/kotlin/com/sample/Main.kt
+++ examples/web_grid_animation/src/jsMain/kotlin/com/sample/Main.kt
@@ -1,7 +1,7 @@
 package com.sample
 
 import androidx.compose.runtime.Composable
-import kotlinx.browser.window
+import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
 
@@ -16,38 +16,27 @@ fun Card(title: String, subTitle: String, imgUrl: String) {
     }
 }
 
+@Composable
+fun App() {
+    Card("IDEA", "3 out of every 4 Java developers choose IntelliJ IDEA", "/idea.svg")
+    Card("PyCharm", "The Python IDE for Professional Developers", "/pycharm.svg")
+    Card("PHPStorm", "The Lightning-Smart PHP IDE", "/phpstorm.svg")
+    Card("WebStorm", "The smartest JavaScript IDE", "/webstorm.png")
+    Card("RubyMine", "The most intelligent Ruby IDE", "/rubymine.svg")
+    Card("AppCode", "Smart IDE for iOS/macOS development", "/appcode.svg")
+    Card("GoLand", "Built specially for Go developers", "/goland.png")
+    Card("DataGrip", "Many databases, one tool", "/datagrip.png")
+    Card("Datalore", "The online data science notebook", "/datalore.png")
+    Card("Clion", "A cross-platform IDE for C and C++", "/clion.png")
+}
+
+object AppStyleSheet: StyleSheet() {
+}
+
 
 fun main() {
+    //Style(AppStyleSheet)
     renderComposable(rootElementId = "root") {
-        Card(
-            "IDEA",
-            "3 out of every 4 Java developers choose IntelliJ IDEA",
-            "/idea.svg"
-        )
-        Card(
-            "PyCharm",
-            "The Python IDE for Professional Developers",
-            "/pycharm.svg"
-        )
-        Card(
-            "PHPStorm",
-            "The Lightning-Smart PHP IDE",
-            "/phpstorm.svg"
-        )
-        Card("WebStorm", "The smartest JavaScript IDE", "/webstorm.png")
-        Card(
-            "RubyMine",
-            "The most intelligent Ruby IDE",
-            "/rubymine.svg"
-        )
-        Card(
-            "AppCode",
-            "Smart IDE for iOS/macOS development",
-            "/appcode.svg"
-        )
-        Card("GoLand", "Built specially for Go developers", "/goland.png")
-        Card("DataGrip", "Many databases, one tool", "/datagrip.png")
-        Card("Datalore", "The online data science notebook", "/datalore.png")
-        Card("Clion", "A cross-platform IDE for C and C++", "/clion.png")
+        App()
     }
 }
