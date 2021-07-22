2677dfa25d85e749a1a56d72c3fcfa81039b7c03
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2021-07-22 16:42:44 +0200

    Update WarnAboutComposeWithSerialization.kt
    Update the link for the issue

--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/internal/WarnAboutComposeWithSerialization.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/internal/WarnAboutComposeWithSerialization.kt
@@ -9,7 +9,7 @@ internal fun Project.checkAndWarnAboutComposeWithSerialization() {
             >>> COMPOSE WARNING
             >>> Project `${project.name}` has `compose` and `kotlinx.serialization` plugins applied!
             >>> Consider using these plugins in separate modules to avoid compilation errors
-            >>> Check more details here: https://github.com/JetBrains/compose-jb
+            >>> Check more details here: https://github.com/JetBrains/compose-jb/issues/738
 
         """.trimIndent()
 
