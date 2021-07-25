266e94363d82fa4068411a3779fae61d1cc5c121
andb3 <andb3003@yahoo.com>
2021-07-25 13:13:59 -0400

    Added alpha value to color

--- web/widgets/src/commonMain/kotlin/Color.kt
+++ web/widgets/src/commonMain/kotlin/Color.kt
@@ -1,18 +1,19 @@
 package org.jetbrains.compose.common.core.graphics
 
-public data class Color(val red: Int, val green: Int, val blue: Int) {
+public data class Color(val red: Int, val green: Int, val blue: Int, val alpha: Float) {
 
     companion object {
-        val Black = Color(0, 0, 0)
-        val DarkGray = Color(0x44, 0x44, 0x44)
-        val Gray = Color(0x88, 0x88, 0x88)
-        val LightGray = Color(0xCC, 0xCC, 0xCC)
-        val White = Color(0xFF, 0xFF, 0xFF)
-        val Red = Color(0xFF, 0, 0)
-        val Green = Color(0, 0xFF, 0)
-        val Blue = Color(0, 0, 0xFF)
-        val Yellow = Color(0xFF, 0xFF, 0x00)
-        val Cyan = Color(0, 0xFF, 0xFF)
-        val Magenta = Color(0xFF, 0, 0xFF)
+        val Black = Color(0, 0, 0, 1f)
+        val DarkGray = Color(0x44, 0x44, 0x44, 1f)
+        val Gray = Color(0x88, 0x88, 0x88, 1f)
+        val LightGray = Color(0xCC, 0xCC, 0xCC, 1f)
+        val White = Color(0xFF, 0xFF, 0xFF, 1f)
+        val Red = Color(0xFF, 0, 0, 1f)
+        val Green = Color(0, 0xFF, 0, 1f)
+        val Blue = Color(0, 0, 0xFF, 1f)
+        val Yellow = Color(0xFF, 0xFF, 0x00, 1f)
+        val Cyan = Color(0, 0xFF, 0xFF, 1f)
+        val Magenta = Color(0xFF, 0, 0xFF, 1f)
+        val Transparent = Color(0x00, 0x00, 0x00, 0f)
     }
 }
