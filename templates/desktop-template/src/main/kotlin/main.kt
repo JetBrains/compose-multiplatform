82468294f43952758fb00f5e13f3267734979d34
Igor Demin <igordmn@users.noreply.github.com>
2021-07-27 15:04:08 +0300

    Update Compose

--- templates/desktop-template/src/main/kotlin/main.kt
+++ templates/desktop-template/src/main/kotlin/main.kt
@@ -1,4 +1,3 @@
-import androidx.compose.desktop.Window
 import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -6,15 +5,19 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
 
-fun main() = Window {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun main() = application {
+    Window(onCloseRequest = ::exitApplication) {
+        var text by remember { mutableStateOf("Hello, World!") }
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        MaterialTheme {
+            Button(onClick = {
+                text = "Hello, Desktop!"
+            }) {
+                Text(text)
+            }
         }
     }
 }
