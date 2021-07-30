fa2264b7615fd357f3420226464d850f09d190f7
Igor Demin <igordmn@gmail.com>
2021-07-30 21:51:52 +0300

    Add google()

--- idea-plugin/examples/simple-preview-example/build.gradle.kts
+++ idea-plugin/examples/simple-preview-example/build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
     }
     dependencies {
         // __LATEST_COMPOSE_RELEASE_VERSION__
-        classpath("org.jetbrains.compose:compose-gradle-plugin:0.5.0-build262")
+        classpath("org.jetbrains.compose:compose-gradle-plugin:0.0.0-sync-2021-07-28-build290")
         // __KOTLIN_COMPOSE_VERSION__
         classpath(kotlin("gradle-plugin", version = "1.5.21"))
     }
@@ -17,5 +17,6 @@ subprojects {
         mavenLocal()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
     }
 }
