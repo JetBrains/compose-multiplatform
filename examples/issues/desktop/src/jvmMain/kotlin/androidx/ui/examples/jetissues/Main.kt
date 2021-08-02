4c4be0ba712389c050f3f863f1a02765aea10668
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2021-08-02 03:18:08 +0300

    Update issues example
    This change updates:
    * Gradle to 7.1.1;
    * AGP to 7.0.0;
    * Compose to 1.0.0-alpha1-rc1;
    * Kotlin to 1.5.21.

--- examples/issues/desktop/src/jvmMain/kotlin/androidx/ui/examples/jetissues/Main.kt
+++ examples/issues/desktop/src/jvmMain/kotlin/androidx/ui/examples/jetissues/Main.kt
@@ -1,8 +1,11 @@
 package androidx.ui.examples.jetissues
 
-import androidx.compose.desktop.Window
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowSize
+import androidx.compose.ui.window.WindowState
+import androidx.compose.ui.window.application
 import androidx.ui.examples.jetissues.view.JetIssuesView
 import androidx.ui.examples.jetissues.view.Repository
 import androidx.ui.examples.jetissues.data.IssuesRepositoryImpl
@@ -11,11 +14,14 @@ import androidx.ui.examples.jetissues.data.defaultRepo
 
 val repo = IssuesRepositoryImpl(defaultRepo.first, defaultRepo.second, System.getenv("GITHUB_TOKEN") ?: defaultAuth)
 
-fun main() = Window(
-    title = "JetIssues",
-    size = IntSize(1440, 768)
-) {
-    CompositionLocalProvider(Repository provides repo) {
-        JetIssuesView()
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "JetIssues",
+        state = WindowState(size = WindowSize(1440.dp, 768.dp))
+    ) {
+        CompositionLocalProvider(Repository provides repo) {
+            JetIssuesView()
+        }
     }
 }
