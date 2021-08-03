aa531e7b32d49907476a18bde4f09fed5a5c2eca
Igor Demin <igordmn@gmail.com>
2021-08-03 13:09:36 +0300

    Codeviewer. Refactor

--- examples/codeviewer/common/src/desktopMain/kotlin/org/jetbrains/codeviewer/platform/Mouse.kt
+++ examples/codeviewer/common/src/desktopMain/kotlin/org/jetbrains/codeviewer/platform/Mouse.kt
@@ -1,13 +1,15 @@
 package org.jetbrains.codeviewer.platform
 
-import androidx.compose.desktop.LocalAppWindow
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.awtCursor
+import androidx.compose.ui.input.pointer.pointerIcon
 import androidx.compose.ui.input.pointer.pointerMoveFilter
 import java.awt.Cursor
 
@@ -17,17 +19,20 @@ actual fun Modifier.pointerMoveFilter(
     onMove: (Offset) -> Boolean
 ): Modifier = this.pointerMoveFilter(onEnter = onEnter, onExit = onExit, onMove = onMove)
 
+@OptIn(ExperimentalComposeUiApi::class)
 actual fun Modifier.cursorForHorizontalResize(): Modifier = composed {
     var isHover by remember { mutableStateOf(false) }
-
-    if (isHover) {
-        LocalAppWindow.current.window.cursor = Cursor(Cursor.E_RESIZE_CURSOR)
-    } else {
-        LocalAppWindow.current.window.cursor = Cursor.getDefaultCursor()
-    }
 
     pointerMoveFilter(
         onEnter = { isHover = true; true },
         onExit = { isHover = false; true }
+    ).pointerIcon(
+        awtCursor(
+            if (isHover) {
+                Cursor(Cursor.E_RESIZE_CURSOR)
+            } else {
+                Cursor.getDefaultCursor()
+            }
+        )
     )
 }
