40b808eaa519dad0fe1015bb686103598524559f
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-08-17 17:51:35 +0200

    Temporary commented out one of modules

--- web/settings.gradle.kts
+++ web/settings.gradle.kts
@@ -44,7 +44,7 @@ module(":compose-compiler-integration", "compose-compiler-integration")
 
 if (extra["compose.web.buildSamples"]!!.toString().toBoolean() == true) {
     println("building with examples")
-    module(":examples:falling-balls-web", "../examples/falling-balls-web")
+    //module(":examples:falling-balls-web", "../examples/falling-balls-web")
     module(":examples:compose-web-lp", "../examples/web_landing")
     module(":examples:web-compose-bird", "../examples/web-compose-bird")
     module(":examples:web-with-react", "../examples/web-with-react")
