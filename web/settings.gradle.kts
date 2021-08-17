c513eef2e6f4fbc553df7b067834731dd6c6b13e
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-08-17 23:19:49 +0200

    [web] [build] Fix all missing and incorrect sample modules

--- web/settings.gradle.kts
+++ web/settings.gradle.kts
@@ -26,26 +26,25 @@ pluginManagement {
 
 fun module(name: String, path: String) {
     include(name)
-    val projectPath = file(path)
-    if (!projectPath.exists()) {
-        throw AssertionError("file $path does not exist")
+    val projectDir = rootDir.resolve(path).normalize().absoluteFile
+    if (!projectDir.exists()) {
+        throw AssertionError("file $projectDir does not exist")
     }
-    project(name).projectDir = projectPath
+    project(name).projectDir = projectDir 
 }
 
 
-module(":web-core", "$rootDir/core")
-module(":web-widgets", "$rootDir/widgets")
-module(":web-integration-core", "$rootDir/integration-core")
-module(":web-integration-widgets", "$rootDir/integration-widgets")
-module(":web-benchmark-core", "$rootDir/benchmark-core")
-module(":compose-compiler-integration", "$rootDir/compose-compiler-integration")
+module(":web-core", "core")
+module(":web-widgets", "widgets")
+module(":web-integration-core", "integration-core")
+module(":web-integration-widgets", "integration-widgets")
+module(":web-benchmark-core", "benchmark-core")
+module(":compose-compiler-integration", "compose-compiler-integration")
 
 if (extra["compose.web.buildSamples"]!!.toString().toBoolean() == true) {
     println("building with examples")
     module(":examples:falling-balls-web", "../examples/falling-balls-web")
-    module(":examples:compose-web-lp", "../examples/web_landing")
+    module(":examples:compose-web-lp", "../examples/web-landing")
     module(":examples:web-compose-bird", "../examples/web-compose-bird")
     module(":examples:web-with-react", "../examples/web-with-react")
-    module(":examples:web-getting-started", "../examples/web-getting-started")
 }
