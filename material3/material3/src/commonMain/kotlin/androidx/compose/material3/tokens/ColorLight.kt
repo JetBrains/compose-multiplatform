3f3b6bcc1f14ccf0a1b7abfefa565daf5534ec04
Connie Shi <connieshi@google.com>
2021-08-18 08:53:16 -0400

    Add color system design tokens with placeholder values.
    Bug: 196873503
    Test: Manually ran ./gradlew compose:material3:material3:compileDebugKotlin
    Relnote: N/A
    
    Change-Id: I8cf07467202813c88fe71ba1dbbbe09a592d4021

---
+++ material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/ColorLight.kt
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+package androidx.compose.material3.tokens
+
+internal object ColorLight {
+    val Background = Palette.Neutral0
+    val Error = Palette.Error500
+    val ErrorStateContent = Palette.Error800
+    val ErrorStateLayer = Error
+    val Hairline = Palette.NeutralVariant300
+    val InverseOnSurface = Palette.Neutral50
+    val InversePrimary = Palette.Primary200
+    val InversePrimaryStateContent = Palette.Primary200
+    val InversePrimaryStateLayer = Palette.Primary300
+    val InverseSurface = Palette.Neutral800
+    val OnBackground = Palette.Neutral900
+    val OnError = Palette.Error0
+    val OnErrorStateContent = OnError
+    val OnErrorStateLayer = Palette.Neutral900
+    val OnPrimary = Palette.Primary0
+    val OnPrimaryContainer = Palette.Primary900
+    val OnPrimaryContainerStateContent = OnPrimaryContainer
+    val OnPrimaryContainerStateLayer = OnPrimaryContainer
+    val OnPrimaryStateContent = OnPrimary
+    val OnPrimaryStateLayer = Palette.Primary900
+    val OnSecondary = Palette.Secondary0
+    val OnSecondaryContainer = Palette.Secondary900
+    val OnSecondaryContainerStateContent = OnSecondaryContainer
+    val OnSecondaryContainerStateLayer = OnSecondaryContainer
+    val OnSecondaryStateContent = OnSecondary
+    val OnSecondaryStateLayer = Palette.Secondary900
+    val OnSurface = Palette.Neutral900
+    val OnSurfaceStateContent = OnSurface
+    val OnSurfaceStateLayer = OnSurface
+    val OnSurfaceVariant = Palette.NeutralVariant700
+    val OnSurfaceVariantStateContent = OnSurfaceVariant
+    val OnSurfaceVariantStateLayer = OnSurfaceVariant
+    val OnTertiaryContainer = Palette.Tertiary900
+    val OnTertiaryContainerStateContent = OnTertiaryContainer
+    val OnTertiaryContainerStateLayer = OnTertiaryContainer
+    val Outline = Palette.NeutralVariant500
+    val Primary = Palette.Primary600
+    val PrimaryContainer = Palette.Primary100
+    val PrimaryStateContent = Palette.Primary700
+    val PrimaryStateLayer = Primary
+    val PrimaryVariant = Primary
+    val Secondary = Palette.Secondary600
+    val SecondaryContainer = Palette.Secondary100
+    val SecondaryStateContent = Palette.Secondary700
+    val SecondaryStateLayer = Secondary
+    val SecondaryVariant = Secondary
+    val Shadow = Palette.Neutral1000
+    val Surface = Palette.Neutral0
+    val SurfaceVariant = Palette.NeutralVariant100
+    val TertiaryContainer = Palette.Tertiary100
+    val TextfieldError = Palette.Red700
+    val TextfieldHairline = Palette.Grey600
+    val TextfieldOnSurfaceVariant = Palette.Grey700
+    val TextfieldPrimary = Palette.Blue700
+    val TextfieldStateLayer = Palette.Grey800
+    val TextfieldSurface = Palette.Grey100
+}
