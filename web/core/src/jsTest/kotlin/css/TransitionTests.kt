0eded0209a65df9e44ac94d5d138a0c0a92e3eae
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-08-18 19:13:23 +0200

    tranformation fun matrix

---
+++ web/core/src/jsTest/kotlin/css/TransitionTests.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2020-2021 JetBrains s.r.o. and respective authors and developers.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
+ */
+
+package org.jetbrains.compose.web.core.tests.css
+
+import org.jetbrains.compose.web.core.tests.runTest
+import org.jetbrains.compose.web.css.matrixTransform
+import org.jetbrains.compose.web.css.transform
+import org.jetbrains.compose.web.dom.Div
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TransitionTests {
+    @Test
+    fun matrix() = runTest {
+        composition {
+            Div({style { transform(matrixTransform(1, 2, -1, 1, 80, 80)) }})
+        }
+
+        assertEquals("matrix(1, 2, -1, 1, 80, 80)", nextChild().style.transform)
+    }
+}
