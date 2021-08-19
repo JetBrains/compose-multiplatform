7f3de3308d1989503af865057519b50b51f72c3e
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-08-20 00:29:03 +0200

    BROKEN TransformationBuilder

--- web/core/src/jsMain/kotlin/org/jetbrains/compose/web/css/properties/transform.kt
+++ web/core/src/jsMain/kotlin/org/jetbrains/compose/web/css/properties/transform.kt
@@ -5,62 +5,91 @@
 
 package org.jetbrains.compose.web.css
 
-
 fun interface TransformFunction {
     fun apply(): String
 }
 
-fun matrixTransform(a: Number, b: Number, c: Number, d: Number, tx: Number, ty: Number)
-    = TransformFunction { "matrix($a, $b, $c, $d, $tx, $ty)" }
+class TransformBuilder {
+    private val transformations = mutableListOf<TransformFunction>()
 
-fun matrix3dTransform(
-    a1: Number, b1: Number, c1: Number, d1: Number,
-    a2: Number, b2: Number, c2: Number, d2: Number,
-    a3: Number, b3: Number, c3: Number, d3: Number,
-    a4: Number, b4: Number, c4: Number, d4: Number
-) = TransformFunction { "matrix3d($a1, $b1, $c1, $d1, $a2, $b2, $c2, $d2, $a3, $b3, $c3, $d3, $a4, $b4, $c4, $d4)" }
+    fun matrix(a: Number, b: Number, c: Number, d: Number, tx: Number, ty: Number) =
+        transformations.add(TransformFunction { "matrix($a, $b, $c, $d, $tx, $ty)" })
 
-fun perspectiveTransform(d: CSSLengthValue) = TransformFunction { "perspective($d)" }
+    fun matrix3d(
+        a1: Number, b1: Number, c1: Number, d1: Number,
+        a2: Number, b2: Number, c2: Number, d2: Number,
+        a3: Number, b3: Number, c3: Number, d3: Number,
+        a4: Number, b4: Number, c4: Number, d4: Number
+    ) =
+        transformations.add(TransformFunction { "matrix3d($a1, $b1, $c1, $d1, $a2, $b2, $c2, $d2, $a3, $b3, $c3, $d3, $a4, $b4, $c4, $d4)" })
 
-fun rotateTransform(a: CSSAngleValue) = TransformFunction { "rotate($a)" }
-fun rotate3dTransform(x: Number, y: Number, z: Number, a: CSSAngleValue) = TransformFunction { "rotate3d($x, $y, $z, $a)" }
-fun rotateXTransform(a: CSSAngleValue) = TransformFunction { "rotateX($a)" }
-fun rotateYTransform(a: CSSAngleValue) = TransformFunction { "rotateY($a)" }
-fun rotateZTransform(a: CSSAngleValue) = TransformFunction { "rotateZ($a)" }
+    fun perspective(d: CSSLengthValue) = transformations.add(TransformFunction { "perspective($d)" })
 
-fun scaleTransform(sx: Number) = TransformFunction { "scale($sx)" }
-fun scaleTransform(sx: Number, sy: Number) = TransformFunction { "scale($sx, $sy)" }
-fun scale3dTransform(sx: Number, sy: Number, sz: Number) = TransformFunction { "scale3d($sx, $sy, $sz)" }
-fun scaleXTransform(s: Number) = TransformFunction { "scaleX($s)" }
-fun scaleYTransform(s: Number) = TransformFunction { "scaleY($s)" }
-fun scaleZTransform(s: Number) = TransformFunction { "scaleZ($s)" }
+    fun rotate(a: CSSAngleValue) = transformations.add(TransformFunction { "rotate($a)" })
+    fun rotate3d(x: Number, y: Number, z: Number, a: CSSAngleValue) =
+        transformations.add(TransformFunction { "rotate3d($x, $y, $z, $a)" })
 
-fun skewTransform(ax: CSSAngleValue) = TransformFunction { "skew($ax)" }
-fun skewTransform(ax: CSSAngleValue, ay: CSSAngleValue) = TransformFunction { "skew($ax, $ay)" }
-fun skewXTransform(a: CSSAngleValue) = TransformFunction { "skewX($a)" }
-fun skewYTransform(a: CSSAngleValue) = TransformFunction { "skewY($a)" }
+    fun rotateX(a: CSSAngleValue) = transformations.add(TransformFunction { "rotateX($a)" })
+    fun rotateY(a: CSSAngleValue) = transformations.add(TransformFunction { "rotateY($a)" })
+    fun rotateZ(a: CSSAngleValue) = transformations.add(TransformFunction { "rotateZ($a)" })
 
-fun translate(tx: CSSLengthValue) = TransformFunction { "translate($tx)" }
-fun translate(tx: CSSPercentageValue) = TransformFunction { "translate($tx)" }
+    fun scale(sx: Number) = transformations.add(TransformFunction { "scale($sx)" })
+    fun scale(sx: Number, sy: Number) = transformations.add(TransformFunction { "scale($sx, $sy)" })
+    fun scale3d(sx: Number, sy: Number, sz: Number) =
+        transformations.add(TransformFunction { "scale3d($sx, $sy, $sz)" })
 
-fun translate(tx: CSSLengthValue, ty: CSSLengthValue) = TransformFunction { "translate($tx, $ty)" }
-fun translate(tx: CSSLengthValue, ty: CSSPercentageValue) = TransformFunction { "translate($tx, $ty)" }
-fun translate(tx: CSSPercentageValue, ty: CSSLengthValue) = TransformFunction { "translate($tx, $ty)" }
-fun translate(tx: CSSPercentageValue, ty: CSSPercentageValue) = TransformFunction { "translate($tx, $ty)" }
+    fun scaleX(s: Number) = transformations.add(TransformFunction { "scaleX($s)" })
+    fun scaleY(s: Number) = transformations.add(TransformFunction { "scaleY($s)" })
+    fun scaleZ(s: Number) = transformations.add(TransformFunction { "scaleZ($s)" })
 
-fun translate3d(tx: CSSLengthValue, ty: CSSLengthValue, tz: CSSLengthValue) = TransformFunction { "translate3d($tx, $ty, $tz)" }
-fun translate3d(tx: CSSLengthValue, ty: CSSPercentageValue, tz: CSSLengthValue) = TransformFunction { "translate3d($tx, $ty, $tz)" }
-fun translate3d(tx: CSSPercentageValue, ty: CSSLengthValue, tz: CSSLengthValue) = TransformFunction { "translate3d($tx, $ty, $tz)" }
-fun translate3d(tx: CSSPercentageValue, ty: CSSPercentageValue, tz: CSSLengthValue) = TransformFunction { "translate3d($tx, $ty, $tz)" }
+    fun skew(ax: CSSAngleValue) = transformations.add(TransformFunction { "skew($ax)" })
+    fun skew(ax: CSSAngleValue, ay: CSSAngleValue) = transformations.add(TransformFunction { "skew($ax, $ay)" })
+    fun skewX(a: CSSAngleValue) = transformations.add(TransformFunction { "skewX($a)" })
+    fun skewY(a: CSSAngleValue) = transformations.add(TransformFunction { "skewY($a)" })
 
-fun translateXTransform(tx: CSSLengthValue) = TransformFunction { "translateX($tx)" }
-fun translateXTransform(tx: CSSPercentageValue) = TransformFunction { "translateX($tx)" }
+    fun translate(tx: CSSLengthValue) = transformations.add(TransformFunction { "translate($tx)" })
+    fun translate(tx: CSSPercentageValue) = transformations.add(TransformFunction { "translate($tx)" })
 
-fun translateYTransform(ty: CSSLengthValue) = TransformFunction { "translateY($ty)" }
-fun translateYTransform(ty: CSSPercentageValue) = TransformFunction { "translateY($ty)" }
+    fun translate(tx: CSSLengthValue, ty: CSSLengthValue) =
+        transformations.add(TransformFunction { "translate($tx, $ty)" })
 
-fun translateZTransform(tz: CSSLengthValue) = TransformFunction { "translateZ($tz)" }
+    fun translate(tx: CSSLengthValue, ty: CSSPercentageValue) =
+        transformations.add(TransformFunction { "translate($tx, $ty)" })
 
-fun StyleBuilder.transform(vararg transformFunction: TransformFunction) {
-    property("transform", transformFunction.joinToString(" ") { it.apply() })
+    fun translate(tx: CSSPercentageValue, ty: CSSLengthValue) =
+        transformations.add(TransformFunction { "translate($tx, $ty)" })
+
+    fun translate(tx: CSSPercentageValue, ty: CSSPercentageValue) =
+        transformations.add(TransformFunction { "translate($tx, $ty)" })
+
+    fun translate3d(tx: CSSLengthValue, ty: CSSLengthValue, tz: CSSLengthValue) =
+        transformations.add(TransformFunction { "translate3d($tx, $ty, $tz)" })
+
+    fun translate3d(tx: CSSLengthValue, ty: CSSPercentageValue, tz: CSSLengthValue) =
+        transformations.add(TransformFunction { "translate3d($tx, $ty, $tz)" })
+
+    fun translate3d(tx: CSSPercentageValue, ty: CSSLengthValue, tz: CSSLengthValue) =
+        transformations.add(TransformFunction { "translate3d($tx, $ty, $tz)" })
+
+    fun translate3d(tx: CSSPercentageValue, ty: CSSPercentageValue, tz: CSSLengthValue) =
+        transformations.add(TransformFunction { "translate3d($tx, $ty, $tz)" })
+
+    fun translateX(tx: CSSLengthValue) = transformations.add(TransformFunction { "translateX($tx)" })
+    fun translateX(tx: CSSPercentageValue) = transformations.add(TransformFunction { "translateX($tx)" })
+
+    fun translateY(ty: CSSLengthValue) = transformations.add(TransformFunction { "translateY($ty)" })
+    fun translateY(ty: CSSPercentageValue) = transformations.add(TransformFunction { "translateY($ty)" })
+
+    fun translateZ(tz: CSSLengthValue) = transformations.add(TransformFunction { "translateZ($tz)" })
+
+    fun apply(): String {
+        return transformations.joinToString(", ") { it.apply() }
+    }
+}
+
+fun StyleBuilder.transform(transformFunction: TransformBuilder.() -> Unit) {
+    property("transform", TransformBuilder().let { transformBuilder ->
+        transformFunction.invoke(transformBuilder)
+        transformBuilder.apply()
+    })
 }
--- web/core/src/jsTest/kotlin/css/TransformTests.kt
+++ web/core/src/jsTest/kotlin/css/TransformTests.kt
@@ -15,7 +15,7 @@ class TransformTests {
     @Test
     fun matrix() = runTest {
         composition {
-            Div({ style { transform(matrixTransform(1, 2, -1, 1, 80, 80)) } })
+            Div({ style { transform { matrix(1, 2, -1, 1, 80, 80) } } })
         }
 
         assertEquals("matrix(1, 2, -1, 1, 80, 80)", nextChild().style.transform)
@@ -24,7 +24,7 @@ class TransformTests {
     @Test
     fun matrix3d() = runTest {
         composition {
-            Div({ style { transform(matrix3dTransform(1, 0, 0, 0, 0, 1, 6, 0, 0, 0, 1, 0, 50, 100, 0, 1.1)) } })
+            Div({ style { transform { matrix3d(1, 0, 0, 0, 0, 1, 6, 0, 0, 0, 1, 0, 50, 100, 0, 1.1) } } })
         }
 
         assertEquals("matrix3d(1, 0, 0, 0, 0, 1, 6, 0, 0, 0, 1, 0, 50, 100, 0, 1.1)", nextChild().style.transform)
@@ -33,7 +33,7 @@ class TransformTests {
     @Test
     fun perspective() = runTest {
         composition {
-            Div({ style { transform(perspectiveTransform(3.cm)) } })
+            Div({ style { transform { perspective(3.cm) } } })
         }
 
         assertEquals("perspective(3cm)", nextChild().style.transform)
@@ -42,7 +42,7 @@ class TransformTests {
     @Test
     fun rotate() = runTest {
         composition {
-            Div({ style { transform(rotateTransform(3.deg)) } })
+            Div({ style { transform { rotate(3.deg) } } })
         }
 
         assertEquals("rotate(3deg)", nextChild().style.transform)
@@ -51,7 +51,7 @@ class TransformTests {
     @Test
     fun rotate3d() = runTest {
         composition {
-            Div({ style { transform(rotate3dTransform(1, 1, 0, 2.deg)) } })
+            Div({ style { transform { rotate3d(1, 1, 0, 2.deg) } } })
         }
 
         assertEquals("rotate3d(1, 1, 0, 2deg)", nextChild().style.transform)
@@ -60,9 +60,9 @@ class TransformTests {
     @Test
     fun rotateX() = runTest {
         composition {
-            Div({ style { transform(rotateXTransform(60.deg)) } })
-            Div({ style { transform(rotateXTransform(-0.25.turn)) } })
-            Div({ style { transform(rotateXTransform(3.14.rad)) } })
+            Div({ style { transform { rotateX(60.deg) } } })
+            Div({ style { transform { rotateX(-0.25.turn) } } })
+            Div({ style { transform { rotateX(3.14.rad) } } })
         }
 
         assertEquals("rotateX(60deg)", nextChild().style.transform)
@@ -73,9 +73,9 @@ class TransformTests {
     @Test
     fun rotateY() = runTest {
         composition {
-            Div({ style { transform(rotateYTransform(60.deg)) } })
-            Div({ style { transform(rotateYTransform(-0.25.turn)) } })
-            Div({ style { transform(rotateYTransform(3.14.rad)) } })
+            Div({ style { transform { rotateY(60.deg) } } })
+            Div({ style { transform { rotateY(-0.25.turn) } } })
+            Div({ style { transform { rotateY(3.14.rad) } } })
         }
 
         assertEquals("rotateY(60deg)", nextChild().style.transform)
@@ -86,9 +86,9 @@ class TransformTests {
     @Test
     fun rotateZ() = runTest {
         composition {
-            Div({ style { transform(rotateZTransform(60.deg)) } })
-            Div({ style { transform(rotateZTransform(-0.25.turn)) } })
-            Div({ style { transform(rotateZTransform(3.14.rad)) } })
+            Div({ style { transform { rotateZ(60.deg) } } })
+            Div({ style { transform { rotateZ(-0.25.turn) } } })
+            Div({ style { transform { rotateZ(3.14.rad) } } })
         }
 
         assertEquals("rotateZ(60deg)", nextChild().style.transform)
@@ -99,8 +99,8 @@ class TransformTests {
     @Test
     fun scale() = runTest {
         composition {
-            Div({ style { transform(scaleTransform(0.6)) } })
-            Div({ style { transform(scaleTransform(0.2, 0.3)) } })
+            Div({ style { transform { scale(0.6) } } })
+            Div({ style { transform { scale(0.2, 0.3) } } })
         }
 
         assertEquals("scale(0.6)", nextChild().style.transform)
@@ -110,7 +110,7 @@ class TransformTests {
     @Test
     fun scale3d() = runTest {
         composition {
-            Div({ style { transform(scale3dTransform(0.2, 0.3, 0.1)) } })
+            Div({ style { transform { scale3d(0.2, 0.3, 0.1) } } })
         }
 
         assertEquals("scale3d(0.2, 0.3, 0.1)", nextChild().style.transform)
@@ -119,7 +119,7 @@ class TransformTests {
     @Test
     fun scaleX() = runTest {
         composition {
-            Div({ style { transform(scaleXTransform(0.5)) } })
+            Div({ style { transform { scaleX(0.5) } } })
         }
 
         assertEquals("scaleX(0.5)", nextChild().style.transform)
@@ -128,7 +128,7 @@ class TransformTests {
     @Test
     fun scaleY() = runTest {
         composition {
-            Div({ style { transform(scaleYTransform(0.7)) } })
+            Div({ style { transform { scaleY(0.7) } } })
         }
 
         assertEquals("scaleY(0.7)", nextChild().style.transform)
@@ -137,7 +137,7 @@ class TransformTests {
     @Test
     fun scaleZ() = runTest {
         composition {
-            Div({ style { transform(scaleZTransform(0.12)) } })
+            Div({ style { transform { scaleZ(0.12) } } })
         }
 
         assertEquals("scaleZ(0.12)", nextChild().style.transform)
@@ -146,8 +146,8 @@ class TransformTests {
     @Test
     fun skew() = runTest {
         composition {
-            Div({ style { transform(skewTransform(2.deg)) } })
-            Div({ style { transform(skewTransform(1.rad, 2.deg)) } })
+            Div({ style { transform { skew(2.deg) } } })
+            Div({ style { transform { skew(1.rad, 2.deg) } } })
         }
 
         assertEquals("skew(2deg)", nextChild().style.transform)
@@ -157,7 +157,7 @@ class TransformTests {
     @Test
     fun skewX() = runTest {
         composition {
-            Div({ style { transform(skewXTransform(2.deg)) } })
+            Div({ style { transform { skewX(2.deg) } } })
         }
 
         assertEquals("skewX(2deg)", nextChild().style.transform)
@@ -166,7 +166,7 @@ class TransformTests {
     @Test
     fun skewY() = runTest {
         composition {
-            Div({ style { transform(skewYTransform(2.rad)) } })
+            Div({ style { transform { skewY(2.rad) } } })
         }
 
         assertEquals("skewY(2rad)", nextChild().style.transform)
@@ -175,12 +175,12 @@ class TransformTests {
     @Test
     fun translate() = runTest {
         composition {
-            Div({ style { transform(translate(10.px)) } })
-            Div({ style { transform(translate(4.percent)) } })
-            Div({ style { transform(translate(2.percent, 10.px)) } })
-            Div({ style { transform(translate(10.px, 3.percent)) } })
-            Div({ style { transform(translate(20.px, 10.px)) } })
-            Div({ style { transform(translate(5.percent, 8.percent)) } })
+            Div({ style { transform { translate(10.px) } } })
+            Div({ style { transform { translate(4.percent) } } })
+            Div({ style { transform { translate(2.percent, 10.px) } } })
+            Div({ style { transform { translate(10.px, 3.percent) } } })
+            Div({ style { transform { translate(20.px, 10.px) } } })
+            Div({ style { transform { translate(5.percent, 8.percent) } } })
         }
 
         assertEquals("translate(10px)", nextChild().style.transform)
@@ -194,10 +194,10 @@ class TransformTests {
     @Test
     fun translate3d() = runTest {
         composition {
-            Div({ style { transform(translate3d(2.percent, 10.px, 1.em)) } })
-            Div({ style { transform(translate3d(10.px, 3.percent, 2.em)) } })
-            Div({ style { transform(translate3d(20.px, 10.px, 3.em)) } })
-            Div({ style { transform(translate3d(5.percent, 8.percent, 4.em)) } })
+            Div({ style { transform { translate3d(2.percent, 10.px, 1.em) } } })
+            Div({ style { transform { translate3d(10.px, 3.percent, 2.em) } } })
+            Div({ style { transform { translate3d(20.px, 10.px, 3.em) } } })
+            Div({ style { transform { translate3d(5.percent, 8.percent, 4.em) } } })
         }
 
         assertEquals("translate3d(2%, 10px, 1em)", nextChild().style.transform)
@@ -210,8 +210,8 @@ class TransformTests {
     @Test
     fun translateX() = runTest {
         composition {
-            Div({ style { transform(translateXTransform(10.px)) } })
-            Div({ style { transform(translateXTransform(4.percent)) } })
+            Div({ style { transform{translateX(10.px)} } })
+            Div({ style { transform{translateX(4.percent)} } })
         }
 
         assertEquals("translateX(10px)", nextChild().style.transform)
@@ -221,8 +221,8 @@ class TransformTests {
     @Test
     fun translateY() = runTest {
         composition {
-            Div({ style { transform(translateYTransform(12.px)) } })
-            Div({ style { transform(translateYTransform(3.percent)) } })
+            Div({ style { transform { translateY(12.px) } } })
+            Div({ style { transform { translateY(3.percent) } } })
         }
 
         assertEquals("translateY(12px)", nextChild().style.transform)
@@ -232,16 +232,24 @@ class TransformTests {
     @Test
     fun translateZ() = runTest {
         composition {
-            Div({ style { transform(translateZTransform(7.px)) } })
+            Div({ style { transform { translateZ(7.px) } } })
         }
 
         assertEquals("translateZ(7px)", nextChild().style.transform)
     }
 
     @Test
-    fun mutlipleTransforms() = runTest {
+    fun mutliples() = runTest {
         composition {
-            Div({ style { transform(perspectiveTransform(3.cm), translate(10.px, 3.px), rotateYTransform(3.deg)) } })
+            Div({
+                style {
+                    transform {
+                        perspective(3.cm)
+                        translate(10.px, 3.px)
+                        rotateY(3.deg)
+                    }
+                }
+            })
         }
 
         assertEquals("perspective(3cm) translate(10px, 3px) rotateY(3deg)", nextChild().style.transform)
