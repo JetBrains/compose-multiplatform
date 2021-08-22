9606e6db1bce6e61005826ff9da30ce82ba6a419
Connie Shi <connieshi@google.com>
2021-08-22 14:51:35 -0400

    Add design token values for shape, typescale and typeface.
    Bug: 197318412
    Test: Manually ran material3:compileDebugKotlin
    Relnote: N/A
    Change-Id: I3e831d1068bded2cf56a703d197f32a8159b6534

---
+++ material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/TypeScale.kt
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+package androidx.compose.material3.tokens
+
+import androidx.compose.ui.unit.sp
+
+internal object TypeScale {
+    val BodyLargeFont = Typeface.PlainRegular
+    val BodyLargeLineHeightPoints = 24.0.sp
+    val BodyLargeSizePoints = 16.sp
+    val BodyLargeTrackingPoints = 0.5.sp
+    val BodyLargeWeight = Typeface.WeightRegular
+    val BodyMediumFont = Typeface.PlainRegular
+    val BodyMediumLineHeightPoints = 20.0.sp
+    val BodyMediumSizePoints = 14.sp
+    val BodyMediumTrackingPoints = 0.2.sp
+    val BodyMediumWeight = Typeface.WeightRegular
+    val BodySmallFont = Typeface.PlainRegular
+    val BodySmallLineHeightPoints = 16.0.sp
+    val BodySmallSizePoints = 12.sp
+    val BodySmallTrackingPoints = 0.4.sp
+    val BodySmallWeight = Typeface.WeightRegular
+    val Display1Font = Typeface.BrandRegular
+    val Display1LineHeightPoints = 76.0.sp
+    val Display1SizePoints = 64.sp
+    val Display1TrackingPoints = -0.5.sp
+    val Display1Weight = Typeface.WeightRegular
+    val DisplayLargeFont = Typeface.BrandRegular
+    val DisplayLargeLineHeightPoints = 64.0.sp
+    val DisplayLargeSizePoints = 57.sp
+    val DisplayLargeTrackingPoints = -0.2.sp
+    val DisplayLargeWeight = Typeface.WeightRegular
+    val DisplayMediumFont = Typeface.BrandRegular
+    val DisplayMediumLineHeightPoints = 52.0.sp
+    val DisplayMediumSizePoints = 45.sp
+    val DisplayMediumTrackingPoints = 0.0.sp
+    val DisplayMediumWeight = Typeface.WeightRegular
+    val DisplaySmallFont = Typeface.BrandRegular
+    val DisplaySmallLineHeightPoints = 44.0.sp
+    val DisplaySmallSizePoints = 36.sp
+    val DisplaySmallTrackingPoints = 0.0.sp
+    val DisplaySmallWeight = Typeface.WeightRegular
+    val Headline6Font = Typeface.BrandRegular
+    val Headline6LineHeightPoints = 24.0.sp
+    val Headline6SizePoints = 18.sp
+    val Headline6TrackingPoints = 0.0.sp
+    val Headline6Weight = Typeface.WeightRegular
+    val HeadlineLargeFont = Typeface.BrandRegular
+    val HeadlineLargeLineHeightPoints = 40.0.sp
+    val HeadlineLargeSizePoints = 32.sp
+    val HeadlineLargeTrackingPoints = 0.0.sp
+    val HeadlineLargeWeight = Typeface.WeightRegular
+    val HeadlineMediumFont = Typeface.BrandRegular
+    val HeadlineMediumLineHeightPoints = 36.0.sp
+    val HeadlineMediumSizePoints = 28.sp
+    val HeadlineMediumTrackingPoints = 0.0.sp
+    val HeadlineMediumWeight = Typeface.WeightRegular
+    val HeadlineSmallFont = Typeface.BrandRegular
+    val HeadlineSmallLineHeightPoints = 32.0.sp
+    val HeadlineSmallSizePoints = 24.sp
+    val HeadlineSmallTrackingPoints = 0.0.sp
+    val HeadlineSmallWeight = Typeface.WeightRegular
+    val LabelLargeFont = Typeface.PlainMedium
+    val LabelLargeLineHeightPoints = 20.0.sp
+    val LabelLargeSizePoints = 14.sp
+    val LabelLargeTrackingPoints = 0.1.sp
+    val LabelLargeWeight = Typeface.WeightMedium
+    val LabelMediumFont = Typeface.PlainMedium
+    val LabelMediumLineHeightPoints = 16.0.sp
+    val LabelMediumSizePoints = 12.sp
+    val LabelMediumTrackingPoints = 0.5.sp
+    val LabelMediumWeight = Typeface.WeightMedium
+    val LabelSmallFont = Typeface.PlainMedium
+    val LabelSmallLineHeightPoints = 16.0.sp
+    val LabelSmallSizePoints = 11.sp
+    val LabelSmallTrackingPoints = 0.5.sp
+    val LabelSmallWeight = Typeface.WeightMedium
+    val Subtitle1Font = Typeface.PlainMedium
+    val Subtitle1LineHeightPoints = 24.0.sp
+    val Subtitle1SizePoints = 16.sp
+    val Subtitle1TrackingPoints = 0.2.sp
+    val Subtitle1Weight = Typeface.WeightMedium
+    val Subtitle2Font = Typeface.PlainMedium
+    val Subtitle2LineHeightPoints = 20.0.sp
+    val Subtitle2SizePoints = 14.sp
+    val Subtitle2TrackingPoints = 0.1.sp
+    val Subtitle2Weight = Typeface.WeightMedium
+    val TitleLargeFont = Typeface.BrandRegular
+    val TitleLargeLineHeightPoints = 28.0.sp
+    val TitleLargeSizePoints = 22.sp
+    val TitleLargeTrackingPoints = 0.0.sp
+    val TitleLargeWeight = Typeface.WeightRegular
+    val TitleMediumFont = Typeface.PlainMedium
+    val TitleMediumLineHeightPoints = 24.0.sp
+    val TitleMediumSizePoints = 16.sp
+    val TitleMediumTrackingPoints = 0.2.sp
+    val TitleMediumWeight = Typeface.WeightMedium
+    val TitleSmallFont = Typeface.PlainMedium
+    val TitleSmallLineHeightPoints = 20.0.sp
+    val TitleSmallSizePoints = 14.sp
+    val TitleSmallTrackingPoints = 0.1.sp
+    val TitleSmallWeight = Typeface.WeightMedium
+}
