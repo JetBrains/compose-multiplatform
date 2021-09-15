275d3c6317f8c463284751dc5ca1cdd7fb44706f
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2021-09-15 17:31:02 +0200

    update web examples to workaround a bug when using `jsRun`
    see https://youtrack.jetbrains.com/issue/KT-48273

--- examples/todoapp/web/build.gradle.kts
+++ examples/todoapp/web/build.gradle.kts
@@ -36,7 +36,7 @@ kotlin {
 
 // a temporary workaround for a bug in jsRun invocation - see https://youtrack.jetbrains.com/issue/KT-48273
 afterEvaluate {
-    extensions.configure<NodeJsRootExtension> {
+    rootProject.extensions.configure<NodeJsRootExtension> {
         versions.webpackDevServer.version = "4.0.0"
     }
 }
