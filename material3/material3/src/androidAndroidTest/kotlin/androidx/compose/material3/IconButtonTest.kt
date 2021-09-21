82424ea3ab5d7d90ed0621179bfa68c6a4920a9e
Connie Shi <connieshi@google.com>
2021-09-21 14:00:15 -0400

    Added IconTest and IconButtonTest.
    Fixes: b/198209809,
    Fixes: b/199318760
    Test: Ran tests in IconTest and IconButtonTest
    Relnote: N/A
    Change-Id: I29692e8b72446f7f18a43ce5233a998ce9f3a05f

---
+++ material3/material3/src/androidAndroidTest/kotlin/androidx/compose/material3/IconButtonTest.kt
@@ -0,0 +1,228 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package androidx.compose.material3
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.samples.IconButtonSample
+import androidx.compose.material3.samples.IconToggleButtonSample
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.assertHeightIsEqualTo
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.assertIsOff
+import androidx.compose.ui.test.assertIsOn
+import androidx.compose.ui.test.assertLeftPositionInRootIsEqualTo
+import androidx.compose.ui.test.assertTopPositionInRootIsEqualTo
+import androidx.compose.ui.test.assertTouchHeightIsEqualTo
+import androidx.compose.ui.test.assertTouchWidthIsEqualTo
+import androidx.compose.ui.test.assertWidthIsEqualTo
+import androidx.compose.ui.test.click
+import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.isToggleable
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.performTouchInput
+import androidx.compose.ui.unit.dp
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+/**
+ * Test for [IconButton] and [IconToggleButton].
+ */
+class IconButtonTest {
+    @get:Rule
+    val rule = createComposeRule()
+
+    @Test
+    fun iconButton_size() {
+        val width = 48.dp
+        val height = 48.dp
+        rule
+            .setMaterialContentForSizeAssertions {
+                IconButtonSample()
+            }
+            .assertWidthIsEqualTo(width)
+            .assertHeightIsEqualTo(height)
+    }
+
+    @Test
+    fun iconButton_defaultSemantics() {
+        rule.setMaterialContent {
+            IconButtonSample()
+        }
+        rule.onNode(hasClickAction()).apply {
+            assertIsEnabled()
+        }
+    }
+
+    @Test
+    fun iconButton_disabledSemantics() {
+        rule.setMaterialContent {
+            IconButton(onClick = {}, enabled = false) {}
+        }
+        rule.onNode(hasClickAction()).apply {
+            assertIsNotEnabled()
+        }
+    }
+
+    @Test
+    fun iconButton_materialIconSize_iconPositioning() {
+        val diameter = 24.dp
+        rule.setMaterialContent {
+            Box {
+                IconButton(onClick = {}) {
+                    Box(Modifier.size(diameter).testTag("icon"))
+                }
+            }
+        }
+
+        // Icon should be centered inside the IconButton
+        rule.onNodeWithTag("icon", useUnmergedTree = true)
+            .assertLeftPositionInRootIsEqualTo(24.dp / 2)
+            .assertTopPositionInRootIsEqualTo(24.dp / 2)
+    }
+
+    @Test
+    fun iconButton_customIconSize_iconPositioning() {
+        val width = 36.dp
+        val height = 14.dp
+        rule.setMaterialContent {
+            Box {
+                IconButton(onClick = {}) {
+                    Box(Modifier.size(width, height).testTag("icon"))
+                }
+            }
+        }
+
+        // Icon should be centered inside the IconButton
+        rule.onNodeWithTag("icon", useUnmergedTree = true)
+            .assertLeftPositionInRootIsEqualTo((48.dp - width) / 2)
+            .assertTopPositionInRootIsEqualTo((48.dp - height) / 2)
+    }
+
+    @Test
+    fun iconToggleButton_size() {
+        val width = 48.dp
+        val height = 48.dp
+        rule
+            .setMaterialContentForSizeAssertions {
+                IconToggleButtonSample()
+            }
+            .assertWidthIsEqualTo(width)
+            .assertHeightIsEqualTo(height)
+    }
+
+    @Test
+    fun iconToggleButton_defaultSemantics() {
+        rule.setMaterialContent {
+            IconToggleButtonSample()
+        }
+        rule.onNode(isToggleable()).apply {
+            assertIsEnabled()
+            assertIsOff()
+            performClick()
+            assertIsOn()
+        }
+    }
+
+    @Test
+    fun iconToggleButton_disabledSemantics() {
+        rule.setMaterialContent {
+            IconToggleButton(checked = false, onCheckedChange = {}, enabled = false) {}
+        }
+        rule.onNode(isToggleable()).apply {
+            assertIsNotEnabled()
+            assertIsOff()
+        }
+    }
+
+    @Test
+    fun iconToggleButton_materialIconSize_iconPositioning() {
+        val diameter = 24.dp
+        rule.setMaterialContent {
+            Box {
+                IconToggleButton(checked = false, onCheckedChange = {}) {
+                    Box(Modifier.size(diameter).testTag("icon"))
+                }
+            }
+        }
+
+        // Icon should be centered inside the IconButton
+        rule.onNodeWithTag("icon", useUnmergedTree = true)
+            .assertLeftPositionInRootIsEqualTo(24.dp / 2)
+            .assertTopPositionInRootIsEqualTo(24.dp / 2)
+    }
+
+    @Test
+    fun iconToggleButton_customIconSize_iconPositioning() {
+        val width = 36.dp
+        val height = 14.dp
+        rule.setMaterialContent {
+            Box {
+                IconToggleButton(checked = false, onCheckedChange = {}) {
+                    Box(Modifier.size(width, height).testTag("icon"))
+                }
+            }
+        }
+
+        // Icon should be centered inside the IconButton
+        rule.onNodeWithTag("icon", useUnmergedTree = true)
+            .assertLeftPositionInRootIsEqualTo((48.dp - width) / 2)
+            .assertTopPositionInRootIsEqualTo((48.dp - height) / 2)
+    }
+
+    @Test
+    fun iconToggleButton_clickInMinimumTouchTarget(): Unit = with(rule.density) {
+        val tag = "iconToggleButton"
+        var checked by mutableStateOf(false)
+        rule.setMaterialContent {
+            // Box is needed because otherwise the control will be expanded to fill its parent
+            Box(Modifier.fillMaxSize()) {
+                IconToggleButton(
+                    checked = checked,
+                    onCheckedChange = { checked = it },
+                    modifier = Modifier.align(Alignment.Center).requiredSize(2.dp).testTag(tag)
+                ) {
+                    Box(Modifier.size(2.dp))
+                }
+            }
+        }
+        rule.onNodeWithTag(tag)
+            .assertIsOff()
+            .assertWidthIsEqualTo(2.dp)
+            .assertHeightIsEqualTo(2.dp)
+            .assertTouchWidthIsEqualTo(48.dp)
+            .assertTouchHeightIsEqualTo(48.dp)
+            .performTouchInput {
+                click(position = Offset(-1f, -1f))
+            }.assertIsOn()
+    }
+}
---
+++ material3/material3/src/androidAndroidTest/kotlin/androidx/compose/material3/IconTest.kt
@@ -0,0 +1,273 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package androidx.compose.material3
+
+import android.os.Build
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.testutils.assertPixels
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Canvas
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.drawscope.CanvasDrawScope
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.SemanticsProperties
+import androidx.compose.ui.test.SemanticsMatcher
+import androidx.compose.ui.test.assert
+import androidx.compose.ui.test.assertContentDescriptionEquals
+import androidx.compose.ui.test.assertHeightIsEqualTo
+import androidx.compose.ui.test.assertWidthIsEqualTo
+import androidx.compose.ui.test.captureToImage
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.filters.SdkSuppress
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class IconTest {
+    @get:Rule
+    val rule = createComposeRule()
+
+    @Test
+    fun vector_materialIconSize_dimensions() {
+        val width = 24.dp
+        val height = 24.dp
+        val vector = Icons.Filled.Menu
+        rule
+            .setMaterialContentForSizeAssertions {
+                Icon(vector, null)
+            }
+            .assertWidthIsEqualTo(width)
+            .assertHeightIsEqualTo(height)
+    }
+
+    @Test
+    fun vector_customIconSize_dimensions() {
+        val width = 35.dp
+        val height = 83.dp
+        val vector = ImageVector.Builder(
+            defaultWidth = width, defaultHeight = height,
+            viewportWidth = width.value, viewportHeight = height.value
+        ).build()
+        rule
+            .setMaterialContentForSizeAssertions {
+                Icon(vector, null)
+            }
+            .assertWidthIsEqualTo(width)
+            .assertHeightIsEqualTo(height)
+    }
+
+    @Test
+    fun image_noIntrinsicSize_dimensions() {
+        val width = 24.dp
+        val height = 24.dp
+        rule
+            .setMaterialContentForSizeAssertions {
+                val image = with(LocalDensity.current) {
+                    ImageBitmap(width.roundToPx(), height.roundToPx())
+                }
+
+                Icon(image, null)
+            }
+            .assertWidthIsEqualTo(width)
+            .assertHeightIsEqualTo(height)
+    }
+
+    @Test
+    fun image_withIntrinsicSize_dimensions() {
+        val width = 35.dp
+        val height = 83.dp
+
+        rule
+            .setMaterialContentForSizeAssertions {
+                val image = with(LocalDensity.current) {
+                    ImageBitmap(width.roundToPx(), height.roundToPx())
+                }
+
+                Icon(image, null)
+            }
+            .assertWidthIsEqualTo(width)
+            .assertHeightIsEqualTo(height)
+    }
+
+    @Test
+    fun painter_noIntrinsicSize_dimensions() {
+        val width = 24.dp
+        val height = 24.dp
+        val painter = ColorPainter(Color.Red)
+        rule
+            .setMaterialContentForSizeAssertions {
+                Icon(painter, null)
+            }
+            .assertWidthIsEqualTo(width)
+            .assertHeightIsEqualTo(height)
+    }
+
+    @Test
+    fun painter_withIntrinsicSize_dimensions() {
+        val width = 35.dp
+        val height = 83.dp
+
+        rule
+            .setMaterialContentForSizeAssertions {
+                val image = with(LocalDensity.current) {
+                    ImageBitmap(width.roundToPx(), height.roundToPx())
+                }
+
+                val bitmapPainter = BitmapPainter(image)
+                Icon(bitmapPainter, null)
+            }
+            .assertWidthIsEqualTo(width)
+            .assertHeightIsEqualTo(height)
+    }
+
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.O)
+    @Test
+    fun iconScalesToFitSize() {
+        // Image with intrinsic size of 24dp
+        val width = 24.dp
+        val height = 24.dp
+        val testTag = "testTag"
+        var expectedIntSize: IntSize? = null
+        rule.setMaterialContent {
+            val image: ImageBitmap
+            with(LocalDensity.current) {
+                image = createBitmapWithColor(
+                    this,
+                    width.roundToPx(),
+                    height.roundToPx(),
+                    Color.Red
+                )
+            }
+            Icon(
+                image,
+                null,
+                // Force Icon to be 50dp
+                modifier = Modifier.requiredSize(50.dp).testTag(testTag),
+                tint = Color.Unspecified
+            )
+            with(LocalDensity.current) {
+                val dimension = 50.dp.roundToPx()
+                expectedIntSize = IntSize(dimension, dimension)
+            }
+        }
+
+        rule.onNodeWithTag(testTag)
+            .captureToImage()
+            // The icon should be 50x50 and fill the whole size with red pixels
+            .assertPixels(expectedSize = expectedIntSize!!) {
+                Color.Red
+            }
+    }
+
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.O)
+    @Test
+    fun iconUnspecifiedTintColorIgnored() {
+        val width = 35.dp
+        val height = 83.dp
+        val testTag = "testTag"
+        rule.setMaterialContent {
+            val image: ImageBitmap
+            with(LocalDensity.current) {
+                image = createBitmapWithColor(
+                    this,
+                    width.roundToPx(),
+                    height.roundToPx(),
+                    Color.Red
+                )
+            }
+            Icon(image, null, modifier = Modifier.testTag(testTag), tint = Color.Unspecified)
+        }
+
+        // With no color provided for a tint, the icon should render the original pixels
+        rule.onNodeWithTag(testTag).captureToImage().assertPixels { Color.Red }
+    }
+
+    @SdkSuppress(minSdkVersion = Build.VERSION_CODES.O)
+    @Test
+    fun iconSpecifiedTintColorApplied() {
+        val width = 35.dp
+        val height = 83.dp
+        val testTag = "testTag"
+        rule.setMaterialContent {
+            val image: ImageBitmap
+            with(LocalDensity.current) {
+                image = createBitmapWithColor(
+                    this,
+                    width.roundToPx(),
+                    height.roundToPx(),
+                    Color.Red
+                )
+            }
+            Icon(image, null, modifier = Modifier.testTag(testTag), tint = Color.Blue)
+        }
+
+        // With a tint color provided, all pixels should be blue
+        rule.onNodeWithTag(testTag).captureToImage().assertPixels { Color.Blue }
+    }
+
+    @Test
+    fun defaultSemanticsWhenContentDescriptionProvided() {
+        val testTag = "TestTag"
+        rule.setContent {
+            Icon(
+                bitmap = ImageBitmap(100, 100),
+                contentDescription = "qwerty",
+                modifier = Modifier.testTag(testTag)
+            )
+        }
+
+        rule.onNodeWithTag(testTag)
+            .assertContentDescriptionEquals("qwerty")
+            .assert(SemanticsMatcher.expectValue(SemanticsProperties.Role, Role.Image))
+    }
+
+    private fun createBitmapWithColor(
+        density: Density,
+        width: Int,
+        height: Int,
+        color: Color
+    ): ImageBitmap {
+        val size = Size(width.toFloat(), height.toFloat())
+        val image = ImageBitmap(width, height)
+        CanvasDrawScope().draw(
+            density,
+            LayoutDirection.Ltr,
+            Canvas(image),
+            size
+        ) {
+            drawRect(color)
+        }
+        return image
+    }
+}
