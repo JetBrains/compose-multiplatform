435408d5d5c7c746eec72498333e66d5a329215a
Shalom Gibly <sgibly@google.com>
2021-09-24 11:17:05 -0700

    Updates Material3 typography tokens support.
    Bug: 197874682
    Test: Manually ran material3:compileDebugKotlin
    Relnote: N/A
    Change-Id: I87be0956d84d2435a7313565cad7ec6da3997288

--- material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/Typeface.kt
+++ material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/Typeface.kt
@@ -21,12 +21,10 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 
 internal object Typeface {
-    val BrandDisplayRegular = FontFamily.SansSerif
     val BrandMedium = FontFamily.SansSerif
     val BrandRegular = FontFamily.SansSerif
     val PlainMedium = FontFamily.SansSerif
     val PlainRegular = FontFamily.SansSerif
-
     val WeightBold = FontWeight.Bold
     val WeightMedium = FontWeight.Medium
     val WeightRegular = FontWeight.Normal
