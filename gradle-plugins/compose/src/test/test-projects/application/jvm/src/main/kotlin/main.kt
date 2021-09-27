3d66e5462bbc06cddae8e32a7667f76c91424dce
Igor Demin <igordmn@gmail.com>
2021-09-27 15:01:03 +0300

    Fix Gradle plugin test

--- gradle-plugins/compose/src/test/test-projects/application/jvm/src/main/kotlin/main.kt
+++ gradle-plugins/compose/src/test/test-projects/application/jvm/src/main/kotlin/main.kt
@@ -1,10 +1,10 @@
-import androidx.compose.desktop.Window
 import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.window.singleWindowApplication
 import androidx.compose.runtime.*
 
-fun main() = Window {
+fun main() = singleWindowApplication {
     val scope = rememberCoroutineScope()
     var text by remember { mutableStateOf("Hello, World!") }
 
