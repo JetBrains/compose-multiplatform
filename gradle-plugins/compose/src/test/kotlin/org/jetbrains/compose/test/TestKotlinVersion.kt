86bf3115b5153cefe9f549e0734ee0effe0fc7a6
Nikolay Igotti <olonho@users.noreply.github.com>
2021-09-27 18:03:34 +0300

    Sync/2021 09 27 (#1203)

--- gradle-plugins/compose/src/test/kotlin/org/jetbrains/compose/test/TestKotlinVersion.kt
+++ gradle-plugins/compose/src/test/kotlin/org/jetbrains/compose/test/TestKotlinVersion.kt
@@ -8,6 +8,6 @@ package org.jetbrains.compose.test
 @Suppress("EnumEntryName")
 enum class TestKotlinVersion(val versionString: String) {
     // __KOTLIN_COMPOSE_VERSION__
-    Default("1.5.30"),
+    Default("1.5.31"),
     V1_5_20("1.5.20")
 }
--- templates/web-template/build.gradle.kts
+++ templates/web-template/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     // __KOTLIN_COMPOSE_VERSION__
-    kotlin("multiplatform") version "1.5.30"
+    kotlin("multiplatform") version "1.5.31"
     // __LATEST_COMPOSE_RELEASE_VERSION__
     id("org.jetbrains.compose") version ("1.0.0-alpha4-build328")
 }
