cd3ce20e97961f4a2271d9b9528684e7ed32cd2a
Igor Demin <igordmn@gmail.com>
2021-09-28 10:37:10 +0300

    Miggrate to 1.0.0-alpha4-build362

--- components/VideoPlayer/demo/src/jvmMain/kotlin/org/jetbrains/compose/videoplayer/demo/Main.kt
+++ components/VideoPlayer/demo/src/jvmMain/kotlin/org/jetbrains/compose/videoplayer/demo/Main.kt
@@ -1,24 +1,21 @@
 package org.jetbrains.compose.videoplayer.demo
 
-import androidx.compose.desktop.DesktopTheme
-import androidx.compose.desktop.Window
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.window.singleWindowApplication
 import org.jetbrains.compose.videoplayer.VideoPlayer
 
 fun main() {
-    Window(
+    singleWindowApplication(
         title = "Video Player",
         size = IntSize(800, 800)
     ) {
         MaterialTheme {
-            DesktopTheme {
-                VideoPlayer(
-                    url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-                    width = 640,
-                    height = 480
-                )
-            }
+            VideoPlayer(
+                url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+                width = 640,
+                height = 480
+            )
         }
     }
 }
--- templates/multiplatform-template/android/build.gradle.kts
+++ templates/multiplatform-template/android/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    compileSdkVersion(30)
+    compileSdkVersion(31)
 
     defaultConfig {
         minSdkVersion(21)
-        targetSdkVersion(30)
+        targetSdkVersion(31)
         versionCode = 1
         versionName = "1.0"
     }
