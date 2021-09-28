469c1a8e7f3f62cab87701e5f083b8a510d9dc5d
Igor Demin <igordmn@gmail.com>
2021-09-28 13:37:31 +0300

    Fix tutorials

--- templates/web-template/build.gradle.kts
+++ templates/web-template/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     // __KOTLIN_COMPOSE_VERSION__
     kotlin("multiplatform") version "1.5.31"
     // __LATEST_COMPOSE_RELEASE_VERSION__
-    id("org.jetbrains.compose") version ("1.0.0-alpha4-build328")
+    id("org.jetbrains.compose") version ("1.0.0-alpha4-build362")
 }
 
 repositories {
