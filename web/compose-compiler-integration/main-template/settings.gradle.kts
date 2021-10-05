3795ad1fb950091a53c25c3120f3e507485949f1
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2021-10-05 10:35:43 +0200

    web: add compiler test case `Composable With Default Values Defined By Other Params` (#1230)
    Co-authored-by: Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>

--- web/compose-compiler-integration/main-template/settings.gradle.kts
+++ web/compose-compiler-integration/main-template/settings.gradle.kts
@@ -1,5 +1,6 @@
 pluginManagement {
     repositories {
+        mavenLocal()
         gradlePluginPortal()
         maven {
             url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev")
