5d499362463c472705210235bd3a2406e28e387b
Nikolay Igotti <igotti@gmail.com>
2021-10-20 09:25:00 +0300

    Move Falling Balls to 0.0.0-master-build413

--- examples/falling-balls/build.gradle.kts
+++ examples/falling-balls/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     // __KOTLIN_COMPOSE_VERSION__
     kotlin("jvm") version "1.5.31"
     // __LATEST_COMPOSE_RELEASE_VERSION__
-    id("org.jetbrains.compose") version "1.0.0-alpha4-build361"
+    id("org.jetbrains.compose") version "0.0.0-master-build413"
 }
 
 group = "me.user"
--- examples/falling-balls/src/main/kotlin/main.kt
+++ examples/falling-balls/src/main/kotlin/main.kt
@@ -1,14 +1,14 @@
 package org.jetbrains.compose.demo.falling
 
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.WindowSize
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.singleWindowApplication
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() = singleWindowApplication(
-    title = "Falling Balls", state = WindowState(size = WindowSize(800.dp, 800.dp))
+    title = "Falling Balls", state = WindowState(size = DpSize(800.dp, 800.dp))
 ) {
     FallingBallsGame()
 }
