0aedbb535944885bcb9079e513d8599746d07929
akurasov <alexander.kurasov@jetbrains.com>
2021-10-20 15:06:56 +0300

    Switching to latest Compose build and making UI more consistent

--- examples/todoapp-lite/common/src/commonMain/kotlin/example/todoapp/lite/common/EditDialog.kt
+++ examples/todoapp-lite/common/src/commonMain/kotlin/example/todoapp/lite/common/EditDialog.kt
@@ -1,18 +1,14 @@
 package example.todoapp.lite.common
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.sizeIn
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -37,9 +33,7 @@ internal fun EditDialog(
             Spacer(modifier = Modifier.height(8.dp))
 
             Row {
-                Text(text = "Completed")
-
-                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = "Completed", Modifier.padding(15.dp))
 
                 Checkbox(
                     checked = item.isDone,
