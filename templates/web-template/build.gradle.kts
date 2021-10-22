96fe1979e83792c67ab4c85c8c0445a34092667d
Nikolay Igotti <igotti@gmail.com>
2021-10-22 15:30:46 +0300

    WIP

--- templates/web-template/build.gradle.kts
+++ templates/web-template/build.gradle.kts
@@ -1,11 +1,12 @@
 import org.jetbrains.compose.compose
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
 
 plugins {
     // __KOTLIN_COMPOSE_VERSION__
     kotlin("multiplatform") version "1.5.31"
     // __LATEST_COMPOSE_RELEASE_VERSION__
-    id("org.jetbrains.compose") version ("1.0.0-alpha4-build362")
+    id("org.jetbrains.compose") version ("1.0.0-beta1")
 }
 
 repositories {
@@ -31,3 +32,12 @@ kotlin {
         }
     }
 }
+
+// a temporary workaround for a bug in jsRun invocation - see https://youtrack.jetbrains.com/issue/KT-48273
+afterEvaluate {
+        rootProject.extensions.configure<NodeJsRootExtension> {
+        versions.webpackDevServer.version = "4.0.0"
+        versions.webpackCli.version = "4.9.0"
+    }
+}
+
