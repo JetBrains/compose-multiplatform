8002705c9e851c748f8f50a6def68934c852a81c
Nikolay Igotti <olonho@users.noreply.github.com>
2021-10-22 11:46:55 +0300

    Update codeviewer example (#1292)

--- examples/codeviewer/common/src/desktopMain/kotlin/org/jetbrains/codeviewer/platform/Mouse.kt
+++ examples/codeviewer/common/src/desktopMain/kotlin/org/jetbrains/codeviewer/platform/Mouse.kt
@@ -1,38 +1,21 @@
 package org.jetbrains.codeviewer.platform
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.pointer.PointerIcon
-import androidx.compose.ui.input.pointer.pointerIcon
+import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.input.pointer.pointerMoveFilter
 import java.awt.Cursor
 
+@OptIn(ExperimentalComposeUiApi::class)
 actual fun Modifier.pointerMoveFilter(
     onEnter: () -> Boolean,
     onExit: () -> Boolean,
     onMove: (Offset) -> Boolean
 ): Modifier = this.pointerMoveFilter(onEnter = onEnter, onExit = onExit, onMove = onMove)
 
-@OptIn(ExperimentalComposeUiApi::class)
-actual fun Modifier.cursorForHorizontalResize(): Modifier = composed {
-    var isHover by remember { mutableStateOf(false) }
 
-    pointerMoveFilter(
-        onEnter = { isHover = true; true },
-        onExit = { isHover = false; true }
-    ).pointerIcon(
-        PointerIcon(
-            if (isHover) {
-                Cursor(Cursor.E_RESIZE_CURSOR)
-            } else {
-                Cursor.getDefaultCursor()
-            }
-        )
-    )
-}
+@OptIn(ExperimentalComposeUiApi::class)
+actual fun Modifier.cursorForHorizontalResize(): Modifier =
+    this.pointerHoverIcon(PointerIcon(Cursor(Cursor.E_RESIZE_CURSOR)))
