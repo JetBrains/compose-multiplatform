591475fa083abb74a9e44a5c1279f903111bfc5a
Igor Demin <igordmn@gmail.com>
2021-10-25 13:45:05 +0300

    Examples. Issues stayed opened even after closing the window

--- examples/issues/desktop/src/jvmMain/kotlin/androidx/ui/examples/jetissues/Main.kt
+++ examples/issues/desktop/src/jvmMain/kotlin/androidx/ui/examples/jetissues/Main.kt
@@ -1,6 +1,7 @@
 package androidx.ui.examples.jetissues
 
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowSize
@@ -11,17 +12,21 @@ import androidx.ui.examples.jetissues.view.Repository
 import androidx.ui.examples.jetissues.data.IssuesRepositoryImpl
 import androidx.ui.examples.jetissues.data.defaultAuth
 import androidx.ui.examples.jetissues.data.defaultRepo
+import kotlin.system.exitProcess
 
 val repo = IssuesRepositoryImpl(defaultRepo.first, defaultRepo.second, System.getenv("GITHUB_TOKEN") ?: defaultAuth)
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "JetIssues",
-        state = WindowState(size = WindowSize(1440.dp, 768.dp))
-    ) {
-        CompositionLocalProvider(Repository provides repo) {
-            JetIssuesView()
+fun main() {
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "JetIssues",
+            state = WindowState(size = DpSize(1440.dp, 768.dp))
+        ) {
+            CompositionLocalProvider(Repository provides repo) {
+                JetIssuesView()
+            }
         }
     }
+    exitProcess(0) // force close Apollo Client, using inside IssuesRepositoryImpl
 }
