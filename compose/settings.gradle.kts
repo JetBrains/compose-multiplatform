80c23a975d9dd8468c53d4486542cdcfd9a906e4
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2021-10-28 15:39:05 +0300

    Configure publishing to Maven Central
    Fixes #1106

--- compose/settings.gradle.kts
+++ compose/settings.gradle.kts
@@ -1,1 +1,14 @@
+pluginManagement {
+    buildscript {
+        repositories {
+            mavenCentral()
+            maven("https://maven.pkg.jetbrains.space/public/p/compose/internal")
+            maven("https://maven.pkg.jetbrains.space/public/p/space/maven")
+        }
+        dependencies {
+            classpath("org.jetbrains.compose.internal.build-helpers:publishing:0.0.2")
+        }
+    }
+}
+
 includeBuild("frameworks/support")
