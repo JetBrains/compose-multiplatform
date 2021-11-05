6fbe65ba256b0d2c57e6e547b3fcbc9bc6887065
Andrey Kulikov <andreykulikov@google.com>
2021-11-05 17:23:52 +0000

    Improve the error message for measuring Lazy lists with infinity constraints
    More info were added as nesting same axis scrollable components is not the only reason for the issue to happen.
    
    Fixes: 193415858
    Bug: 192017018
    Test: n/a
    Change-Id: I1d8119c76f75d679840a0b4c90005401bd80a6cc

--- foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/Scroll.kt
+++ foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/Scroll.kt
@@ -381,19 +381,27 @@ private data class ScrollingLayoutModifier(
 internal fun Constraints.assertNotNestingScrollableContainers(isVertical: Boolean) {
     if (isVertical) {
         check(maxHeight != Constraints.Infinity) {
-            "Nesting scrollable in the same direction layouts like LazyColumn and Column(Modifier" +
-                ".verticalScroll()) is not allowed. If you want to add a header before the list " +
-                "of items please take a look on LazyColumn component which has a DSL api which" +
-                " allows to first add a header via item() function and then the list of " +
-                "items via items()."
+            "Vertically scrollable component was measured with an infinity maximum height " +
+                "constraints, which is disallowed. One of the common reasons is nesting layouts " +
+                "like LazyColumn and Column(Modifier.verticalScroll()). If you want to add a " +
+                "header before the list of items please add a header as a separate item() before " +
+                "the main items() inside the LazyColumn scope. There are could be other reasons " +
+                "for this to happen: your ComposeView was added into a LinearLayout with some " +
+                "weight, you applied Modifier.wrapContentSize(unbounded = true) or wrote a " +
+                "custom layout. Please try to remove the source of infinite constraints in the " +
+                "hierarchy above the scrolling container."
         }
     } else {
         check(maxWidth != Constraints.Infinity) {
-            "Nesting scrollable in the same direction layouts like LazyRow and Row(Modifier" +
-                ".horizontalScroll() is not allowed. If you want to add a header before the list " +
-                "of items please take a look on LazyRow component which has a DSL api which " +
-                "allows to first add a fixed element via item() function and then the " +
-                "list of items via items()."
+            "Horizontally scrollable component was measured with an infinity maximum width " +
+                "constraints, which is disallowed. One of the common reasons is nesting layouts " +
+                "like LazyRow and Row(Modifier.horizontalScroll()). If you want to add a " +
+                "header before the list of items please add a header as a separate item() before " +
+                "the main items() inside the LazyRow scope. There are could be other reasons " +
+                "for this to happen: your ComposeView was added into a LinearLayout with some " +
+                "weight, you applied Modifier.wrapContentSize(unbounded = true) or wrote a " +
+                "custom layout. Please try to remove the source of infinite constraints in the " +
+                "hierarchy above the scrolling container."
         }
     }
 }
