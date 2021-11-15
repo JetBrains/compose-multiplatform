d226a43e79ef8c2ca29335bc792aea6e498a4da0
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2021-11-15 16:22:56 +0100

    try mpp-template with kotlin 1.6.0-rc2

--- templates/multiplatform-template/build.gradle.kts
+++ templates/multiplatform-template/build.gradle.kts
@@ -1,6 +1,6 @@
 buildscript {
     // __LATEST_COMPOSE_RELEASE_VERSION__
-    val composeVersion = System.getenv("COMPOSE_TEMPLATE_COMPOSE_VERSION") ?: "1.0.0-beta5"
+    val composeVersion = "0.0.0-use_new_compose_version-dev459"//System.getenv("COMPOSE_TEMPLATE_COMPOSE_VERSION") ?: "1.0.0-beta5"
 
     repositories {
         mavenCentral()
@@ -10,9 +10,9 @@ buildscript {
 
     dependencies {
         classpath("org.jetbrains.compose:compose-gradle-plugin:$composeVersion")
-        classpath("com.android.tools.build:gradle:4.1.3")
+        classpath("com.android.tools.build:gradle:7.0.0")
         // __KOTLIN_COMPOSE_VERSION__
-        classpath(kotlin("gradle-plugin", version = "1.5.31"))
+        classpath(kotlin("gradle-plugin", version = "1.6.0-RC2"))
     }
 }
 
--- templates/multiplatform-template/common/build.gradle.kts
+++ templates/multiplatform-template/common/build.gradle.kts
@@ -35,8 +35,8 @@ android {
     defaultConfig {
         minSdkVersion(21)
         targetSdkVersion(31)
-        versionCode = 1
-        versionName = "1.0"
+//        versionCode = 1
+//        versionName = "1.0"
     }
 
     compileOptions {
