55c6908ec388c82bd98f4cc492339f0e63bde273
David Saff <saff@google.com>
2021-11-18 13:50:02 -0500

    Flaky and broken tests for 2021.11.18
    Test: N/A
    Change-Id: Id82a3c5e3ca99d15c83d35c0a7e813723f957210

--- ui/ui/src/androidAndroidTest/kotlin/androidx/compose/ui/input/pointer/PointerInteropFilterAndroidViewHookupTest.kt
+++ ui/ui/src/androidAndroidTest/kotlin/androidx/compose/ui/input/pointer/PointerInteropFilterAndroidViewHookupTest.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.test.TestActivity
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.FlakyTest
 import androidx.test.filters.MediumTest
 import com.google.common.truth.Truth.assertThat
 import org.junit.Before
@@ -477,6 +478,7 @@ class PointerInteropFilterAndroidViewHookupTest {
         assertThat(captureRequestDisallow.disallowIntercept).isFalse()
     }
 
+    @FlakyTest(bugId = 206967867)
     @Test
     fun disallowTriggeredWhenMovementInClickChildAfterRequestDisallow() {
         var clicked = false
