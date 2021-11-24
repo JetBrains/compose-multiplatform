80d5a1fbb5fb71b327a1431d70226298e55aefdd
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2021-11-24 13:51:21 +0100

    fix typo

--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposePlugin.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposePlugin.kt
@@ -205,7 +205,7 @@ class ComposePlugin : Plugin<Project> {
             composeDependency("org.jetbrains.compose.web:web-svg")
         }
 
-        @Deprecated("compose.web.web-widgets API is not deprecated")
+        @Deprecated("compose.web.web-widgets API is deprecated")
         val widgets by lazy {
             composeDependency("org.jetbrains.compose.web:web-widgets")
         }
