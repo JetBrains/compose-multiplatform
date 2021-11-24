8a5f0d73d9e3504eb52e0791bcb5612eb6c73ebd
Andrey Kulikov <andreykulikov@google.com>
2021-11-24 17:57:14 +0000

    Support negative offsets in scroll functions on LazyListState
    Fixes: 184252837
    Test: new tests in LazyScrollTest
    Relnote: Now it is allowed to pass negative scroll offsets into LazyListState.scrollToItem() and LazyListState.animateScrollToItem().
    Change-Id: Iceb907f268a19db3e9315154ebd136764ad975a3

--- foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/lazy/LazyListState.kt
+++ foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/lazy/LazyListState.kt
@@ -181,18 +181,15 @@ class LazyListState constructor(
      * Instantly brings the item at [index] to the top of the viewport, offset by [scrollOffset]
      * pixels.
      *
-     * Cancels the currently running scroll, if any, and suspends until the cancellation is
-     * complete.
-     *
-     * @param index the data index to snap to. Must be between 0 and the number of elements.
-     * @param scrollOffset the number of pixels past the start of the item to snap to. Must
-     * not be negative.
+     * @param index the index to which to scroll. Must be non-negative.
+     * @param scrollOffset the offset that the item should end up after the scroll. Note that
+     * positive offset refers to forward scroll, so in a top-to-bottom list, positive offset will
+     * scroll the item further upward (taking it partly offscreen).
      */
     @OptIn(ExperimentalFoundationApi::class)
     suspend fun scrollToItem(
         /*@IntRange(from = 0)*/
         index: Int,
-        /*@IntRange(from = 0)*/
         scrollOffset: Int = 0
     ) {
         return scrollableState.scroll {
@@ -212,9 +209,6 @@ class LazyListState constructor(
      * via [ScrollScope.scrollBy]. All actions that change the logical scroll position must be
      * performed within a [scroll] block (even if they don't call any other methods on this
      * object) in order to guarantee that mutual exclusion is enforced.
-     *
-     * Cancels the currently running scroll, if any, and suspends until the cancellation is
-     * complete.
      *
      * If [scroll] is called from elsewhere, this will be canceled.
      */
@@ -306,16 +300,14 @@ class LazyListState constructor(
     /**
      * Animate (smooth scroll) to the given item.
      *
-     * @param index the index to which to scroll
-     * @param scrollOffset the offset that the item should end up after the scroll (same as
-     * [scrollToItem]) - note that positive offset refers to forward scroll, so in a
-     * top-to-bottom list, positive offset will scroll the item further upward (taking it partly
-     * offscreen)
+     * @param index the index to which to scroll. Must be non-negative.
+     * @param scrollOffset the offset that the item should end up after the scroll. Note that
+     * positive offset refers to forward scroll, so in a top-to-bottom list, positive offset will
+     * scroll the item further upward (taking it partly offscreen)
      */
     suspend fun animateScrollToItem(
         /*@IntRange(from = 0)*/
         index: Int,
-        /*@IntRange(from = 0)*/
         scrollOffset: Int = 0
     ) {
         doSmoothScrollToItem(index, scrollOffset)
