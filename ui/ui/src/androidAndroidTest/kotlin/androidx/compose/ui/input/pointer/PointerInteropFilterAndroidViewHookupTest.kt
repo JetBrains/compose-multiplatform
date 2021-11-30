d98dcbb8f0928d53820f44e26756d93ea4e92db6
George Mount <mount@google.com>
2021-11-30 21:58:00 +0000

    Revert "Child Views now receive pointer events during Main pass"
    This reverts commit 9d8d577ac629d83419cf9f441140a44d8f08430f.
    
    Reason for revert: b/207462546
    
    Change-Id: Id34b9268c8f50866d8a0b8a45790bdaf81db2b35

--- ui/ui/src/androidAndroidTest/kotlin/androidx/compose/ui/input/pointer/PointerInteropFilterAndroidViewHookupTest.kt
+++ ui/ui/src/androidAndroidTest/kotlin/androidx/compose/ui/input/pointer/PointerInteropFilterAndroidViewHookupTest.kt
@@ -280,7 +280,7 @@ class PointerInteropFilterAndroidViewHookupTest {
     }
 
     @Test
-    fun ui_downMove_moveIsDispatchedDuringMain() {
+    fun ui_downMove_moveIsDispatchedDuringFinal() {
         val down =
             MotionEvent(
                 0,
@@ -310,9 +310,9 @@ class PointerInteropFilterAndroidViewHookupTest {
 
         assertThat(eventStringLog).hasSize(4)
         assertThat(eventStringLog[0]).isEqualTo(PointerEventPass.Initial.toString())
-        assertThat(eventStringLog[1]).isEqualTo("motionEvent")
-        assertThat(eventStringLog[2]).isEqualTo(PointerEventPass.Main.toString())
-        assertThat(eventStringLog[3]).isEqualTo(PointerEventPass.Final.toString())
+        assertThat(eventStringLog[1]).isEqualTo(PointerEventPass.Main.toString())
+        assertThat(eventStringLog[2]).isEqualTo(PointerEventPass.Final.toString())
+        assertThat(eventStringLog[3]).isEqualTo("motionEvent")
     }
 
     @Test
@@ -353,7 +353,7 @@ class PointerInteropFilterAndroidViewHookupTest {
     }
 
     @Test
-    fun ui_downDisallowInterceptMoveAllowInterceptMove_2ndMoveIsDispatchedDuringMain() {
+    fun ui_downDisallowInterceptMoveAllowInterceptMove_2ndMoveIsDispatchedDuringFinal() {
         val down =
             MotionEvent(
                 0,
@@ -396,13 +396,13 @@ class PointerInteropFilterAndroidViewHookupTest {
 
         assertThat(eventStringLog).hasSize(4)
         assertThat(eventStringLog[0]).isEqualTo(PointerEventPass.Initial.toString())
-        assertThat(eventStringLog[1]).isEqualTo("motionEvent")
-        assertThat(eventStringLog[2]).isEqualTo(PointerEventPass.Main.toString())
-        assertThat(eventStringLog[3]).isEqualTo(PointerEventPass.Final.toString())
+        assertThat(eventStringLog[1]).isEqualTo(PointerEventPass.Main.toString())
+        assertThat(eventStringLog[2]).isEqualTo(PointerEventPass.Final.toString())
+        assertThat(eventStringLog[3]).isEqualTo("motionEvent")
     }
 
     @Test
-    fun ui_downDisallowInterceptUpDownMove_2ndMoveIsDispatchedDuringMain() {
+    fun ui_downDisallowInterceptUpDownMove_2ndMoveIsDispatchedDuringFinal() {
         val down =
             MotionEvent(
                 0,
@@ -455,9 +455,9 @@ class PointerInteropFilterAndroidViewHookupTest {
 
         assertThat(eventStringLog).hasSize(4)
         assertThat(eventStringLog[0]).isEqualTo(PointerEventPass.Initial.toString())
-        assertThat(eventStringLog[1]).isEqualTo("motionEvent")
-        assertThat(eventStringLog[2]).isEqualTo(PointerEventPass.Main.toString())
-        assertThat(eventStringLog[3]).isEqualTo(PointerEventPass.Final.toString())
+        assertThat(eventStringLog[1]).isEqualTo(PointerEventPass.Main.toString())
+        assertThat(eventStringLog[2]).isEqualTo(PointerEventPass.Final.toString())
+        assertThat(eventStringLog[3]).isEqualTo("motionEvent")
     }
 
     @Test
