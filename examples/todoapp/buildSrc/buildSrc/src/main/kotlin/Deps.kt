2300750f329657cdaa4bf18b51335c705b29c85b
Nikolay Igotti <igotti@gmail.com>
2021-12-01 16:18:20 +0300

    Move todoapp to 10.0.0-rc12

--- examples/todoapp/buildSrc/buildSrc/src/main/kotlin/Deps.kt
+++ examples/todoapp/buildSrc/buildSrc/src/main/kotlin/Deps.kt
@@ -13,7 +13,7 @@ object Deps {
 
         object Compose {
             // __LATEST_COMPOSE_RELEASE_VERSION__
-            private const val VERSION = "1.0.0-beta5"
+            private const val VERSION = "1.0.0-rc12"
             const val gradlePlugin = "org.jetbrains.compose:compose-gradle-plugin:$VERSION"
         }
     }
