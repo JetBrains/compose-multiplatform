9dce4fe0fc001ada2ac37447b0b37dabbd788006
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2021-12-06 10:28:11 +0100

    web: add KT-48273 workaround to web-integration-core module

--- web/integration-core/build.gradle.kts
+++ web/integration-core/build.gradle.kts
@@ -89,3 +89,11 @@ tasks.named<Test>("jvmTest") {
         }
     }
 }
+
+// a temporary workaround for a bug in jsRun invocation - see https://youtrack.jetbrains.com/issue/KT-48273
+afterEvaluate {
+    rootProject.extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
+        versions.webpackDevServer.version = "4.0.0"
+        versions.webpackCli.version = "4.9.0"
+    }
+}
