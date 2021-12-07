5880c7be6a621b569ad6cd7ca3582461c986557d
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-12-07 15:25:20 +0100

    Move config path to test-utils/conf/karma.config.common.d

--- web/test-utils/conf/karma.config.common.d/patch.js
+++ web/test-utils/conf/karma.config.common.d/patch.js
@@ -1,3 +1,4 @@
 config.plugins = config.plugins || [];
 config.plugins = config.plugins.filter(it => it !== "kotlin-test-js-runner/karma-kotlin-reporter.js");
-config.plugins.push("../../../../../karma-kotlin-runner-decorator/karma-kotlin-reporter-decorated.js")
+config.plugins.push("../../../../../test-utils/conf/karma-kotlin-runner-decorator/karma-kotlin-reporter-decorated.js");
+
