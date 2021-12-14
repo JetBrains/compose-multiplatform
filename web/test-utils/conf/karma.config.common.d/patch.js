5d8f0fe103d78f36aeef770e7871012f5f81e56c
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2021-12-14 15:52:10 +0100

    web: increase timeout for tests (in karma.config.common.d) (#1592)
    * web: increase timeout for tests (in karma.config.common.d)
    
    * web: add semicolons in karma.config.common.d
    
    Co-authored-by: Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>

--- web/test-utils/conf/karma.config.common.d/patch.js
+++ web/test-utils/conf/karma.config.common.d/patch.js
@@ -2,3 +2,10 @@ config.plugins = config.plugins || [];
 config.plugins = config.plugins.filter(it => it !== "kotlin-test-js-runner/karma-kotlin-reporter.js");
 config.plugins.push("../../../../../test-utils/conf/karma-kotlin-runner-decorator/karma-kotlin-reporter-decorated.js");
 
+
+config.client.mocha = config.client.mocha || {};
+config.client.mocha.timeout = 10000;
+
+config.browserNoActivityTimeout = 10000;
+config.browserDisconnectTimeout = 10000;
+config.browserDisconnectTolerance = 3;
