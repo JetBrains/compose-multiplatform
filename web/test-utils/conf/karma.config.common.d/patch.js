3f2842aac61722c87e2a486e96aece39a8337401
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2021-12-14 15:47:52 +0100

    web: add semicolons in karma.config.common.d

--- web/test-utils/conf/karma.config.common.d/patch.js
+++ web/test-utils/conf/karma.config.common.d/patch.js
@@ -3,9 +3,9 @@ config.plugins = config.plugins.filter(it => it !== "kotlin-test-js-runner/karma
 config.plugins.push("../../../../../test-utils/conf/karma-kotlin-runner-decorator/karma-kotlin-reporter-decorated.js");
 
 
-config.client.mocha = config.client.mocha || {}
-config.client.mocha.timeout = 10000
+config.client.mocha = config.client.mocha || {};
+config.client.mocha.timeout = 10000;
 
-config.browserNoActivityTimeout = 10000
-config.browserDisconnectTimeout = 10000
-config.browserDisconnectTolerance = 3
+config.browserNoActivityTimeout = 10000;
+config.browserDisconnectTimeout = 10000;
+config.browserDisconnectTolerance = 3;
