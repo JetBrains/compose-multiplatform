8c6875893f2e0d6a5644e80e9a7093f10e501b32
alexander-gorshenev <alexander.gorshenev@jetbrains.com>
2021-12-16 23:53:54 +0300

    [mpp example] Added xcode things (#1608)
    * [mpp example] Added xcode things for native targets
    
    * no cache for all native targets

--- examples/falling-balls-mpp/build.gradle.kts
+++ examples/falling-balls-mpp/build.gradle.kts
@@ -56,7 +56,18 @@ kotlin {
         }
     }
     iosX64("uikitX64") {
-        binaries.executable()
+        binaries {
+            executable() {
+                entryPoint = "main"
+                freeCompilerArgs += listOf(
+                    "-linker-option", "-framework", "-linker-option", "Metal",
+                    "-linker-option", "-framework", "-linker-option", "CoreText",
+                    "-linker-option", "-framework", "-linker-option", "CoreGraphics"
+                )
+                // TODO: the current compose binary surprises LLVM, so disable checks for now.
+                freeCompilerArgs += "-Xdisable-phases=VerifyBitcode"
+            }
+        }
     }
     iosArm64("uikitArm64") {
         binaries.executable()
@@ -146,5 +157,61 @@ afterEvaluate {
     rootProject.extensions.configure<NodeJsRootExtension> {
         versions.webpackDevServer.version = "4.0.0"
         versions.webpackCli.version = "4.9.0"
+    }
+}
+
+enum class Target(val simulator: Boolean, val key: String) {
+    UIKIT_X64(true, "uikitX64"), UIKIT_ARM64(false, "uikitArm64")
+}
+
+if (System.getProperty("os.name") == "Mac OS X") {
+// Create Xcode integration tasks.
+    val sdkName: String? = System.getenv("SDK_NAME")
+
+    val target = sdkName.orEmpty().let {
+        when {
+            it.startsWith("iphoneos") -> Target.UIKIT_ARM64
+            it.startsWith("iphonesimulator") -> Target.UIKIT_X64
+            else -> Target.UIKIT_X64
+        }
+    }
+
+    val targetBuildDir: String? = System.getenv("TARGET_BUILD_DIR")
+    val executablePath: String? = System.getenv("EXECUTABLE_PATH")
+    val buildType = System.getenv("CONFIGURATION")?.let {
+        org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType.valueOf(it.toUpperCase())
+    } ?: org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType.DEBUG
+
+    val currentTarget = kotlin.targets[target.key] as org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+    val kotlinBinary = currentTarget.binaries.getExecutable(buildType)
+    val xcodeIntegrationGroup = "Xcode integration"
+
+    val packForXCode = if (sdkName == null || targetBuildDir == null || executablePath == null) {
+        // The build is launched not by Xcode ->
+        // We cannot create a copy task and just show a meaningful error message.
+        tasks.create("packForXCode").doLast {
+            throw IllegalStateException("Please run the task from Xcode")
+        }
+    } else {
+        // Otherwise copy the executable into the Xcode output directory.
+        tasks.create("packForXCode", Copy::class.java) {
+            dependsOn(kotlinBinary.linkTask)
+
+            destinationDir = file(targetBuildDir)
+
+            val dsymSource = kotlinBinary.outputFile.absolutePath + ".dSYM"
+            val dsymDestination = File(executablePath).parentFile.name + ".dSYM"
+            val oldExecName = kotlinBinary.outputFile.name
+            val newExecName = File(executablePath).name
+
+            from(dsymSource) {
+                into(dsymDestination)
+                rename(oldExecName, newExecName)
+            }
+
+            from(kotlinBinary.outputFile) {
+                rename { executablePath }
+            }
+        }
     }
 }
