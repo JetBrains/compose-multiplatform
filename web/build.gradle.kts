abc6d1f60aee1edae898e6a5cdd7c207618d2a90
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2021-12-22 12:58:09 +0100

    [web] Force kotlin/js to use node@16.13.1

--- web/build.gradle.kts
+++ web/build.gradle.kts
@@ -19,6 +19,11 @@ tasks.register("printBundleSize") {
     )
 }
 
+// see https://youtrack.jetbrains.com/issue/KT-49109#focus=Comments-27-5381158.0-0
+rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin> {
+    rootProject.the<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>().nodeVersion = "16.13.1"
+}
+
 subprojects { 
     apply(plugin = "maven-publish")
 
