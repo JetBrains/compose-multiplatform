32dd7e50a853670b1fdc831ee609977708bd46d7
Nikolay Igotti <igotti@gmail.com>
2021-12-24 20:52:37 +0300

    Add preview.

--- examples/falling-balls-mpp/src/jvmMain/kotlin/main.jvm.kt
+++ examples/falling-balls-mpp/src/jvmMain/kotlin/main.jvm.kt
@@ -3,6 +3,8 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
  */
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.unit.DpSize
@@ -23,3 +25,10 @@ fun main() =
         val game = remember { Game(JvmTime) }
         FallingBalls(game)
     }
+
+@Preview
+@Composable
+fun GamePreview() {
+    val game = remember { Game(JvmTime) }
+    FallingBalls(game)
+}
