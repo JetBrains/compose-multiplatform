6e5b77aadb0a789e73d2b11917e6eb5b280a7ae7
Jon Boekenoogen <jboekeno@google.com>
2022-01-02 18:53:10 +0000

    Fix AOT compilation breaking issues
    Most important is the missing @DoNotInline annotations. Also switched
    from nested companion objects to use an outer object directly. This
    helps remove an additional class in the generated bytecode.
    
    Highest impact is likely RenderNodeLayer which would fail class
    verification and cannot compile its methods to native code without this
    change. It has a few methods that are heavily used.
    
    Change-Id: Ie6cd4bd4b8c6c364d7ac366b08c7cde9fd4a39e3

--- ui/ui/src/androidMain/kotlin/androidx/compose/ui/platform/ViewLayer.android.kt
+++ ui/ui/src/androidMain/kotlin/androidx/compose/ui/platform/ViewLayer.android.kt
@@ -98,12 +98,10 @@ internal class ViewLayer(
         }
 
     @RequiresApi(29)
-    private class UniqueDrawingIdApi29 {
-        @RequiresApi(29)
-        companion object {
-            @JvmStatic
-            fun getUniqueDrawingId(view: View) = view.uniqueDrawingId
-        }
+    private object UniqueDrawingIdApi29 {
+        @JvmStatic
+        @androidx.annotation.DoNotInline
+        fun getUniqueDrawingId(view: View) = view.uniqueDrawingId
     }
 
     /**
