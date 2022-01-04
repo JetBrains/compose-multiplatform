b9d704c792a43278cac9c9c8b23bb7d2410c1c76
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2022-01-04 13:42:16 +0100

    Remove files borrowed from web-landing

--- examples/web-skia/src/jsMain/kotlin/com/sample/Main.kt
+++ examples/web-skia/src/jsMain/kotlin/com/sample/Main.kt
@@ -1,25 +1,8 @@
-package com.sample
+package org.jetbrains.compose.sample
 
-import org.jetbrains.compose.web.css.Style
 import org.jetbrains.compose.web.renderComposable
-import com.sample.components.*
-import com.sample.content.*
-import com.sample.style.AppStylesheet
 
 fun main() {
     renderComposable(rootElementId = "root") {
-        Style(AppStylesheet)
-
-        Layout {
-            Header()
-            MainContentLayout {
-                Intro()
-                ComposeWebLibraries()
-                GetStarted()
-                CodeSamples()
-                JoinUs()
-            }
-            PageFooter()
-        }
     }
 }
