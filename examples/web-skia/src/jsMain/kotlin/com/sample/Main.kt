2091135a3d78d7f868c4021179e1381779137a57
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2022-01-04 16:30:51 +0100

    Minimal example of what do I want to achieve

--- examples/web-skia/src/jsMain/kotlin/com/sample/Main.kt
+++ examples/web-skia/src/jsMain/kotlin/com/sample/Main.kt
@@ -1,15 +1,53 @@
 package org.jetbrains.compose.sample
 
-import kotlinx.browser.window
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Switch
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.ComposeCanvas
 import org.jetbrains.compose.web.dom.Canvas
 import org.jetbrains.compose.web.renderComposable
 import org.jetbrains.skiko.wasm.onWasmReady
 
 fun main() {
-    renderComposable(rootElementId = "root") {
-        Canvas {
-            onWasmReady {
-                window.alert("HEY!!!!!")
+    onWasmReady {
+        renderComposable(rootElementId = "root") {
+            var switched by remember { mutableStateOf(false) }
+            Canvas({
+                attr("width", "300")
+                attr("height", "300")
+                style {
+                    property("outline", "1px solid black")
+                }
+            }) {
+                DomSideEffect { canvas ->
+                    ComposeCanvas(canvas).apply {
+                        setContent {
+                            Column {
+                                Button(
+                                    modifier = Modifier.padding(16.dp),
+                                    onClick = {
+                                        println("Button clicked!")
+                                    }
+                                ) {
+                                    Text(if (switched) "🦑 press 🐙" else "Press me!")
+                                }
+                                Switch(
+                                    modifier = Modifier.padding(16.dp),
+                                    checked = switched,
+                                    onCheckedChange = { switched = it }
+                                )
+                            }
+                        }
+                    }
+                }
             }
         }
     }
