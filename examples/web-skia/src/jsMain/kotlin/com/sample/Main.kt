305d98ff9fbe43094d91e0e09d0b37402be25ef2
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2022-01-07 15:00:56 +0100

    Concentrated circles animation

--- examples/web-skia/src/jsMain/kotlin/com/sample/Main.kt
+++ examples/web-skia/src/jsMain/kotlin/com/sample/Main.kt
@@ -2,16 +2,21 @@ package org.jetbrains.compose.sample
 
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.web.dom.Canvas
 import org.jetbrains.compose.web.dom.Table
 import org.jetbrains.compose.web.dom.Td
@@ -31,28 +36,23 @@ fun SomeCanvas() {
         }
     }) {
         skiko {
-            val radius = 100f
-            val animateFloat = remember { Animatable(0f) }
-            LaunchedEffect(animateFloat) {
-                animateFloat.animateTo(
-                    targetValue = 1f,
-                    animationSpec = tween(durationMillis = 3000, easing = LinearEasing)
+            val radius = remember { Animatable(70f) }
+            LaunchedEffect(radius) {
+                radius.animateTo(
+                    targetValue = 100f,
+                    animationSpec = infiniteRepeatable(tween(durationMillis = 1000, easing = LinearEasing), repeatMode = RepeatMode.Reverse)
                 )
             }
 
-            Canvas(modifier = Modifier.fillMaxSize()) {
-                drawArc(
-                    color = androidx.compose.ui.graphics.Color.Black,
-                    startAngle = 0f,
-                    sweepAngle = 360f * animateFloat.value,
-                    useCenter = false,
-                    topLeft = Offset(size.width / 4, size.height / 4),
-                    size = Size(
-                        radius * 2,
-                        radius * 2
-                    ),
-                    style = Stroke(2.0f)
-                )
+            Canvas(
+                modifier = Modifier
+                    .size(300.dp)
+                    .fillMaxSize()
+            ) {
+                drawCircle(color = androidx.compose.ui.graphics.Color.Red, radius = radius.value)
+                drawCircle(color = androidx.compose.ui.graphics.Color.LightGray, radius = 0.8f * radius.value)
+                drawCircle(color = androidx.compose.ui.graphics.Color.Green, radius = 0.6f * radius.value)
+                drawCircle(color = androidx.compose.ui.graphics.Color.Yellow, radius = 0.4f * radius.value)
             }
         }
     }
