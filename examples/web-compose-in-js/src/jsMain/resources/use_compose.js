d0ba519b8a2bec4f17ebc280ec7b0012376010e9
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2022-01-11 13:12:51 +0100

    web: add example for dom-based-composables usage in plain JS (review improvements)

--- examples/web-compose-in-js/src/jsMain/resources/use_compose.js
+++ examples/web-compose-in-js/src/jsMain/resources/use_compose.js
@@ -1,36 +1,34 @@
-// reference for ComposeCounterApp controller
-let counterController = undefined;
+window.onload = (event) => {
+    // reference for ComposeCounterApp controller
+    let counterController = undefined;
 
-const showCounterButton = document.getElementById('showCounter');
-const container = document.getElementById('container');
+    const showCounterCheckbox = document.getElementById('showCounter');
+    const container = document.getElementById('container');
 
-showCounterButton.addEventListener('click', () => {
-    if (!counterController) {
+    showCounterCheckbox.addEventListener('change', (event) => {
+        if (showCounterCheckbox.checked) {
+            // create a div that will serve as a root of Composition
+            const divContainerForCounter = document.createElement('div');
+            container.appendChild(divContainerForCounter);
+            divContainerForCounter.id = 'counterByCompose';
 
-        // create a div that will serve as a root of Composition
-        const divContainerForCounter = document.createElement('div');
-        container.appendChild(divContainerForCounter);
-        divContainerForCounter.id = 'counterByCompose';
+            // create a composition with a root in <div id='counterByCompose'>
+            counterController = MyComposables.ComposeCounterApp('counterByCompose', (newCount) => {
+                console.log(`Counter was updated. New value = ${newCount}`);
+            });
 
-        // create a composition with a root in <div id='counterByCompose'>
-        counterController = MyComposables.ComposeCounterApp('counterByCompose', (newCount) => {
-            console.log(`Counter was updated. New value = ${newCount}`);
-        });
+            const randomInitialCount = Math.floor(Math.random() * 1000);
+            // Controller can be used to update the composition's state
+            counterController.setCount(randomInitialCount);
 
-        const randomInitialCount = Math.floor(Math.random() * 1000);
-        // Controller can be used to update the composition's state
-        counterController.setCount(randomInitialCount);
+        } else {
+            // the composition is not needed anymore. It's necessary to dispose it:
+            counterController.dispose();
+            counterController = undefined;
 
-        showCounterButton.innerText = 'Hide counter (managed by Compose)';
-    } else {
-        // the composition is not needed anymore. It's necessary to dispose it:
-        counterController.dispose();
-        counterController = undefined;
-
-        // now we can remove the root of the composition.
-        container.removeChild(document.getElementById('counterByCompose'));
-
-        showCounterButton.innerText = 'Show counter (managed by Compose)';
-    }
-});
+            // now we can remove the root of the composition.
+            container.removeChild(document.getElementById('counterByCompose'));
+        }
+    });
+}
 
