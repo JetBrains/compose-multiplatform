1eaaf86d132e0fecd21e7c3fd8a35508686174be
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2022-01-18 16:12:45 +0100

    Move css files to a separate folder

---
+++ web/css/build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.compose.gradle.standardConf
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+
+kotlin {
+    jvm()
+    js(IR) {
+        browser() {
+            testTask {
+                useKarma {
+                    standardConf()
+                }
+            }
+        }
+        binaries.executable()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation(project(":internal-web-core-runtime"))
+                implementation(kotlin("stdlib-js"))
+            }
+        }
+
+        val jsTest by getting {
+            dependencies {
+                implementation(project(":test-utils"))
+                implementation(kotlin("test-js"))
+            }
+        }
+
+        all {
+            languageSettings {
+                useExperimentalAnnotation("org.jetbrains.compose.web.testutils.ComposeWebExperimentalTestsApi")
+            }
+        }
+    }
+}
