d9ecc6b8090cf9036b86f449ef2a682f10c68432
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2022-01-20 15:57:31 +0100

    add readme

--- examples/runtime-measurements/src/macosMain/kotlin/Main.kt
+++ examples/runtime-measurements/src/macosMain/kotlin/Main.kt
@@ -3,4 +3,6 @@ import kotlinx.coroutines.runBlocking
 fun main() = runBlocking {
     // try to run in debug and release modes to compare the time of execution
     addNComposableItems(1000)
+    //removeAllItems(preAddedItemsCount = 1000)
+    //updateEveryXth(3000, xth = 2, repeatUpdate = 20)
 }
