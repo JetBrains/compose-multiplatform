1496def82e8373900a1fa9417fe0bd068b1d0fc3
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2022-01-21 17:17:46 +0300

    Fix gradle plugins classloading in examples and templates

--- templates/multiplatform-template/build.gradle.kts
+++ templates/multiplatform-template/build.gradle.kts
@@ -1,3 +1,14 @@
+plugins {
+    // this is necessary to avoid the plugins to be loaded multiple times
+    // in each subproject's classloader
+    kotlin("jvm") apply false
+    kotlin("multiplatform") apply false
+    kotlin("android") apply false
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.compose") apply false
+}
+
 subprojects {
     repositories {
         google()
