4fee37620e43caa54db0b34b342e11dc4d745376
Shalom Gibly <sgibly@google.com>
2022-02-01 15:34:57 -0800

    Deprecate a Surface function that takes an onClick
    - This change deprecates the Surface with an onClick function and directs to use the one with the InteractionSource and a Modifier.clickable().
    - Updates to the Button component that used the deprecated API.
    - Updates to the Catalog app to use clickable OutlinedCards instead of
      Surface.
    
    Bug: 217271359
    Test: Ran all Material3 compose tests
    Relnote: "Deprecation of the Surface function that takes an onClick
    callback. Clickable surfaces should be created with an InteractionSource
    and a Modifier.clickable()."
    
    Change-Id: I211c691cf894c2c3cc6a4fbc60983a7bcb608cd4

--- material3/material3/integration-tests/material3-catalog/src/main/java/androidx/compose/material3/catalog/library/ui/example/ExampleItem.kt
+++ material3/material3/integration-tests/material3-catalog/src/main/java/androidx/compose/material3/catalog/library/ui/example/ExampleItem.kt
@@ -16,7 +16,8 @@
 
 package androidx.compose.material3.catalog.library.ui.example
 
-import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -24,33 +25,35 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.material3.catalog.library.model.Example
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ExampleItem(
     example: Example,
     onClick: (example: Example) -> Unit
 ) {
-    // TODO: Replace with M3 Card when available
-    Surface(
-        onClick = { onClick(example) },
-        modifier = Modifier.fillMaxWidth(),
-        shape = ExampleItemShape,
-        border = BorderStroke(
-            width = ExampleItemBorderWidth,
-            color = MaterialTheme.colorScheme.outline
-        )
+    val interactionSource = remember { MutableInteractionSource() }
+    OutlinedCard(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null,
+                onClick = { onClick(example) }),
+        interactionSource = interactionSource
     ) {
         Row(modifier = Modifier.padding(ExampleItemPadding)) {
             Column(modifier = Modifier.weight(1f, fill = true)) {
@@ -76,5 +79,3 @@ fun ExampleItem(
 
 private val ExampleItemPadding = 16.dp
 private val ExampleItemTextPadding = 8.dp
-private val ExampleItemBorderWidth = 1.dp
-private val ExampleItemShape = RoundedCornerShape(12.dp)
