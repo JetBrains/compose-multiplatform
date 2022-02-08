f61c9efc6b2c783e9db9fb38ed1ef63274772716
George Mount <mount@google.com>
2022-02-08 12:41:17 -0800

    Rename excludeFromSystemGestures to systemGestureExclusion
    Fixes: 218320848
    
    Relnote: "Renamed excludeFromSystemGestures to systemGesturesExclusion"
    
    Test: now testing the new methods
    Change-Id: I19526f6c1f89946c5ec433f477c65ff41ec3b10d

--- foundation/foundation/src/androidMain/kotlin/androidx/compose/foundation/ExcludeFromSystemGesture.android.kt
+++ foundation/foundation/src/androidMain/kotlin/androidx/compose/foundation/ExcludeFromSystemGesture.android.kt
@@ -16,41 +16,18 @@
 
 package androidx.compose.foundation
 
-import android.annotation.SuppressLint
-import android.os.Build
 import android.view.View
-import androidx.annotation.RequiresApi
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.collection.mutableVectorOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.graphics.toAndroidRect
 import androidx.compose.ui.layout.LayoutCoordinates
-import androidx.compose.ui.layout.OnGloballyPositionedModifier
-import androidx.compose.ui.layout.boundsInRoot
-import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.platform.debugInspectorInfo
-import kotlin.math.roundToInt
-import android.graphics.Rect as AndroidRect
 
 /**
  * Excludes the layout rectangle from the system gesture.
  *
  * @see View.setSystemGestureExclusionRects
  */
-fun Modifier.excludeFromSystemGesture() =
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
-        this
-    } else {
-        composed(inspectorInfo = debugInspectorInfo {
-            name = "excludeFromSystemGesture"
-        }) {
-            excludeFromSystemGestureR(null)
-        }
-    }
+@Deprecated("Use systemGestureExclusion", replaceWith = ReplaceWith("systemGestureExclusion"))
+fun Modifier.excludeFromSystemGesture() = systemGestureExclusion()
 
 /**
  * Excludes a rectangle within the local layout coordinates from the system gesture.
@@ -62,95 +39,6 @@ fun Modifier.excludeFromSystemGesture() =
  *
  * @see View.setSystemGestureExclusionRects
  */
+@Deprecated("Use systemGestureExclusion", replaceWith = ReplaceWith("systemGestureExclusion"))
 fun Modifier.excludeFromSystemGesture(exclusion: (LayoutCoordinates) -> Rect) =
-    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
-        this
-    } else {
-        composed(inspectorInfo = debugInspectorInfo {
-            name = "excludeFromSystemGesture"
-            properties["exclusion"] = exclusion
-        }) {
-            excludeFromSystemGestureR(exclusion)
-        }
-    }
-
-@SuppressLint("ComposableModifierFactory", "ModifierFactoryExtensionFunction")
-@Suppress("NOTHING_TO_INLINE")
-@RequiresApi(Build.VERSION_CODES.R)
-@Composable
-private inline fun excludeFromSystemGestureR(
-    noinline exclusion: ((LayoutCoordinates) -> Rect)?
-): Modifier {
-    val view = LocalView.current
-    val modifier = remember(view, exclusion) { ExcludeFromSystemGestureModifier(view, exclusion) }
-    DisposableEffect(modifier) {
-        onDispose {
-            modifier.removeRect()
-        }
-    }
-    return modifier
-}
-
-@RequiresApi(Build.VERSION_CODES.R)
-private class ExcludeFromSystemGestureModifier(
-    val view: View,
-    val exclusion: ((LayoutCoordinates) -> Rect)?
-) : OnGloballyPositionedModifier {
-    var rect: AndroidRect? = null
-
-    override fun onGloballyPositioned(coordinates: LayoutCoordinates) {
-        val newRect = if (exclusion == null) {
-            coordinates.boundsInRoot().toAndroidRect()
-        } else {
-            calcBounds(coordinates, exclusion.invoke(coordinates))
-        }
-        replaceRect(newRect)
-    }
-
-    fun removeRect() {
-        replaceRect(null)
-    }
-
-    fun replaceRect(newRect: AndroidRect?) {
-        val rects = mutableVectorOf<AndroidRect>()
-        rects.addAll(view.systemGestureExclusionRects)
-
-        rect?.let { rects.remove(it) }
-
-        if (newRect?.isEmpty == false) {
-            rects += newRect
-        }
-        view.systemGestureExclusionRects = rects.asMutableList()
-        rect = newRect
-    }
-
-    private fun calcBounds(layoutCoordinates: LayoutCoordinates, rect: Rect): AndroidRect {
-        val root = findRoot(layoutCoordinates)
-        val topLeft = root.localPositionOf(layoutCoordinates, rect.topLeft)
-        val topRight = root.localPositionOf(layoutCoordinates, rect.topRight)
-        val bottomLeft = root.localPositionOf(layoutCoordinates, rect.bottomLeft)
-        val bottomRight = root.localPositionOf(layoutCoordinates, rect.bottomRight)
-
-        val left = minOf(topLeft.x, topRight.x, bottomLeft.x, bottomRight.x)
-        val top = minOf(topLeft.y, topRight.y, bottomLeft.y, bottomRight.y)
-        val right = maxOf(topLeft.x, topRight.x, bottomLeft.x, bottomRight.x)
-        val bottom = maxOf(topLeft.y, topRight.y, bottomLeft.y, bottomRight.y)
-
-        return AndroidRect(
-            left.roundToInt(),
-            top.roundToInt(),
-            right.roundToInt(),
-            bottom.roundToInt()
-        )
-    }
-
-    private fun findRoot(layoutCoordinates: LayoutCoordinates): LayoutCoordinates {
-        var coordinates = layoutCoordinates
-        var parent = layoutCoordinates.parentLayoutCoordinates
-        while (parent != null) {
-            coordinates = parent
-            parent = coordinates.parentLayoutCoordinates
-        }
-        return coordinates
-    }
-}
+    systemGestureExclusion(exclusion)
