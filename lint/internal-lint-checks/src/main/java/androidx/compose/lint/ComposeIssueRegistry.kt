d0be7d1de3a13656d238865f53fae9617220800a
Aurimas Liutikas <aurimas@google.com>
2022-02-11 14:09:51 -0800

    Upgrade to AGP 7.3.0-alpha03
    - Remove @RequiresApi that are redundant due
      to minSdkVersion or parent scope.
    - Suppress existing lint failures
    - Add messages.ignore for b/219761630
    
    Relnote: N/A
    Test: ./gradlew tasks
          ./studiow all -> run test
    Change-Id: I750ba098d243ad82d4e3e5805e34f857a24849f2

--- lint/internal-lint-checks/src/main/java/androidx/compose/lint/ComposeIssueRegistry.kt
+++ lint/internal-lint-checks/src/main/java/androidx/compose/lint/ComposeIssueRegistry.kt
@@ -25,7 +25,7 @@ import com.android.tools.lint.detector.api.Issue
 
 class ComposeIssueRegistry : IssueRegistry() {
     override val minApi = CURRENT_API
-    override val api = 11
+    override val api = 13
     override val issues get(): List<Issue> {
         return listOf(
             ListIteratorDetector.ISSUE,
