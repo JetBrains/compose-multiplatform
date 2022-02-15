8d6991676689715be4e707e401156fb070825b58
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2022-02-15 20:11:44 +0100

    Moved to coroutines-1.5.1-mm-dev1 for experimental memory model
    Change-Id: Ibeefebfef2838bf17208ef3902a2f279cac20a73

--- util/util/src/nativeMain/kotlin/androidx/compose/util/Synchronization.native.kt
+++ util/util/src/nativeMain/kotlin/androidx/compose/util/Synchronization.native.kt
@@ -20,11 +20,6 @@ internal actual class SynchronizedObject : kotlinx.atomicfu.locks.SynchronizedOb
 
 internal actual fun createSynchronizedObject() = SynchronizedObject()
 
-// When we decide to switch to Experimental Memory Model,
-// we need to change the synchronized implementation:
-//
-// internal actual inline fun <R> synchronized(lock: SynchronizedObject, block: () -> R): R =
-//    kotlinx.atomicfu.locks.synchronized(lock, block)
-
 @PublishedApi
-internal actual inline fun <R> synchronized(lock: SynchronizedObject, block: () -> R): R = block()
+internal actual inline fun <R> synchronized(lock: SynchronizedObject, block: () -> R): R =
+    kotlinx.atomicfu.locks.synchronized(lock, block)
