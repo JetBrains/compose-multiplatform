800f46b8ca1665ff0dcf7cbfe1c45502c0b134e1
Jelle Fresen <jellefresen@google.com>
2022-02-16 10:01:14 +0000

    Wait for idle before starting a compose test
    This will prevent flaky tests if the activity start races with the test
    set up. When waiting for idle before starting the set up, we make sure
    that the activity always wins.
    
    Fix: 219882991
    Test: ./gradlew compose:ui:ui-test-junit4:cC
    Change-Id: Idf25c262e141128a78ff4f039907c9fc2b8116b4

---
+++ ui/ui-test-junit4/src/androidAndroidTest/kotlin/androidx/compose/ui/test/junit4/LaunchActivityTooEarlyTest.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.ui.test.junit4
+
+import androidx.compose.testutils.expectError
+import androidx.compose.ui.test.AndroidComposeTest
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@MediumTest
+@RunWith(AndroidJUnit4::class)
+class LaunchActivityTooEarlyTest {
+    @Test
+    fun test() {
+        // Launching the CustomActivity _before_ calling runTest
+        ActivityScenario.launch(CustomActivity::class.java)
+
+        AndroidComposeTest {
+            throw NotImplementedError("This is not called in this test")
+        }.runTest {
+            expectError<IllegalStateException>(
+                expectedMessage = "No compose views found in the app. Is your Activity resumed\\?"
+            ) {
+                onNodeWithText("Hello").assertExists()
+            }
+        }
+    }
+}
