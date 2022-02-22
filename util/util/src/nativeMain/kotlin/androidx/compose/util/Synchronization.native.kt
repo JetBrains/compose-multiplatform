6778723be045ed3441146cf414d3d69902582b5a
Alexander Gorshenev <alexander.gorshenev@jetbrains.com>
2022-02-22 19:07:06 +0300

    Typos in comments

--- util/util/src/nativeMain/kotlin/androidx/compose/util/Synchronization.native.kt
+++ util/util/src/nativeMain/kotlin/androidx/compose/util/Synchronization.native.kt
@@ -20,8 +20,8 @@ internal actual class SynchronizedObject : kotlinx.atomicfu.locks.SynchronizedOb
 
 internal actual fun createSynchronizedObject() = SynchronizedObject()
 
-// When we decide to switch to Experimental Memoty Model,
-// we need to change the synchronized impementation:
+// When we decide to switch to Experimental Memory Model,
+// we need to change the synchronized implementation:
 //
 // internal actual inline fun <R> synchronized(lock: SynchronizedObject, block: () -> R): R =
 //    kotlinx.atomicfu.locks.synchronized(lock, block)
