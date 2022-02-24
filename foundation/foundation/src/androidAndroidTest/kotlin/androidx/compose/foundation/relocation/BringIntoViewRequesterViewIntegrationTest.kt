cb8643ef6d304c7f9d93518f9e6bd9f21f71c06b
Zach Klippenstein <klippenstein@google.com>
2022-02-23 16:10:57 -0800

    Make BringIntoViewRequester use OnPlacedModifier instead of OnGloballyPositionedModifier.
    This addresses the performance concern raised in
    https://android-review.googlesource.com/c/platform/frameworks/support/+/1965665/comments/83556539_69b46746
    
    Test: ./gradlew :compose:foundation:foundation:cDAT
    Relnote: "`Modifier.bringIntoViewRequester` no longer uses
        `onGloballyPositioned`."
    Change-Id: I630f552bc59f31e91852aeaf3345216bb1f8c403

--- foundation/foundation/src/androidAndroidTest/kotlin/androidx/compose/foundation/relocation/BringIntoViewRequesterViewIntegrationTest.kt
+++ foundation/foundation/src/androidAndroidTest/kotlin/androidx/compose/foundation/relocation/BringIntoViewRequesterViewIntegrationTest.kt
@@ -25,7 +25,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
@@ -46,7 +45,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@OptIn(ExperimentalFoundationApi::class, ExperimentalComposeUiApi::class)
+@OptIn(ExperimentalFoundationApi::class)
 @MediumTest
 @RunWith(AndroidJUnit4::class)
 class BringIntoViewRequesterViewIntegrationTest {
