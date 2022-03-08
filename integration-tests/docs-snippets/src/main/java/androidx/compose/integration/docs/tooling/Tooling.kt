7a0e2da75abb1eec39b2578bdc45109e8de88844
Andrew Solovay <asolovay@google.com>
2022-03-08 12:42:09 -0800

    [Docs snippets] New code snippet for tooling doc
    Added snippet demonstrating LocalInspectionMode, and necessary
    fakes.
    
    Bug: 222752454
    Test: ./gradlew :compose:integration-tests:docs-snippets:lintDebug
    Change-Id: I5655d46abbd48584ab53a9a7985551ec50113a6e

--- integration-tests/docs-snippets/src/main/java/androidx/compose/integration/docs/tooling/Tooling.kt
+++ integration-tests/docs-snippets/src/main/java/androidx/compose/integration/docs/tooling/Tooling.kt
@@ -35,6 +35,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -60,6 +61,17 @@ private class ToolingSnippet2 {
     @Composable
     fun ComposablePreview() {
         SimpleComposable()
+    }
+}
+
+@Composable
+private fun ToolingSnippetLocalInspectionMode() {
+    if (LocalInspectionMode.current) {
+        // Show this text in a preview window:
+        Text("Hello preview user!")
+    } else {
+        // Show this text in the app:
+        Text("Hello $name!")
     }
 }
 
@@ -181,3 +193,9 @@ private object R {
         const val greetings = 1
     }
 }
+
+/*
+ * Fakes needed for snippets to build:
+ */
+
+private val name = "friend"
