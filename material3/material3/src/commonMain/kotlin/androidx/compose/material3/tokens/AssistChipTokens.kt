ef83bd6a774ceff7ea326e2e646dafaba0382464
Shalom Gibly <sgibly@google.com>
2022-03-10 14:30:32 -0800

    Adds Material3 Chip tokens
    Bug: 197399111
    Test: material3:compileDebugKotlin
    Relnote: N/A
    Change-Id: I31f8bbfefc56c1e7989557dd8d9ff0e0ae0adaef

---
+++ material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/AssistChipTokens.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// VERSION: v0_92
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+package androidx.compose.material3.tokens
+
+import androidx.compose.ui.unit.dp
+
+internal object AssistChipTokens {
+    val ContainerHeight = 32.0.dp
+    val ContainerShape = ShapeKeyTokens.CornerSmall
+    val ContainerSurfaceTintLayerColor = ColorSchemeKeyTokens.SurfaceTint
+    val DisabledLabelTextColor = ColorSchemeKeyTokens.OnSurface
+    const val DisabledLabelTextOpacity = 0.38f
+    val DraggedContainerElevation = ElevationTokens.Level4
+    val DraggedLabelTextColor = ColorSchemeKeyTokens.OnSurface
+    val ElevatedContainerColor = ColorSchemeKeyTokens.Surface
+    val ElevatedContainerElevation = ElevationTokens.Level1
+    val ElevatedDisabledContainerColor = ColorSchemeKeyTokens.OnSurface
+    val ElevatedDisabledContainerElevation = ElevationTokens.Level0
+    const val ElevatedDisabledContainerOpacity = 0.12f
+    val ElevatedFocusContainerElevation = ElevationTokens.Level1
+    val ElevatedHoverContainerElevation = ElevationTokens.Level2
+    val ElevatedPressedContainerElevation = ElevationTokens.Level1
+    val FlatContainerElevation = ElevationTokens.Level0
+    val FlatDisabledOutlineColor = ColorSchemeKeyTokens.OnSurface
+    const val FlatDisabledOutlineOpacity = 0.12f
+    val FlatFocusOutlineColor = ColorSchemeKeyTokens.OnSurface
+    val FlatOutlineColor = ColorSchemeKeyTokens.Outline
+    val FlatOutlineWidth = 1.0.dp
+    val FocusLabelTextColor = ColorSchemeKeyTokens.OnSurface
+    val HoverLabelTextColor = ColorSchemeKeyTokens.OnSurface
+    val LabelTextColor = ColorSchemeKeyTokens.OnSurface
+    val LabelTextFont = TypographyKeyTokens.LabelLarge
+    val PressedLabelTextColor = ColorSchemeKeyTokens.OnSurface
+    val DisabledIconColor = ColorSchemeKeyTokens.OnSurface
+    const val DisabledIconOpacity = 0.38f
+    val DraggedIconColor = ColorSchemeKeyTokens.Primary
+    val FocusIconColor = ColorSchemeKeyTokens.Primary
+    val HoverIconColor = ColorSchemeKeyTokens.Primary
+    val IconColor = ColorSchemeKeyTokens.Primary
+    val IconSize = 18.0.dp
+    val PressedIconColor = ColorSchemeKeyTokens.Primary
+}
---
+++ material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/SuggestionChipTokens.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+// VERSION: v0_92
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+package androidx.compose.material3.tokens
+
+import androidx.compose.ui.unit.dp
+
+internal object SuggestionChipTokens {
+    val ContainerHeight = 32.0.dp
+    val ContainerShape = ShapeKeyTokens.CornerSmall
+    val ContainerSurfaceTintLayerColor = ColorSchemeKeyTokens.SurfaceTint
+    val DisabledLabelTextColor = ColorSchemeKeyTokens.OnSurface
+    const val DisabledLabelTextOpacity = 0.38f
+    val DraggedContainerElevation = ElevationTokens.Level4
+    val DraggedLabelTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val ElevatedContainerColor = ColorSchemeKeyTokens.Surface
+    val ElevatedContainerElevation = ElevationTokens.Level1
+    val ElevatedDisabledContainerColor = ColorSchemeKeyTokens.OnSurface
+    val ElevatedDisabledContainerElevation = ElevationTokens.Level0
+    const val ElevatedDisabledContainerOpacity = 0.12f
+    val ElevatedFocusContainerElevation = ElevationTokens.Level1
+    val ElevatedHoverContainerElevation = ElevationTokens.Level2
+    val ElevatedPressedContainerElevation = ElevationTokens.Level1
+    val FlatContainerElevation = ElevationTokens.Level0
+    val FlatDisabledOutlineColor = ColorSchemeKeyTokens.OnSurface
+    const val FlatDisabledOutlineOpacity = 0.12f
+    val FlatFocusOutlineColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val FlatOutlineColor = ColorSchemeKeyTokens.Outline
+    val FlatOutlineWidth = 1.0.dp
+    val FocusLabelTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val HoverLabelTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val LabelTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val LabelTextFont = TypographyKeyTokens.LabelLarge
+    val PressedLabelTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+}
