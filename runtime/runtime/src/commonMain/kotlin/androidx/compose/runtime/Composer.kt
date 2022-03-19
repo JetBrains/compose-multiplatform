ff3f6fb599af68e1c76e92dc4c63a2c50c8694e0
Chuck Jazdzewski <chuckj@google.com>
2022-03-18 18:11:34 -0700

    Fix corner a case where the composer would crash
    If a group with nodes is deleted in front of a group that is doesn't contain
    nodes and is moved to new location and nodes are added to it could cause the
    composer to crash with an invalid start index.
    
    The most likely case that can cause this is a for loop with nested if
    statements like the included test case.
    
    Test: ./gradlew :comopse:r:r:tDUT
    Fixes: 199136503
    Change-Id: I89f6abce423030d4345351d2533f867fd1cb519f

--- runtime/runtime/src/commonMain/kotlin/androidx/compose/runtime/Composer.kt
+++ runtime/runtime/src/commonMain/kotlin/androidx/compose/runtime/Composer.kt
@@ -184,8 +184,11 @@ private class Pending(
             groupInfo.nodeCount = newCount
             if (difference != 0) {
                 groupInfos.values.forEach { childGroupInfo ->
-                    if (childGroupInfo.nodeIndex >= index && childGroupInfo != groupInfo)
-                        childGroupInfo.nodeIndex += difference
+                    if (childGroupInfo.nodeIndex >= index && childGroupInfo != groupInfo) {
+                        val newIndex = childGroupInfo.nodeIndex + difference
+                        if (newIndex >= 0)
+                            childGroupInfo.nodeIndex = newIndex
+                    }
                 }
             }
             return true
