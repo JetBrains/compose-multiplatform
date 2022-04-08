3a39e751d0a4ed0a8c9f8476884c545f3b05c25f
Travis Reitter <travis@doublestrain.com>
2022-04-08 15:43:58 -0700

    upgrade SQLDelight for Apple Silicon support

--- examples/todoapp/buildSrc/buildSrc/src/main/kotlin/Deps.kt
+++ examples/todoapp/buildSrc/buildSrc/src/main/kotlin/Deps.kt
@@ -79,7 +79,7 @@ object Deps {
 
     object Squareup {
         object SQLDelight {
-            private const val VERSION = "1.5.0"
+            private const val VERSION = "1.5.3"
 
             const val gradlePlugin = "com.squareup.sqldelight:gradle-plugin:$VERSION"
             const val androidDriver = "com.squareup.sqldelight:android-driver:$VERSION"
