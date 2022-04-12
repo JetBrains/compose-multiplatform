ff6e2880e2d8f028ee35a534c56c8a2bc7580c88
Jakub Gielzak <jgielzak@google.com>
2022-04-12 22:37:05 +0000

    Compose tracing API: added isTracingInProgress
    Compose tracing API: added isTracingInProgress to CompositionTracer
    in anticipation of compose-runtime-tracing library always setting the
    tracer and in consequence making the tracer presence check no longer
    a valid approach for detecting if to skip the tracing step.
    
    Relnote: Added isTracingInProgress to the CompositionTracer interface.
    
    Bug: 214560414
    Test: n/a
    
    Change-Id: Ic6f12d84c8622c7a796d44cf3eb04b0e077efc97

--- runtime/runtime/src/commonMain/kotlin/androidx/compose/runtime/Composer.kt
+++ runtime/runtime/src/commonMain/kotlin/androidx/compose/runtime/Composer.kt
@@ -1090,6 +1090,7 @@ fun sourceInformationMarkerStart(composer: Composer, key: Int, sourceInformation
 interface CompositionTracer {
     fun traceEventStart(key: Int, dirty1: Int, dirty2: Int, info: String): Unit
     fun traceEventEnd(): Unit
+    fun isTraceInProgress(): Boolean
 }
 
 @OptIn(InternalComposeTracingApi::class)
@@ -1102,7 +1103,7 @@ private var compositionTracer: CompositionTracer? = null
  */
 @OptIn(InternalComposeTracingApi::class)
 @ComposeCompilerApi
-fun isTraceInProgress(): Boolean = compositionTracer != null
+fun isTraceInProgress(): Boolean = compositionTracer.let { it != null && it.isTraceInProgress() }
 
 /**
  * Internal tracing API.
