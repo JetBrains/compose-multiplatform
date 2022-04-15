79f0099359c4843e943e6ab80e4f0927dd3bab0d
dima.avdeev <dima.avdeev@jetbrains.com>
2022-04-15 13:41:42 +0400

    measure cpu

--- benchmarks-intellij/src/main/kotlin/com/jetbrains/compose/measure.kt
+++ benchmarks-intellij/src/main/kotlin/com/jetbrains/compose/measure.kt
@@ -6,31 +6,35 @@
 package com.jetbrains.compose
 
 import com.jetbrains.compose.benchmark.PerformanceInfoDialog
+import java.lang.management.ManagementFactory
 import java.util.concurrent.ConcurrentHashMap
 
 val performanceDialog = PerformanceInfoDialog()
 private val runtime = Runtime.getRuntime()
 private val measurements: MutableMap<String, Measure> = ConcurrentHashMap()
 
-class Measure(val averageMemory: Float = 0f, val count: Int = 0)
+class Measure(val averageMemory: Double = 0.0, val systemLoad: Double = 0.0, val count: Int = 0)
 
 fun doMeasure(key: String) {
     runtime.gc()
     val memory = runtime.totalMemory() - runtime.freeMemory()
+    val systemLoad = ManagementFactory.getOperatingSystemMXBean().systemLoadAverage
     val previous = measurements.getOrDefault(key, Measure())
     val current =
         Measure(
             averageMemory = ((previous.averageMemory * previous.count) + memory) / (previous.count + 1),
+            systemLoad = ((previous.systemLoad * previous.count) + systemLoad) / (previous.count + 1),
             count = previous.count + 1
         )
     measurements[key] = current
 
     performanceDialog.setText(
         buildString {
-            measurements.entries.sortedBy { it.value.averageMemory }.forEach { (key, measure)->
+            measurements.entries.sortedBy { it.key }.forEach { (key, measure) ->
                 appendLine("---------")
-                append("$key:")
-                append(String.format("%.2f MB", measure.averageMemory / 1e6f))
+                appendLine("$key:")
+                appendLine("memory: " + String.format("%.1f MB", measure.averageMemory / 1e6f))
+                appendLine("system load: " + String.format("%.3f", measure.systemLoad) + " % ")
                 appendLine("")
             }
         }
