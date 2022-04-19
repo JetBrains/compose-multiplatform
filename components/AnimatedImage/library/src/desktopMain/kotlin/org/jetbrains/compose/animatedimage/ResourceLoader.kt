4b8da3ed3d474c404f34a7c264bbcf984cfa9815
Abdelilah El Aissaoui <aeab13@gmail.com>
2022-04-19 20:19:32 +0200

    Resource load is now done in the IO Dispatchers

--- components/AnimatedImage/library/src/desktopMain/kotlin/org/jetbrains/compose/animatedimage/ResourceLoader.kt
+++ components/AnimatedImage/library/src/desktopMain/kotlin/org/jetbrains/compose/animatedimage/ResourceLoader.kt
@@ -1,9 +1,11 @@
 package org.jetbrains.compose.animatedimage
 
 import androidx.compose.ui.res.useResource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class ResourceLoader(private val resourcePath: String) : AnimatedImageLoader {
-    override suspend fun loadBytes(): ByteArray {
-        return useResource(resourcePath) { it.readAllBytes() }
+    override suspend fun loadBytes(): ByteArray = withContext(Dispatchers.IO) {
+        return@withContext useResource(resourcePath) { it.readAllBytes() }
     }
 }
