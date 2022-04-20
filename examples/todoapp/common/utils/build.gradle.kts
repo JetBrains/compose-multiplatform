7341c29555895c131ffae1ae357632a92d7a2abf
dima.avdeev <dima.avdeev@jetbrains.com>
2022-04-20 22:17:07 +0400

    revert iOS Kotlin to 1.6.10

--- examples/todoapp/common/utils/build.gradle.kts
+++ examples/todoapp/common/utils/build.gradle.kts
@@ -6,12 +6,6 @@ plugins {
 }
 
 kotlin {
-    fun isIphoneSimulatorBuild(): Boolean =
-        System.getenv("NATIVE_ARCH") == "arm64" && System.getenv("SDK_NAME")?.startsWith("iphonesimulator") == true
-
-    fun isIphoneOsBuild(): Boolean =
-        System.getenv("SDK_NAME")?.startsWith("iphoneos") == true
-
     val iosTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
         when {
             isIphoneSimulatorBuild() -> ::iosSimulatorArm64
