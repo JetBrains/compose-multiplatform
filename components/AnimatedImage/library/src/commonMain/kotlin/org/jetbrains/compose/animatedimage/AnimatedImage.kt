fbf44b653cd73144690f728229fc3a1f75fd4498
Abdelilah El Aissaoui <aeab13@gmail.com>
2022-05-03 19:43:27 +0200

    Added additional docs to public methods

--- components/AnimatedImage/library/src/commonMain/kotlin/org/jetbrains/compose/animatedimage/AnimatedImage.kt
+++ components/AnimatedImage/library/src/commonMain/kotlin/org/jetbrains/compose/animatedimage/AnimatedImage.kt
@@ -2,13 +2,30 @@ package org.jetbrains.compose.animatedimage
 
 import androidx.compose.ui.graphics.ImageBitmap
 
+/**
+ * Represents an image formed by a sequence of frames with a specific duration each one.
+ */
 expect class AnimatedImage
 
+/**
+ * Loads an [AnimatedImage] from a given path.
+ */
 expect suspend fun loadAnimatedImage(path: String): AnimatedImage
 
+/**
+ * Loads an [AnimatedImage] from the resources.
+ */
 expect suspend fun loadResourceAnimatedImage(path: String): AnimatedImage
 
+/**
+ * Animates an [AnimatedImage] by returning an [ImageBitmap] for each frame of the image.
+ * The parent will be with the recomposed with each new frame that has been rendered.
+ */
 expect fun AnimatedImage.animate(): ImageBitmap
 
 private val BlankBitmap = ImageBitmap(1, 1)
+
+/**
+ * Object used to represent a blank ImageBitmap with the minimum possible size.
+ */
 val ImageBitmap.Companion.Blank get() = BlankBitmap
