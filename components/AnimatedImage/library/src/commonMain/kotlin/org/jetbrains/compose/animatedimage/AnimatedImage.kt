907b657c89c005f661148efd0c3a26ebe77fdb2c
Abdelilah El Aissaoui <aeab13@gmail.com>
2022-05-05 14:30:03 +0200

    Docs to AnimatedImage public methods (#2050)
    * Added additional docs to public methods
    
    * Replaced parent with caller
    
    * Updated AnimatedImage docs
    
    * Replaced path with image

--- components/AnimatedImage/library/src/commonMain/kotlin/org/jetbrains/compose/animatedimage/AnimatedImage.kt
+++ components/AnimatedImage/library/src/commonMain/kotlin/org/jetbrains/compose/animatedimage/AnimatedImage.kt
@@ -2,13 +2,32 @@ package org.jetbrains.compose.animatedimage
 
 import androidx.compose.ui.graphics.ImageBitmap
 
+/**
+ * Represents an image formed by a sequence of frames with a specific duration each one.
+ */
 expect class AnimatedImage
 
+/**
+ * Loads an [AnimatedImage] from a given path.
+ * The image can be loaded from different sources depending on the platform (only desktop currently):
+ * Desktop: From the network if it's a valid URL, from the local storage otherwise.
+ */
 expect suspend fun loadAnimatedImage(path: String): AnimatedImage
 
+/**
+ * Loads an [AnimatedImage] from the resources.
+ */
 expect suspend fun loadResourceAnimatedImage(path: String): AnimatedImage
 
+/**
+ * Animates an [AnimatedImage] by returning an [ImageBitmap] for each frame of the image.
+ * The caller will be recomposed with each new frame that has been rendered.
+ */
 expect fun AnimatedImage.animate(): ImageBitmap
 
 private val BlankBitmap = ImageBitmap(1, 1)
+
+/**
+ * Object used to represent a blank ImageBitmap with the minimum possible size.
+ */
 val ImageBitmap.Companion.Blank get() = BlankBitmap
