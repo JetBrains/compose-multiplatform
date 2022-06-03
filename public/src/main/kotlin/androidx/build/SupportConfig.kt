21139e51a9ec20016f5416d01badd1d472daa896
Alan Viverette <alanv@google.com>
2022-06-03 16:32:34 -0400

    Update compileSdk to 33
    Fixes: 234870295
    Test: ./gradlew bOS
    Change-Id: Ifdae01a180217e8e140e998c7ecf511076535b51

--- public/src/main/kotlin/androidx/build/SupportConfig.kt
+++ public/src/main/kotlin/androidx/build/SupportConfig.kt
@@ -34,7 +34,7 @@ object SupportConfig {
      * Either an integer value or a pre-release platform code, prefixed with "android-" (ex.
      * "android-28" or "android-Q") as you would see within the SDK's platforms directory.
      */
-    const val COMPILE_SDK_VERSION = "android-Tiramisu"
+    const val COMPILE_SDK_VERSION = "android-33"
 
     /**
      * The Android SDK version to use for targetSdkVersion meta-data.
