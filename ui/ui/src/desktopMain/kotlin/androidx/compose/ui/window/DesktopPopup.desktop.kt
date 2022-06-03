194785f30e52fe89572d9b52a2fbb6c4994a83c7
dima.avdeev <dima.avdeev@jetbrains.com>
2022-06-03 12:13:43 +0400

    Revert "fix value class inline bug (#230)"
    This reverts commit 35b82b2f8e4a187fec4a0365b8717a6bf86a2a5b.

--- ui/ui/src/desktopMain/kotlin/androidx/compose/ui/window/DesktopPopup.desktop.kt
+++ ui/ui/src/desktopMain/kotlin/androidx/compose/ui/window/DesktopPopup.desktop.kt
@@ -184,7 +184,7 @@ private fun PopupLayout(
                     val width = constraints.maxWidth
                     val height = constraints.maxHeight
 
-                    layout(width, height) {
+                    layout(constraints.maxWidth, constraints.maxHeight) {
                         measurables.forEach {
                             val placeable = it.measure(constraints)
                             val position = popupPositionProvider.calculatePosition(
