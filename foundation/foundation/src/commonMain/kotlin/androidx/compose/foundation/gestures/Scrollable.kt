7518fd3390d04e88d7f7c4dec43fd497f6512f6b
Zach Klippenstein <klippenstein@google.com>
2022-06-15 12:13:03 -0700

    Fix scrollable focused child tracking.
    When calculating whether we need to scroll to keep the focused child
    in-view after the scrollable viewport is resized, we were using the
    bounds of the focused child to figure out if it was previously visible.
    That was incorrect, because if the viewport is animating while the
    scroll animation is running, it's possible for the viewport to shrink
    fast enough that the focused child isn't actually visible, but would
    have eventually became visible after the animation, so we need to
    continue to animate.
    
    This approach is documented as Option 0 in the design doc:
    https://docs.google.com/document/d/16yYJOIcde0Pm7vB--ExSdrY7lrZQ2rqHIn6nYWb7ccg/edit#heading=h.ytkawqefedyn
    
    Note that while this fixes b/230756508 and will cause the animations to
    end up in the correct final state, it makes b/220119990 more visible,
    since it looks like the scroll animation doesn't start at all until the
    viewport animation stops.
    
    Fixes: b/230756508
    Test: Manually using the 'Foundation > Scrollable with focused child"
        and "Text > Input fields > Inside scrollable" demos."
    Test: ./gradlew :compose:f:f:cDAT
    Relnote: "When a scrollable has a focused child, it will now correctly
        scroll to keep the focused child in view when its size is decreased,
        even when the size is animated."
    Change-Id: I8086717b14174e566b299f2643f1dd6c0b250773

--- foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/gestures/Scrollable.kt
+++ foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/gestures/Scrollable.kt
@@ -33,10 +33,12 @@ import androidx.compose.foundation.relocation.bringIntoViewResponder
 import androidx.compose.foundation.rememberOverscrollEffect
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.geometry.Offset
@@ -66,6 +68,8 @@ import androidx.compose.ui.util.fastAll
 import androidx.compose.ui.util.fastForEach
 import kotlin.math.abs
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.launch
 
 /**
@@ -521,6 +525,12 @@ private class ContentInViewModifier(
     private var coordinates: LayoutCoordinates? = null
     private var oldSize: IntSize? = null
 
+    // These properties are used to detect the case where the viewport size is animated shrinking
+    // while the scroll animation used to keep the focused child in view is still running.
+    private var focusedChildBeingAnimated: LayoutCoordinates? = null
+    private var focusTargetBounds: Rect? by mutableStateOf(null)
+    private var focusAnimationJob: Job? = null
+
     val modifier: Modifier = this
         .onFocusedBoundsChanged { focusedChild = it }
         .bringIntoViewResponder(this)
@@ -549,9 +559,36 @@ private class ContentInViewModifier(
     }
 
     override suspend fun bringChildIntoView(localRect: Rect) {
-        performBringIntoView(localRect, calculateRectForParent(localRect))
+        performBringIntoView(
+            source = localRect,
+            destination = calculateRectForParent(localRect)
+        )
     }
 
+    /**
+     * Handles when the size of the scroll viewport changes by making sure any focused child is kept
+     * appropriately visible when the viewport shrinks and would otherwise hide it.
+     *
+     * One common instance of this is when a text field in a scrollable near the bottom is focused
+     * while the soft keyboard is hidden, causing the keyboard to show, and cover the field.
+     * See b/192043120 and related bugs.
+     *
+     * To future debuggers of this method, it might be helpful to add a draw modifier to the chain
+     * above to draw the focus target bounds:
+     * ```
+     * .drawWithContent {
+     *   drawContent()
+     *   focusTargetBounds?.let {
+     *     drawRect(
+     *       Color.Red,
+     *       topLeft = it.topLeft,
+     *       size = it.size,
+     *       style = Stroke(1.dp.toPx())
+     *     )
+     *   }
+     * }
+     * ```
+     */
     private fun onSizeChanged(coordinates: LayoutCoordinates, oldSize: IntSize) {
         val containerShrunk = if (orientation == Horizontal) {
             coordinates.size.width < oldSize.width
@@ -562,17 +599,75 @@ private class ContentInViewModifier(
         // soon be _more_ visible, so don't scroll.
         if (!containerShrunk) return
 
-        val focusedBounds = focusedChild
-            ?.let { coordinates.localBoundingBoxOf(it, clipBounds = false) }
-            ?: return
-        val myOldBounds = Rect(Offset.Zero, oldSize.toSize())
-        val adjustedBounds = computeDestination(focusedBounds, coordinates.size)
-        val wasVisible = myOldBounds.overlaps(focusedBounds)
-        val isFocusedChildClipped = adjustedBounds != focusedBounds
+        val focusedChild = focusedChild ?: return
+        val focusedBounds = coordinates.localBoundingBoxOf(focusedChild, clipBounds = false)
 
-        if (wasVisible && isFocusedChildClipped) {
-            scope.launch {
-                performBringIntoView(focusedBounds, adjustedBounds)
+        // In order to check if we need to scroll to bring the focused child into view, it's not
+        // enough to consider where the child actually is right now. If the viewport was recently
+        // shrunk, we may have already started a scroll animation to bring it into view. In that
+        // case, we need to compare with the target of the animation, not the current position. If
+        // we don't do that, then in some cases when the viewport size is being animated (e.g. when
+        // the keyboard insets are being animated on API 30+) we might stop trying to keep the
+        // focused child in view before the viewport animation is finished, and the scroll animation
+        // will stop short and leave the focused child out of the viewport. See b/230756508.
+        val eventualFocusedBounds = if (focusedChild === focusedChildBeingAnimated) {
+            // A previous call to this method started an animation that is still running, so compare
+            // with the target of that animation.
+            checkNotNull(focusTargetBounds)
+        } else {
+            focusedBounds
+        }
+
+        val myOldBounds = Rect(Offset.Zero, oldSize.toSize())
+        if (!myOldBounds.overlaps(eventualFocusedBounds)) {
+            // The focused child was not visible before the resize, so we don't need to keep
+            // it visible.
+            return
+        }
+
+        val targetBounds = computeDestination(eventualFocusedBounds, coordinates.size)
+        if (targetBounds == eventualFocusedBounds) {
+            // The focused child is already fully visible (not clipped or hidden) after the resize,
+            // or will be after it finishes animating, so we don't need to do anything.
+            return
+        }
+
+        // If execution has gotten to this point, it means the focused child was at least partially
+        // visible before the resize, and it is either partially clipped or completely hidden after
+        // the resize, so we need to adjust scroll to keep it in view.
+        focusedChildBeingAnimated = focusedChild
+        focusTargetBounds = targetBounds
+        scope.launch(NonCancellable) {
+            val job = launch {
+                // Animate the scroll offset to keep the focused child in view. This is a suspending
+                // call that will suspend until the animation is finished, and only return if it
+                // completes. If any other scroll operations are performed after the animation starts,
+                // e.g. the viewport shrinks again or the user manually scrolls, this animation will
+                // be cancelled and this function will throw a CancellationException.
+                performBringIntoView(source = focusedBounds, destination = targetBounds)
+            }
+            focusAnimationJob = job
+
+            // If the scroll was interrupted by another viewport shrink that happens while the
+            // animation is running, we don't want to clear these fields since the later call to
+            // this onSizeChanged method will have updated the fields with its own values.
+            // If the animation completed, or was cancelled for any other reason, we need to clear
+            // them so the next viewport shrink doesn't think there's already a scroll animation in
+            // progress.
+            // Doing this wrong has a few implications:
+            // 1. If the fields are nulled out when another onSizeChange call happens, it will not
+            //    use the current animation target and viewport animations will lose track of the
+            //    focusable.
+            // 2. If the fields are not nulled out in other cases, the next viewport animation will
+            //    not keep the focusable in view if the focus hasn't changed.
+            try {
+                job.join()
+            } finally {
+                if (focusAnimationJob === job) {
+                    focusedChildBeingAnimated = null
+                    focusTargetBounds = null
+                    focusAnimationJob = null
+                }
             }
         }
     }
@@ -580,16 +675,30 @@ private class ContentInViewModifier(
     /**
      * Compute the destination given the source rectangle and current bounds.
      *
-     * @param source The bounding box of the item that sent the request to be brought into view.
+     * @param childBounds The bounding box of the item that sent the request to be brought into view.
      * @return the destination rectangle.
      */
-    private fun computeDestination(source: Rect, intSize: IntSize): Rect {
-        val size = intSize.toSize()
+    private fun computeDestination(childBounds: Rect, containerSize: IntSize): Rect {
+        val size = containerSize.toSize()
         return when (orientation) {
             Vertical ->
-                source.translate(0f, relocationDistance(source.top, source.bottom, size.height))
+                childBounds.translate(
+                    translateX = 0f,
+                    translateY = -relocationDistance(
+                        childBounds.top,
+                        childBounds.bottom,
+                        size.height
+                    )
+                )
             Horizontal ->
-                source.translate(relocationDistance(source.left, source.right, size.width), 0f)
+                childBounds.translate(
+                    translateX = -relocationDistance(
+                        childBounds.left,
+                        childBounds.right,
+                        size.width
+                    ),
+                    translateY = 0f
+                )
         }
     }
 
@@ -598,8 +707,8 @@ private class ContentInViewModifier(
      */
     private suspend fun performBringIntoView(source: Rect, destination: Rect) {
         val offset = when (orientation) {
-            Vertical -> source.top - destination.top
-            Horizontal -> source.left - destination.left
+            Vertical -> destination.top - source.top
+            Horizontal -> destination.left - source.left
         }
         val scrollDelta = if (reverseDirection) -offset else offset
 
@@ -622,7 +731,8 @@ private class ContentInViewModifier(
             // If the item is visible but larger than the parent, we don't scroll.
             leadingEdge < 0 && trailingEdge > parentSize -> 0f
 
-            // Find the minimum scroll needed to make one of the edges coincide with the parent's edge.
+            // Find the minimum scroll needed to make one of the edges coincide with the parent's
+            // edge.
             abs(leadingEdge) < abs(trailingEdge - parentSize) -> leadingEdge
             else -> trailingEdge - parentSize
         }
