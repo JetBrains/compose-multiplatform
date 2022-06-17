2ff12477135a2341dd1d25825ba932801d0db47c
Sam Gilbert <sjgilbert@google.com>
2022-06-17 15:35:34 +0000

    Revert "Fix KMP source metadata"
    This reverts commit d61385a4daefe9bf6969c4b0e9f6458fc1c64cfa.
    
    Reason for revert: stopped building many artifacts e.g. librarymetrics/ and repository/androidx/webkit. See https://chat.google.com/room/AAAAeWvosJo/wQ1maMyTliY for discussion
    
    Change-Id: I530ad51ac2db04490714c54b0333d02fd7da560d

--- private/src/main/kotlin/androidx/build/AndroidXExtension.kt
+++ private/src/main/kotlin/androidx/build/AndroidXExtension.kt
@@ -215,10 +215,6 @@ open class AndroidXExtension(val project: Project) {
 
     var bypassCoordinateValidation = false
 
-    // This should only be set for self-tests like AndroidXRootPluginTest.  It allows, for example,
-    // the lint project to be missing without triggering an immediate error.
-    var looseStructureForSelfTestsOnly = false
-
     /**
      * Which KMP platforms are published by this project, as a list of artifact suffixes or an empty
      * list for non-KMP projects.
