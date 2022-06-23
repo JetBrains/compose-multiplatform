61267f162592d0cb0f610d0235d5364ac22f3b1a
Halil Ozercan <halilibo@google.com>
2022-06-23 13:51:14 +0100

    Fix crash when selected text is cleared by state changes and re-added later
    BasicTextField's String API manages the selection and composition completely internally. Therefore, its selection value stays the same across value changes that comes from hoisted state, instead of IME commands. In a case where selected text is removed by a state change and later added, text layout result lags behind. This change makes sure that selection handles are only drawn when layout result is in sync with the latest TextFieldValue.
    
    Fix: b/208655565
    Fix: b/214253689
    Test: CoreTextFieldSelectionContentChangeTest.kt
    Relnote: Fixed a crash where TextField is cleared and refilled while selection is active.
    Change-Id: I1235b669921e67dcbec9e55a6d1a95ff609fe4b6

---
+++ foundation/foundation/src/androidAndroidTest/kotlin/androidx/compose/foundation/text/CoreTextFieldSelectionContentChangeTest.kt
@@ -0,0 +1,169 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.foundation.text
+
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.KeyEvent
+import androidx.compose.ui.input.key.NativeKeyEvent
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.SemanticsProperties
+import androidx.compose.ui.semantics.getOrNull
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.longClick
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performKeyPress
+import androidx.compose.ui.test.performTextInputSelection
+import androidx.compose.ui.test.performTouchInput
+import androidx.compose.ui.text.TextRange
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.google.common.truth.Truth.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@OptIn(ExperimentalTestApi::class)
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class CoreTextFieldSelectionContentChangeTest {
+
+    @get:Rule
+    val rule = createComposeRule()
+
+    private val Tag = "textField"
+
+    private val backspaceKeyDown = KeyEvent(
+        NativeKeyEvent(
+            NativeKeyEvent.ACTION_DOWN,
+            NativeKeyEvent.KEYCODE_DEL
+        )
+    )
+    private val backspaceKeyUp = KeyEvent(
+        NativeKeyEvent(
+            NativeKeyEvent.ACTION_UP,
+            NativeKeyEvent.KEYCODE_DEL
+        )
+    )
+
+    @Test
+    fun whenSelectedTextIsRemoved_SelectionCoerces() {
+        val textFieldValue = mutableStateOf("Hello")
+        rule.setContent {
+            BasicTextField(
+                value = textFieldValue.value,
+                onValueChange = {
+                    textFieldValue.value = it
+                },
+                modifier = Modifier.testTag(Tag).wrapContentSize()
+            )
+        }
+        val textNode = rule.onNodeWithTag(Tag)
+        textNode.performTouchInput { longClick() }
+        textNode.performTextInputSelection(TextRange(0, 4))
+        textFieldValue.value = ""
+
+        rule.waitForIdle()
+        val expected = TextRange(0, 0)
+        val actual = textNode.fetchSemanticsNode().config
+            .getOrNull(SemanticsProperties.TextSelectionRange)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun whenPartiallySelectedTextIsRemoved_SelectionCoercesToEdges() {
+        val textFieldValue = mutableStateOf("Hello World!")
+        rule.setContent {
+            BasicTextField(
+                value = textFieldValue.value,
+                onValueChange = {
+                    textFieldValue.value = it
+                },
+                modifier = Modifier.testTag(Tag).wrapContentSize()
+            )
+        }
+        val textNode = rule.onNodeWithTag(Tag)
+        textNode.performTouchInput { longClick() }
+        textNode.performTextInputSelection(TextRange(2, 8))
+        textFieldValue.value = "Hello"
+
+        rule.waitForIdle()
+
+        val expected = TextRange(2, 5)
+        val actual = textNode.fetchSemanticsNode().config
+            .getOrNull(SemanticsProperties.TextSelectionRange)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun whenSelectedTextIsRemoved_addedLater_SelectionRemains() {
+        val textFieldValue = mutableStateOf("Hello")
+        rule.setContent {
+            BasicTextField(
+                value = textFieldValue.value,
+                onValueChange = {
+                    textFieldValue.value = it
+                },
+                modifier = Modifier.testTag(Tag).wrapContentSize()
+            )
+        }
+        val textNode = rule.onNodeWithTag(Tag)
+        textNode.performTouchInput { longClick() }
+        textNode.performTextInputSelection(TextRange(0, 4))
+        textFieldValue.value = ""
+        rule.waitForIdle()
+        textFieldValue.value = "Hello"
+
+        val expected = TextRange(0, 4)
+        val actual = textNode.fetchSemanticsNode().config
+            .getOrNull(SemanticsProperties.TextSelectionRange)
+        assertThat(actual).isEqualTo(expected)
+    }
+
+    @Test
+    fun whenSelectedTextIsRemovedByIME_SelectionDoesNotRevert() {
+        // hard to find a descriptive name. Take a look at
+        // `whenSelectedTextIsRemoved_addedLater_SelectionRemains` to understand this case better.
+
+        val textFieldValue = mutableStateOf("Hello")
+        rule.setContent {
+            BasicTextField(
+                value = textFieldValue.value,
+                onValueChange = {
+                    textFieldValue.value = it
+                },
+                modifier = Modifier.testTag(Tag).wrapContentSize()
+            )
+        }
+        val textNode = rule.onNodeWithTag(Tag)
+        textNode.performTouchInput { longClick() }
+        textNode.performTextInputSelection(TextRange(0, 4))
+        textNode.performKeyPress(backspaceKeyDown)
+        textNode.performKeyPress(backspaceKeyUp)
+
+        textFieldValue.value = "Hello"
+
+        rule.waitForIdle()
+
+        val expected = TextRange(0, 0)
+        val actual = textNode.fetchSemanticsNode().config
+            .getOrNull(SemanticsProperties.TextSelectionRange)
+        assertThat(actual).isEqualTo(expected)
+    }
+}
