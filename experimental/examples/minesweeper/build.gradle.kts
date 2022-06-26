f124783ffd6565897b94571823b721c104605174
dima.avdeev <99798741+dima-avdeev-jb@users.noreply.github.com>
2022-06-26 15:29:41 +0300

    uikit experimental samples update version (#2140)

--- experimental/examples/minesweeper/build.gradle.kts
+++ experimental/examples/minesweeper/build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
 
 plugins {
     kotlin("multiplatform") version "1.6.21"
-    id("org.jetbrains.compose") version "1.2.0-alpha01-dev724"
+    id("org.jetbrains.compose") version "1.2.0-alpha01-dev725"
 }
 
 version = "1.0-SNAPSHOT"
