453b8263f8395e67872cb867f8ad52326f89f1bd
Connie Shi <connieshi@google.com>
2022-07-24 09:57:55 -0400

    Mark Badge and BadgedBox as experimental because the anchor alignment is influx.
    Bug: b/239955112, 236524516, 234156868
    Test: Ran existing tests.
    
    Relnote: "Mark Badge and BadgedBox as experimental because the anchor alignment is still influx."
    Change-Id: I1712ebf74e212414f3fb6b3dde9834a7f9ec0a16

--- material3/material3/samples/src/main/java/androidx/compose/material3/samples/BadgeSamples.kt
+++ material3/material3/samples/src/main/java/androidx/compose/material3/samples/BadgeSamples.kt
@@ -21,6 +21,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
@@ -30,6 +31,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Sampled
 @Composable
 fun NavigationBarItemWithBadge() {
--- material3/material3/src/commonMain/kotlin/androidx/compose/material3/Badge.kt
+++ material3/material3/src/commonMain/kotlin/androidx/compose/material3/Badge.kt
@@ -57,6 +57,7 @@ import androidx.compose.ui.unit.dp
  * @param content the anchor to which this badge will be positioned
  *
  */
+@ExperimentalMaterial3Api
 @Composable
 fun BadgedBox(
     badge: @Composable BoxScope.() -> Unit,
@@ -134,6 +135,7 @@ fun BadgedBox(
  * [containerColor] is not a color from the theme.
  * @param content optional content to be rendered inside this badge
  */
+@ExperimentalMaterial3Api
 @Composable
 fun Badge(
     modifier: Modifier = Modifier,
