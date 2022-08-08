0b455666c8a62bd0062db84ba946147ecd230cc4
Sean McQuillan <seanmcq@google.com>
2022-08-08 15:39:42 -0700

    Remove operator from equals on all equals definitions
    This does not have a defined behavior in Kotlin, and at present does not modify the resulting bytecode or protobuffs.
    
    Removing in case it gains meaning in a future Kotlin.
    
    Tested by diffing release build output and confirming the bit-equality of resulting aar files.
    
    Test: ./gradlew createArchive && diff ui-release.aar beforechange.aar
    Bug: b/241558188
    Relnote: "Removed unecessary operator from several equals definitions - this has no effect."
    
    Change-Id: I6c309cccd358bc76c82e7cd9e88e8a8bdf705c95

--- ui/ui-text/src/commonMain/kotlin/androidx/compose/ui/text/style/TextGeometricTransform.kt
+++ ui/ui-text/src/commonMain/kotlin/androidx/compose/ui/text/style/TextGeometricTransform.kt
@@ -46,7 +46,7 @@ class TextGeometricTransform(
         return TextGeometricTransform(scaleX, skewX)
     }
 
-    override operator fun equals(other: Any?): Boolean {
+    override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is TextGeometricTransform) return false
         if (scaleX != other.scaleX) return false
