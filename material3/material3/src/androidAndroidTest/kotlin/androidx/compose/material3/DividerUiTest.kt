47ee2a00284fa0d29d22cc055a4b301d6535eadd
José Figueroa Santos <serniebanders@google.com>
2022-08-16 14:28:14 -0400

    [Material3][Divider] Move color to last parameter, remove start indent.
    Additionally links to updated M3 spec
    
    Test: Update UI test to use new padding approach.
    RelNote: Removes startIndent from Divider, moves color to last parameter
    Bug: 242737152
    Change-Id: If7be24e6a8dc6b3386a4419b87ee275dd513d630
    (cherry picked from commit 073cb75f69e446e35529e809843269a586a72e81)
    Merged-In: If7be24e6a8dc6b3386a4419b87ee275dd513d630

--- material3/material3/src/androidAndroidTest/kotlin/androidx/compose/material3/DividerUiTest.kt
+++ material3/material3/src/androidAndroidTest/kotlin/androidx/compose/material3/DividerUiTest.kt
@@ -20,6 +20,7 @@ import android.os.Build
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.testutils.assertPixels
 import androidx.compose.ui.Modifier
@@ -79,7 +80,7 @@ class DividerUiTest {
 
         rule
             .setMaterialContentForSizeAssertions {
-                Divider(startIndent = indent, thickness = height)
+                Divider(modifier = Modifier.padding(start = indent), thickness = height)
             }
             .assertHeightIsEqualTo(height)
             .assertWidthIsEqualTo(rule.rootWidth())
