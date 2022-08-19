31c4889cc831a908166168edb9d6f3aa8cd511a2
Sean McQuillan <seanmcq@google.com>
2022-08-19 11:08:05 -0700

    [TextBenchmark] Benchmark theoretical baselines (floor)
    Benchmark theoretical minimum possible cost for Text:
    
    - EmptyLayoutBaseline - empty layout determined by text size
    - StaticLayoutBaseline - minimum cost for laying out (cached) text
    
    These baselines are useful when evaluating potential upside from various optimizations to Text(). However, they do not represent a realistic implementation of Text() and will always be more performant than any possible implementation of Text().
    
    ```
    161,073   ns          97 allocs    trace    AllAppsEmptyLayoutBaselineBaseline.recomposeOnly[size=2]
    372,952   ns         128 allocs    trace    AllAppsEmptyLayoutBaselineBaseline.recomposeMeasureLayout[size=2]
    190,102   ns         101 allocs    trace    AllAppsEmptyLayoutBaselineBaseline.recomposeOnly[size=16]
    413,750   ns         132 allocs    trace    AllAppsEmptyLayoutBaselineBaseline.recomposeMeasureLayout[size=16]
    208,272   ns         105 allocs    trace    AllAppsEmptyLayoutBaselineBaseline.recomposeOnly[size=32]
    443,551   ns         136 allocs    trace    AllAppsEmptyLayoutBaselineBaseline.recomposeMeasureLayout[size=32]
    231,720   ns         109 allocs    trace    AllAppsEmptyLayoutBaselineBaseline.recomposeOnly[size=64]
    491,478   ns         140 allocs    trace    AllAppsEmptyLayoutBaselineBaseline.recomposeMeasureLayout[size=64]
    87,280   ns          38 allocs    trace    AllAppsStaticLayoutBaseline.constructLayoutDraw[size=2]
    105,019   ns          38 allocs    trace    AllAppsStaticLayoutBaseline.constructLayoutDraw[size=16]
    123,191   ns          38 allocs    trace    AllAppsStaticLayoutBaseline.constructLayoutDraw[size=32]
    158,273   ns          38 allocs    trace    AllAppsStaticLayoutBaseline.constructLayoutDraw[size=64]
    249,290   ns         113 allocs    trace    ChatAppEmptyLayoutBaselineBaseline.recomposeOnly[size=256]
    535,818   ns         144 allocs    trace    ChatAppEmptyLayoutBaselineBaseline.recomposeMeasureLayout[size=256]
    272,061   ns         117 allocs    trace    ChatAppEmptyLayoutBaselineBaseline.recomposeOnly[size=512]
    555,766   ns         148 allocs    trace    ChatAppEmptyLayoutBaselineBaseline.recomposeMeasureLayout[size=512]
    377,175   ns          39 allocs    trace    ChatAppsStaticLayoutBaseline.constructLayoutDraw[size=256]
    665,131   ns          38 allocs    trace    ChatAppsStaticLayoutBaseline.constructLayoutDraw[size=512]
    ```
    
    Bug: b/239104335
    Test: Is a test
    Change-Id: I5cdf662feb373243ea1decd6c4f08489c2cd1b97

---
+++ foundation/foundation/benchmark/src/androidTest/java/androidx/compose/foundation/benchmark/text/empirical/baselines/EmptyLayoutBaseline.kt
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.foundation.benchmark.text.empirical.baselines
+
+import androidx.compose.foundation.benchmark.text.empirical.AllApps
+import androidx.compose.foundation.benchmark.text.empirical.ChatApps
+import androidx.compose.foundation.benchmark.text.empirical.generateCacheableStringOf
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.testutils.LayeredComposeTestCase
+import androidx.compose.testutils.ToggleableTestCase
+import androidx.compose.testutils.benchmark.ComposeBenchmarkRule
+import androidx.compose.testutils.benchmark.toggleStateBenchmarkComposeMeasureLayout
+import androidx.compose.testutils.benchmark.toggleStateBenchmarkRecompose
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.MeasurePolicy
+import androidx.test.filters.LargeTest
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+/**
+ * This benchmark emits an empty Layout and does not do _any_ Text work.
+ *
+ * Any impl. of text must emit at least one Layout node sized on soft wrapping, so this is a floor
+ * of work that Text could theoretically do if everything were fetched from a hypothetical free
+ * cache that could compute results for future requests magically..
+ *
+ * Text will never be able to be _this_ fast (as we don't _yet_ have time-travel chips), but it is
+ * useful to use this number as a floor when evaluating potential optimizations.
+ */
+class EmptyLayoutBaseline(
+    private val text: String
+) : LayeredComposeTestCase(), ToggleableTestCase {
+    private var toggleText = mutableStateOf("")
+
+    private val measurePolicy = MeasurePolicy { _, _ ->
+        val width = 300
+        val length = toggleText.value.length
+        val height = /* line height */ 20 * (length / (/* width */ 300 / /* char width */ 12))
+        layout(width, height.coerceAtLeast(20)) {}
+    }
+
+    @Composable
+    override fun MeasuredContent() {
+        // emit a layout based on text length, which is the minimal work that Text must perform
+        Layout(modifier = modifier, measurePolicy = measurePolicy)
+
+        // benchmark framework _requires_ that there is a read in composition, though this is not
+        // strictly necessary work for a minimal-Text impl (e.g. if passed AnnotatedCharSequence)
+
+        // TODO: update the benchmark framework to remove this line in a followup CL
+        toggleText.value
+    }
+
+    override fun toggleState() {
+        if (toggleText.value == "") {
+            toggleText.value = text
+        } else {
+            toggleText.value = ""
+        }
+    }
+}
+
+private val modifier = Modifier.fillMaxSize()
+
+@LargeTest
+@RunWith(Parameterized::class)
+open class EmptyLayoutBaselineParent(private val size: Int) {
+
+    @get:Rule
+    val benchmarkRule = ComposeBenchmarkRule()
+
+    private val caseFactory = {
+        val text = generateCacheableStringOf(size)
+        EmptyLayoutBaseline(text)
+    }
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "size={0}")
+        fun initParameters(): Array<Any> = arrayOf()
+    }
+
+    @Test
+    fun recomposeOnly() {
+        benchmarkRule.toggleStateBenchmarkRecompose(caseFactory)
+    }
+
+    @Test
+    fun recomposeMeasureLayout() {
+        benchmarkRule.toggleStateBenchmarkComposeMeasureLayout(caseFactory)
+    }
+}
+
+@LargeTest
+@RunWith(Parameterized::class)
+class AllAppsEmptyLayoutBaselineBaseline(size: Int) : EmptyLayoutBaselineParent(size) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "size={0}")
+        fun initParameters(): Array<Any> = AllApps.TextLengths
+    }
+}
+
+@LargeTest
+@RunWith(Parameterized::class)
+class ChatAppEmptyLayoutBaselineBaseline(size: Int) : EmptyLayoutBaselineParent(size) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "size={0}")
+        fun initParameters(): Array<Any> = ChatApps.TextLengths
+    }
+}
---
+++ foundation/foundation/benchmark/src/androidTest/java/androidx/compose/foundation/benchmark/text/empirical/baselines/StaticLayoutBaseline.kt
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.foundation.benchmark.text.empirical.baselines
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Typeface
+import android.os.Build
+import android.text.Layout
+import android.text.StaticLayout
+import android.text.TextPaint
+import androidx.annotation.RequiresApi
+import androidx.benchmark.junit4.BenchmarkRule
+import androidx.benchmark.junit4.measureRepeated
+import androidx.compose.foundation.benchmark.text.empirical.AllApps
+import androidx.compose.foundation.benchmark.text.empirical.ChatApps
+import androidx.compose.foundation.benchmark.text.empirical.generateCacheableStringOf
+import androidx.test.filters.LargeTest
+import androidx.test.filters.SdkSuppress
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+/**
+ * This benchmark tests the *absolute* lower bound of drawing perfectly optimal text on Android.
+ *
+ * It intentionally hits the word-layout cache and emits monospace.
+ *
+ * No real-world text usage will lay out this fast, but it provides a useful check for the lowest
+ * cost possible in text layout when evaluating potential optimizations.
+ */
+@RunWith(Parameterized::class)
+@LargeTest
+@SdkSuppress(minSdkVersion = 23)
+open class StaticLayoutBaseline(private val size: Int) {
+
+    @get:Rule
+    val benchmarkRule = BenchmarkRule()
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "size={0}")
+        fun initParameters(): Array<Any> = emptyArray()
+    }
+
+    @Test
+    fun constructLayoutDraw() {
+        val text = generateCacheableStringOf(size)
+        val textPaint = TextPaint()
+        textPaint.typeface = Typeface.MONOSPACE
+        val canvas = Canvas(Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888))
+        var isEmpty = false
+        benchmarkRule.measureRepeated {
+            val measureText = runWithTimingDisabled {
+                isEmpty = !isEmpty
+                if (isEmpty) "" else text
+            }
+            val layout = makeStaticLayout(measureText, textPaint)
+            layout.draw(canvas)
+        }
+    }
+}
+
+@RunWith(Parameterized::class)
+@LargeTest
+class AllAppsStaticLayoutBaseline(size: Int) : StaticLayoutBaseline(size) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "size={0}")
+        fun initParameters(): Array<Any> = AllApps.TextLengths
+    }
+}
+
+@RunWith(Parameterized::class)
+@LargeTest
+class ChatAppsStaticLayoutBaseline(size: Int) : StaticLayoutBaseline(size) {
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters(name = "size={0}")
+        fun initParameters(): Array<Any> = ChatApps.TextLengths
+    }
+}
+
+// do compat code to make a realistic lowest-possible benchmark for API21+
+// since StaticLayout.Builder is 23+
+fun makeStaticLayout(text: String, textPaint: TextPaint): StaticLayout {
+    return if (Build.VERSION.SDK_INT >= 23) {
+        StaticLayoutBuilderCompat_Api23.build(text, textPaint)
+    } else {
+        @Suppress("DEPRECATION")
+        StaticLayout(/* source = */ text, /* paint = */
+            textPaint, /* width = */
+            Int.MAX_VALUE, /* align = */
+            Layout.Alignment.ALIGN_NORMAL, /* spacingmult = */
+            1.0f, /* spacingadd = */
+            0.0f, /* includepad = */
+            true)
+    }
+}
+
+@RequiresApi(23)
+object StaticLayoutBuilderCompat_Api23 {
+    fun build(text: String, textPaint: TextPaint): StaticLayout {
+        return StaticLayout.Builder.obtain(/* source = */ text, /* start = */
+            0, /* end = */
+            text.length, /* paint = */
+            textPaint, /* width = */
+            Int.MAX_VALUE).build()
+    }
+}
