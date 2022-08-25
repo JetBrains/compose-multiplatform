5d83ce92c57df9ca57c85cb12741d08ea070171c
Louis Pullen-Freilich <lpf@google.com>
2022-08-25 18:34:45 +0100

    Fix for small negative overscroll deltas causing the glow effect to dissapear
    Small negative deltas that happen when a pointer is mostly stationary cause the glow effect to release - the subsequent positive deltas cause a new overscroll to start and run into a codepath where the delta is so small that the existing glow just instantly dissapears. To workaround this, we now track the cumulative negative delta and only release if it crosses a minimum threshold to prevent accidental releases in the majority of cases.
    
    Test: manual
    Fixes: b/242864658
    Change-Id: I1428417187084c447890ab19b16577b5c51fadff

--- foundation/foundation/src/androidMain/kotlin/androidx/compose/foundation/EdgeEffectCompat.kt
+++ foundation/foundation/src/androidMain/kotlin/androidx/compose/foundation/EdgeEffectCompat.kt
@@ -22,13 +22,18 @@ import android.util.AttributeSet
 import android.widget.EdgeEffect
 import androidx.annotation.DoNotInline
 import androidx.annotation.RequiresApi
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.dp
+import kotlin.math.abs
 
 internal object EdgeEffectCompat {
 
     fun create(context: Context, attrs: AttributeSet?): EdgeEffect {
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             Api31Impl.create(context, attrs)
-        } else EdgeEffect(context)
+        } else {
+            GlowEdgeEffectCompat(context)
+        }
     }
 
     fun EdgeEffect.onPullDistanceCompat(
@@ -50,12 +55,69 @@ internal object EdgeEffectCompat {
         }
     }
 
+    /**
+     * Used for calls to [EdgeEffect.onRelease] that happen because of scroll delta in the opposite
+     * direction to the overscroll. See [GlowEdgeEffectCompat].
+     */
+    fun EdgeEffect.onReleaseWithOppositeDelta(delta: Float) {
+        if (this is GlowEdgeEffectCompat) {
+            releaseWithOppositeDelta(delta)
+        } else {
+            onRelease()
+        }
+    }
+
     val EdgeEffect.distanceCompat: Float
         get() {
             return if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)) {
                 Api31Impl.getDistance(this)
             } else 0f
         }
+}
+
+/**
+ * Compat class to work around a framework issue (b/242864658) - small negative deltas that release
+ * an overscroll followed by positive deltas cause the glow overscroll effect to instantly
+ * disappear. This can happen when you pull the overscroll, and keep it there - small fluctuations
+ * in the pointer position can cause these small negative deltas, even though on average it is not
+ * really moving. To workaround this we only release the overscroll if the cumulative negative
+ * deltas are larger than a minimum value - this should catch the majority of cases.
+ */
+private class GlowEdgeEffectCompat(context: Context) : EdgeEffect(context) {
+    // Minimum distance in the opposite scroll direction to trigger a release
+    private val oppositeReleaseDeltaThreshold = with(Density(context)) { 1.dp.toPx() }
+    private var oppositeReleaseDelta = 0f
+
+    override fun onPull(deltaDistance: Float, displacement: Float) {
+        oppositeReleaseDelta = 0f
+        super.onPull(deltaDistance, displacement)
+    }
+
+    override fun onPull(deltaDistance: Float) {
+        oppositeReleaseDelta = 0f
+        super.onPull(deltaDistance)
+    }
+
+    override fun onRelease() {
+        oppositeReleaseDelta = 0f
+        super.onRelease()
+    }
+
+    override fun onAbsorb(velocity: Int) {
+        oppositeReleaseDelta = 0f
+        super.onAbsorb(velocity)
+    }
+
+    /**
+     * Increments the current cumulative delta, and calls [onRelease] if it is greater than
+     * [oppositeReleaseDeltaThreshold].
+     */
+    fun releaseWithOppositeDelta(delta: Float) {
+        oppositeReleaseDelta += delta
+        if (abs(oppositeReleaseDelta) > oppositeReleaseDeltaThreshold) {
+            onRelease()
+        }
+    }
 }
 
 @RequiresApi(Build.VERSION_CODES.S)
