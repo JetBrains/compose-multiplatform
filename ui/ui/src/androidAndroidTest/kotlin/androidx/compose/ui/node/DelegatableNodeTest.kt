06d93e5ac1aa1f4119e7f9736568a9403ed32f80
Ralston Da Silva <ralu@google.com>
2022-09-21 16:11:30 -0700

    Fixing bugs in ModifierNode.visitChildren
    visitChildren crashes when there are no children present.
    visitChildren does not visit children in the composition order.
    
    Bug: 248112489
    Bug: 248114359
    Fixes: 248112489
    Fixes: 248114359
    Test: ./gradlew compose:ui:ui:cC -P android.testInstrumentationRunnerArguments.class=androidx.compose.ui.node.DelegatableNodeTest
    Change-Id: I3f2cb8f4d83cae5b8e34844da46a9a0ef0d5b052

---
+++ ui/ui/src/androidAndroidTest/kotlin/androidx/compose/ui/node/DelegatableNodeTest.kt
@@ -0,0 +1,301 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package androidx.compose.ui.node
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.unit.dp
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@OptIn(ExperimentalComposeUiApi::class)
+@SmallTest
+@RunWith(AndroidJUnit4::class)
+class DelegatableNodeTest {
+
+    @get:Rule
+    val rule = createComposeRule()
+
+    @Test
+    fun visitChildren_noChildren() {
+        // Arrange.
+        val testNode = object : Modifier.Node() {}
+        val visitedChildren = mutableListOf<Modifier.Node>()
+        rule.setContent {
+            Box(modifier = modifierElementOf { testNode })
+        }
+
+        // Act.
+        rule.runOnIdle {
+            testNode.visitChildren(Nodes.Any) {
+                visitedChildren.add(it)
+            }
+        }
+
+        // Assert.
+        assertThat(visitedChildren).isEmpty()
+    }
+
+    @Test
+    fun visitChildWithinCurrentLayoutNode_immediateChild() {
+        // Arrange.
+        val (node1, node2, node3) = List(3) { object : Modifier.Node() {} }
+        val visitedChildren = mutableListOf<Modifier.Node>()
+        rule.setContent {
+            Box(
+                modifier = modifierElementOf { node1 }
+                    .then(modifierElementOf { node2 })
+                    .then(modifierElementOf { node3 })
+            )
+        }
+
+        // Act.
+        rule.runOnIdle {
+            node1.visitChildren(Nodes.Any) {
+                visitedChildren.add(it)
+            }
+        }
+
+        // Assert.
+        assertThat(visitedChildren).containsExactly(node2)
+    }
+
+    @Test
+    fun visitChildWithinCurrentLayoutNode_nonContiguousChild() {
+        // Arrange.
+        val (node1, node2) = List(2) { object : Modifier.Node() {} }
+        val visitedChildren = mutableListOf<Modifier.Node>()
+        rule.setContent {
+            Box(
+                modifier = modifierElementOf { node1 }
+                    .otherModifier()
+                    .then(modifierElementOf { node2 })
+            )
+        }
+
+        // Act.
+        rule.runOnIdle {
+            node1.visitChildren(Nodes.Any) {
+                visitedChildren.add(it)
+            }
+        }
+
+        // Assert.
+        assertThat(visitedChildren).containsExactly(node2)
+    }
+
+    @Test
+    fun visitChildrenInOtherLayoutNodes() {
+        // Arrange.
+        val (node1, node2, node3, node4, node5) = List(5) { object : Modifier.Node() {} }
+        val visitedChildren = mutableListOf<Modifier.Node>()
+        rule.setContent {
+            Box(modifier = modifierElementOf { node1 }) {
+                Box(modifier = modifierElementOf { node2 }) {
+                    Box(modifier = modifierElementOf { node3 })
+                }
+                Box(modifier = modifierElementOf { node4 }) {
+                    Box(modifier = modifierElementOf { node5 })
+                }
+            }
+        }
+
+        // Act.
+        rule.runOnIdle {
+            node1.visitChildren(Nodes.Any) {
+                visitedChildren.add(it)
+            }
+        }
+
+        // Assert.
+        assertThat(visitedChildren).containsExactly(node2, node4).inOrder()
+    }
+
+    @Test
+    fun visitAncestorWithinCurrentLayoutNode_immediateParent() {
+        // Arrange.
+        val (node1, node2) = List(2) { object : Modifier.Node() {} }
+        val visitedAncestors = mutableListOf<Modifier.Node>()
+        rule.setContent {
+            Box(
+                modifier = modifierElementOf { node1 }
+                    .then(modifierElementOf { node2 })
+            )
+        }
+
+        // Act.
+        rule.runOnIdle {
+            node2.visitAncestors(Nodes.Any) {
+                visitedAncestors.add(it)
+            }
+        }
+
+        // Assert.
+        assertThat(visitedAncestors.first()).isEqualTo(node1)
+    }
+
+    @Test
+    fun visitAncestorWithinCurrentLayoutNode_nonContiguousAncestor() {
+        // Arrange.
+        val (node1, node2) = List(2) { object : Modifier.Node() {} }
+        val visitedAncestors = mutableListOf<Modifier.Node>()
+        rule.setContent {
+            Box(
+                modifier = modifierElementOf { node1 }
+                    .border(10.dp, Color.Red)
+                    .then(modifierElementOf { node2 })
+            )
+        }
+
+        // Act.
+        rule.runOnIdle {
+            node2.visitAncestors(Nodes.Any) {
+                visitedAncestors.add(it)
+            }
+        }
+
+        // Assert.
+        assertThat(visitedAncestors).contains(node1)
+    }
+
+    @Test
+    fun visitAncestorsInOtherLayoutNodes() {
+        // Arrange.
+        val (node1, node2, node3, node4, node5) = List(5) { object : Modifier.Node() {} }
+        val visitedAncestors = mutableListOf<Modifier.Node>()
+        rule.setContent {
+            Box(modifier = modifierElementOf { node1 }) {
+                Box(
+                    modifier = Modifier
+                        .then(modifierElementOf { node2 })
+                        .then(modifierElementOf { node3 })
+                ) {
+                    Box {
+                        Box(
+                            modifier = Modifier
+                                .then(modifierElementOf { node4 })
+                                .then(modifierElementOf { node5 })
+                        )
+                    }
+                }
+            }
+        }
+
+        // Act.
+        rule.runOnIdle {
+            node5.visitAncestors(Nodes.Any) {
+                visitedAncestors.add(it)
+            }
+        }
+
+        // Assert.
+        assertThat(visitedAncestors)
+            .containsAtLeastElementsIn(arrayOf(node4, node3, node2, node1))
+            .inOrder()
+    }
+
+    @Test
+    fun nearestAncestorWithinCurrentLayoutNode_immediateParent() {
+        // Arrange.
+        val (node1, node2) = List(2) { object : Modifier.Node() {} }
+        rule.setContent {
+            Box(
+                modifier = modifierElementOf { node1 }
+                    .then(modifierElementOf { node2 })
+            )
+        }
+
+        // Act.
+        val parent = rule.runOnIdle {
+            node2.nearestAncestor(Nodes.Any)
+        }
+
+        // Assert.
+        assertThat(parent).isEqualTo(node1)
+    }
+
+    @Test
+    fun nearestAncestorWithinCurrentLayoutNode_nonContiguousAncestor() {
+        // Arrange.
+        val (node1, node2) = List(2) { object : Modifier.Node() {} }
+        rule.setContent {
+            Box(
+                modifier = modifierElementOf { node1 }
+                    .otherModifier()
+                    .then(modifierElementOf { node2 })
+            )
+        }
+
+        // Act.
+        val parent = rule.runOnIdle {
+            node2.nearestAncestor(Nodes.Any)
+        }
+
+        // Assert.
+        assertThat(parent).isEqualTo(node1)
+    }
+
+    @Test
+    fun nearestAncestorInDifferentLayoutNode_immediateParentLayoutNode() {
+        // Arrange.
+        val (node1, node2) = List(2) { object : Modifier.Node() {} }
+        rule.setContent {
+            Box(modifier = modifierElementOf { node1 }) {
+                Box(modifier = modifierElementOf { node2 })
+            }
+        }
+
+        // Act.
+        val parent = rule.runOnIdle {
+            node2.nearestAncestor(Nodes.Any)
+        }
+
+        // Assert.
+        assertThat(parent).isEqualTo(node1)
+    }
+
+    @Test
+    fun nearestAncestorInDifferentLayoutNode_nonContiguousParentLayoutNode() {
+        // Arrange.
+        val (node1, node2) = List(2) { object : Modifier.Node() {} }
+        rule.setContent {
+            Box(modifier = modifierElementOf { node1 }) {
+                Box {
+                    Box(modifier = modifierElementOf { node2 })
+                }
+            }
+        }
+
+        // Act.
+        val parent = rule.runOnIdle {
+            node2.nearestAncestor(Nodes.Any)
+        }
+
+        // Assert.
+        assertThat(parent).isEqualTo(node1)
+    }
+
+    private fun Modifier.otherModifier(): Modifier = this.then(Modifier)
+}
