31ea9286c29fdd25d855f844bce7fcf5a081dfe9
Andrey Kulikov <andreykulikov@google.com>
2022-09-28 14:37:00 +0100

    Correctly handle remeasure requests happening while node is not yet placed
    It was happening when AnimatedVisibility(which requests new remeasures frequently in order to animate) is used inside SubcomposeLayout and when the child was measured in the placement block (it was written like this in the Scaffold). Our logic was assuming that remeasure requests from a not placed child could be ignored, however we were not rerunning the request once this node is placed. Such children were just stuck in not remeasured state.
    
    Fixes: 247605039
    Fixes: 243852341
    Fixes: 244063674
    Fixes: 244781258
    Test: manually and a new test in MeasureInPlacementTest
    Change-Id: I385bb15818655587422ab9daf1d2add5bdc61c17

--- ui/ui/src/commonMain/kotlin/androidx/compose/ui/node/MeasureAndLayoutDelegate.kt
+++ ui/ui/src/commonMain/kotlin/androidx/compose/ui/node/MeasureAndLayoutDelegate.kt
@@ -81,9 +81,7 @@ internal class MeasureAndLayoutDelegate(private val root: LayoutNode) {
      * during the previous measure/layout pass and they were already measured as part of it.
      * See [requestRemeasure] for more details.
      */
-    private val postponedMeasureRequests = mutableVectorOf<LayoutNode>()
-
-    private val postponedLookaheadMeasureRequests = mutableVectorOf<LayoutNode>()
+    private val postponedMeasureRequests = mutableVectorOf<PostponedRequest>()
 
     private var rootConstraints: Constraints? = null
 
@@ -105,7 +103,6 @@ internal class MeasureAndLayoutDelegate(private val root: LayoutNode) {
                 root,
                 relayoutNodes,
                 postponedMeasureRequests.asMutableList(),
-                postponedLookaheadMeasureRequests.asMutableList(),
             )
         } else {
             null
@@ -135,7 +132,9 @@ internal class MeasureAndLayoutDelegate(private val root: LayoutNode) {
             Measuring, LookaheadLayingOut, LayingOut -> {
                 // requestLookaheadRemeasure is currently laying out and it is incorrect to
                 // request lookahead remeasure now, let's postpone it.
-                postponedLookaheadMeasureRequests.add(layoutNode)
+                postponedMeasureRequests.add(
+                    PostponedRequest(node = layoutNode, isLookahead = true, isForced = forced)
+                )
                 consistencyChecker?.assertConsistent()
                 false
             }
@@ -176,7 +175,9 @@ internal class MeasureAndLayoutDelegate(private val root: LayoutNode) {
             LookaheadLayingOut, LayingOut -> {
                 // requestMeasure is currently laying out and it is incorrect to request remeasure
                 // now, let's postpone it.
-                postponedMeasureRequests.add(layoutNode)
+                postponedMeasureRequests.add(
+                    PostponedRequest(node = layoutNode, isLookahead = false, isForced = forced)
+                )
                 consistencyChecker?.assertConsistent()
                 false
             }
@@ -450,20 +451,16 @@ internal class MeasureAndLayoutDelegate(private val root: LayoutNode) {
             }
             // execute postponed `onRequestMeasure`
             if (postponedMeasureRequests.isNotEmpty()) {
-                postponedMeasureRequests.forEach {
-                    if (it.isAttached) {
-                        requestRemeasure(it)
+                postponedMeasureRequests.forEach { request ->
+                    if (request.node.isAttached) {
+                        if (!request.isLookahead) {
+                            requestRemeasure(request.node, request.isForced)
+                        } else {
+                            requestLookaheadRemeasure(request.node, request.isForced)
+                        }
                     }
                 }
                 postponedMeasureRequests.clear()
-            }
-            if (postponedLookaheadMeasureRequests.isNotEmpty()) {
-                postponedLookaheadMeasureRequests.forEach {
-                    if (it.isAttached) {
-                        requestLookaheadRemeasure(it)
-                    }
-                }
-                postponedLookaheadMeasureRequests.clear()
             }
         }
         return sizeChanged
@@ -554,4 +551,6 @@ internal class MeasureAndLayoutDelegate(private val root: LayoutNode) {
         get() = lookaheadLayoutPending &&
             (measuredByParentInLookahead == InMeasureBlock ||
                 layoutDelegate.lookaheadAlignmentLinesOwner?.alignmentLines?.required == true)
+
+    class PostponedRequest(val node: LayoutNode, val isLookahead: Boolean, val isForced: Boolean)
 }
