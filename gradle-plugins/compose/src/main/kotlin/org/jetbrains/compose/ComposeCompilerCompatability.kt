4dd1c4a48321ff770dbea6c6774af458613e1df9
Igor Demin <igordmn@users.noreply.github.com>
2022-10-10 14:42:01 +0200

    Update Compose Compiler version to stable (#2381)

--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerCompatability.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerCompatability.kt
@@ -4,9 +4,9 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 
 internal object ComposeCompilerCompatability {
     fun compilerVersionFor(kotlinVersion: String): ComposeCompilerVersion? = when (kotlinVersion) {
-        "1.7.10" -> ComposeCompilerVersion("1.3.0-alpha01")
+        "1.7.10" -> ComposeCompilerVersion("1.3.0")
         "1.7.20" -> ComposeCompilerVersion(
-            "1.3.2-alpha01",
+            "1.3.2",
             unsupportedPlatforms = setOf(KotlinPlatformType.js)
         )
         else -> null
