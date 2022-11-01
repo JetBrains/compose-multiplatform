a091db2726d690669d1fa3e777da1dfdea0e0a09
Igor Demin <igordmn@users.noreply.github.com>
2022-11-01 18:22:18 +0100

    Update ComposeCompilerCompatability.kt

--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerCompatability.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerCompatability.kt
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 internal object ComposeCompilerCompatability {
     fun compilerVersionFor(kotlinVersion: String): ComposeCompilerVersion? = when (kotlinVersion) {
         "1.7.10" -> ComposeCompilerVersion("1.3.0")
-        "1.7.20" -> ComposeCompilerVersion("1.3.2.1-rc01")
+        "1.7.20" -> ComposeCompilerVersion("1.3.2.1-rc02")
         else -> null
     }
 }
