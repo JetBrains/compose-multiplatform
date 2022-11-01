77d22a57ac5ac38989161460f92412e8f1223416
Shalom Gibly <sgibly@google.com>
2022-11-01 13:46:54 -0700

    Adds tokens for M3 date picker and date input
    Bug: 245821760
    Test: N/A
    Relnote: N/A
    Change-Id: I2188b7b2801ba8b94e1ff616b97f10b95f3be33d

---
+++ material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/DateInputModalTokens.kt
@@ -0,0 +1,21 @@
+// VERSION: v0_126
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+package androidx.compose.material3.tokens
+
+import androidx.compose.ui.unit.dp
+
+internal object DateInputModalTokens {
+    val ContainerColor = ColorSchemeKeyTokens.Surface
+    val ContainerElevation = ElevationTokens.Level3
+    val ContainerHeight = 512.0.dp
+    val ContainerShape = ShapeKeyTokens.CornerExtraLarge
+    val ContainerSurfaceTintLayerColor = ColorSchemeKeyTokens.SurfaceTint
+    val ContainerWidth = 328.0.dp
+    val HeaderContainerHeight = 120.0.dp
+    val HeaderContainerWidth = 328.0.dp
+    val HeaderHeadlineColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val HeaderHeadlineFont = TypographyKeyTokens.HeadlineLarge
+    val HeaderSupportingTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val HeaderSupportingTextFont = TypographyKeyTokens.LabelMedium
+}
---
+++ material3/material3/src/commonMain/kotlin/androidx/compose/material3/tokens/DatePickerModalTokens.kt
@@ -0,0 +1,55 @@
+// VERSION: v0_126
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+package androidx.compose.material3.tokens
+
+import androidx.compose.ui.unit.dp
+
+internal object DatePickerModalTokens {
+    val ContainerColor = ColorSchemeKeyTokens.Surface
+    val ContainerElevation = ElevationTokens.Level3
+    val ContainerHeight = 512.0.dp
+    val ContainerShape = ShapeKeyTokens.CornerExtraLarge
+    val ContainerSurfaceTintLayerColor = ColorSchemeKeyTokens.SurfaceTint
+    val ContainerWidth = 328.0.dp
+    val DateContainerHeight = 40.0.dp
+    val DateContainerShape = ShapeKeyTokens.CornerFull
+    val DateContainerWidth = 40.0.dp
+    val DateLabelTextFont = TypographyKeyTokens.BodySmall
+    val DateSelectedContainerColor = ColorSchemeKeyTokens.Primary
+    val DateSelectedLabelTextColor = ColorSchemeKeyTokens.OnPrimary
+    val DateStateLayerHeight = 40.0.dp
+    val DateStateLayerShape = ShapeKeyTokens.CornerFull
+    val DateStateLayerWidth = 40.0.dp
+    val DateTodayContainerOutlineColor = ColorSchemeKeyTokens.Primary
+    val DateTodayContainerOutlineWidth = 1.0.dp
+    val DateTodayLabelTextColor = ColorSchemeKeyTokens.Primary
+    val DateUnselectedLabelTextColor = ColorSchemeKeyTokens.OnSurface
+    val HeaderContainerHeight = 120.0.dp
+    val HeaderContainerWidth = 328.0.dp
+    val HeaderHeadlineColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val HeaderHeadlineFont = TypographyKeyTokens.HeadlineLarge
+    val HeaderSupportingTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val HeaderSupportingTextFont = TypographyKeyTokens.LabelMedium
+    val RangeSelectionActiveIndicatorContainerColor = ColorSchemeKeyTokens.PrimaryContainer
+    val RangeSelectionActiveIndicatorContainerHeight = 40.0.dp
+    val RangeSelectionActiveIndicatorContainerShape = ShapeKeyTokens.CornerFull
+    val RangeSelectionContainerElevation = ElevationTokens.Level0
+    val RangeSelectionContainerShape = ShapeKeyTokens.CornerNone
+    val SelectionDateInRangeLabelTextColor = ColorSchemeKeyTokens.OnPrimaryContainer
+    val RangeSelectionHeaderContainerHeight = 128.0.dp
+    val RangeSelectionHeaderHeadlineFont = TypographyKeyTokens.TitleLarge
+    val RangeSelectionMonthSubheadColor = ColorSchemeKeyTokens.OnSurfaceVariant
+    val RangeSelectionMonthSubheadFont = TypographyKeyTokens.TitleSmall
+    val WeekdaysLabelTextColor = ColorSchemeKeyTokens.OnSurface
+    val WeekdaysLabelTextFont = TypographyKeyTokens.BodySmall
+    val SelectionYearContainerHeight = 36.0.dp
+    val SelectionYearContainerWidth = 72.0.dp
+    val SelectionYearLabelTextFont = TypographyKeyTokens.BodyLarge
+    val SelectionYearSelectedContainerColor = ColorSchemeKeyTokens.Primary
+    val SelectionYearSelectedLabelTextColor = ColorSchemeKeyTokens.OnPrimary
+    val SelectionYearStateLayerHeight = 36.0.dp
+    val SelectionYearStateLayerShape = ShapeKeyTokens.CornerFull
+    val SelectionYearStateLayerWidth = 72.0.dp
+    val SelectionYearUnselectedLabelTextColor = ColorSchemeKeyTokens.OnSurfaceVariant
+}
