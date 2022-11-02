c08e4d909ed879d50a2e8106de928817d3fd0733
Yigit Boyar <yboyar@google.com>
2022-11-01 19:05:06 -0700

    Don't publish all android variants from KMP
    This CL fixes a bug in KMP publishing where debug builds
    were also being published along with release.
    
    This is a quick fix, we still need to add tests here to
    ensure it doesn't break in the future
    
    Bug: 256947618
    Test: manually by running createArchieve and checking outputs
    Change-Id: I6ade8d847feb08698b6b1e69ec6109bf62dbf77d

--- private/src/main/kotlin/androidx/build/MavenUploadHelper.kt
+++ private/src/main/kotlin/androidx/build/MavenUploadHelper.kt
@@ -314,7 +314,9 @@ private fun Project.configureMultiplatformPublication(componentFactory: Software
 
     multiplatformExtension.targets.all { target ->
         if (target is KotlinAndroidTarget) {
-            target.publishAllLibraryVariants()
+            target.publishLibraryVariants(
+                Release.DEFAULT_PUBLISH_CONFIG
+            )
         }
     }
 
