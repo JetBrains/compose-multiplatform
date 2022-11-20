eed6d377277132c50886c1372c89d765b376ad76
dima.avdeev <dima.avdeev@jetbrains.com>
2022-11-20 19:13:56 +0400

    test android

--- components/resources/library/build.gradle.kts
+++ components/resources/library/build.gradle.kts
@@ -33,6 +33,12 @@ kotlin {
         val desktopMain by getting {
             dependsOn(skikoMain)
         }
+        val desktopTest by getting {
+            dependencies {
+                implementation("junit:junit:4.13.2")
+                implementation("org.jetbrains.compose.ui:ui-test-junit4:1.2.1")
+            }
+        }
         val androidMain by getting {}
         val androidTest by getting {
             dependencies {
@@ -68,11 +74,35 @@ android {
     defaultConfig {
         minSdk = 24
         targetSdk = 33
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    testOptions {
+        managedDevices {
+            devices {
+                maybeCreate<com.android.build.api.dsl.ManagedVirtualDevice>("pixel5").apply {
+                    device = "Pixel 5"
+                    apiLevel = 31
+                    systemImageSource = "aosp"
+                }
+            }
+        }
+    }
+}
+
+dependencies {
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.3.0")
+    testImplementation("junit:junit:4.12")
+    testImplementation("androidx.test:core:1.3.0")
+    testImplementation("org.robolectric:robolectric:4.6.1")
+    // Compose testing dependencies
+    androidTestImplementation("androidx.compose.ui:ui-test:1.3.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.3.0")
+    debugImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+//    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 }
 
 // TODO it seems that argument isn't applied to the common sourceSet. Figure out why
