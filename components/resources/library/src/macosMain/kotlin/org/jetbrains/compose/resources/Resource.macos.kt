db6ddd4db44c5fd50800807959be261472998e57
dima.avdeev <dima.avdeev@jetbrains.com>
2022-11-20 10:47:30 +0400

    resources: Mac demo

--- components/resources/library/src/macosMain/kotlin/org/jetbrains/compose/resources/Resource.macos.kt
+++ components/resources/library/src/macosMain/kotlin/org/jetbrains/compose/resources/Resource.macos.kt
@@ -18,9 +18,10 @@ actual fun resource(path: String): Resource = MacOSResourceImpl(path)
 private class MacOSResourceImpl(val path: String) : Resource {
     override suspend fun readBytes(): ByteArray {
         val currentDirectoryPath = NSFileManager.defaultManager().currentDirectoryPath
-        val contentsAtPath: NSData = NSFileManager.defaultManager().contentsAtPath(
-            "$currentDirectoryPath/src/commonMain/resources/" + path
-        )!!
+        val contentsAtPath: NSData = NSFileManager.defaultManager().run {
+            contentsAtPath("$currentDirectoryPath/src/macosMain/resources/$path")
+                ?: contentsAtPath("$currentDirectoryPath/src/commonMain/resources/$path")!!
+        }
         val byteArray = ByteArray(contentsAtPath.length.toInt())
         byteArray.usePinned {
             memcpy(it.addressOf(0), contentsAtPath.bytes, contentsAtPath.length)
