e900f561f2a9e17370205df8a2a672ac1f928c63
dima.avdeev <99798741+dima-avdeev-jb@users.noreply.github.com>
2022-11-22 11:26:57 +0300

    update experimental/examples/falling-balls (#2486)

--- experimental/examples/falling-balls-mpp/build.gradle.kts
+++ experimental/examples/falling-balls-mpp/build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
-        classpath("com.android.tools.build:gradle:7.0.4")
+        classpath("com.android.tools.build:gradle:7.3.1")
         classpath("org.jetbrains.compose:compose-gradle-plugin:1.2.1")
     }
 }
