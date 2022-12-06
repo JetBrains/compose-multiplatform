be283d91b0271d08066ae58789b852979c94dfbc
dima.avdeev <dima.avdeev@jetbrains.com>
2022-12-06 22:43:32 +0400

    read svg resource

--- components/resources/demo/shared/src/commonMain/kotlin/org/jetbrains/compose/resources/demo/shared/UseResources.kt
+++ components/resources/demo/shared/src/commonMain/kotlin/org/jetbrains/compose/resources/demo/shared/UseResources.kt
@@ -4,13 +4,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import org.jetbrains.compose.resources.*
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 internal fun UseResources() {
+    var textState by remember {  mutableStateOf("Hello, resources") }
     Column {
-        Text("Hello, resources")
+        Text(textState)
         Image(
             bitmap = resource("dir/img.png").rememberImageBitmap().orEmpty(),
             contentDescription = null,
@@ -19,5 +22,8 @@ internal fun UseResources() {
             bitmap = resource("img.webp").rememberImageBitmap().orEmpty(),
             contentDescription = null,
         )
+    }
+    LaunchedEffect(Unit) {
+        textState = resource("android.svg").readBytes().decodeToString()
     }
 }
