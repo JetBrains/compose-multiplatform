abc71c721ec99ce1ed0b7bba16568a7f03782bf5
dima.avdeev <dima.avdeev@jetbrains.com>
2022-12-12 14:31:29 +0400

    fix widgets gallery uikit

--- experimental/examples/widgets-gallery/shared/src/iosMain/kotlin/org/jetbrains/compose/demo/widgets/platform/main.ios.kt
+++ experimental/examples/widgets-gallery/shared/src/iosMain/kotlin/org/jetbrains/compose/demo/widgets/platform/main.ios.kt
@@ -1,7 +1,9 @@
-package org.jetbrains.compose.demo.widgets.platform/*
- * Copyright 2020-2021 JetBrains s.r.o. and respective authors and developers.
+/*
+ * Copyright 2020-2022 JetBrains s.r.o. and respective authors and developers.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
  */
+
+package org.jetbrains.compose.demo.widgets.platform
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
