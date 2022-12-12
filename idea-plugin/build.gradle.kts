27ce04b037c8e1f8f1d75096160bd8a50b5832e4
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2022-12-12 08:17:53 +0100

    Update Intellij Gradle plugin

--- idea-plugin/build.gradle.kts
+++ idea-plugin/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.5.10"
-    id("org.jetbrains.intellij") version "1.6.0"
+    id("org.jetbrains.intellij") version "1.10.1"
     id("org.jetbrains.changelog") version "1.3.1"
 }
 
