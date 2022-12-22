b12a0c3d9df22b9bd6a7b989f0d6ac75ee121068
dima.avdeev <dima.avdeev@jetbrains.com>
2022-12-23 01:52:37 +0400

    fix imageviewer uikit

--- experimental/examples/imageviewer/shared/src/commonMain/kotlin/example/imageviewer/view/ScrollableColumn.common.kt
+++ experimental/examples/imageviewer/shared/src/commonMain/kotlin/example/imageviewer/view/ScrollableColumn.common.kt
@@ -7,10 +7,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-expect fun ScrollableColumn(modifier: Modifier, content: @Composable () -> Unit)
+internal expect fun ScrollableColumn(modifier: Modifier, content: @Composable () -> Unit)
 
 @Composable
-fun TouchScrollableColumn(modifier: Modifier, content: @Composable () -> Unit) {
+internal fun TouchScrollableColumn(modifier: Modifier, content: @Composable () -> Unit) {
     val scrollState = rememberScrollState()
     Column(modifier.verticalScroll(scrollState)) {
         content()
