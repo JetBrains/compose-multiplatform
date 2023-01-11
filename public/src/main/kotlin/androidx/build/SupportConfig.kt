e8653435a2237cca81dc5b9083f2550df863d2ff
Alan Viverette <alanv@google.com>
2023-01-11 10:19:06 -0500

    Bump compileSdk to android-33-ext4
    Fixes: 248219194
    Test: ./gradlew bOS (it builds!)
    Merged-In: I130a61d0e84888a8809eace5be9e3f98da3928f8
    Change-Id: I2c9b4aca759db74a0a321ee8bda2f8d75210c905

--- public/src/main/kotlin/androidx/build/SupportConfig.kt
+++ public/src/main/kotlin/androidx/build/SupportConfig.kt
@@ -33,7 +33,7 @@ object SupportConfig {
      * Either an integer value or a pre-release platform code, prefixed with "android-" (ex.
      * "android-28" or "android-Q") as you would see within the SDK's platforms directory.
      */
-    const val COMPILE_SDK_VERSION = "android-33"
+    const val COMPILE_SDK_VERSION = "android-33-ext4"
 
     /**
      * The Android SDK version to use for targetSdkVersion meta-data.
