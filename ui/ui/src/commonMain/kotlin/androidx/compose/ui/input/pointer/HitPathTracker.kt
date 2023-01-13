vladertel/compose-multiplatform-core

Temporarily revert "Remove duplicate events."
It breaks hover and pointer icon.

Hover is fixed here: https://github.com/JetBrains/androidx/pull/172

This reverts commit f5e4ff8938d84846e995198004fdb0fd81728426.

compose/ui/ui/src/commonMain/kotlin/androidx/compose/ui/input/pointer/HitPathTracker.kt
@@ -86,15 +86,6 @@ internal class HitPathTracker(private val rootCoordinates: LayoutCoordinates) {
         internalPointerEvent: InternalPointerEvent,
         isInBounds: Boolean = true
     ): Boolean {
-        val changed = root.buildCache(
-            internalPointerEvent.changes,
-            rootCoordinates,
-            internalPointerEvent,
-            isInBounds
-        )
-        if (!changed) {
-            return false
-        }
         var dispatchHit = root.dispatchMainEventPass(
             internalPointerEvent.changes,
             rootCoordinates,
@@ -137,24 +128,6 @@ internal class HitPathTracker(private val rootCoordinates: LayoutCoordinates) {
 @OptIn(InternalCoreApi::class)
 internal open class NodeParent {
     val children: MutableVector<Node> = mutableVectorOf()
-
-    open fun buildCache(
-        changes: Map<PointerId, PointerInputChange>,
-        parentCoordinates: LayoutCoordinates,
-        internalPointerEvent: InternalPointerEvent,
-        isInBounds: Boolean
-    ): Boolean {
-        var changed = false
-        children.forEach {
-            changed = it.buildCache(
-                changes,
-                parentCoordinates,
-                internalPointerEvent,
-                isInBounds
-            ) || changed
-        }
-        return changed
-    }
 
     /**
      * Dispatches [changes] down the tree, for the initial and main pass.
@@ -278,6 +251,9 @@ internal class Node(val pointerInputFilter: PointerInputFilter) : NodeParent() {
         internalPointerEvent: InternalPointerEvent,
         isInBounds: Boolean
     ): Boolean {
+        // Build the cache that will be used for both the main and final pass
+        buildCache(changes, parentCoordinates, internalPointerEvent, isInBounds)
+
         // TODO(b/158243568): The below dispatching operations may cause the pointerInputFilter to
         //  become detached. Currently, they just no-op if it becomes detached and the detached
         //  pointerInputFilters are removed from being tracked with the next event. I currently
@@ -342,22 +318,14 @@ internal class Node(val pointerInputFilter: PointerInputFilter) : NodeParent() {
      *
      * @see clearCache
      */
-    override fun buildCache(
+    private fun buildCache(
         changes: Map<PointerId, PointerInputChange>,
         parentCoordinates: LayoutCoordinates,
         internalPointerEvent: InternalPointerEvent,
         isInBounds: Boolean
-    ): Boolean {
-        val childChanged =
-            super.buildCache(
-                changes,
-                parentCoordinates,
-                internalPointerEvent,
-                isInBounds
-            )
-
+    ) {
         // Avoid future work if we know this node will no-op
-        if (!pointerInputFilter.isAttached) return true
+        if (!pointerInputFilter.isAttached) return
 
         coordinates = pointerInputFilter.layoutCoordinates
 
@@ -395,7 +363,7 @@ internal class Node(val pointerInputFilter: PointerInputFilter) : NodeParent() {
         if (relevantChanges.isEmpty()) {
             pointerIds.clear()
             children.clear()
-            return true // not hit
+            return
         }
 
         // Clean up any pointerIds that weren't dispatched
@@ -433,25 +401,7 @@ internal class Node(val pointerInputFilter: PointerInputFilter) : NodeParent() {
             if (event.type == PointerEventType.Enter) hasEntered = true
             if (event.type == PointerEventType.Exit) hasEntered = false
         }
-
-        val changed = childChanged || event.type != PointerEventType.Move ||
-            hasPositionChanged(pointerEvent, event)
         pointerEvent = event
-        return changed
-    }
-
-    private fun hasPositionChanged(oldEvent: PointerEvent?, newEvent: PointerEvent): Boolean {
-        if (oldEvent == null || oldEvent.changes.size != newEvent.changes.size) {
-            return true
-        }
-        for (i in 0 until newEvent.changes.size) {
-            val old = oldEvent.changes[i]
-            val current = newEvent.changes[i]
-            if (old.position != current.position) {
-                return true
-            }
-        }
-        return false
     }
 
     /**
@@ -463,6 +413,7 @@ internal class Node(val pointerInputFilter: PointerInputFilter) : NodeParent() {
     private fun clearCache() {
         relevantChanges.clear()
         coordinates = null
+        pointerEvent = null
     }
 
     /**

JetBrains/compose-multiplatform

ui/ui/src/commonMain/kotlin/androidx/compose/ui/input/pointer/HitPathTracker.kt
@@ -90,15 +90,6 @@ internal class HitPathTracker(private val rootCoordinates: LayoutCoordinates) {
         internalPointerEvent: InternalPointerEvent,
         isInBounds: Boolean = true
     ): Boolean {
-        val changed = root.buildCache(
-            internalPointerEvent.changes,
-            rootCoordinates,
-            internalPointerEvent,
-            isInBounds
-        )
-        if (!changed) {
-            return false
-        }
         var dispatchHit = root.dispatchMainEventPass(
             internalPointerEvent.changes,
             rootCoordinates,
@@ -141,24 +132,6 @@ internal class HitPathTracker(private val rootCoordinates: LayoutCoordinates) {
 @OptIn(InternalCoreApi::class, ExperimentalComposeUiApi::class)
 internal open class NodeParent {
     val children: MutableVector<Node> = mutableVectorOf()
-
-    open fun buildCache(
-        changes: Map<PointerId, PointerInputChange>,
-        parentCoordinates: LayoutCoordinates,
-        internalPointerEvent: InternalPointerEvent,
-        isInBounds: Boolean
-    ): Boolean {
-        var changed = false
-        children.forEach {
-            changed = it.buildCache(
-                changes,
-                parentCoordinates,
-                internalPointerEvent,
-                isInBounds
-            ) || changed
-        }
-        return changed
-    }
 
     /**
      * Dispatches [changes] down the tree, for the initial and main pass.
@@ -283,6 +256,9 @@ internal class Node(val pointerInputNode: PointerInputModifierNode) : NodeParent
         internalPointerEvent: InternalPointerEvent,
         isInBounds: Boolean
     ): Boolean {
+        // Build the cache that will be used for both the main and final pass
+        buildCache(changes, parentCoordinates, internalPointerEvent, isInBounds)
+
         // TODO(b/158243568): The below dispatching operations may cause the pointerInputFilter to
         //  become detached. Currently, they just no-op if it becomes detached and the detached
         //  pointerInputFilters are removed from being tracked with the next event. I currently
@@ -347,22 +323,14 @@ internal class Node(val pointerInputNode: PointerInputModifierNode) : NodeParent
      *
      * @see clearCache
      */
-    override fun buildCache(
+    private fun buildCache(
         changes: Map<PointerId, PointerInputChange>,
         parentCoordinates: LayoutCoordinates,
         internalPointerEvent: InternalPointerEvent,
         isInBounds: Boolean
-    ): Boolean {
-        val childChanged =
-            super.buildCache(
-                changes,
-                parentCoordinates,
-                internalPointerEvent,
-                isInBounds
-            )
-
+    ) {
         // Avoid future work if we know this node will no-op
-        if (!pointerInputNode.isAttached) return true
+        if (!pointerInputNode.isAttached) return
 
         coordinates = pointerInputNode.layoutCoordinates
 
@@ -400,7 +368,7 @@ internal class Node(val pointerInputNode: PointerInputModifierNode) : NodeParent
         if (relevantChanges.isEmpty()) {
             pointerIds.clear()
             children.clear()
-            return true // not hit
+            return
         }
 
         // Clean up any pointerIds that weren't dispatched
@@ -442,25 +410,7 @@ internal class Node(val pointerInputNode: PointerInputModifierNode) : NodeParent
                 event.type = PointerEventType.Move // We are still in.
             }
         }
-
-        val changed = childChanged || event.type != PointerEventType.Move ||
-            hasPositionChanged(pointerEvent, event)
         pointerEvent = event
-        return changed
-    }
-
-    private fun hasPositionChanged(oldEvent: PointerEvent?, newEvent: PointerEvent): Boolean {
-        if (oldEvent == null || oldEvent.changes.size != newEvent.changes.size) {
-            return true
-        }
-        for (i in 0 until newEvent.changes.size) {
-            val old = oldEvent.changes[i]
-            val current = newEvent.changes[i]
-            if (old.position != current.position) {
-                return true
-            }
-        }
-        return false
     }
 
     /**
@@ -472,6 +422,7 @@ internal class Node(val pointerInputNode: PointerInputModifierNode) : NodeParent
     private fun clearCache() {
         relevantChanges.clear()
         coordinates = null
+        pointerEvent = null
     }
 
     /**