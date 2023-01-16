c3726347ef68cf3fe79bf39ded0fc8dc114c8d8f
Alexey Tsvetkov <654232+AlexeyTsvetkov@users.noreply.github.com>
2023-01-16 14:41:44 +0100

    Test with Gradle 8-RC-1 (#2621)
    * Test with Gradle 8-RC-1
    
    * Fix Gradle tests for 8.0-rc-1

--- gradle-plugins/compose/src/test/kotlin/org/jetbrains/compose/test/utils/jdkUtls.kt
+++ gradle-plugins/compose/src/test/kotlin/org/jetbrains/compose/test/utils/jdkUtls.kt
@@ -6,6 +6,7 @@
 package org.jetbrains.compose.test.utils
 
 import java.io.File
+import kotlin.io.path.isExecutable
 
 const val JDK_11_BYTECODE_VERSION = 55
 
@@ -22,10 +23,38 @@ fun readClassFileVersion(classFile: File): Int {
     return match.groupValues[1].toInt()
 }
 
-fun runJavaTool(toolName: String, vararg args: String): ProcessRunResult {
+internal fun javaToolExecutableName(name: String): String =
+    if (isWindows) "$name.exe" else name
+
+internal fun runJavaTool(toolName: String, vararg args: String): ProcessRunResult {
     val javaHome = File(System.getProperty("java.home"))
-    val toolExecutableName = if (isWindows) "$toolName.exe" else toolName
-    val executable = javaHome.resolve("bin/$toolExecutableName")
+    val executable = javaHome.resolve("bin/${javaToolExecutableName(toolName)}")
     check(executable.isFile) { "Could not find tool '$toolName' at specified path: $executable" }
     return runProcess(executable, args.toList())
+}
+
+/**
+ * Expects the following structure:
+ * [rootDir]
+ * -- JDK_VERSION_1
+ *    -- UNPACKED_JDK_1_DISTRIBUTION_ARCHIVE
+ * -- JDK_VERSION_2
+ *    -- UNPACKED_JDK_2_DISTRIBUTION_ARCHIVE
+ *
+ * where JDK_VERSION_* is an integer corresponding to the major version of JDK distribution
+ */
+internal fun listTestJdks(rootDir: File): List<String> {
+    if (!rootDir.isDirectory) return emptyList()
+
+    return rootDir.listFiles()!!
+        .filter { it.isDirectory }
+        .map { findJavaHome(it).absolutePath }
+}
+
+private fun findJavaHome(dir: File): File {
+    val javaExecutableName = javaToolExecutableName("java")
+    val javaExecutable = dir.walk()
+        .firstOrNull { it.isFile && it.name == javaExecutableName && it.toPath().isExecutable() }
+        ?: error("Could not find executable '$javaExecutableName' in '$dir' directory")
+    return javaExecutable.parentFile.parentFile.absoluteFile
 }
