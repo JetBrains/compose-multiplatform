7b9642ad0541021b6addae9b144e90fcbde50425
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2023-01-19 10:17:31 +0100

    Add ktx serialization integration test (#2642)
    * Add compose with kotlinx.serialization integration test
    
    * Add versions configuration
    
    * add linuxX64 target
    
    * remove a default value for `compose.kotlinCompilerPluginVersion`
    
    * add README.MD
    
    * update README.MD

---
+++ compose/integrations/compose-with-ktx-serialization/common/build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.compose.compose
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+    kotlin("plugin.serialization")
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+kotlin {
+    jvm("desktop") {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+    }
+    js(IR) {
+        browser()
+        nodejs()
+    }
+    ios()
+    macosX64()
+    macosArm64()
+    mingwX64()
+    linuxX64()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                val ktxSerializationVer = project.property("kotlinx.serializationCore")
+                implementation(compose.runtime)
+                api("org.jetbrains.kotlinx:kotlinx-serialization-core:$ktxSerializationVer")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+}
+
+compose {
+    val compilerPluginVersion = project.properties["compose.kotlinCompilerPluginVersion"] as? String
+    if (!compilerPluginVersion.isNullOrEmpty()) {
+        println("using compilerPluginVersion = $compilerPluginVersion")
+        kotlinCompilerPlugin.set(compilerPluginVersion)
+    }
+}
