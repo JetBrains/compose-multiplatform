5d7c87a6a769dd34315a6452ae30fdb0851dea8c
Faithful Uchenna Okoye <uokoye@google.com>
2023-01-26 19:08:29 +0000

    FlowLayout Samples
    This CL provides FlowLayout samples for FlowRow and FlowColumn
    Test: Tested in prior CLs and ran to confirm samples work
    Bug: 261230429
    Fixed: 261230429
    
    Change-Id: I01eae61a249a15fd8b7f03f3469e6e2cd5f6cafd

--- foundation/foundation-layout/integration-tests/layout-demos/src/main/java/androidx/compose/foundation/layout/demos/SimpleFlowRowDemo.kt
+++ foundation/foundation-layout/integration-tests/layout-demos/src/main/java/androidx/compose/foundation/layout/demos/SimpleFlowRowDemo.kt
@@ -16,90 +16,17 @@
 
 package androidx.compose.foundation.layout.demos
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.samples.SimpleFlowRow
+import androidx.compose.foundation.layout.samples.SimpleFlowRowWithWeights
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun SimpleFlowRowDemo() {
     Column() {
-        FlowRow(
-            Modifier
-                .wrapContentWidth(align = Alignment.Start)
-                .wrapContentHeight(align = Alignment.Top)
-                .requiredHeight(150.dp)
-                .border(BorderStroke(2.dp, Color.Gray)),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
-            maxItemsInEachRow = Int.MAX_VALUE
-        ) {
-            repeat(50) {
-                Text("Heldo")
-            }
-        }
-        FlowRow(
-            Modifier
-                .fillMaxWidth(1f)
-                .wrapContentHeight(align = Alignment.Top)
-                .border(BorderStroke(2.dp, Color.Gray)),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.End,
-            maxItemsInEachRow = 3
-        ) {
-            repeat(10) {
-                Box(
-                    Modifier
-                        .padding(10.dp)
-                        .width(50.dp)
-                        .height(
-                            if (it % 2 == 0) {
-                                30.dp
-                            } else {
-                                50.dp
-                            }
-                        )
-                        .background(Color(0xFF6200ED))
-                        .weight(1f, false)
-                ) {
-                    Text(text = it.toString(), fontSize = 18.sp, modifier = Modifier.padding(3.dp))
-                }
-            }
-        }
-
-        FlowRow(
-            Modifier
-                .wrapContentHeight()
-                .width(200.dp),
-            horizontalArrangement = Arrangement.Start,
-            maxItemsInEachRow = 5
-        ) {
-            repeat(6) { _ ->
-                Box(
-                    Modifier
-                        .size(20.dp)
-                )
-            }
-        }
+        SimpleFlowRow()
+        SimpleFlowRowWithWeights()
     }
 }
