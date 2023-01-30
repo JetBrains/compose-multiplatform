bc6d42ef449eaa74aac0516ae7916e9f5fd2b002
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2023-01-30 21:02:23 +0100

    Update versions: compose - 1.3.0 and kotlin - 1.8.0 (#2651)
    * Update versions in examples
    
    * Add more scripts to validate example on android and k/js
    
    * Add validateExamplesIos.sh
    
    * Update CHANGELOG.md for 1.3.0
    
    * check examples with 1.3.0-rc06
    
    * Update the versions: compose-multiplatform - 1.3.0, kotlin - 1.8.0
    
    * Update ComposeCompilerCompatibility (1.4.0)
    
    * Update web/yarn.lock

--- examples/widgets-gallery/common/build.gradle.kts
+++ examples/widgets-gallery/common/build.gradle.kts
@@ -32,11 +32,11 @@ kotlin {
 }
 
 android {
-    compileSdk = 32
+    compileSdk = 33
 
     defaultConfig {
         minSdk = 26
-        targetSdk = 32
+        targetSdk = 33
     }
 
     compileOptions {
