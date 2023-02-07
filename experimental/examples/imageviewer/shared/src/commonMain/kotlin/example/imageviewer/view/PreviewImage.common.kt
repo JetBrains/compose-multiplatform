314de4b2f72a6d19612e7aca118756ed32e77bc1
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2023-02-07 16:21:21 +0100

    Use animations to move between different images

--- experimental/examples/imageviewer/shared/src/commonMain/kotlin/example/imageviewer/view/PreviewImage.common.kt
+++ experimental/examples/imageviewer/shared/src/commonMain/kotlin/example/imageviewer/view/PreviewImage.common.kt
@@ -1,26 +1,35 @@
 package example.imageviewer.view
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.with
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import example.imageviewer.model.*
+import example.imageviewer.model.Picture
 import example.imageviewer.model.State
-import example.imageviewer.style.*
+import example.imageviewer.model.toFullscreen
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.orEmpty
-import org.jetbrains.compose.resources.rememberImageBitmap
-import org.jetbrains.compose.resources.resource
 
-@OptIn(ExperimentalResourceApi::class)
+@OptIn(ExperimentalResourceApi::class, ExperimentalAnimationApi::class)
 @Composable
 internal fun PreviewImage(state: MutableState<State>, getImage: suspend (Picture) -> ImageBitmap) {
     val pictures = state.value.pictures
@@ -48,14 +57,28 @@ internal fun PreviewImage(state: MutableState<State>, getImage: suspend (Picture
         shape = RoundedCornerShape(10.dp, 10.dp, 10.dp, 10.dp),
 //        elevation = 1.dp
     ) {
-        Image(
-            bitmap = image ?: resource("empty.png").rememberImageBitmap().orEmpty(),
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxSize()//.padding(start = 1.dp, top = 1.dp, end = 1.dp, bottom = 5.dp),
-            ,
-            contentScale = ContentScale.Crop
-        )
+        AnimatedContent(
+            targetState = image,// ?: resource("empty.png").rememberImageBitmap().orEmpty(),
+            transitionSpec = {
+                slideInVertically(initialOffsetY = { it }) with slideOutVertically(targetOffsetY = { -it })
+            }
+        ) { imageBitmap ->
+            if (imageBitmap != null) {
+                Image(
+                    bitmap = imageBitmap,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxSize()//.padding(start = 1.dp, top = 1.dp, end = 1.dp, bottom = 5.dp),
+                    ,
+                    contentScale = ContentScale.Crop
+                )
+            } else {
+                Spacer(
+                    modifier = Modifier.fillMaxSize()
+                        .background(brush = kotlinHorizontalGradientBrush)
+                )
+            }
+        }
     }
 }
 
