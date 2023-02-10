f2195d15dd8d4326979cf81809f50581115a77bb
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2023-02-10 16:02:03 +0100

    More design changes, rounded corners and animations

--- experimental/examples/imageviewer/shared/src/commonMain/kotlin/example/imageviewer/ImageViewer.common.kt
+++ experimental/examples/imageviewer/shared/src/commonMain/kotlin/example/imageviewer/ImageViewer.common.kt
@@ -1,11 +1,22 @@
 package example.imageviewer
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
-import example.imageviewer.model.*
+import example.imageviewer.model.ScreenState
+import example.imageviewer.model.State
+import example.imageviewer.model.bigUrl
+import example.imageviewer.model.nextImage
+import example.imageviewer.model.picture
+import example.imageviewer.model.previousImage
+import example.imageviewer.model.refresh
 import example.imageviewer.view.FullscreenImage
 import example.imageviewer.view.MainScreen
 
@@ -14,22 +25,27 @@ internal fun ImageViewerCommon(state: MutableState<State>, dependencies: Depende
     state.refresh(dependencies)
 
     Surface(modifier = Modifier.fillMaxSize()) {
-        when (state.value.screen) {
-            ScreenState.Miniatures -> {
-                MainScreen(state, dependencies)
-            }
+        AnimatedVisibility(
+            state.value.screen == ScreenState.Miniatures,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            MainScreen(state, dependencies)
+        }
 
-            ScreenState.FullScreen -> {
-                FullscreenImage(
-                    picture = state.value.picture,
-                    getImage = { dependencies.imageRepository.loadContent(it.bigUrl) },
-                    getFilter = { dependencies.getFilter(it) },
-                    localization = dependencies.localization,
-                    back = { state.value = state.value.copy(screen = ScreenState.Miniatures) },
-                    nextImage = { state.nextImage() },
-                    previousImage = { state.previousImage() },
-                )
-            }
+        AnimatedVisibility(
+            state.value.screen == ScreenState.FullScreen,
+            enter = slideInHorizontally { -it },
+            exit = slideOutHorizontally { -it }) {
+            FullscreenImage(
+                picture = state.value.picture,
+                getImage = { dependencies.imageRepository.loadContent(it.bigUrl) },
+                getFilter = { dependencies.getFilter(it) },
+                localization = dependencies.localization,
+                back = { state.value = state.value.copy(screen = ScreenState.Miniatures) },
+                nextImage = { state.nextImage() },
+                previousImage = { state.previousImage() },
+            )
         }
     }
 }
--- experimental/examples/imageviewer/shared/src/commonMain/kotlin/example/imageviewer/view/Miniature.kt
+++ experimental/examples/imageviewer/shared/src/commonMain/kotlin/example/imageviewer/view/Miniature.kt
@@ -4,24 +4,29 @@ import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardElevation
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import example.imageviewer.model.*
-import example.imageviewer.style.*
+import example.imageviewer.model.Picture
+import example.imageviewer.model.name
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.orEmpty
 import org.jetbrains.compose.resources.rememberImageBitmap
@@ -58,6 +63,7 @@ internal fun Miniature(
                     contentDescription = null,
                     modifier = modifier
                         .clip(CircleShape)
+                        .border(BorderStroke(1.dp, Color.White), CircleShape)
                         .clickable { onClickFullScreen() },
                     contentScale = ContentScale.Crop
                 )
