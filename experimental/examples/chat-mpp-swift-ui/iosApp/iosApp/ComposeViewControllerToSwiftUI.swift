1fc6bf38883c741443562ecbcd9303da22917e43
dima.avdeev <dima.avdeev@jetbrains.com>
2023-02-14 13:43:26 +0400

    ComposeViewControllerToSwiftUI.swift

---
+++ experimental/examples/chat-mpp-swift-ui/iosApp/iosApp/ComposeViewControllerToSwiftUI.swift
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import UIKit
+import SwiftUI
+import shared
+
+struct ComposeViewControllerToSwiftUI: UIViewControllerRepresentable {
+    private let onInteract: (InteractResult) -> ()
+    init(onInteract: @escaping (InteractResult) -> ()) {
+        self.onInteract = onInteract
+    }
+
+    func makeUIViewController(context: Context) -> UIViewController {
+        return Main_iosKt.MainViewController(/*onInteract: { interactData in
+            onInteract(InteractResult.init(Color(getCGColor(Int(interactData.swiftUIColor))), interactData.darkTheme))
+        }*/)
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+
+    }
+
+    func sizeThatFits(_ proposal: ProposedViewSize, uiViewController: UIViewControllerType, context: Context) -> CGSize? {
+        return uiViewController.view.frame.size
+    }
+}
+
+public struct InteractResult {
+    public let swiftUIColor:Color
+    public let darkTheme:Bool
+
+    public init(_ swiftUIColor: Color, _ darkTheme: Bool) {
+        self.swiftUIColor = swiftUIColor
+        self.darkTheme = darkTheme
+    }
+}
+
+func getCGColor(_ argb:Int) -> CGColor {
+    func clr(_ component: Int) -> CGFloat {
+        CGFloat(component & 0xff) / 255.0
+    }
+    return CGColor(red: clr(argb >> 16), green: clr(argb >> 8), blue: clr(argb), alpha: clr(argb >> 24))
+}
--- experimental/examples/chat-mpp-swift-ui/iosApp/iosApp/iosApp.swift
+++ experimental/examples/chat-mpp-swift-ui/iosApp/iosApp/iosApp.swift
@@ -2,9 +2,87 @@ import SwiftUI
 
 @main
 struct iOSApp: App {
+
+	@State private var textState: String = "text state"
+	@State private var interactResult: InteractResult = InteractResult.init(Color.teal, false)
+
+	let gradient = LinearGradient(colors: [.blue.opacity(1.0), .green.opacity(1.0)],
+			startPoint: .topLeading,
+			endPoint: .bottomTrailing)
+
+	init() {
+
+	}
+
 	var body: some Scene {
 		WindowGroup {
-			Text("Hello SwiftUI")
+			TabView {
+
+				ZStack {
+					NavigationView {
+						VStack {
+							ComposeViewControllerToSwiftUI { result in
+								interactResult = result
+							}
+									.position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2)
+
+							HStack {
+								TextField("Type message...", text: $textState, axis: .vertical)
+										.lineLimit(3)
+										.background(Color.pink.opacity(0.0), in: RoundedRectangle(cornerRadius: 10))
+//                                    .textFieldStyle(.roundedBorder)
+								Button(action: {}) {
+									HStack {
+										Image(systemName: "play.fill")
+										Text("Send")
+									}
+								}
+							}.padding(10)
+									.background(
+											RoundedRectangle(cornerRadius: 10)
+													.fill(interactResult.swiftUIColor.opacity(0.7))
+									)
+									.padding(6)
+
+							Rectangle()
+									.fill(Color.clear)
+									.frame(height: 10)
+									.background(interactResult.swiftUIColor)
+						}
+								.navigationBarTitleDisplayMode(.inline)
+								.navigationTitle("Compose inside SwiftUI")
+								.toolbarBackground(interactResult.swiftUIColor, for: .navigationBar)
+								.toolbarBackground(.visible, for: .navigationBar)
+								.statusBar(hidden: false)
+					}.frame(maxHeight: .infinity)
+
+				}
+						.tabItem {
+							Label("Compose", systemImage: "square.and.pencil")
+						}
+						.toolbar(.visible, for: .tabBar)
+						//                        .toolbarBackground(interactResult.swiftUIColor, for: .tabBar)
+						//                        .toolbarBackground(.visible, for: .tabBar)
+						.preferredColorScheme(interactResult.darkTheme ? .dark : .light)
+
+
+				NavigationView {
+					VStack {
+						Text("Page 2")
+					}
+							.navigationBarTitleDisplayMode(.inline)
+							.navigationTitle("SwiftUI")
+				}
+						.tabItem {
+							Label("SwiftUI", systemImage: "list.dash")
+						}
+						.toolbar(.visible, for: .tabBar)
+						//                        .toolbarBackground(interactResult.swiftUIColor, for: .tabBar)
+						//                        .toolbarBackground(.visible, for: .tabBar)
+						.preferredColorScheme(interactResult.darkTheme ? .dark : .light)
+
+
+			}
 		}
 	}
 }
