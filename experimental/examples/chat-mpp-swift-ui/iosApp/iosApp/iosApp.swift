a3a9779078132d6c383a01106d2b08c44938ee84
dima.avdeev <dima.avdeev@jetbrains.com>
2023-02-15 13:53:30 +0400

    GradientTemplate

--- experimental/examples/chat-mpp-swift-ui/iosApp/iosApp/iosApp.swift
+++ experimental/examples/chat-mpp-swift-ui/iosApp/iosApp/iosApp.swift
@@ -17,61 +17,65 @@ struct iOSApp: App {
 	var body: some Scene {
 		WindowGroup {
 			TabView {
-                NavigationView {
-                    ZStack {
-                        VStack {
-                            gradient.ignoresSafeArea(edges: .top).frame(height: 0)
-                            Spacer()
-                        }
-                        VStack {
-                            ComposeViewControllerToSwiftUI { result in
-                                interactResult = result
-                            }.position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2)
-                            HStack {
-                                TextField("Type message...", text: $textState, axis: .vertical)
-                                        .focused($textFieldFocused)
-                                        .lineLimit(3)
-                                if (textState.count > 0) {
-                                    Button(action: {
-                                        sendMessage(textState)
-                                        textFieldFocused = false
-                                        textState = ""
-                                    }) {
-                                        HStack {
-                                            Image(systemName: "play.fill")
-                                            Text("Send")
-                                        }.tint(.white)
-                                    }
+                GradientTemplate(gradient: gradient, title: "Compose inside SwiftUI") {
+                    VStack {
+                        ComposeViewControllerToSwiftUI { result in
+                            interactResult = result
+                        }.position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2)
+                        HStack {
+                            TextField("Type message...", text: $textState, axis: .vertical)
+                                    .focused($textFieldFocused)
+                                    .lineLimit(3)
+                            if (textState.count > 0) {
+                                Button(action: {
+                                    sendMessage(textState)
+                                    textFieldFocused = false
+                                    textState = ""
+                                }) {
+                                    HStack {
+                                        Image(systemName: "play.fill")
+                                        Text("Send")
+                                    }.tint(.white)
                                 }
                             }
+                        }
                                 .padding(10)
                                 .background(RoundedRectangle(cornerRadius: 10).fill(gradient).opacity(0.8))
                                 .padding(6)
-                            Rectangle().fill(Color.clear).frame(height: 0).background(gradient)
-                        }
+                        Rectangle().fill(Color.clear).frame(height: 0).background(gradient)
                     }
-                        .navigationBarTitleDisplayMode(.inline)
-                        .navigationTitle("Compose inside SwiftUI")
-                        .statusBar(hidden: false)
-                }
-                    .tabItem { Label("Compose", systemImage: "square.and.pencil") }
-                    .toolbar(.visible, for: .tabBar)
-                    .preferredColorScheme(interactResult.darkTheme ? .dark : .light)
+                }.tabItem { Label("Compose", systemImage: "square.and.pencil") }
 
-				NavigationView {
-					VStack {
-						Text("SwiftUI screen")
-					}
-                        .navigationBarTitleDisplayMode(.inline)
-                        .navigationTitle("SwiftUI")
-				}
-                    .tabItem { Label("SwiftUI", systemImage: "list.dash") }
-                    .toolbar(.visible, for: .tabBar)
-                    .toolbarBackground(Color.yellow, for: .tabBar)
-                    .toolbarBackground(.visible, for: .tabBar)
-                    .preferredColorScheme(interactResult.darkTheme ? .dark : .light)
-
-			}
+                GradientTemplate(gradient: gradient, title: "SwiftUI") {
+                    Text("SwiftUI screen")
+                }.tabItem { Label("SwiftUI", systemImage: "list.dash") }
+			}.preferredColorScheme(interactResult.darkTheme ? .dark : .light)
 		}
 	}
+}
+
+struct GradientTemplate<Content: View>: View {
+    var gradient: LinearGradient
+    var title: String
+    var content: () -> Content
+
+    var body: some View {
+        NavigationView {
+            ZStack {
+                VStack {
+                    gradient.ignoresSafeArea(edges: .top).frame(height: 0)
+                    Spacer()
+                }
+                VStack {
+                    content().frame(maxHeight: .infinity)
+                    Rectangle().fill(Color.clear).frame(height: 0).background(gradient)
+                }
+            }
+                .navigationTitle(title)
+                .navigationBarTitleDisplayMode(.inline)
+                .statusBar(hidden: false)
+        }
+            .toolbar(.visible, for: .tabBar)
+
+    }
 }
