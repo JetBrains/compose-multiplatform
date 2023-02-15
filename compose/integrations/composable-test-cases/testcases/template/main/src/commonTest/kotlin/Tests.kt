d3e50f3690a14346defe243aa3d73cb5c462cdd2
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2023-02-15 15:33:13 +0100

    Update README.MD

--- compose/integrations/composable-test-cases/testcases/template/main/src/commonTest/kotlin/Tests.kt
+++ compose/integrations/composable-test-cases/testcases/template/main/src/commonTest/kotlin/Tests.kt
@@ -1,3 +1,5 @@
+import com.example.common.TextContainerNode
+import com.example.common.TextLeafNode
 import com.example.common.composeText
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
@@ -14,5 +16,19 @@ class Tests {
         }
 
         assertEquals("root:{SimpleComposable}", root.dump())
+    }
+
+    @Test
+    fun testExample2() = runTest {
+        val root = composeText {
+            TextLeafNode("Leaf")
+            TextContainerNode("node") {
+                TextLeafNode("child1")
+                TextLeafNode("child2")
+                TextLeafNode("child3")
+            }
+        }
+
+        assertEquals("root:{Leaf, node:{child1, child2, child3}}", root.dump())
     }
 }
