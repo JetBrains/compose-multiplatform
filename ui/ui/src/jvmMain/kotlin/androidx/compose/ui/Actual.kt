477f5b85a3dfac9833388e6f078c79537a05a497
Andrew Bailey <anbailey@google.com>
2023-02-16 11:14:21 -0500

    Use JVM reflection for modifier inspector defaults
    This reverts a change from 5e133af1a058178895ce0b60207788536680d48e
    where we used kotlin-reflect to provide an automatic population of
    Modifier.Node default inspectable properties. This now uses an
    expect/actual declaration, which should make the default implementation
    more available in general, and indirectly fixes an issue in Play on G3
    where the dependency on kotlin-reflect was falsely detected as a
    transitive dependency and prevented shipping.
    
    Fixes: b/269496185
    Test: ./gradlew :compose:ui:ui:tDUT
    Change-Id: I4cbd7d08f1ccabe5e50917a7280d3450ed6c89df

--- ui/ui/src/jvmMain/kotlin/androidx/compose/ui/Actual.kt
+++ ui/ui/src/jvmMain/kotlin/androidx/compose/ui/Actual.kt
@@ -16,6 +16,35 @@
 
 package androidx.compose.ui
 
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.platform.InspectorInfo
+import androidx.compose.ui.util.fastForEach
+
 internal actual fun areObjectsOfSameType(a: Any, b: Any): Boolean {
     return a::class.java === b::class.java
+}
+
+// TODO: For non-JVM platforms, you can revive the kotlin-reflect implementation from
+//  https://android-review.googlesource.com/c/platform/frameworks/support/+/2441379
+@OptIn(ExperimentalComposeUiApi::class)
+internal actual fun InspectorInfo.tryPopulateReflectively(
+    element: ModifierNodeElement<*>
+) {
+    element.javaClass.declaredFields
+        // Sort by the field name to make the result more well-defined
+        .sortedBy { it.name }
+        .fastForEach { field ->
+            if (!field.declaringClass.isAssignableFrom(ModifierNodeElement::class.java)) {
+                try {
+                    field.isAccessible = true
+                    properties[field.name] = field.get(element)
+                } catch (_: SecurityException) {
+                    // Do nothing. Just ignore the field and prevent the error from crashing
+                    // the application and ending the debugging session.
+                } catch (_: IllegalAccessException) {
+                    // Do nothing. Just ignore the field and prevent the error from crashing
+                    // the application and ending the debugging session.
+                }
+            }
+        }
 }
