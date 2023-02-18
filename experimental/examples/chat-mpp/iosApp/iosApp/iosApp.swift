96ac2d809d54f6a597be849c165aaf5a27d79b7c
dima.avdeev <dima.avdeev@jetbrains.com>
2023-02-18 21:20:50 +0400

    correct keyboard appearance

--- experimental/examples/chat-mpp/iosApp/iosApp/iosApp.swift
+++ experimental/examples/chat-mpp/iosApp/iosApp/iosApp.swift
@@ -7,12 +7,40 @@ let gradient = LinearGradient(
 
 @main
 struct iOSApp: App {
+    @State private var textState: String = "text message"
+    @FocusState private var textFieldFocused: Bool
+
 	var body: some Scene {
 		WindowGroup {
 			TabView {
-                GradientTemplate(title: "Compose inside SwiftUI") {
-                    ComposeScreen()
+                ZStack {
+                    GradientTemplate(title: "Compose inside SwiftUI") {
+                        ComposeScreen()
+                    }
+
+                    VStack {
+                        Spacer()
+                        HStack {
+                            TextField("Type message...", text: $textState, axis: .vertical)
+                                    .focused($textFieldFocused)
+                                    .lineLimit(3)
+                            if (!textState.isEmpty) {
+                                Button(action: {
+                                    sendMessage(textState)
+                                    textFieldFocused = false
+                                    textState = ""
+                                }) {
+                                    HStack {
+                                        Image(systemName: "play.fill")
+                                        Text("Send")
+                                    }.tint(.white)
+                                }
+                            }
+                        }.padding(10).background(RoundedRectangle(cornerRadius: 10).fill(gradient).opacity(0.8)).padding(6)
+                    }
+                    Rectangle().fill(Color.clear).frame(height: 0).background(gradient)
                 }.tabItem { Label("Compose", systemImage: "square.and.pencil") }
+
                 GradientTemplate(title: "SwiftUI") {
                     SwiftUIScreen()
                 }.tabItem { Label("SwiftUI", systemImage: "list.dash") }
@@ -22,33 +50,9 @@ struct iOSApp: App {
 }
 
 struct ComposeScreen: View {
-    @State private var textState: String = "text message"
-    @FocusState private var textFieldFocused: Bool
     var body: some View {
-        ZStack {
-            ComposeViewControllerToSwiftUI().ignoresSafeArea(.keyboard)
-            VStack {
-                Spacer()
-                HStack {
-                    TextField("Type message...", text: $textState, axis: .vertical)
-                            .focused($textFieldFocused)
-                            .lineLimit(3)
-                    if (!textState.isEmpty) {
-                        Button(action: {
-                            sendMessage(textState)
-                            textFieldFocused = false
-                            textState = ""
-                        }) {
-                            HStack {
-                                Image(systemName: "play.fill")
-                                Text("Send")
-                            }.tint(.white)
-                        }
-                    }
-                }.padding(10).background(RoundedRectangle(cornerRadius: 10).fill(gradient).opacity(0.8)).padding(6)
-            }
-            Rectangle().fill(Color.clear).frame(height: 0).background(gradient)
-        }.onTapGesture {
+        ComposeViewControllerToSwiftUI().ignoresSafeArea(.keyboard).onTapGesture {
+            // When tap on Compose - hide keyboard
             UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
         }
     }
@@ -77,7 +81,7 @@ struct GradientTemplate<Content: View>: View {
                 VStack {
                     content().frame(maxHeight: .infinity)
                     Rectangle().fill(Color.clear).frame(height: 0).background(gradient)
-                }
+                }.ignoresSafeArea(.keyboard, edges: .bottom)
             }
                 .navigationTitle(title)
                 .navigationBarTitleDisplayMode(.inline)
