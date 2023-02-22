7936736b7a11ded55bd63b863c17f5927b1946f8
dima.avdeev <dima.avdeev@jetbrains.com>
2023-02-22 13:05:59 +0400

    open separate camere screen

--- experimental/examples/imageviewer/iosApp/iosApp/ContentView.swift
+++ experimental/examples/imageviewer/iosApp/iosApp/ContentView.swift
@@ -16,11 +16,20 @@ struct ComposeView: UIViewControllerRepresentable {
 }
 
 struct ContentView: View {
+    @State var cameraScreen:Bool = false
     var body: some View {
-        ComposeView(openCamera: { print("open camera in SwiftUI") })
-                .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
+        if(cameraScreen) {
+            CameraScreen()
+        } else {
+            ComposeView(openCamera: { cameraScreen = true })
+                    .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
+        }
     }
 }
 
-
+struct CameraScreen: View {
+    var body: some View {
+        Text("Camera screen")//todo
+    }
+}
 
