ed45596292c557203d84de66cfe9ebdddccc2eeb
Nikita Lipsky <nikita.lipsky@jetbrains.com>
2023-02-22 13:03:05 +0200

    Copy templates to experimental as is ...
    ... to add iOS target later

---
+++ experimental/templates/multiplatform-template/desktop/build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("multiplatform") 
+    id("org.jetbrains.compose")
+}
+
+kotlin {
+    jvm {
+        withJava()
+    }
+    sourceSets {
+        named("jvmMain") {
+            dependencies {
+                implementation(compose.desktop.currentOs)
+                implementation(project(":common"))
+            }
+        }
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "KotlinMultiplatformComposeDesktopApplication"
+            packageVersion = "1.0.0"
+        }
+    }
+}
