4dc050046ec4fb6c0d1b98005e881e9f23fda106
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2023-02-22 19:33:56 +0100

    remove Dispatchers.Default usage for recomposer

--- compose/integrations/composable-test-cases/common/src/commonMain/kotlin/com/example/common/Applier.kt
+++ compose/integrations/composable-test-cases/common/src/commonMain/kotlin/com/example/common/Applier.kt
@@ -114,7 +114,8 @@ fun composeText(content: @Composable () -> Unit): StringsNodeWrapper {
     GlobalSnapshotManager.ensureStarted()
     RecompositionObserver.reset()
 
-    val context = Dispatchers.Default + MonotonicClockImpl()
+//    val context = Dispatchers.Default + MonotonicClockImpl()
+    val context = MonotonicClockImpl()
     val recomposer = Recomposer(context)
 
     CoroutineScope(context).launch(start = CoroutineStart.UNDISPATCHED) {
