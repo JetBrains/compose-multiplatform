2304d8fc58b4fc72fac14822afe79a738a693e81
dima.avdeev <dima.avdeev@jetbrains.com>
2023-03-13 18:10:08 +0400

    SimlulatorStub on simulator

--- experimental/examples/imageviewer/shared/src/commonMain/kotlin/example/imageviewer/view/CameraScreen.kt
+++ experimental/examples/imageviewer/shared/src/commonMain/kotlin/example/imageviewer/view/CameraScreen.kt
@@ -1,27 +1,20 @@
 package example.imageviewer.view
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
+import example.imageviewer.Localization
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
-internal fun CameraScreen(onBack: () -> Unit) {
+internal fun CameraScreen(localization: Localization, onBack: () -> Unit) {
     Box(Modifier.fillMaxSize()) {
         CameraView(Modifier.fillMaxSize())
         TopLayout(
             alignLeftContent = {
-                Tooltip("TODO localization.back") {//todo
+                Tooltip(localization.back) {
                     CircularButton(
                         painterResource("arrowleft.png"),
                         onClick = { onBack() }
@@ -32,6 +25,3 @@ internal fun CameraScreen(onBack: () -> Unit) {
         )
     }
 }
-
-@Composable
-internal expect fun CameraView(modifier: Modifier)
---
+++ experimental/examples/imageviewer/shared/src/commonMain/kotlin/example/imageviewer/view/CameraView.common.kt
@@ -0,0 +1,7 @@
+package example.imageviewer.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+internal expect fun CameraView(modifier: Modifier)
