35f0f72253cef860b85cfd5349d9406133ddf266
dima.avdeev <99798741+dima-avdeev-jb@users.noreply.github.com>
2023-03-23 14:52:21 +0300

    resources library minSdk 21 (#2911)

--- components/resources/demo/shared/build.gradle.kts
+++ components/resources/demo/shared/build.gradle.kts
@@ -82,7 +82,7 @@ android {
     compileSdk = 33
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     defaultConfig {
-        minSdk = 24
+        minSdk = 21
         targetSdk = 33
     }
     compileOptions {
