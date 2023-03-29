5439a65694ef30b615dbc045463ac7487d5f1552
dima.avdeev <dima.avdeev@jetbrains.com>
2023-03-29 14:00:10 +0400

    apply suggestion with choosing language

--- experimental/examples/imageviewer/shared/src/desktopMain/kotlin/example/imageviewer/Localization.desktop.kt
+++ experimental/examples/imageviewer/shared/src/desktopMain/kotlin/example/imageviewer/Localization.desktop.kt
@@ -1,8 +1,7 @@
 package example.imageviewer
 
 actual fun getCurrentLanguage(): AvailableLanguages =
-    if (System.getProperty("user.language").equals("de")) {
-        AvailableLanguages.DE
-    } else {
-        AvailableLanguages.EN
+    when (System.getProperties()["user.language"]) {
+        "de" -> AvailableLanguages.DE
+        else -> AvailableLanguages.EN
     }
