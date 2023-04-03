480dcdf48a9246669e692f5a82c0f36765a3505a
Ivan Matkov <ivan.matkov@jetbrains.com>
2023-04-03 15:22:08 +0200

    Fix regex for number values

--- examples/codeviewer/common/src/commonMain/kotlin/org/jetbrains/codeviewer/ui/editor/EditorView.kt
+++ examples/codeviewer/common/src/commonMain/kotlin/org/jetbrains/codeviewer/ui/editor/EditorView.kt
@@ -169,7 +169,7 @@ private fun codeString(str: String) = buildAnnotatedString {
         addStyle(AppTheme.code.keyword, strFormatted, "package ")
         addStyle(AppTheme.code.value, strFormatted, "true")
         addStyle(AppTheme.code.value, strFormatted, "false")
-        addStyle(AppTheme.code.value, strFormatted, Regex("[0-9]*"))
+        addStyle(AppTheme.code.value, strFormatted, Regex("[0-9]+"))
         addStyle(AppTheme.code.annotation, strFormatted, Regex("^@[a-zA-Z_]*"))
         addStyle(AppTheme.code.comment, strFormatted, Regex("^\\s*//.*"))
     }
