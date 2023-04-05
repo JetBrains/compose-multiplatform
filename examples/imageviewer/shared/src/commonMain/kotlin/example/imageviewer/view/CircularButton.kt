857a93cfb68ba98e663be753e9559d942f8a9f03
dima.avdeev <dima.avdeev@jetbrains.com>
2023-04-05 20:13:01 +0400

    downscale icons

--- examples/imageviewer/shared/src/commonMain/kotlin/example/imageviewer/view/CircularButton.kt
+++ examples/imageviewer/shared/src/commonMain/kotlin/example/imageviewer/view/CircularButton.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
