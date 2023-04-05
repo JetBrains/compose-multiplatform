4ec9b7ba384b5ade21bbe734a0e33ebc378134cc
dima.avdeev <dima.avdeev@jetbrains.com>
2023-04-05 20:14:07 +0400

    comment

--- examples/imageviewer/shared/build.gradle.kts
+++ examples/imageviewer/shared/build.gradle.kts
@@ -35,7 +35,7 @@ kotlin {
                 implementation(compose.runtime)
                 implementation(compose.foundation)
                 implementation(compose.material)
-//                implementation(compose.materialIconsExtended) // TODO not working on iOS for now
+                //implementation(compose.materialIconsExtended) // TODO not working on iOS for now
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
