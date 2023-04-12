9938601496fa9592057ac7cc7a5b3856cb51d6ce
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2023-04-12 20:56:55 +0200

    Update resource lib version

--- components/build.gradle.kts
+++ components/build.gradle.kts
@@ -11,7 +11,6 @@ allprojects {
         mavenLocal()
 
         maven("https://maven.pkg.jetbrains.space/kotlin/p/wasm/experimental")
-        maven("https://packages.jetbrains.team/maven/p/karpovich-sandbox/ksandbox")
     }
 }
 
