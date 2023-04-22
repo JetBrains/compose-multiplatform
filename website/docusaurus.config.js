772249a298bb90ff6d910006b5fc5e31a5344658
whitescent <Nthily@outlook.com>
2023-04-23 00:29:48 +0800

    update footer, change website logo

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/',
               },
             ],
           },
