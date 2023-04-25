93323538b90439be76d3c15980f6b0b323af702b
mdigman <mdigman@users.noreply.github.com>
2023-04-25 14:10:14 -0700

    Remove unneeded null checks in Parser
    Last commit handled null management

--- components/resources/library/src/commonMain/kotlin/org/jetbrains/compose/resources/vector/XmlVectorParser.kt
+++ components/resources/library/src/commonMain/kotlin/org/jetbrains/compose/resources/vector/XmlVectorParser.kt
@@ -237,7 +237,7 @@ private fun Element.parseColorStop(defaultOffset: Float): Pair<Float, Color>? {
 }
 
 private fun Element.attributeOrNull(namespace: String, name: String): String? {
-    val value = getAttributeNS(namespace, name) ?: return null
+    val value = getAttributeNS(namespace, name)
     return if (value.isNotBlank()) value else null
 }
 
@@ -260,7 +260,7 @@ private fun Element.apptAttr(
     namespace: String,
     name: String
 ): Element? {
-    val prefix = lookupPrefix(namespace) ?: return null
+    val prefix = lookupPrefix(namespace)
     return childrenSequence
         .filterIsInstance<Element>()
         .find {
@@ -271,6 +271,6 @@ private fun Element.apptAttr(
 
 private val Element.childrenSequence get() = sequence<Node> {
     for (i in 0 until childNodes.length) {
-        yield(childNodes.item(i)!!)
+        yield(childNodes.item(i))
     }
 }
