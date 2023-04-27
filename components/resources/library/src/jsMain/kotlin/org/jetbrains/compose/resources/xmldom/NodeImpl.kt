461152cf8dc2318f22ed769b0dcae8d6ddac21cf
mdigman <mdigman@users.noreply.github.com>
2023-04-27 08:54:10 -0700

    Make NodeList impl lazy
    Generates only when needed, serves cache after

--- components/resources/library/src/jsMain/kotlin/org/jetbrains/compose/resources/xmldom/NodeImpl.kt
+++ components/resources/library/src/jsMain/kotlin/org/jetbrains/compose/resources/xmldom/NodeImpl.kt
@@ -11,18 +11,17 @@ internal open class NodeImpl(val n: DomNode): Node {
 
     override val namespaceURI = "" /* namespaceURI is not a Node property, only applies to Elements and Attrs */
 
-    override val childNodes: NodeList
-        get() =
-            object: NodeList {
-                override fun item(i: Int): Node {
-                    val child = n.childNodes.item(i)
-                        ?: throw IndexOutOfBoundsException("no child node accessible at index=$i")
-                    return if (child is DomElement) ElementImpl(child) else NodeImpl(child)
-                }
-
-                override val length: Int = n.childNodes.length
-
+    override val childNodes: NodeList by lazy {
+        object: NodeList {
+            override fun item(i: Int): Node {
+                val child = n.childNodes.item(i)
+                    ?: throw IndexOutOfBoundsException("no child node accessible at index=$i")
+                return if (child is DomElement) ElementImpl(child) else NodeImpl(child)
             }
+
+            override val length: Int = n.childNodes.length
+        }
+    }
 
     override fun lookupPrefix(namespaceURI: String): String = n.lookupPrefix(namespaceURI) ?: ""
 
