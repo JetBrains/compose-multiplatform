ba55b5a47c7e3cfd04b29d98177186a7212d31a2
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2023-05-15 20:40:25 +0200

    Remove variant redirection from Gradle plugin
    It was experimental and it is not needed
    since our published Gradle Metadata
    already has references for Android artifacts
    for Android variants

--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/android/AndroidExtension.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/android/AndroidExtension.kt
@@ -7,10 +7,4 @@ import org.gradle.api.tasks.Input
 import javax.inject.Inject
 
 abstract class AndroidExtension @Inject constructor(private val objectFactory: ObjectFactory) : ExtensionAware {
-    @Input
-    var useAndroidX : Boolean = false
-    @Input
-    var androidxVersion: String = "1.0.1" //should be set by CI, but could be overridden in build script
-    @Input
-    var mppSolutionGroup: String = "org.jetbrains.compose" //it is added for future use by Google
 }
