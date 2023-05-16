7c080f1651ec614f0503b7f716963068804969bf
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2023-05-16 16:54:35 +0200

    Make codeviewer example more photogenic (top bar color and bottom margins)

--- examples/codeviewer/iosApp/iosApp/ContentView.swift
+++ examples/codeviewer/iosApp/iosApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ComposeView: UIViewControllerRepresentable {
 struct ContentView: View {
     var body: some View {
         ComposeView()
-                .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
+                .ignoresSafeArea(.all, edges: .bottom) // Compose has own keyboard handler
     }
 }
 
