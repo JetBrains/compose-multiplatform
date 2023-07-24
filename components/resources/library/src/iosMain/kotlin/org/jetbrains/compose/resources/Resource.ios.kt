99cec1086ae72afdaec95a5a72090bca259d0428
Igor Demin <igordmn@users.noreply.github.com>
2023-07-24 10:52:15 +0200

    Update Resource.ios.kt

--- components/resources/library/src/iosMain/kotlin/org/jetbrains/compose/resources/Resource.ios.kt
+++ components/resources/library/src/iosMain/kotlin/org/jetbrains/compose/resources/Resource.ios.kt
@@ -5,7 +5,6 @@
 
 package org.jetbrains.compose.resources
 
-import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.usePinned
 import platform.Foundation.NSBundle
@@ -18,7 +17,6 @@ actual fun resource(path: String): Resource = UIKitResourceImpl(path)
 
 @ExperimentalResourceApi
 private class UIKitResourceImpl(path: String) : AbstractResourceImpl(path) {
-    @OptIn(ExperimentalForeignApi::class)
     override suspend fun readBytes(): ByteArray {
         val fileManager = NSFileManager.defaultManager()
         // todo: support fallback path at bundle root?
