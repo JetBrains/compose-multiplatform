61b06fcaee2db2d8fab5700995adf3718223ff55
Igor Demin <igordmn@users.noreply.github.com>
2023-07-25 21:39:53 +0200

    Widgets gallery: fix for mismatched @Composable annotation (desktop) (#3409)
    Folloup after https://github.com/JetBrains/compose-multiplatform/pull/3402

--- examples/widgets-gallery/shared/src/desktopMain/kotlin/org/jetbrains/compose/demo/widgets/platform/System.kt
+++
@@ -1,5 +0,0 @@
-package org.jetbrains.compose.demo.widgets.platform
-
-import org.jetbrains.skiko.SystemTheme
-
-actual fun isSystemInDarkTheme(): Boolean = org.jetbrains.skiko.currentSystemTheme == SystemTheme.DARK
