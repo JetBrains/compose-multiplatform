be7e78cf797ae38fe5e2cd061a65672e2f749e45
Nikita Lipsky <nikita.lipsky@jetbrains.com>
2023-07-25 11:48:13 +0300

    Widgets gallery: fix for mismatched @Composable annotation (#3402)
    * Widgets gallery: fix for mismatched @Composable annotation
    
    * Widgets gallery: remove isSystemInDarkTheme expect function

--- examples/widgets-gallery/shared/src/androidMain/kotlin/org/jetbrains/compose/demo/widgets/platform/System.kt
+++
@@ -1,8 +0,0 @@
-package org.jetbrains.compose.demo.widgets.platform
-
-import androidx.compose.runtime.Composable
-import androidx.compose.foundation.isSystemInDarkTheme as androidSystemIsInDarkTheme
-
-@Composable
-actual fun isSystemInDarkTheme(): Boolean =
-    androidSystemIsInDarkTheme()
