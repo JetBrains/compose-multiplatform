563575a866c09c283ba9998e3ab44888e8a00779
Sebastian Aigner <SebastianAigner@users.noreply.github.com>
2023-08-02 19:46:25 +0200

    Update Navigation tutorial to refer to Awesome List (#3441)
    Remove third-party specific samples

--- examples/todoapp/common/database/src/iosMain/kotlin/example/todo/common/database/TodoDatabaseDriverFactory.kt
+++
@@ -1,9 +0,0 @@
-package example.todo.common.database
-
-import com.squareup.sqldelight.db.SqlDriver
-import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
-import example.todo.database.TodoDatabase
-
-@Suppress("FunctionName") // Factory function
-fun TodoDatabaseDriver(): SqlDriver =
-    NativeSqliteDriver(TodoDatabase.Schema, "TodoDatabase.db")
