87d1972a39d433edd4912275fec5f9c375c48e62
Alexey Tsvetkov <654232+AlexeyTsvetkov@users.noreply.github.com>
2023-08-17 18:45:01 +0200

    Fix cache kind management with nested subprojects (#3519)
    * Fix cache kind management with nested subprojects
    
    Previously, cache kind property management
    worked incorrectly when Compose Gradle plugin
    was applied to both parent and child subprojects,
    e.g. :compose-subproject-1:compose-subproject-2.
    With this example the plugin would successfully
    set the property for compose-subproject-1,
    but then for compose-subproject-2 the following snippet
    would fail:
    ```
    if (project.hasProperty(targetCacheKindPropertyName)) {
      project.setProperty(targetCacheKindPropertyName, NONE_VALUE)
    }
    ```
    because project.hasProperty would have return true
    (because it checks parent subproject properties too),
    but project.setProperty would fail, because
    parent project's properties are read only.
    
    Warnings were also handled incorrectly in this case,
    because during the configuration of compose-subproject-1 we might set
    `kotlin.native.cacheKind.ios*=none`,
    which would then cause a warning during the configuration of compose-subproject-2.
    To avoid incorrect warnings, we now
    record the snapshot of relevant properties
    during Compose Multiplatform build service initialization
    
    Resolves #3515
    
    * Fix issues from code review

--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeMultiplatformBuildService.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeMultiplatformBuildService.kt
@@ -2,24 +2,35 @@ package org.jetbrains.compose
 
 import org.gradle.api.Project
 import org.gradle.api.logging.Logging
+import org.gradle.api.provider.MapProperty
 import org.gradle.api.provider.Provider
 import org.gradle.api.provider.SetProperty
 import org.gradle.api.services.BuildService
 import org.gradle.api.services.BuildServiceParameters
 import org.gradle.tooling.events.FinishEvent
 import org.gradle.tooling.events.OperationCompletionListener
+import org.jetbrains.compose.experimental.internal.SUPPORTED_NATIVE_CACHE_KIND_PROPERTIES
 import org.jetbrains.compose.internal.utils.BuildEventsListenerRegistryProvider
+import org.jetbrains.compose.internal.utils.loadProperties
+import org.jetbrains.compose.internal.utils.localPropertiesFile
 import org.jetbrains.kotlin.gradle.plugin.SubpluginArtifact
 
 // The service implements OperationCompletionListener just so Gradle would use the service
 // even if the service is not used by any task or transformation
-abstract class ComposeMultiplatformBuildService : BuildService<BuildServiceParameters.None>,
+abstract class ComposeMultiplatformBuildService : BuildService<ComposeMultiplatformBuildService.Parameters>,
     OperationCompletionListener, AutoCloseable {
+
+    abstract class Parameters : BuildServiceParameters {
+        abstract val gradlePropertiesCacheKindSnapshot: MapProperty<String, String>
+        abstract val localPropertiesCacheKindSnapshot: MapProperty<String, String>
+    }
 
     private val log = Logging.getLogger(this.javaClass)
 
     internal abstract val unsupportedCompilerPlugins: SetProperty<Provider<SubpluginArtifact?>>
     internal abstract val delayedWarnings: SetProperty<String>
+    internal val gradlePropertiesSnapshot: Map<String, String> = parameters.gradlePropertiesCacheKindSnapshot.get()
+    internal val localPropertiesSnapshot: Map<String, String> = parameters.localPropertiesCacheKindSnapshot.get()
 
     fun warnOnceAfterBuild(message: String) {
         delayedWarnings.add(message)
@@ -74,6 +85,9 @@ abstract class ComposeMultiplatformBuildService : BuildService<BuildServiceParam
             return null
         }
 
+        fun getInstance(project: Project): ComposeMultiplatformBuildService =
+            findExistingComposeService(project) ?: error("ComposeMultiplatformBuildService was not initialized!")
+
         @Suppress("UnstableApiUsage")
         fun init(project: Project) {
             val existingService = findExistingComposeService(project)
@@ -82,13 +96,24 @@ abstract class ComposeMultiplatformBuildService : BuildService<BuildServiceParam
             }
 
             val newService = project.gradle.sharedServices.registerIfAbsent(COMPOSE_SERVICE_FQ_NAME, ComposeMultiplatformBuildService::class.java) {
+                it.parameters.initPropertiesSnapshots(project.rootProject)
             }
             // workaround to instanciate a  service even if it not binded to a task
             BuildEventsListenerRegistryProvider.getInstance(project).onTaskCompletion(newService)
         }
 
-        fun getInstance(project: Project): ComposeMultiplatformBuildService =
-            findExistingComposeService(project) ?: error("ComposeMultiplatformBuildService was not initialized!")
+        private fun Parameters.initPropertiesSnapshots(rootProject: Project) {
+            // we want to record original properties (explicitly set by a user)
+            // before we possibly change them in configureNativeCompilerCaching.kt
+            val localProperties = loadProperties(rootProject.localPropertiesFile)
+            for (cacheKindProperty in SUPPORTED_NATIVE_CACHE_KIND_PROPERTIES) {
+                rootProject.findProperty(cacheKindProperty)?.toString()?.let { value ->
+                    gradlePropertiesCacheKindSnapshot.put(cacheKindProperty, value)
+                }
+                localProperties[cacheKindProperty]?.toString()?.let { value ->
+                    localPropertiesCacheKindSnapshot.put(cacheKindProperty, value)
+                }
+            }
+        }
     }
 }
-
