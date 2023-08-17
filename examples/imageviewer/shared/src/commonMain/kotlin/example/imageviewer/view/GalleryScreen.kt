e1987d791f1942919e9379f8d35bfdecabe54b0c
Alexander Maryanovsky <msasha@gmail.com>
2023-08-17 18:56:31 +0300

    Add a scrollbar to the SquaresGalleryView on the desktop

--- examples/imageviewer/shared/src/commonMain/kotlin/example/imageviewer/view/GalleryScreen.kt
+++ examples/imageviewer/shared/src/commonMain/kotlin/example/imageviewer/view/GalleryScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
@@ -32,10 +32,13 @@ import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import example.imageviewer.*
+import example.imageviewer.ExternalImageViewerEvent
+import example.imageviewer.LocalImageProvider
+import example.imageviewer.LocalInternalEvents
+import example.imageviewer.LocalNotification
 import example.imageviewer.icon.IconMenu
 import example.imageviewer.icon.IconVisibility
-import example.imageviewer.model.*
+import example.imageviewer.model.PictureData
 import example.imageviewer.style.ImageviewerColors
 import kotlinx.coroutines.launch
 import kotlin.math.absoluteValue
@@ -171,6 +174,15 @@ fun GalleryScreen(
     }
 }
 
+@Composable
+expect fun GalleryLazyVerticalGrid(
+    columns: GridCells,
+    modifier: Modifier,
+    verticalArrangement: Arrangement.Vertical,
+    horizontalArrangement: Arrangement.Horizontal,
+    content: LazyGridScope.() -> Unit
+)
+
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 private fun SquaresGalleryView(
@@ -178,9 +190,9 @@ private fun SquaresGalleryView(
     pagerState: PagerState,
     onSelect: (index: Int) -> Unit,
 ) {
-    LazyVerticalGrid(
-        modifier = Modifier.padding(top = 4.dp),
+    GalleryLazyVerticalGrid(
         columns = GridCells.Adaptive(minSize = 130.dp),
+        modifier = Modifier.padding(top = 4.dp),
         verticalArrangement = Arrangement.spacedBy(1.dp),
         horizontalArrangement = Arrangement.spacedBy(1.dp)
     ) {
