3b01b9480e07cc237361b0d4dd5bd92cde4476b0
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2023-08-21 15:59:20 +0200

    Add example with cocoapods

---
+++ examples/cocoapods-ios-example/README.md
@@ -0,0 +1,8 @@
+# [Compose Multiplatform](https://github.com/JetBrains/compose-multiplatform) iOS CocoaPods example
+
+This example showcases using Kotlin Multiplatform shared module in Swift as a CocoaPods framework.
+
+The official Kotlin documentation provides more information on working with CocoaPods:
+* [CocoaPods overview and setup](https://kotlinlang.org/docs/native-cocoapods.html);
+* [Add dependencies on a Pod library](https://kotlinlang.org/docs/native-cocoapods-libraries.html);
+* [Use a Kotlin Gradle project as a CocoaPods dependency](https://kotlinlang.org/docs/native-cocoapods-xcode.html);
---
+++ examples/cocoapods-ios-example/shared/src/commonMain/kotlin/App.kt
@@ -0,0 +1,41 @@
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+fun App() {
+    MaterialTheme {
+        var greetingText by remember { mutableStateOf("Hello, World!") }
+        var showImage by remember { mutableStateOf(false) }
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = {
+                greetingText = "Hello, ${getPlatformName()}"
+                showImage = !showImage
+            }) {
+                Text(greetingText)
+            }
+            AnimatedVisibility(showImage) {
+                Image(
+                    painterResource("compose-multiplatform.xml"),
+                    null
+                )
+            }
+        }
+    }
+}
+
+expect fun getPlatformName(): String
