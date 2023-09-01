beb93c6d29d8afc2141763555cb50c8531b7a370
Alexey Tsvetkov <alexey.tsvetkov@jetbrains.com>
2023-09-01 16:21:18 +0200

    Try running compiler out of process

--- gradle-plugins/compose/src/test/kotlin/org/jetbrains/compose/test/utils/TestProject.kt
+++ gradle-plugins/compose/src/test/kotlin/org/jetbrains/compose/test/utils/TestProject.kt
@@ -60,11 +60,13 @@ class TestProject(
         } else {
             "-Porg.gradle.java.installations.paths=${testJdks.joinToString(",")}"
         },
-        "-Porg.gradle.jvmargs=-Xmx=1g -XX:+UseSerialGC",
+        "-Porg.gradle.jvmargs=-Xmx=256m -XX:+UseSerialGC",
         "-Pkotlin.daemon.jvmargs=-Xmx=512m -XX:+UseSerialGC",
         // compile in-process, when a non-default Kotlin version is used, to avoid creating lots of Kotlin daemons,
         // which waste limited RAM on CI
-        "-Pkotlin.compiler.execution.strategy=in-process".takeIf { testEnvironment.kotlinVersion != TestKotlinVersions.Default },
+        "-Pkotlin.compiler.execution.strategy=out-of-process".takeIf { testEnvironment.kotlinVersion != TestKotlinVersions.Default },
+        "-Pkotlin.native.disableCompilerDaemon=true",
+        "-Pkotlin.native.jvmArgs=-Xmx1g -XX:+UseSerialGC",
         "-Pcompose.tests.yarn.mutex.file=${System.getProperty("compose.tests.yarn.mutex.file")}",
     )
 
