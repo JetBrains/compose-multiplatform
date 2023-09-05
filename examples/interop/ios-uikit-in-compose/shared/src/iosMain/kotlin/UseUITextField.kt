32a8b2a5951ea9250743e124246104fc0cb2c130
dima.avdeev <dima.avdeev@jetbrains.com>
2023-09-05 12:30:14 +0400

    remove redundant imports

--- examples/interop/ios-uikit-in-compose/shared/src/iosMain/kotlin/UseUITextField.kt
+++ examples/interop/ios-uikit-in-compose/shared/src/iosMain/kotlin/UseUITextField.kt
@@ -1,14 +1,11 @@
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.interop.UIKitView
 import androidx.compose.ui.unit.dp
 import kotlinx.cinterop.ExperimentalForeignApi
