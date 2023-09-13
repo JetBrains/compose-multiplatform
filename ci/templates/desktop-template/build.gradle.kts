JetBrains/compose-multiplatform

commit c44601a1f4ea83f9d644aa3fdc28f20331d50e8d

Fix Error in snippet at /mnt/agent/work/b8625f0804e53334/tutorials/./UI_Testing/README.md:9 (#3628)
Crashes, because tutorial checker uses desktop-template without uiTesting module

See https://teamcity.jetbrains.com/buildConfiguration/JetBrainsPublicProjects_Compose_Publish_2_AllGitHubRelease/4301157?hideProblemsFromDependencies=false&hideTestsFromDependencies=false&expandBuildProblemsSection=true&expandBuildChangesSection=true

Also adding "``` kotlin` hach to avoid compilation of some snippets 9we use it in another places)

ci/templates/desktop-template/build.gradle.kts
@@ -17,6 +17,9 @@ dependencies {
     // (in a separate module for demo project and in testMain).
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
+
+    // Include the Test API
+    implementation(compose.desktop.uiTestJUnit4)
 }
 
 compose.desktop {

ci/templates/desktop-template/gradle/wrapper/gradle-wrapper.properties
@@ -1,5 +1,5 @@
 distributionBase=GRADLE_USER_HOME
 distributionPath=wrapper/dists
-distributionUrl=https\://services.gradle.org/distributions/gradle-7.5.1-bin.zip
+distributionUrl=https\://services.gradle.org/distributions/gradle-8.2.1-bin.zip
 zipStoreBase=GRADLE_USER_HOME
 zipStorePath=wrapper/dists

commit 6b5af2b97bb23810c5caf8ad8205ada44ecda0bf

Fix Proguard error on CI
The error:
```
java.io.IOException: Please correct the above warnings first.
```
https://teamcity.jetbrains.com/buildConfiguration/JetBrainsPublicProjects_Compose_Publish_2_AllGitHubRelease/4312357?expandBuildProblemsSection=true&hideProblemsFromDependencies=false&expandBuildTestsSection=true&hideTestsFromDependencies=false&expandBuildChangesSection=true&showLog=4312349_1096_787&logFilter=debug&logView=flowAware

Regression after https://github.com/JetBrains/compose-multiplatform/pull/3628

We don't need `implementation` here, `compileOnly` works for our case

ci/templates/desktop-template/build.gradle.kts
@@ -19,7 +19,8 @@ dependencies {
     implementation(compose.desktop.currentOs)
 
     // Include the Test API
-    implementation(compose.desktop.uiTestJUnit4)
+    // compileOnly instead of testImplementation for checking compilation of the tutorials
+    compileOnly(compose.desktop.uiTestJUnit4)
 }
 
 compose.desktop {