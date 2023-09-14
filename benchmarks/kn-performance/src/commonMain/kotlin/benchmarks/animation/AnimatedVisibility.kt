d695cf59090808a727a3a3d4861831939cf0a269
Elijah Semyonov <elijah.semenov@gmail.com>
2023-09-14 17:20:50 +0200

    Add benchmark for macOS K/N + rendering (#3555)
    * Copy jvm/native project
    
    * Implement benchmark
    
    * Add minor changes
    
    * Add minor changes
    
    * Add minor changes
    
    * Update
    
    * Change "lowest" to "worse"
    
    * Add p75
    
    * Remove p75
    
    * Fix naming
    
    * Update gradle properties

---
+++ benchmarks/kn-performance/src/commonMain/kotlin/benchmarks/animation/AnimatedVisibility.kt
@@ -0,0 +1,38 @@
+package benchmarks.animation
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.delay
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+
+@OptIn(ExperimentalResourceApi::class)
+@Composable
+fun AnimatedVisibility() {
+    MaterialTheme {
+        var showImage by remember { mutableStateOf(false) }
+        LaunchedEffect(showImage) {
+            delay(200)
+            showImage = !showImage
+        }
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            AnimatedVisibility(showImage) {
+                Image(
+                    painterResource("compose-multiplatform.xml"),
+                    null
+                )
+            }
+        }
+    }
+}
