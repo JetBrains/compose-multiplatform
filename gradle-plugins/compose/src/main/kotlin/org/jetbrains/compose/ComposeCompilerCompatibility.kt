b5355dbf709eaccb4aebd384fc908d76faa408de
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2023-09-22 12:18:03 +0200

    Support kotlin 1.9.20-Beta2

--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerCompatibility.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerCompatibility.kt
@@ -16,7 +16,8 @@ internal object ComposeCompilerCompatibility {
         "1.9.0-RC" to "1.4.8-beta",
         "1.9.0" to "1.5.1",
         "1.9.10" to "1.5.2",
-        "1.9.20-Beta" to "1.5.2.1-Beta2"
+        "1.9.20-Beta" to "1.5.2.1-Beta2",
+        "1.9.20-Beta2" to "1.5.2.1-Beta3"
     )
 
     fun compilerVersionFor(kotlinVersion: String): String {
