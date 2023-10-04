f7b3a09c909f7740a8bd9e0bba78c15678ff2bc3
dima.avdeev <dima.avdeev@jetbrains.com>
2023-10-04 19:20:54 +0400

    Added minesweeper tests

--- examples/graphics-2d/shared/build.gradle.kts
+++ examples/graphics-2d/shared/build.gradle.kts
@@ -65,6 +65,12 @@ kotlin {
                 }
             }
         }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
         val androidMain by getting {
             dependencies {
                 api("androidx.activity:activity-compose:1.7.2")
