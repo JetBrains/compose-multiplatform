3d4db14b5164cea876c8802be7f42c4b403b22d5
dima.avdeev <dima.avdeev@jetbrains.com>
2023-10-05 04:19:37 +0400

    COMPOSE-350 Remove old Gradle tasks to launch iOS App (#3753)
    Issue https://youtrack.jetbrains.com/issue/COMPOSE-350
    
    Also removed kotlinx-serialization library because we don't need to
    parse json output of `xrun simctl` anymore

--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/experimental/internal/configureExperimental.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/experimental/internal/configureExperimental.kt
@@ -8,7 +8,6 @@ package org.jetbrains.compose.experimental.internal
 import org.gradle.api.Project
 import org.jetbrains.compose.ComposeExtension
 import org.jetbrains.compose.experimental.dsl.ExperimentalExtension
-import org.jetbrains.compose.experimental.uikit.internal.configureExperimentalUikitApplication
 import org.jetbrains.compose.experimental.web.internal.configureExperimentalWebApplication
 import org.jetbrains.compose.web.WebExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
@@ -17,12 +16,6 @@ internal fun Project.configureExperimental(
     composeExt: ComposeExtension,
     experimentalExt: ExperimentalExtension
 ) {
-    if (experimentalExt.uikit._isApplicationInitialized) {
-        val mppExt = project.extensions.findByType(KotlinMultiplatformExtension::class.java)
-            ?: error("'org.jetbrains.kotlin.multiplatform' plugin is required for using 'compose.experimental.uikit {}'")
-        configureExperimentalUikitApplication(mppExt, experimentalExt.uikit.application)
-    }
-
     if (experimentalExt.web._isApplicationInitialized) {
         val webExt = composeExt.extensions.getByType(WebExtension::class.java)
         webExt.targetsToConfigure(project)
