75e7d174d7a838497e5e9d7c39eb2322e615c83f
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2023-10-24 18:02:16 +0200

    Add kotlin 1.9.20-RC2 support (#3861)
    Add kotlin 1.9.20-RC2 support

--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerCompatibility.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerCompatibility.kt
@@ -19,6 +19,7 @@ internal object ComposeCompilerCompatibility {
         "1.9.20-Beta" to "1.5.2.1-Beta2",
         "1.9.20-Beta2" to "1.5.2.1-Beta3",
         "1.9.20-RC" to "1.5.2.1-rc01",
+        "1.9.20-RC2" to "1.5.3-rc01",
     )
 
     fun compilerVersionFor(kotlinVersion: String): String {
