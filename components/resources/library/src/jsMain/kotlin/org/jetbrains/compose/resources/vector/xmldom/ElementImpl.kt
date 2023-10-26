0cd5ddb0bc313f16084c7bc4d4f0c4f7614a4a84
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2023-10-26 20:03:15 +0200

    Improve xml parser to read text content

--- components/resources/library/src/jsMain/kotlin/org/jetbrains/compose/resources/vector/xmldom/ElementImpl.kt
+++ components/resources/library/src/jsMain/kotlin/org/jetbrains/compose/resources/vector/xmldom/ElementImpl.kt
@@ -3,6 +3,8 @@ package org.jetbrains.compose.resources.vector.xmldom
 import org.w3c.dom.Element as DomElement
 
 internal class ElementImpl(val element: DomElement): NodeImpl(element), Element {
+    override val textContent: String?
+        get() = element.textContent
 
     override val localName: String
         get() = element.localName
--- components/resources/library/src/nativeMain/kotlin/org/jetbrains/compose/resources/vector/xmldom/DomXmlParser.kt
+++ components/resources/library/src/nativeMain/kotlin/org/jetbrains/compose/resources/vector/xmldom/DomXmlParser.kt
@@ -4,7 +4,6 @@
  */
 package org.jetbrains.compose.resources.vector.xmldom
 
-import org.jetbrains.compose.resources.vector.xmldom.MalformedXMLException
 import platform.Foundation.*
 import platform.darwin.NSObject
 
@@ -18,11 +17,15 @@ internal fun parse(xml: String): Element {
     return parser.root!!
 }
 
-private class ElementImpl(override val localName: String,
-                          override val nodeName: String,
-                          override val namespaceURI: String,
-                          val prefixMap: Map<String, String>,
-                          val attributes: Map<Any?, *>): Element {
+private class ElementImpl(
+    override val localName: String,
+    override val nodeName: String,
+    override val namespaceURI: String,
+    val prefixMap: Map<String, String>,
+    val attributes: Map<Any?, *>
+) : Element {
+
+    override var textContent: String? = null
 
     override val childNodes: NodeList
         get() = object : NodeList {
@@ -43,15 +46,13 @@ private class ElementImpl(override val localName: String,
         return getAttribute(attrKey)
     }
 
-    override fun getAttribute(name: String): String =  attributes[name] as String? ?:""
+    override fun getAttribute(name: String): String = attributes[name] as String? ?: ""
 
-    override fun lookupPrefix(uri: String): String = prefixMap[uri]?:""
+    override fun lookupPrefix(uri: String): String = prefixMap[uri] ?: ""
 }
 
-@Suppress("CONFLICTING_OVERLOADS")
-private class DomXmlParser(
-
-) : NSObject(), NSXMLParserDelegateProtocol {
+@Suppress("CONFLICTING_OVERLOADS", "PARAMETER_NAME_CHANGED_ON_OVERRIDE")
+private class DomXmlParser : NSObject(), NSXMLParserDelegateProtocol {
 
     val curPrefixMapInverted = mutableMapOf<String, String>()
 
@@ -68,8 +69,13 @@ private class DomXmlParser(
         qualifiedName: String?,
         attributes: Map<Any?, *>
     ) {
-        val node = ElementImpl(didStartElement, qualifiedName!!, namespaceURI?:"",
-            curPrefixMap, attributes)
+        val node = ElementImpl(
+            didStartElement,
+            qualifiedName!!,
+            namespaceURI ?: "",
+            curPrefixMap,
+            attributes
+        )
 
         if (root == null) root = node
 
@@ -77,6 +83,10 @@ private class DomXmlParser(
             nodeStack.last().childs.add(node)
 
         nodeStack.add(node)
+    }
+
+    override fun parser(parser: NSXMLParser, foundCharacters: String) {
+        nodeStack.lastOrNull()?.textContent = foundCharacters
     }
 
     override fun parser(
@@ -89,9 +99,10 @@ private class DomXmlParser(
         assert(node.localName.equals(didEndElement))
     }
 
-    override fun parser(parser: NSXMLParser,
-                        didStartMappingPrefix: String,
-                        toURI: String
+    override fun parser(
+        parser: NSXMLParser,
+        didStartMappingPrefix: String,
+        toURI: String
     ) {
         curPrefixMapInverted.put(didStartMappingPrefix, toURI)
         curPrefixMap = curPrefixMapInverted.entries.associateBy({ it.value }, { it.key })
