6a3d33f4eb172a2d7d9ec9518e223ec3a900f2df
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2023-11-06 20:43:05 +0100

    Update samples

--- components/resources/demo/shared/src/jsMain/kotlin/main.js.kt
+++ components/resources/demo/shared/src/jsMain/kotlin/main.js.kt
@@ -3,6 +3,7 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
  */
 
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.configureWebResources
@@ -10,9 +11,8 @@ import org.jetbrains.compose.resources.demo.shared.UseResources
 import org.jetbrains.skiko.wasm.onWasmReady
 
 
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalResourceApi::class)
 fun main() {
-
-    @OptIn(ExperimentalResourceApi::class)
     configureWebResources {
         // Not necessary - It's the same as the default. We add it here just to present this feature.
         setResourcePathCustomization { "./$it" }
