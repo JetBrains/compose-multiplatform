fa8d491eeffa006ef6c6360537fc6188ebff481f
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2024-01-03 12:36:13 +0100

    Update test data

--- gradle-plugins/compose/src/test/test-projects/misc/commonResources/expected/Res.kt
+++ gradle-plugins/compose/src/test/test-projects/misc/commonResources/expected/Res.kt
@@ -13,9 +13,17 @@ import org.jetbrains.compose.resources.ThemeQualifier
 import org.jetbrains.compose.resources.readResourceBytes
 
 internal object Res {
+    /**
+     * Reads the content of the resource file at the specified path and returns it as a byte array.
+     *
+     * Example: `val bytes = Res.readBytes("files/key.bin")`
+     *
+     * @param path The path of the file to read in the compose resource's directory.
+     * @return The content of the file as a byte array.
+     */
     @OptIn(org.jetbrains.compose.resources.InternalResourceApi::class)
-    public suspend fun readFileBytes(path: String): ByteArray =
-        readResourceBytes("composeResources/" + path)
+    public suspend fun readBytes(path: String): ByteArray = readResourceBytes("composeResources/" +
+            path)
 
     public object drawable {
         public val _3_strange_name: DrawableResource = DrawableResource(
