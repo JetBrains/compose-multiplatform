b1e86ade3607d4e9a7e65c93e61e21d9574311b1
Konstantin <konstantin.tskhovrebov@jetbrains.com>
2024-01-31 12:41:38 +0100

    Generate initializer functions in the Res file to avoid the MethodTooLargeException (#4205)

--- gradle-plugins/compose/src/test/test-projects/misc/commonResources/expected/Res.kt
+++ gradle-plugins/compose/src/test/test-projects/misc/commonResources/expected/Res.kt
@@ -1,3 +1,8 @@
+@file:OptIn(
+    org.jetbrains.compose.resources.InternalResourceApi::class,
+    org.jetbrains.compose.resources.ExperimentalResourceApi::class,
+)
+
 package app.group.resources_test.generated.resources
 
 import kotlin.ByteArray
@@ -6,14 +11,9 @@ import kotlin.String
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.FontResource
-import org.jetbrains.compose.resources.LanguageQualifier
-import org.jetbrains.compose.resources.RegionQualifier
-import org.jetbrains.compose.resources.ResourceItem
 import org.jetbrains.compose.resources.StringResource
-import org.jetbrains.compose.resources.ThemeQualifier
 import org.jetbrains.compose.resources.readResourceBytes
 
-@OptIn(org.jetbrains.compose.resources.InternalResourceApi::class)
 @ExperimentalResourceApi
 internal object Res {
     /**
@@ -27,115 +27,99 @@ internal object Res {
     public suspend fun readBytes(path: String): ByteArray = readResourceBytes(path)
 
     public object drawable {
-        public val _3_strange_name: DrawableResource = DrawableResource(
-            "drawable:_3_strange_name",
-            setOf(
-                ResourceItem(
-                    setOf(),
-                    "drawable/3-strange-name.xml"
-                ),
-            )
-        )
+        public val _3_strange_name: DrawableResource = get__3_strange_name()
 
-        public val vector: DrawableResource = DrawableResource(
-            "drawable:vector",
-            setOf(
-                ResourceItem(
-                    setOf(LanguageQualifier("au"), RegionQualifier("US"), ),
-                    "drawable-au-rUS/vector.xml"
-                ),
-                ResourceItem(
-                    setOf(ThemeQualifier.DARK, LanguageQualifier("ge"), ),
-                    "drawable-dark-ge/vector.xml"
-                ),
-                ResourceItem(
-                    setOf(LanguageQualifier("en"), ),
-                    "drawable-en/vector.xml"
-                ),
-                ResourceItem(
-                    setOf(),
-                    "drawable/vector.xml"
-                ),
-            )
-        )
+        public val vector: DrawableResource = get_vector()
 
-        public val vector_2: DrawableResource = DrawableResource(
-            "drawable:vector_2",
-            setOf(
-                ResourceItem(
-                    setOf(),
-                    "drawable/vector_2.xml"
-                ),
-            )
-        )
-    }
-
-    public object font {
-        public val emptyfont: FontResource = FontResource(
-            "font:emptyfont",
-            setOf(
-                ResourceItem(
-                    setOf(),
-                    "font/emptyFont.otf"
-                ),
-            )
-        )
+        public val vector_2: DrawableResource = get_vector_2()
     }
 
     public object string {
-        public val app_name: StringResource = StringResource(
-            "string:app_name",
-            "app_name",
-            setOf(
-                ResourceItem(
-                    setOf(),
-                    "values/strings.xml"
-                ),
-            )
-        )
+        public val app_name: StringResource = get_app_name()
 
-        public val hello: StringResource = StringResource(
-            "string:hello",
-            "hello",
-            setOf(
-                ResourceItem(
-                    setOf(),
-                    "values/strings.xml"
-                ),
-            )
-        )
+        public val hello: StringResource = get_hello()
 
-        public val multi_line: StringResource = StringResource(
-            "string:multi_line",
-            "multi_line",
-            setOf(
-                ResourceItem(
-                    setOf(),
-                    "values/strings.xml"
-                ),
-            )
-        )
+        public val multi_line: StringResource = get_multi_line()
 
-        public val str_arr: StringResource = StringResource(
-            "string:str_arr",
-            "str_arr",
-            setOf(
-                ResourceItem(
-                    setOf(),
-                    "values/strings.xml"
-                ),
-            )
-        )
+        public val str_arr: StringResource = get_str_arr()
 
-        public val str_template: StringResource = StringResource(
-            "string:str_template",
-            "str_template",
-            setOf(
-                ResourceItem(
-                    setOf(),
-                    "values/strings.xml"
-                ),
-            )
-        )
+        public val str_template: StringResource = get_str_template()
+    }
+
+    public object font {
+        public val emptyfont: FontResource = get_emptyfont()
     }
 }
+
+private fun get__3_strange_name(): DrawableResource =
+    org.jetbrains.compose.resources.DrawableResource(
+        "drawable:_3_strange_name",
+        setOf(
+            org.jetbrains.compose.resources.ResourceItem(setOf(), "drawable/3-strange-name.xml"),
+        )
+    )
+
+private fun get_vector(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+    "drawable:vector",
+    setOf(
+
+        org.jetbrains.compose.resources.ResourceItem(setOf(org.jetbrains.compose.resources.LanguageQualifier("au"),
+            org.jetbrains.compose.resources.RegionQualifier("US"), ), "drawable-au-rUS/vector.xml"),
+
+        org.jetbrains.compose.resources.ResourceItem(setOf(org.jetbrains.compose.resources.ThemeQualifier.DARK,
+            org.jetbrains.compose.resources.LanguageQualifier("ge"), ), "drawable-dark-ge/vector.xml"),
+
+        org.jetbrains.compose.resources.ResourceItem(setOf(org.jetbrains.compose.resources.LanguageQualifier("en"),
+        ), "drawable-en/vector.xml"),
+        org.jetbrains.compose.resources.ResourceItem(setOf(), "drawable/vector.xml"),
+    )
+)
+
+private fun get_vector_2(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
+    "drawable:vector_2",
+    setOf(
+        org.jetbrains.compose.resources.ResourceItem(setOf(), "drawable/vector_2.xml"),
+    )
+)
+
+private fun get_app_name(): StringResource = org.jetbrains.compose.resources.StringResource(
+    "string:app_name", "app_name",
+    setOf(
+        org.jetbrains.compose.resources.ResourceItem(setOf(), "values/strings.xml"),
+    )
+)
+
+private fun get_hello(): StringResource = org.jetbrains.compose.resources.StringResource(
+    "string:hello", "hello",
+    setOf(
+        org.jetbrains.compose.resources.ResourceItem(setOf(), "values/strings.xml"),
+    )
+)
+
+private fun get_multi_line(): StringResource = org.jetbrains.compose.resources.StringResource(
+    "string:multi_line", "multi_line",
+    setOf(
+        org.jetbrains.compose.resources.ResourceItem(setOf(), "values/strings.xml"),
+    )
+)
+
+private fun get_str_arr(): StringResource = org.jetbrains.compose.resources.StringResource(
+    "string:str_arr", "str_arr",
+    setOf(
+        org.jetbrains.compose.resources.ResourceItem(setOf(), "values/strings.xml"),
+    )
+)
+
+private fun get_str_template(): StringResource = org.jetbrains.compose.resources.StringResource(
+    "string:str_template", "str_template",
+    setOf(
+        org.jetbrains.compose.resources.ResourceItem(setOf(), "values/strings.xml"),
+    )
+)
+
+private fun get_emptyfont(): FontResource = org.jetbrains.compose.resources.FontResource(
+    "font:emptyfont",
+    setOf(
+        org.jetbrains.compose.resources.ResourceItem(setOf(), "font/emptyFont.otf"),
+    )
+)
