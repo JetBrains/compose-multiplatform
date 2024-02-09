63334b346cbf4eeb5f9278c798df36e6a0ad5390
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2024-02-09 19:08:23 +0100

    Add hierarchical resource to the demo

--- components/resources/demo/shared/src/commonMain/kotlin/org/jetbrains/compose/resources/demo/shared/FileRes.kt
+++ components/resources/demo/shared/src/commonMain/kotlin/org/jetbrains/compose/resources/demo/shared/FileRes.kt
@@ -1,22 +1,11 @@
 package org.jetbrains.compose.resources.demo.shared
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import components.resources.demo.shared.generated.resources.Res
@@ -55,6 +44,38 @@ fun FileRes(paddingValues: PaddingValues) {
                 }
                 LaunchedEffect(Unit) {
                   bytes = Res.readFileBytes("files/icon.xml")
+                }
+                Text(bytes.decodeToString())
+            """.trimIndent()
+        )
+        Text(
+            modifier = Modifier.padding(16.dp),
+            text = "File: 'files/platform-text.txt'",
+            style = MaterialTheme.typography.titleLarge
+        )
+        OutlinedCard(
+            modifier = Modifier.padding(horizontal = 16.dp),
+            shape = RoundedCornerShape(4.dp),
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer)
+        ) {
+            var bytes by remember { mutableStateOf(ByteArray(0)) }
+            LaunchedEffect(Unit) {
+                bytes = Res.readBytes("files/platform-text.txt")
+            }
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text = bytes.decodeToString(),
+                color = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+        }
+        Text(
+            modifier = Modifier.padding(16.dp),
+            text = """
+                var bytes by remember { 
+                  mutableStateOf(ByteArray(0))
+                }
+                LaunchedEffect(Unit) {
+                  bytes = Res.readFileBytes("files/platform-text.txt")
                 }
                 Text(bytes.decodeToString())
             """.trimIndent()
