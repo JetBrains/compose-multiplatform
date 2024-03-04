7c32dd03d3ea534398d063c8fe33e71854b81071
Oleksandr.Karpovich <a.n.karpovich@gmail.com>
2024-03-04 12:03:27 +0100

    add comment

--- compose/integrations/composable-test-cases/common/build.gradle.kts
+++ compose/integrations/composable-test-cases/common/build.gradle.kts
@@ -13,6 +13,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
+                // Expose it as api here, so other modules don't need to care about it
                 api(project.composeRuntimeDependency)
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
             }
