beb92f06255b43ad3216f2dc42bc33e8f4cd7fb9
Chanjung Kim <freiyer.paxbun@gmail.com>
2024-03-20 23:16:13 +0900

    Parse without using String.split

--- components/resources/library/src/commonMain/kotlin/org/jetbrains/compose/resources/intl/PluralConditionParseException.kt
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright 2020-2024 JetBrains s.r.o. and respective authors and developers.
- * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
- */
-
-package org.jetbrains.compose.resources.intl
-
-internal class PluralConditionParseException(description: String) : Exception("Invalid LDML: $description")
