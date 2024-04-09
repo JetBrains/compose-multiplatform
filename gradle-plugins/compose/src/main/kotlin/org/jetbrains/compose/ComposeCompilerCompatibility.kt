0bcfa4f9d64663933a4c79b3e320ea44f0fa32a4
Pavel Shishkin <shishkin-pavel@users.noreply.github.com>
2024-04-09 18:26:15 +0200

    Support Kotlin 2.0.0-RC1 (#4596)

--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerCompatibility.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerCompatibility.kt
@@ -27,6 +27,7 @@ internal object ComposeCompilerCompatibility {
         "2.0.0-Beta1" to "1.5.4-dev1-kt2.0.0-Beta1",
         "2.0.0-Beta4" to "1.5.9-kt-2.0.0-Beta4",
         "2.0.0-Beta5" to "1.5.11-kt-2.0.0-Beta5",
+        "2.0.0-RC1" to "1.5.11-kt-2.0.0-RC1",
     )
 
     fun compilerVersionFor(kotlinVersion: String): String {
