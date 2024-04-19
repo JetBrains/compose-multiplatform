2447b07f995eacabc250c40fea8e33bc91f2f20f
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2024-04-19 16:30:53 +0200

    [resources] Restore dependency on the foundation

--- components/resources/library/build.gradle.kts
+++ components/resources/library/build.gradle.kts
@@ -68,7 +68,7 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation(compose.runtime)
-                implementation(compose.ui)
+                implementation(compose.foundation)
                 implementation(libs.kotlinx.coroutines.core)
             }
         }
