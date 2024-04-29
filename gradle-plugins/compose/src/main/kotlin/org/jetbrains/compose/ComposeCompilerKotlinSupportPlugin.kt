09ba9dd06f908ec9ce8debb7a5a6502d3221c1fc
Igor Demin <igordmn@users.noreply.github.com>
2024-04-29 17:55:26 +0200

    Fix the link to the Compiler guide (#4730)

--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerKotlinSupportPlugin.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposeCompilerKotlinSupportPlugin.kt
@@ -40,7 +40,7 @@ internal const val newComposeCompilerKotlinSupportPluginId = "org.jetbrains.kotl
 internal const val newComposeCompilerError =
     "Since Kotlin 2.0.0-RC2 to use Compose Multiplatform " +
             "you must apply \"$newComposeCompilerKotlinSupportPluginId\" plugin." +
-            "\nSee the migration guide https://www.jetbrains.com/help/kotlin-multiplatform-dev/compose-compiler/compose-compiler.html#migrating-a-compose-multiplatform-project"
+            "\nSee the migration guide https://www.jetbrains.com/help/kotlin-multiplatform-dev/compose-compiler.html#migrating-a-compose-multiplatform-project"
 
 private fun Project.configureComposeCompilerPlugin(kgp: KotlinBasePlugin) {
     val kgpVersion = kgp.pluginVersion
