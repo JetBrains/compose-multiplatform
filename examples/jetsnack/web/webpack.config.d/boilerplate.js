b9aa84e13abecde539314c7604b4e113193097e9
Oleksandr.Karpovich <a.n.karpovich@gmail.com>
2024-05-03 11:34:31 +0200

    Remove redundant files for web target

--- examples/jetsnack/web/webpack.config.d/boilerplate.js
+++
@@ -1,7 +0,0 @@
-config.entry = {
-    main: [require('path').resolve(__dirname, "kotlin/load.mjs")]
-};
-
-config.resolve ?? (config.resolve = {});
-config.resolve.alias ?? (config.resolve.alias = {});
-config.resolve.alias.skia = false;
