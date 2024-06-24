998c6b9ddf7798dab906bacd7dee35e5fa5e35fe
soleil.colza <soleil.colza@icloud.com>
2024-06-25 01:35:47 +0900

    Replaced wildcard imports with the proper paths without abbreviations

--- examples/graphics-2d/shared/src/commonMain/kotlin/minesweeper/Widgets.kt
+++ examples/graphics-2d/shared/src/commonMain/kotlin/minesweeper/Widgets.kt
@@ -6,7 +6,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -17,7 +21,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import graphics_2d.shared.generated.resources.*
+import graphics_2d.shared.generated.resources.Res
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 
