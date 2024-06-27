2f0e3f9167b585978489c0c2caa04bee6e4f513b
soleil.colza <soleil.colza@icloud.com>
2024-06-27 23:00:16 +0900

    Replaced wildcard imports with the proper paths without abbreviations

--- examples/interop/ios-uikit-in-compose/shared/src/iosMain/kotlin/main.ios.kt
+++ examples/interop/ios-uikit-in-compose/shared/src/iosMain/kotlin/main.ios.kt
@@ -1,5 +1,10 @@
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -8,6 +13,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.interop.UIKitView
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
+import androidx.compose.ui.window.WindowInsets
+import androidx.compose.ui.window.systemBars
 import kotlinx.cinterop.ExperimentalForeignApi
 import platform.MapKit.MKMapView
 import platform.UIKit.UIViewController
