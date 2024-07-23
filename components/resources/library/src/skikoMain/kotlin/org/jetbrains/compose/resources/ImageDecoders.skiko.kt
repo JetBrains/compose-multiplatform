5332ee17fbacad62f7023c21bb9a25db553c70e0
Konstantin Tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2024-07-23 14:42:57 +0200

    [resources] Add a byte array to ImageBitmap converter

--- components/resources/library/src/skikoMain/kotlin/org/jetbrains/compose/resources/ImageDecoders.skiko.kt
+++ components/resources/library/src/skikoMain/kotlin/org/jetbrains/compose/resources/ImageDecoders.skiko.kt
@@ -1,10 +1,13 @@
 package org.jetbrains.compose.resources
 
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.Density
 
 /**
  * Decodes a byte array of an SVG file to a compose Painter.
+ *
+ * @param density density to apply during converting the source units to the [Painter] units.
  *
  * @return The converted Painter.
  */
