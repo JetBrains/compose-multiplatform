1eb1ade9cde9850a48a40f3f948870f558cceb38
Oleksandr.Karpovich <a.n.karpovich@gmail.com>
2024-11-06 12:18:18 +0100

    Add documentation

--- components/resources/library/src/webMain/kotlin/org/jetbrains/compose/resources/Resource.web.kt
+++ components/resources/library/src/webMain/kotlin/org/jetbrains/compose/resources/Resource.web.kt
@@ -59,6 +59,33 @@ internal fun getResourceUrl(windowOrigin: String, windowPathname: String, resour
     }
 }
 
+/**
+ * Preloads a font resource and provides a [State] containing the loaded [Font] or `null` if not yet loaded.
+ *
+ * Internally, it reads font bytes, converts them to a [Font] object, and caches the result, speeding up future
+ * accesses to the same font resource when using @Composable Font function.
+ *
+ * **Usage Example:**
+ * ```
+ * @Composable
+ * fun MyApp() {
+ *     val fontState by preloadFont(Res.font.SomeNiceFont)
+ *
+ *     if (fontState != null) {
+ *         Text(
+ *             text = "Hello, World!",
+ *             fontFamily = FontFamily(Font(Res.font.SomeNiceFont)) // the font is taken from the cache
+ *         )
+ *     }
+ * }
+ * ```
+ *
+ * @param resource The font resource to be used.
+ * @param weight The weight of the font. Default value is [FontWeight.Normal].
+ * @param style The style of the font. Default value is [FontStyle.Normal].
+ * @return A [State]<[Font]?> object that holds the loaded [Font] when available,
+ * or `null` if the font is not yet ready.
+ */
 @ExperimentalResourceApi
 @Composable
 fun preloadFont(
@@ -72,6 +99,28 @@ fun preloadFont(
     return resState
 }
 
+/**
+ * Preloads an image resource and provides a [State] containing the loaded [ImageBitmap] or `null` if not yet loaded.
+ *
+ * Internally, it reads the resource bytes, converts them to a [ImageBitmap] object, and caches the result,
+ * speeding up future accesses to the same resource when using @Composable [imageResource] function.
+ *
+ * **Usage Example:**
+ * ```
+ * @Composable
+ * fun MyApp() {
+ *     val imageState by preloadImageResource(Res.drawable.niceDrawable)
+ *
+ *     if (imageState != null) {
+ *         Image(bitmap = imageResource(Res.drawable.niceDrawable))
+ *     }
+ * }
+ * ```
+ *
+ * @param resource The resource to be used.
+ * @return A [State]<[ImageBitmap]?> object that holds the loaded [ImageBitmap] when available,
+ * or `null` if the resource is not yet ready.
+ */
 @ExperimentalResourceApi
 @Composable
 fun preloadImageResource(
@@ -83,6 +132,29 @@ fun preloadImageResource(
     return resState
 }
 
+
+/**
+ * Preloads a vector image resource and provides a [State] containing the loaded [ImageVector] or `null` if not yet loaded.
+ *
+ * Internally, it reads the resource bytes, converts them to a [ImageVector] object, and caches the result,
+ * speeding up future accesses to the same resource when using @Composable [vectorResource] function.
+ *
+ * **Usage Example:**
+ * ```
+ * @Composable
+ * fun MyApp() {
+ *     val iconState by preloadVectorResource(Res.drawable.niceVectorIcon)
+ *
+ *     if (iconState != null) {
+ *         Image(imageVector = vectorResource(Res.drawable.niceVectorIcon))
+ *     }
+ * }
+ * ```
+ *
+ * @param resource The resource to be used.
+ * @return A [State]<[ImageVector]?> object that holds the loaded [ImageVector] when available,
+ * or `null` if the resource is not yet ready.
+ */
 @ExperimentalResourceApi
 @Composable
 fun preloadVectorResource(
