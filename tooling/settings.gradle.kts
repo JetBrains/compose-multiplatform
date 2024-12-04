7206677234d54d2619933907ac4b8f3c2b0bdacb
Igor Demin <igordmn@users.noreply.github.com>
2024-12-04 21:10:33 +0100

    Update settings.gradle.kts

--- tooling/settings.gradle.kts
+++ tooling/settings.gradle.kts
@@ -8,7 +8,8 @@ pluginManagement {
 include(":compose-intellij-platform")
 include(":compose-intellij-platform:sample:base")
 include(":compose-intellij-platform:sample:plugin-1")
-include(":compose-intellij-platform:sample:plugin-2")
+include(":compose-intellij-platform:sample:p
+lugin-2")
 
 
 include(":compose-intellij-platform:sample:all-plugins")
