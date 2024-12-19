f0468925f557a33510266c0c4c51d22434090f69
Matt Groth <mgroth49@gmail.com>
2024-12-19 22:30:04 +0100

    Suppress "redundant visibility modifier" diagnostics so Res is compatible with `-Werror`, `-Wextra`, and IntelliJ inspections
    (cherry picked from commit bd0564f4a7964c7a64062311ba15a0f02b30330c)

--- gradle-plugins/compose/src/test/test-projects/misc/commonResources/expected-open-res/commonResClass/my/lib/res/Res.kt
+++ gradle-plugins/compose/src/test/test-projects/misc/commonResources/expected-open-res/commonResClass/my/lib/res/Res.kt
@@ -1,10 +1,15 @@
 @file:OptIn(org.jetbrains.compose.resources.InternalResourceApi::class)
+@file:Suppress(
+  "RedundantVisibilityModifier",
+  "REDUNDANT_VISIBILITY_MODIFIER",
+)
 
 package my.lib.res
 
 import kotlin.ByteArray
 import kotlin.OptIn
 import kotlin.String
+import kotlin.Suppress
 import org.jetbrains.compose.resources.getResourceUri
 import org.jetbrains.compose.resources.readResourceBytes
 
