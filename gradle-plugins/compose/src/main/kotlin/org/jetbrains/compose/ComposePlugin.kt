78c116c26b5954ca44e0e15f5f4af3eb866251d6
Igor Demin <igordmn@users.noreply.github.com>
2025-01-09 12:01:33 +0100

    Add Windows ARM alias to Gradle plugin
    Fixes https://youtrack.jetbrains.com/issue/CMP-2584/Publish-Windows-ARM-library#focus=Comments-27-11356341.0-0
    
    ## Testing
    TODO

--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposePlugin.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposePlugin.kt
@@ -98,6 +98,7 @@ abstract class ComposePlugin : Plugin<Project> {
         val linux_x64 = composeDependency("org.jetbrains.compose.desktop:desktop-jvm-linux-x64")
         val linux_arm64 = composeDependency("org.jetbrains.compose.desktop:desktop-jvm-linux-arm64")
         val windows_x64 = composeDependency("org.jetbrains.compose.desktop:desktop-jvm-windows-x64")
+        val windows_arm64 = composeDependency("org.jetbrains.compose.desktop:desktop-jvm-windows-arm64")
         val macos_x64 = composeDependency("org.jetbrains.compose.desktop:desktop-jvm-macos-x64")
         val macos_arm64 = composeDependency("org.jetbrains.compose.desktop:desktop-jvm-macos-arm64")
 
