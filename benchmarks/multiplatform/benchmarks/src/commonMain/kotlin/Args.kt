e22ed2384b3c36a56d1c1d0e9ea4b1873b7ca615
Nikita Lipsky <nikita.lipsky@jetbrains.com>
2025-01-17 19:15:21 +0200

    Add ability to specify frame counts to measure as a benchmarks launch parameter (#5204)
    ## Release Notes
    N/A

--- benchmarks/multiplatform/benchmarks/src/commonMain/kotlin/Args.kt
+++ benchmarks/multiplatform/benchmarks/src/commonMain/kotlin/Args.kt
@@ -7,11 +7,20 @@ enum class Mode {
 object Args {
     private val modes = mutableSetOf<Mode>()
 
-    private val benchmarks = mutableSetOf<String>()
+    private val benchmarks = mutableMapOf<String, Int>()
 
     private fun argToSet(arg: String): Set<String> = arg.substring(arg.indexOf('=') + 1)
         .split(",").filter{!it.isEmpty()}.map{it.uppercase()}.toSet()
 
+
+    private fun argToMap(arg: String): Map<String, Int> = arg.substring(arg.indexOf('=') + 1)
+        .split(",").filter { !it.isEmpty() }.map { it.uppercase() }.associate {
+            if (it.contains('(') && it.contains(')')) {
+                it.substringBefore('(') to it.substringAfter('(').substringBefore(')').toInt()
+            } else {
+                it to -1
+            }
+        }
 
     /**
      * Parses command line arguments to determine modes and benchmarks settings.
@@ -25,7 +34,7 @@ object Args {
             if (arg.startsWith("modes=", ignoreCase = true)) {
                 modes.addAll(argToSet(arg).map { Mode.valueOf(it) })
             } else if (arg.startsWith("benchmarks=", ignoreCase = true)) {
-                benchmarks.addAll(argToSet(arg))
+                benchmarks += argToMap(arg)
             }
         }
     }
@@ -33,4 +42,9 @@ object Args {
     fun isModeEnabled(mode: Mode): Boolean = modes.isEmpty() || modes.contains(mode)
 
     fun isBenchmarkEnabled(benchmark: String): Boolean = benchmarks.isEmpty() || benchmarks.contains(benchmark.uppercase())
+
+    fun getBenchmarkProblemSize(benchmark: String, default: Int): Int {
+        val result = benchmarks[benchmark.uppercase()]?: -1
+        return if (result == -1) default else result
+    }
 }
