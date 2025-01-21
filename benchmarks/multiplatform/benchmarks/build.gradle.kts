9c769f8d29620951001c4e497703f99c38ea83ca
Igor Demin <igordmn@users.noreply.github.com>
2025-01-21 14:29:44 +0100

    [Benchmarks] Copy run1 example from compose-multiplatform-core (#5206)
    - replace images by non-AOSP ones
    - keep AOSP license header in code

--- benchmarks/multiplatform/benchmarks/build.gradle.kts
+++ benchmarks/multiplatform/benchmarks/build.gradle.kts
@@ -54,7 +54,6 @@ kotlin {
                 implementation(compose.foundation)
                 implementation(compose.material)
                 implementation(compose.runtime)
-                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
                 implementation(compose.components.resources)
             }
         }
