aeddeba1659370ab1b553c9e3f285dcf1a814f45
Nikita Lipsky <nikita.lipsky@jetbrains.com>
2025-01-27 15:10:23 +0200

    Benchmarks: fix OOM crash on a real iOS device. Part 2. (#5211)
    - add memory warning monitor to do explicit GC
    
    ## Release Notes
    N/A

--- benchmarks/multiplatform/benchmarks/src/iosMain/kotlin/main.ios.kt
+++ benchmarks/multiplatform/benchmarks/src/iosMain/kotlin/main.ios.kt
@@ -3,9 +3,13 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
  */
 
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 fun main(args : List<String>) {
     Args.parseArgs(args.toTypedArray())
-    runBlocking { runBenchmarks(graphicsContext = graphicsContext()) }
+    MainScope().launch {
+        runBenchmarks(graphicsContext = graphicsContext())
+        println("Completed!")
+    }
 }
--- benchmarks/multiplatform/iosApp/iosApp/iOSApp.swift
+++ benchmarks/multiplatform/iosApp/iosApp/iOSApp.swift
@@ -1,9 +1,28 @@
 import SwiftUI
 import benchmarks
+import Combine
+
+class MemoryWarningMonitor: ObservableObject {
+    private var cancellable: AnyCancellable?
+
+    init() {
+        self.cancellable = NotificationCenter.default
+            .publisher(for: UIApplication.didReceiveMemoryWarningNotification)
+            .sink { _ in
+                self.didReceiveMemoryWarning()
+            }
+    }
+
+    private func didReceiveMemoryWarning() {
+//         print("Memory warning received! Cleaning resources.")
+        RunGC_nativeKt.runGC()
+    }
+}
 
 @main
 struct iOSApp: App {
 	init() {
+	    MemoryWarningMonitor()
 		Main_iosKt.main(args: ProcessInfo.processInfo.arguments)
     }
 
