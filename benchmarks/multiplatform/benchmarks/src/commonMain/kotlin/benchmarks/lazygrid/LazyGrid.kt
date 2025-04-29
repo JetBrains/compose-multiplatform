6def779fd6bf011d7e58b9565503f1a668b876f3
Oleksandr.Karpovich <a.n.karpovich@gmail.com>
2025-04-29 11:30:19 +0200

    Refactor LaunchedEffect in LazyGrid non-smooth scrolling

--- benchmarks/multiplatform/benchmarks/src/commonMain/kotlin/benchmarks/lazygrid/LazyGrid.kt
+++ benchmarks/multiplatform/benchmarks/src/commonMain/kotlin/benchmarks/lazygrid/LazyGrid.kt
@@ -2,7 +2,6 @@ package benchmarks.lazygrid
 
 import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -10,22 +9,15 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material.Card
-import androidx.compose.material.Checkbox
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.withFrameMillis
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.isActive
 import kotlin.coroutines.suspendCoroutine
 
 @Composable
@@ -33,8 +25,6 @@ fun LazyGrid(smoothScroll: Boolean = false, withLaunchedEffectInItem: Boolean = 
     val itemCount = 12000
     val entries = remember {List(itemCount) { Entry("$it") }}
     val state = rememberLazyGridState()
-
-    var scrollIteration by remember { mutableStateOf(0) }
 
     MaterialTheme {
         Column {
@@ -64,18 +54,19 @@ fun LazyGrid(smoothScroll: Boolean = false, withLaunchedEffectInItem: Boolean = 
             }
         }
     } else {
-        LaunchedEffect(scrollIteration) {
-            withFrameMillis {}
-            curItem += if (direct) 50 else -50
-            if (curItem >= itemCount) {
-                direct = false
-                curItem = itemCount - 1
-            } else if (curItem <= 0) {
-                direct = true
-                curItem = 0
+        LaunchedEffect(Unit) {
+            while(isActive) {
+                withFrameMillis {}
+                curItem += if (direct) 50 else -50
+                if (curItem >= itemCount) {
+                    direct = false
+                    curItem = itemCount - 1
+                } else if (curItem <= 0) {
+                    direct = true
+                    curItem = 0
+                }
+                state.scrollToItem(curItem)
             }
-            state.scrollToItem(curItem)
-            scrollIteration += 1
         }
     }
 }
