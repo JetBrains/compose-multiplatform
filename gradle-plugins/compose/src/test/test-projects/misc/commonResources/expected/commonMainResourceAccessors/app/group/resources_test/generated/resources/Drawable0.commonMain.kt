1b6e61592dd44bfc8747f429d00945f6adae533a
Konstantin <konstantin.tskhovrebov@jetbrains.com>
2025-05-06 13:46:55 +0200

    Refactor resource accessors generator to minify output source files. (#5298)
    Refactor resource accessors generator to minify output source files.
    
    - Number of items per file was decreased to 100
    - Common part of the path extracted to the private const
    - Added imports
    - Removed the private object with item's properties
    - The file structure was reorganized
    
    Fixes [CMP-7934](https://youtrack.jetbrains.com/issue/CMP-7934) Too
    large String0.commonMain.kt will break code insight feature in AS
    
    ## Testing
    Integration tests.  
    This should be tested by QA
    
    ## Release Notes
    ### Fixes - Resources
    - Fix IDE highlighting/resoulution when a generated file with resource
    accessors is too big

--- gradle-plugins/compose/src/test/test-projects/misc/commonResources/expected/commonMainResourceAccessors/app/group/resources_test/generated/resources/Drawable0.commonMain.kt
+++ gradle-plugins/compose/src/test/test-projects/misc/commonResources/expected/commonMainResourceAccessors/app/group/resources_test/generated/resources/Drawable0.commonMain.kt
@@ -1,4 +1,4 @@
-@file:OptIn(org.jetbrains.compose.resources.InternalResourceApi::class)
+@file:OptIn(InternalResourceApi::class)
 
 package app.group.resources_test.generated.resources
 
@@ -7,83 +7,52 @@ import kotlin.String
 import kotlin.collections.MutableMap
 import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.InternalResourceApi
+import org.jetbrains.compose.resources.LanguageQualifier
+import org.jetbrains.compose.resources.RegionQualifier
+import org.jetbrains.compose.resources.ResourceItem
+import org.jetbrains.compose.resources.ThemeQualifier
 
-private object CommonMainDrawable0 {
-  public val _3_strange_name: DrawableResource by
-  lazy { init__3_strange_name() }
+private const val MD: String = "composeResources/app.group.resources_test.generated.resources/"
 
-  public val camelCaseName: DrawableResource by
-  lazy { init_camelCaseName() }
+internal val Res.drawable._3_strange_name: DrawableResource by lazy {
+      DrawableResource("drawable:_3_strange_name", setOf(
+        ResourceItem(setOf(), "${MD}drawable/3-strange-name.xml", -1, -1),
+      ))
+    }
 
-  public val `is`: DrawableResource by
-  lazy { init_is() }
+internal val Res.drawable.camelCaseName: DrawableResource by lazy {
+      DrawableResource("drawable:camelCaseName", setOf(
+        ResourceItem(setOf(), "${MD}drawable/camelCaseName.xml", -1, -1),
+      ))
+    }
 
-  public val vector: DrawableResource by
-  lazy { init_vector() }
+internal val Res.drawable.`is`: DrawableResource by lazy {
+      DrawableResource("drawable:is", setOf(
+        ResourceItem(setOf(), "${MD}drawable/is.xml", -1, -1),
+      ))
+    }
 
-  public val vector_2: DrawableResource by
-  lazy { init_vector_2() }
-}
+internal val Res.drawable.vector: DrawableResource by lazy {
+      DrawableResource("drawable:vector", setOf(
+        ResourceItem(setOf(LanguageQualifier("ast"), ), "${MD}drawable-ast/vector.xml", -1, -1),
+        ResourceItem(setOf(LanguageQualifier("au"), RegionQualifier("US"), ), "${MD}drawable-au-rUS/vector.xml", -1, -1),
+        ResourceItem(setOf(ThemeQualifier.DARK, LanguageQualifier("ge"), ), "${MD}drawable-dark-ge/vector.xml", -1, -1),
+        ResourceItem(setOf(LanguageQualifier("en"), ), "${MD}drawable-en/vector.xml", -1, -1),
+        ResourceItem(setOf(), "${MD}drawable/vector.xml", -1, -1),
+      ))
+    }
+
+internal val Res.drawable.vector_2: DrawableResource by lazy {
+      DrawableResource("drawable:vector_2", setOf(
+        ResourceItem(setOf(), "${MD}drawable/vector_2.xml", -1, -1),
+      ))
+    }
 
 @InternalResourceApi
 internal fun _collectCommonMainDrawable0Resources(map: MutableMap<String, DrawableResource>) {
-  map.put("_3_strange_name", CommonMainDrawable0._3_strange_name)
-  map.put("camelCaseName", CommonMainDrawable0.camelCaseName)
-  map.put("is", CommonMainDrawable0.`is`)
-  map.put("vector", CommonMainDrawable0.vector)
-  map.put("vector_2", CommonMainDrawable0.vector_2)
+  map.put("_3_strange_name", Res.drawable._3_strange_name)
+  map.put("camelCaseName", Res.drawable.camelCaseName)
+  map.put("is", Res.drawable.`is`)
+  map.put("vector", Res.drawable.vector)
+  map.put("vector_2", Res.drawable.vector_2)
 }
-
-internal val Res.drawable._3_strange_name: DrawableResource
-  get() = CommonMainDrawable0._3_strange_name
-
-private fun init__3_strange_name(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
-  "drawable:_3_strange_name",
-  setOf(
-    org.jetbrains.compose.resources.ResourceItem(setOf(), "composeResources/app.group.resources_test.generated.resources/drawable/3-strange-name.xml", -1, -1),
-  )
-)
-
-internal val Res.drawable.camelCaseName: DrawableResource
-  get() = CommonMainDrawable0.camelCaseName
-
-private fun init_camelCaseName(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
-  "drawable:camelCaseName",
-  setOf(
-    org.jetbrains.compose.resources.ResourceItem(setOf(), "composeResources/app.group.resources_test.generated.resources/drawable/camelCaseName.xml", -1, -1),
-  )
-)
-
-internal val Res.drawable.`is`: DrawableResource
-  get() = CommonMainDrawable0.`is`
-
-private fun init_is(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
-  "drawable:is",
-  setOf(
-    org.jetbrains.compose.resources.ResourceItem(setOf(), "composeResources/app.group.resources_test.generated.resources/drawable/is.xml", -1, -1),
-  )
-)
-
-internal val Res.drawable.vector: DrawableResource
-  get() = CommonMainDrawable0.vector
-
-private fun init_vector(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
-  "drawable:vector",
-  setOf(
-    org.jetbrains.compose.resources.ResourceItem(setOf(org.jetbrains.compose.resources.LanguageQualifier("ast"), ), "composeResources/app.group.resources_test.generated.resources/drawable-ast/vector.xml", -1, -1),
-    org.jetbrains.compose.resources.ResourceItem(setOf(org.jetbrains.compose.resources.LanguageQualifier("au"), org.jetbrains.compose.resources.RegionQualifier("US"), ), "composeResources/app.group.resources_test.generated.resources/drawable-au-rUS/vector.xml", -1, -1),
-    org.jetbrains.compose.resources.ResourceItem(setOf(org.jetbrains.compose.resources.ThemeQualifier.DARK, org.jetbrains.compose.resources.LanguageQualifier("ge"), ), "composeResources/app.group.resources_test.generated.resources/drawable-dark-ge/vector.xml", -1, -1),
-    org.jetbrains.compose.resources.ResourceItem(setOf(org.jetbrains.compose.resources.LanguageQualifier("en"), ), "composeResources/app.group.resources_test.generated.resources/drawable-en/vector.xml", -1, -1),
-    org.jetbrains.compose.resources.ResourceItem(setOf(), "composeResources/app.group.resources_test.generated.resources/drawable/vector.xml", -1, -1),
-  )
-)
-
-internal val Res.drawable.vector_2: DrawableResource
-  get() = CommonMainDrawable0.vector_2
-
-private fun init_vector_2(): DrawableResource = org.jetbrains.compose.resources.DrawableResource(
-  "drawable:vector_2",
-  setOf(
-    org.jetbrains.compose.resources.ResourceItem(setOf(), "composeResources/app.group.resources_test.generated.resources/drawable/vector_2.xml", -1, -1),
-  )
-)
