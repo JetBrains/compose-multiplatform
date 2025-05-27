4985418f8b5afd7fbbf12d640bdfecaf8d5cd785
Pavel Shishkin <shishkin-pavel@users.noreply.github.com>
2025-05-27 14:44:41 +0200

    rework compose compiler tests for k2 (#5325)
    migrate compose compiler tests to work with k2
    
    ## Testing
    N/A
    ## Release Notes
    N/A

--- compose/integrations/composable-test-cases/build.gradle.kts
+++ compose/integrations/composable-test-cases/build.gradle.kts
@@ -1,9 +1,17 @@
-import internal.InternalComposeSupportPlugin
-import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
-group "com.example"
-version "1.0-SNAPSHOT"
+//group "com.example"
+//version "1.0-SNAPSHOT"
+
+fun Project.disableYarnLockMismatchReport() {
+    plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
+        the<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension>().apply {
+            yarnLockMismatchReport = org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport.NONE
+        }
+    }
+}
 
 allprojects {
     repositories {
@@ -11,50 +19,69 @@ allprojects {
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/dev/") // to test with kotlin dev builds
-        // mavenLocal()
+
+        maven("https://packages.jetbrains.team/maven/p/kt/dev")
+        maven("https://redirector.kotlinlang.org/maven/dev")
+//        mavenLocal()
     }
 
-    // Apply here for all subprojects instead of applying in each build.gradle.kts separately.
-    // It applies the compiler plugin
-    this.apply<InternalComposeSupportPlugin>()
-
-    afterEvaluate {
-        val pluginOptionPrefix = "plugin:androidx.compose.compiler.plugins.kotlin:"
-        val project = this
-        val kotlinVersion = project.properties["kotlin.version"] as? String
-
-        project.tasks.withType(KotlinCompile::class.java).configureEach {
-            kotlinOptions.apply {
-                freeCompilerArgs +=
-                    listOf(
-                        "-P",
-                        "${pluginOptionPrefix}suppressKotlinVersionCompatibilityCheck=$kotlinVersion"
-                    )
-
-            }
-        }
-
-        tasks.withType<KotlinJsCompile>().configureEach {
-            kotlinOptions.freeCompilerArgs += listOf(
-                "-Xklib-enable-signature-clash-checks=false",
-            )
-        }
-
-        tasks.withType<KotlinCompile<*>>().configureEach {
-            kotlinOptions.freeCompilerArgs += "-Xpartial-linkage=disable"
-        }
-    }
     disableYarnLockMismatchReport()
 }
 
 plugins {
-    kotlin("multiplatform") apply false
+    kotlin("multiplatform").version(libs.versions.kotlin).apply(false)
+    alias(libs.plugins.compose.compiler).apply(false)
 }
 
-fun Project.disableYarnLockMismatchReport() {
-    plugins.withType<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnPlugin> {
-        the<org.jetbrains.kotlin.gradle.targets.js.yarn.YarnRootExtension>().apply {
-            yarnLockMismatchReport = org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport.NONE
+subprojects {
+    apply(plugin = "org.jetbrains.kotlin.multiplatform")
+
+    configure<org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension> {
+        jvm("desktop")
+        applyDefaultHierarchyTemplate()
+        js(IR) {
+//                browser()
+            nodejs() // Commented to save a bit of CI time. Testing in a browser should be enough.
         }
+        @OptIn(ExperimentalWasmDsl::class)
+        wasmJs {
+            d8 {}
+        }
+
+        iosArm64()
+        iosSimulatorArm64()
+        iosX64()
+        macosX64()
+        macosArm64()
+        // We use linux agents on CI. So it doesn't run the tests, but it builds the klib anyway which is time consuming.
+        // if (project.isMingwX64Enabled) mingwX64()
+        linuxX64()
+
+        sourceSets {
+            val commonMain by getting {
+                val projectName = project.name
+                dependencies {
+                    if (projectName != "common") {
+                        implementation(project(":common"))
+                    }
+
+                    if (projectName.endsWith("-main")) {
+                        implementation(project(":" + projectName.replace("-main", "-lib")))
+                    }
+                }
+            }
+        }
+
+        targets
+            .filter { it.name != "desktop" } // Exclude JVM target
+            .forEach { target ->
+                target.compilations.all {
+                    compileTaskProvider.configure{
+                        compilerOptions {
+                            freeCompilerArgs.add("-Xpartial-linkage=disable")
+                        }
+                    }
+                }
+            }
     }
 }
