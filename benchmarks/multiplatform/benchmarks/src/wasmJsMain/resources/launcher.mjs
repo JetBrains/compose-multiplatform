82fd5ee86648fadb9c5b9669dc05876806b345b2
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2025-07-03 18:29:36 +0200

    Bump the Kotlin version to 2.2.0 in the multiplatform benchmark (#5352)
    ## Testing
    N/A
    
    
    ## Release Notes
    N/A

--- benchmarks/multiplatform/benchmarks/src/wasmJsMain/resources/launcher.mjs
+++ benchmarks/multiplatform/benchmarks/src/wasmJsMain/resources/launcher.mjs
@@ -1,7 +1,7 @@
 globalThis.isD8 = true;
 
 import * as skiko from './skikod8.mjs';
-import { instantiate } from './compose-benchmarks-benchmarks-wasm-js.uninstantiated.mjs';
+import { instantiate } from './compose-benchmarks-benchmarks.uninstantiated.mjs';
 
 const exports = (await instantiate({
     './skiko.mjs': skiko
