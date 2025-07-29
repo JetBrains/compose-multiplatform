f3b3036508f11d4a72c4b2798b7f240cb8bf4883
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2025-07-29 13:14:52 +0200

    Typo fix

--- gradle-plugins/compose/src/test/kotlin/org/jetbrains/compose/test/tests/integration/GradlePluginTest.kt
+++ gradle-plugins/compose/src/test/kotlin/org/jetbrains/compose/test/tests/integration/GradlePluginTest.kt
@@ -87,7 +87,7 @@ class GradlePluginTest : GradlePluginTestBase() {
             }
         }
 
-    // Note: we can"t test non-jvm targets with Kotlin older than 2.1.0, because of klib abi version bump in 2.1.0
+    // Note: we can't test non-jvm targets with Kotlin older than 2.1.0, because of klib abi version bump in 2.1.0
     private val oldestSupportedKotlinVersion = "2.1.0"
     @Test
     fun testOldestKotlinMpp() = with(
