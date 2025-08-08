28aad4929337bc7bef77e3ce2532d38773d4b6a5
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2025-08-08 16:10:10 +0200

    Introduce CompatibilityDistributionTest::testWebJsWasmReconfigured which tests heavily reconfigured project with composeCompatibilityBrowserDistribution

--- gradle-plugins/compose/src/test/kotlin/org/jetbrains/compose/test/tests/integration/CompatibilityDistributionTest.kt
+++ gradle-plugins/compose/src/test/kotlin/org/jetbrains/compose/test/tests/integration/CompatibilityDistributionTest.kt
@@ -1,66 +1,110 @@
 package org.jetbrains.compose.test.tests.integration
 
-import org.gradle.internal.impldep.junit.framework.TestCase.assertTrue
 import org.jetbrains.compose.test.utils.GradlePluginTestBase
+import org.jetbrains.compose.test.utils.TestProject
 import org.jetbrains.compose.test.utils.checkExists
 import org.jetbrains.compose.test.utils.checks
 import org.junit.jupiter.api.Test
-import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class CompatibilityDistributionTest : GradlePluginTestBase() {
-    @Test
-    fun testWebJsWasm() = with(
+    private val defaultDistDir = "./composeApp/build/dist/composeWebCompatibility/productionExecutable"
+
+    private fun TestProject.assertCompatibilityDistribution(
+        dirPath: String,
+        expectedFileNames: Set<String>
+    ) {
+        file(dirPath).apply {
+            checkExists()
+            assertTrue(isDirectory, "Expected $dirPath to be a directory")
+
+            val listed = listFiles()
+                ?: error("Expected to list files in $dirPath but got null")
+            val distributionNames = listed.map { it.name }
+
+            assertTrue(
+                distributionNames.any { it.endsWith(".wasm") },
+                "Expected at least one .wasm file in $dirPath, found: $distributionNames"
+            )
+
+            val actualFiles = distributionNames.filterNot { it.endsWith(".wasm") }.toSet()
+            assertEquals(expectedFileNames, actualFiles, "files mismatch in $dirPath")
+        }
+    }
+
+    private fun runCompatibilityTest(
+        projectPath: String,
+        successfulTasks: List<String>,
+        distDir: String = defaultDistDir,
+        expectedFiles: Set<String>
+    ) = with(
         testProject(
-            "application/webJsWasm",
+            projectPath,
             testEnvironment = defaultTestEnvironment.copy()
         )
     ) {
         gradle(":composeApp:composeCompatibilityBrowserDistribution").checks {
             check.taskSuccessful(":composeApp:composeCompatibilityBrowserDistribution")
-            check.taskSuccessful(":composeApp:jsBrowserDistribution")
-            check.taskSuccessful(":composeApp:wasmJsBrowserDistribution")
-
-            file("./composeApp/build/dist/composeWebCompatibility/productionExecutable").apply {
-                checkExists()
-                assertTrue(isDirectory)
-                val distributionFiles = listFiles()!!.map { it.name }.toList().sorted()
-
-                assertTrue(distributionFiles.any { it.endsWith(".wasm") })
-
-                assertContentEquals(distributionFiles.filter { !it.endsWith(".wasm") }, listOf(
-                    "composeApp.js", "composeResources", "index.html", "originJsComposeApp.js", "originJsComposeApp.js.map", "originWasmComposeApp.js", "originWasmComposeApp.js.map", "styles.css"
-                ))
-            }
+            successfulTasks.forEach { check.taskSuccessful(it) }
+            assertCompatibilityDistribution(dirPath = distDir, expectedFileNames = expectedFiles)
         }
     }
-
 
     @Test
-    fun testWebJsWasmNonStandard() = with(
-        testProject(
-            "application/webJsWasmNonStandard",
-            testEnvironment = defaultTestEnvironment.copy()
+    fun testWebJsWasm() = runCompatibilityTest(
+        projectPath = "application/webJsWasm",
+        successfulTasks = listOf(
+            ":composeApp:jsBrowserDistribution",
+            ":composeApp:wasmJsBrowserDistribution"
+        ),
+        expectedFiles = setOf(
+            "composeApp.js",
+            "composeResources",
+            "index.html",
+            "originJsComposeApp.js",
+            "originJsComposeApp.js.map",
+            "originWasmComposeApp.js",
+            "originWasmComposeApp.js.map",
+            "styles.css"
         )
-    ) {
-        gradle(":composeApp:composeCompatibilityBrowserDistribution").checks {
-            check.taskSuccessful(":composeApp:composeCompatibilityBrowserDistribution")
-            check.taskSuccessful(":composeApp:webJsBrowserDistribution")
-            check.taskSuccessful(":composeApp:webWasmBrowserDistribution")
+    )
 
-            file("./composeApp/build/dist/composeWebCompatibility/productionExecutable").apply {
-                checkExists()
-                assertTrue(isDirectory)
-                val distributionFiles = listFiles()!!.map { it.name }.toList().sorted()
+    @Test
+    fun testWebJsWasmNonStandard() = runCompatibilityTest(
+        projectPath = "application/webJsWasmNonStandard",
+        successfulTasks = listOf(
+            ":composeApp:webJsBrowserDistribution",
+            ":composeApp:webWasmBrowserDistribution"
+        ),
+        expectedFiles = setOf(
+            "composeApp.js",
+            "composeResources",
+            "index.html",
+            "originJsComposeApp.js",
+            "originJsComposeApp.js.map",
+            "originWasmComposeApp.js",
+            "originWasmComposeApp.js.map",
+            "styles.css"
+        )
+    )
 
-                assertTrue(distributionFiles.any { it.endsWith(".wasm") })
-
-                println(distributionFiles.filter { !it.endsWith(".wasm") }.sorted().joinToString(", ") { "\"$it\"" })
-
-                assertContentEquals(distributionFiles.filter { !it.endsWith(".wasm") }, listOf(
-                    "composeApp.js", "composeResources", "index.html", "originJsComposeApp.js", "originJsComposeApp.js.map", "originWasmComposeApp.js", "originWasmComposeApp.js.map", "styles.css"
-                ))
-            }
-        }
-    }
-
+    @Test
+    fun testWebJsWasmReconfigured() = runCompatibilityTest(
+        projectPath = "application/webJsWasmReconfigured",
+        successfulTasks = listOf(
+            ":composeApp:wasmRepack",
+            ":composeApp:jsRepack"
+        ),
+        expectedFiles = setOf(
+            "composeResources",
+            "index.html",
+            "originJsRepackedApp.js",
+            "originJsRepackedApp.js.map",
+            "originWasmRepackedApp.js",
+            "originWasmRepackedApp.js.map",
+            "repackedApp.js",
+            "styles.css"
+        )
+    )
 }
