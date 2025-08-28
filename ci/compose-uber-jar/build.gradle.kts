959f0c0f709353b33bdf1009b33ebccac30b5834
Ivan Matkov <ivan.matkov@jetbrains.com>
2025-08-28 12:56:55 +0200

    Fix uber-jar dependencies

--- ci/compose-uber-jar/build.gradle.kts
+++ ci/compose-uber-jar/build.gradle.kts
@@ -3,7 +3,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "6.0.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 val properties = ComposeUberJarProperties()
