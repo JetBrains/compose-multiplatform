f6eda377233abd77ec3ef4c2e4e8615dbc872982
Igor Demin <igordmn@users.noreply.github.com>
2025-08-28 14:13:39 +0200

    Revert "Hardcode Material3 to stable (#5360)" (#5400)
    Fixes [CMP-8556](https://youtrack.jetbrains.com/issue/CMP-8556) Revert
    hardcoded `compose.material3`
    ## Release Notes
    N/A

--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposePlugin.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/ComposePlugin.kt
@@ -72,8 +72,8 @@ abstract class ComposePlugin : Plugin<Project> {
         val animationGraphics get() = composeDependency("org.jetbrains.compose.animation:animation-graphics")
         val foundation get() = composeDependency("org.jetbrains.compose.foundation:foundation")
         val material get() = composeDependency("org.jetbrains.compose.material:material")
-        val material3 get() = "org.jetbrains.compose.material3:material3:1.8.2"
-        val material3AdaptiveNavigationSuite get() = "org.jetbrains.compose.material3:material3-adaptive-navigation-suite:1.8.2"
+        val material3 get() = composeDependency("org.jetbrains.compose.material3:material3")
+        val material3AdaptiveNavigationSuite get() = composeDependency("org.jetbrains.compose.material3:material3-adaptive-navigation-suite")
         val runtime get() = composeDependency("org.jetbrains.compose.runtime:runtime")
         val runtimeSaveable get() = composeDependency("org.jetbrains.compose.runtime:runtime-saveable")
         val ui get() = composeDependency("org.jetbrains.compose.ui:ui")
