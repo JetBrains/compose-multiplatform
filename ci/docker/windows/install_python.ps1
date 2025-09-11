327dd6147dd9effd63e5d013d4c645ae412d9f7c
Ivan Matkov <ivan.matkov@jetbrains.com>
2025-09-11 12:37:39 +0200

    Remove skiko window docker image (moved to skiko repo)

--- ci/docker/windows/install_python.ps1
+++
@@ -1,8 +0,0 @@
-$url = ('https://www.python.org/ftp/python/{0}/python-{1}.amd64.msi' -f $env:PYTHON_RELEASE, $env:PYTHON_VERSION)
-$installer='C:\TEMP\python.msi'
-Write-Host ('Downloading {0} ...' -f $url)
-(New-Object System.Net.WebClient).DownloadFile($url, $installer)
-Write-Host 'Installing ...'
-Start-Process msiexec -Wait -ArgumentList @('/i', $installer, '/quiet', '/qn', 'TARGETDIR=C:\Python', 'ALLUSERS=1', 'ADDLOCAL=DefaultFeature,Extensions,TclTk,Tools,PrependPath')
-Remove-Item $installer -Force
-Write-Host 'Python installation is complete.'
