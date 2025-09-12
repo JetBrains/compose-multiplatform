6e73739d2fc41533676e53df940d673b93ce111e
Ivan Matkov <ivan.matkov@jetbrains.com>
2025-09-12 16:34:45 +0200

    Update docker images for CI tests (#5424)
    - Remove windows skiko image (moved to skiko repo -
    https://github.com/JetBrains/skiko/pull/1096)
    - Combined into unified `linux-tests` image
    - Preinstalled Android SDK allows removing this step from 4 build stages
    - Ubuntu 22.04 -> 24.04
    - Update JDK to 17 -> 21
    - Update Google Chrome 121 -> 140, and use specifc pinned version
    instead of "latest"
    - Update Firefox 138 -> 142, and use specifc pinned version instead of
    "latest"
    
    Related fixes:
    - CI change: https://jetbrains.team/p/ui/reviews/49
    - Missing JDK 17 fix -
    https://github.com/JetBrains/compose-multiplatform/pull/5424/commits/51a87ca55cd5bc7e8b9cc78b6c7c1531bda2d96a
    - Failing HTML tests on Chrome 140 - #5426
    - Failing Compose Web test tests on Firefox 142 -
    https://github.com/JetBrains/compose-multiplatform-core/pull/2398
    
    ## Release Notes
    N/A

--- html/buildSrc/build.gradle.kts
+++ html/buildSrc/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 repositories {
     gradlePluginPortal()
     mavenCentral()
@@ -13,13 +16,15 @@ plugins {
     id("org.jetbrains.kotlin.jvm")
 }
 
-kotlin {
-    jvmToolchain(17)
-}
-
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
