79add74a10b0f43502201711094e148f4e49e82b
Andrei Salavei <andrei.salavei@jetbrains.com>
2025-10-06 11:30:24 +0200

    Add the Navigation Event library to release notes

--- tools/changelog/changelog.main.kts
+++ tools/changelog/changelog.main.kts
@@ -130,6 +130,7 @@ fun generateChangelog() {
     val versionComposeMaterial3 = formatAndroidxLibVersion("COMPOSE_MATERIAL3")
     val versionComposeMaterial3Adaptive = formatAndroidxLibVersion("COMPOSE_MATERIAL3_ADAPTIVE")
     val versionLifecycle = formatAndroidxLibVersion("LIFECYCLE")
+    val versionNavigationEvent = formatAndroidxLibVersion("NAVIGATION_EVENT")
     val versionSavedstate = formatAndroidxLibVersion("SAVEDSTATE")
     val versionWindow = formatAndroidxLibVersion("WINDOW")
 
@@ -137,6 +138,7 @@ fun generateChangelog() {
     val versionRedirectingComposeMaterial3 = formatAndroidxLibRedirectingVersion("compose.material3")
     val versionRedirectingComposeMaterial3Adaptive = formatAndroidxLibRedirectingVersion("compose.material3.adaptive")
     val versionRedirectingLifecycle = formatAndroidxLibRedirectingVersion("lifecycle")
+    val versionRedirectingNavigationEvent = formatAndroidxLibRedirectingVersion("navigationevent")
     val versionRedirectingSavedstate = formatAndroidxLibRedirectingVersion("savedstate")
     val versionRedirectingWindow = formatAndroidxLibRedirectingVersion("window")
 
@@ -217,6 +219,7 @@ fun generateChangelog() {
                     - Compose Material3 Adaptive libraries `org.jetbrains.compose.material3.adaptive:adaptive*:$versionComposeMaterial3Adaptive`. Based on [Jetpack Compose Material3 Adaptive $versionRedirectingComposeMaterial3Adaptive](https://developer.android.com/jetpack/androidx/releases/compose-material3-adaptive#$versionRedirectingComposeMaterial3Adaptive)
                     - Lifecycle libraries `org.jetbrains.androidx.lifecycle:lifecycle-*:$versionLifecycle`. Based on [Jetpack Lifecycle $versionRedirectingLifecycle](https://developer.android.com/jetpack/androidx/releases/lifecycle#$versionRedirectingLifecycle)
                     - Navigation libraries `org.jetbrains.androidx.navigation:navigation-*:2.9.0-rc01`. Based on [Jetpack Navigation 2.9.1](https://developer.android.com/jetpack/androidx/releases/navigation#2.9.1)
+                    - Navigation Event library `org.jetbrains.androidx.navigationevent:navigationevent-compose:$versionNavigationEvent`. Based on [Jetpack Navigation Event $versionRedirectingNavigationEvent](https://developer.android.com/jetpack/androidx/releases/navigation#$versionRedirectingNavigationEvent)
                     - Savedstate library `org.jetbrains.androidx.savedstate:savedstate:$versionSavedstate`. Based on [Jetpack Savedstate $versionRedirectingSavedstate](https://developer.android.com/jetpack/androidx/releases/savedstate#$versionRedirectingSavedstate)
                     - WindowManager Core library `org.jetbrains.androidx.window:window-core:$versionWindow`. Based on [Jetpack WindowManager $versionRedirectingWindow](https://developer.android.com/jetpack/androidx/releases/window#$versionRedirectingWindow)
 
