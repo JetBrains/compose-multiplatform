b608da2e7eb05961547b82c730ea244e0d3ae2b4
Sanlorng <fujia625@gmail.com>
2025-10-13 13:41:39 +0800

    feat: Add support for macOS layered icons
    This commit introduces support for using macOS layered icons (`.icon` directory) for both JVM and native application packaging.
    
    - A new `layeredIconDir` property is added to the `compose.desktop.application.nativeDistributions.macOS` and `compose.nativeApplication.macOS` DSLs.
    - The `actool` from Xcode Command Line Tools is used to compile the `.icon` directory into an `Assets.car` file.
    - The `Assets.car` file is included in the `.app` bundle resources.
    - The `Info.plist` is updated with `CFBundleIconName` to reference the compiled asset.
    - A check for a supported `actool` version is performed before compilation.
    - New integration tests (`testMacLayeredIcon`, `testMacLayeredIconRemove`) are added to verify the functionality for both JVM and native targets.

---
+++ gradle-plugins/compose/src/test/test-projects/application/macLayeredIcon/settings.gradle.kts
@@ -0,0 +1,25 @@
+rootProject.name = "macLayeredIcon"
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        google()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://packages.jetbrains.team/maven/p/kt/dev")
+    }
+    plugins {
+        id("org.jetbrains.kotlin.multiplatform").version("KOTLIN_VERSION_PLACEHOLDER")
+        id("org.jetbrains.kotlin.plugin.compose").version("KOTLIN_VERSION_PLACEHOLDER")
+        id("org.jetbrains.compose").version("COMPOSE_GRADLE_PLUGIN_VERSION_PLACEHOLDER")
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://packages.jetbrains.team/maven/p/kt/dev")
+        mavenCentral()
+        gradlePluginPortal()
+        google()
+        mavenLocal()
+    }
+}
---
+++ gradle-plugins/compose/src/test/test-projects/application/macLayeredIcon/src/jvmMain/kotlin/main.kt
@@ -0,0 +1,3 @@
+fun main() {
+    println("Hello, from Mac OS!")
+}
