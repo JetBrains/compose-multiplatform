05a275f9baaf838c97353dd2f9ec5fc3f81c19c0
Nikita Lipsky <nikita.lipsky@jetbrains.com>
2025-10-16 17:42:04 +0300

    Configure hot reload: listen jvm targets to appear instead of looking for them

--- gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/desktop/application/internal/configureHotReload.kt
+++ gradle-plugins/compose/src/main/kotlin/org/jetbrains/compose/desktop/application/internal/configureHotReload.kt
@@ -3,18 +3,26 @@ package org.jetbrains.compose.desktop.application.internal
 import org.gradle.api.Project
 import org.jetbrains.compose.internal.KOTLIN_JVM_PLUGIN_ID
 import org.jetbrains.compose.internal.KOTLIN_MPP_PLUGIN_ID
+import org.jetbrains.compose.internal.javaSourceSets
 import org.jetbrains.compose.internal.mppExt
 import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
 
-private fun Project.isJVMTargetAvailable(): Boolean {
-    return plugins.findPlugin(KOTLIN_JVM_PLUGIN_ID) != null ||
-           plugins.findPlugin(KOTLIN_MPP_PLUGIN_ID)
-               ?.let { mppExt.targets.withType(KotlinJvmTarget::class.java).isNotEmpty() } == true
+private fun Project.applyHotReload() {
+    project.pluginManager.apply("org.jetbrains.compose.hot-reload")
 }
 
 fun Project.configureHotReload() {
-    if (!isJVMTargetAvailable() || ComposeProperties.disableHotReload(providers).get()) {
-        return
+    if (!ComposeProperties.disableHotReload(providers).get()) {
+        plugins.withId(KOTLIN_MPP_PLUGIN_ID) {
+            mppExt.targets.withType(KotlinJvmTarget::class.java) { target ->
+                val runtimeFilesProvider = JvmApplicationRuntimeFilesProvider.FromKotlinMppTarget(target)
+                applyHotReload()
+            }
+        }
+        plugins.withId(KOTLIN_JVM_PLUGIN_ID) {
+            val sourceSet = project.javaSourceSets.getByName("main")
+            val runtimeFilesProvider = JvmApplicationRuntimeFilesProvider.FromGradleSourceSet(sourceSet)
+            applyHotReload()
+        }
     }
-    project.pluginManager.apply("org.jetbrains.compose.hot-reload")
 }
