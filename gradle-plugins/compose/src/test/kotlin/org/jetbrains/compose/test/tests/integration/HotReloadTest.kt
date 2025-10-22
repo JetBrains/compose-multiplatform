c2054519dc7941135ed50703309cddef8cd91b16
Nikita Lipsky <nikita.lipsky@jetbrains.com>
2025-10-22 19:10:48 +0300

    Temporally disable hot reload tests  (#5460)
    Temporally disable hot reload tests until JBR is installed in docker
    container for the tests on CI
    
    ## Release Notes
    N/A

--- gradle-plugins/compose/src/test/kotlin/org/jetbrains/compose/test/tests/integration/HotReloadTest.kt
+++ gradle-plugins/compose/src/test/kotlin/org/jetbrains/compose/test/tests/integration/HotReloadTest.kt
@@ -6,11 +6,13 @@ import org.jetbrains.compose.ComposeBuildConfig
 import org.jetbrains.compose.desktop.application.internal.ComposeProperties
 import org.jetbrains.compose.test.utils.GradlePluginTestBase
 import org.jetbrains.compose.test.utils.checks
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.fail
 import org.junit.jupiter.api.Test
 import kotlin.concurrent.thread
 
 class HotReloadTest : GradlePluginTestBase() {
+    @Disabled("TODO: install JBR on CI for the test to pass")
     @Test
     fun testHotReloadTaskRegisteredInJvmProject() = with(testProject("application/jvm")) {
         gradle("hotRun", "--dry-run").checks {
@@ -18,6 +20,7 @@ class HotReloadTest : GradlePluginTestBase() {
         }
     }
 
+    @Disabled("TODO: install JBR on CI for the test to pass")
     @Test
     fun testHotReloadTaskRegisteredInKmpProject() = with(testProject("application/mpp")) {
         gradle("hotRunJvm", "--dry-run").checks {
@@ -46,6 +49,7 @@ class HotReloadTest : GradlePluginTestBase() {
         Thread.sleep(1000)
     }
 
+    @Disabled("TODO: install JBR on CI for the test to pass")
     @Test
     fun testHotReload() = with(testProject("application/hotReload")) {
         var result: BuildResult? = null
@@ -91,6 +95,7 @@ class HotReloadTest : GradlePluginTestBase() {
         gradleRunnerWorkaround()
     }
 
+    @Disabled("TODO: install JBR on CI for the test to pass")
     @Test
     fun testExternalHotReload() = with(testProject("application/mpp")) {
         val externalHotReloadVersion = "1.0.0-rc01"
