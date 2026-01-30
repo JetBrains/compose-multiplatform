de332137733a165b84d65c5ac651d81e27687c2e
Victor Kropp <victor.kropp@jetbrains.com>
2026-01-30 09:54:25 +0100

    Migrate dev maven (#5524)
    Change Dev Maven repository to public.jetbrains.team
    
    Fixes [CMP-1460](https://youtrack.jetbrains.com/issue/CMP-1460) Migrate
    Compose dev builds from public.jetbrains.space
    
    ## Testing
    All build configurations are green, packages are uploaded.
    
    ## Release Notes
    N/A

--- examples/graphics-2d/build.gradle.kts
+++ examples/graphics-2d/build.gradle.kts
@@ -14,6 +14,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://packages.jetbrains.team/maven/p/cmp/dev")
     }
 }
