caacb35f2c10e77546497c175e2d5dd74890d4f3
Konstantin <konstantin.tskhovrebov@jetbrains.com>
2026-06-08 18:52:04 +0200

    Refactor `AsyncCache` to use internal Supervisor job for deferred jobs.

--- components/resources/library/src/commonMain/kotlin/org/jetbrains/compose/resources/ResourceCaches.kt
+++ components/resources/library/src/commonMain/kotlin/org/jetbrains/compose/resources/ResourceCaches.kt
@@ -1,13 +1,12 @@
 package org.jetbrains.compose.resources
 
-import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 
 internal class AsyncCache<K, V> {
+    private val cacheJob = SupervisorJob()
+    private val cacheScope = CoroutineScope(cacheJob)
     private val mutex = Mutex()
     private val cache = mutableMapOf<K, Deferred<V>>()
 
@@ -15,21 +14,27 @@ internal class AsyncCache<K, V> {
         ResourceCaches.registerCache(this)
     }
 
-    suspend fun getOrLoad(key: K, load: suspend () -> V): V = coroutineScope {
+    suspend fun getOrLoad(key: K, load: suspend () -> V): V {
         val deferred = mutex.withLock {
             var cached = cache[key]
             if (cached == null || cached.isCancelled) {
-                //LAZY - to free the mutex lock as fast as possible
-                cached = async(start = CoroutineStart.LAZY) { load() }
+                cached = cacheScope.async { load() }
                 cache[key] = cached
             }
             cached
         }
-        deferred.await()
+        return deferred.await()
+    }
+
+    suspend fun waitAllJobs() {
+        mutex.withLock {
+            cacheJob.children.forEach { it.join() }
+        }
     }
 
     suspend fun clear() {
         mutex.withLock {
+            cacheJob.children.forEach { it.cancel() }
             cache.clear()
         }
     }
@@ -39,6 +44,17 @@ object ResourceCaches {
     private val caches = mutableListOf<AsyncCache<*, *>>()
 
     internal fun registerCache(cache: AsyncCache<*, *>) = caches.add(cache)
+
+    /**
+     * Waits for all ongoing resource loading jobs to complete.
+     *
+     * This method ensures that all asynchronous resource loading operations
+     * have finished before proceeding. It is useful for testing and ensuring
+     * that resources are fully loaded before further actions are taken.
+     */
+    internal suspend fun waitAllJobs() {
+        caches.forEach { it.waitAllJobs() }
+    }
 
     /**
      * Clears any cached resources maintained internally by the system.
