8173a65b9bc35f5d5785483c88c3e88c028738c1
Konstantin <konstantin.tskhovrebov@jetbrains.com>
2026-06-17 17:13:02 +0200

    Revert 8a4670a3 due KT-87048

--- gradle-plugins/compose/src/test/test-projects/misc/jsNoExecutableTests/karma.config.d/config.js
+++
@@ -1,9 +0,0 @@
-config.set({
-    browsers: ["ChromeHeadless_ComposeCI"],
-    customLaunchers: {
-        ChromeHeadless_ComposeCI: {
-            base: "ChromeHeadless",
-            flags: ["--no-sandbox"]
-        }
-    }
-});
--- gradle-plugins/compose/src/test/test-projects/misc/jsNoExecutableTests/src/commonMain/kotlin/Main.kt
+++
@@ -1,9 +0,0 @@
-
-import androidx.compose.foundation.text.BasicText
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun ReversedTextView(text: String, modifier: Modifier = Modifier) {
-    BasicText(text.reversed(), modifier)
-}
