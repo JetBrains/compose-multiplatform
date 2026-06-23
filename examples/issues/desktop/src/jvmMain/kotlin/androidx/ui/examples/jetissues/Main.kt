72739e9c39f1770cb7c6560bf89d1591ef2faa7d
Victor Kropp <victor.kropp@jetbrains.com>
2026-06-23 14:11:29 +0200

    fix some more warnings

--- examples/issues/desktop/src/jvmMain/kotlin/androidx/ui/examples/jetissues/Main.kt
+++ examples/issues/desktop/src/jvmMain/kotlin/androidx/ui/examples/jetissues/Main.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowSize
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import androidx.ui.examples.jetissues.view.JetIssuesView
