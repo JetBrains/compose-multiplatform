5877c7cf4cb969075f8de0bbf6d42308df7ffd5f
Konstantin <konstantin.tskhovrebov@jetbrains.com>
2026-09-16 12:20:26 +0200

    Configure Android Host tests instead of device runs

---
+++ components/resources/library/src/androidHostTest/kotlin/org/jetbrains/compose/resources/KmpUiTest.android.kt
@@ -0,0 +1,9 @@
+package org.jetbrains.compose.resources
+
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [35])
+actual abstract class KmpUiTest actual constructor()
---
+++ components/resources/library/src/commonTest/kotlin/org/jetbrains/compose/resources/KmpUiTest.kt
@@ -0,0 +1,4 @@
+package org.jetbrains.compose.resources
+
+//on android we need to configure the Robolectric runner for such tests
+expect abstract class KmpUiTest()
