0a838b2e188358979e2a6a4dcc623388c4a01676
Pavel Punegov <pavel.punegov@jetbrains.com>
2026-09-24 14:30:33 +0200

    fixup! Benchmarks: cleanup deprecations and update versions

--- benchmarks/multiplatform/benchmarks/src/commonMain/kotlin/BenchmarksSave.kt
+++ benchmarks/multiplatform/benchmarks/src/commonMain/kotlin/BenchmarksSave.kt
@@ -3,7 +3,6 @@
  * Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE.txt file.
  */
 
-import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.format.FormatStringsInDatetimeFormats
@@ -16,6 +15,7 @@ import kotlinx.io.buffered
 import kotlinx.io.files.Path
 import kotlinx.io.files.SystemFileSystem
 import kotlinx.io.readByteArray
+import kotlin.time.Clock
 
 // port for the benchmarks save server
 val BENCHMARK_SERVER_PORT = 8090
